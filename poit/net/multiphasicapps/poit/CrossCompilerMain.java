1e14d28b86fff4a1f276ca3f1e6ca45a1703093a
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-02-27 03:08:52 +0000

    Base for new layout using Java.

---
+++ poit/net/multiphasicapps/poit/CrossCompilerMain.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.poit;
+
+/**
+ * Main entry point for the cross compiler using a real host virtual machine.
+ *
+ * @since 2016/02/26
+ */
+public class CrossCompilerMain
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2016/02/26
+	 */
+	public static void main(String... __args)
+	{
+		throw new Error("TODO");
+	}
+}
+
