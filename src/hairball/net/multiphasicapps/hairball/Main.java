a83b2c73f838da7093ac536da828cc9f31d9d702
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-02-28 17:14:12 +0000

    More base.

---
+++ src/hairball/net/multiphasicapps/hairball/Main.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package hairball.net.multiphasicapps.hairball;
+
+/**
+ * Main entry point for hairball.
+ *
+ * @since 2016/02/28
+ */
+public class Main
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2016/02/28
+	 */
+	public static void main(String... __args)
+	{
+		throw new Error("TODO");
+	}
+}
+
