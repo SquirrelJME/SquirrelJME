be53e919dffa40a26101736b8d841aa0f63a240a
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-02-28 21:50:54 +0000

    Setup manifest data and allow for reading.

--- src/hairball/net/multiphasicapps/hairball/PackageInfo.java
+++ src/hairball/net/multiphasicapps/hairball/PackageInfo.java
@@ -17,6 +17,11 @@ import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /**
  * This contains package information.
@@ -30,6 +35,9 @@ public class PackageInfo
 	
 	/** Is this package valid? */
 	protected final boolean valid;
+	
+	/** The name of this package. */
+	protected final String name;
 	
 	/**
 	 * The path to the package root.
@@ -51,13 +59,17 @@ public class PackageInfo
 		// Guessed manifest location
 		Path mfp = root.resolve("META-INF").resolve("MANIFEST.MF");
 		
+		// Is hairball?
+		boolean ishb = false;
+		
 		// Read the manifest
 		boolean iv = false;
 		try (FileChannel fc = FileChannel.open(mfp, StandardOpenOption.READ);
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 				Channels.newInputStream(fc), "utf-8")))
 		{
-			// Read line by line
+			// Read in all manifest source lines
+			Deque<String> lines = new LinkedList<>();
 			for (;;)
 			{
 				// Read
@@ -67,20 +79,96 @@ public class PackageInfo
 				if (ln == null)
 					break;
 				
-				// 
+				// If the line is blank, stop reading the manifest
+				if (ln.length() <= 0)
+					break;
+				
+				// Lowercase it to make it easier to parse
+				ln = __asciiLowerCase(ln);
+				
+				// If it starts with a space, then it is a continuation of the
+				// last line and just gets pasted on it
+				if (ln.startsWith(" "))
+				{
+					// The manifest could be badly formed
+					try
+					{
+						// Get the last line that this is to be appended to
+						String top = lines.getLast();
+						
+						// Remove the last line
+						lines.removeLast();
+						
+						// Append it
+						lines.offerLast(top + ln.substring(1));
+					}
+					
+					// Bad manifest format
+					catch (NoSuchElementException nsee)
+					{
+						throw new IOException("Malformed manifest file.",
+							nsee);
+					}
+				}
+				
+				// Otherwise it is not, just add it
+				else
+					lines.add(ln);
 			}
 			
-			// Is valid
-			iv = true;
+			// Go through lines again
+			for (String ln : lines)
+			{
+				// Find the first colon
+				int col = ln.indexOf(':');
+				
+				// If not found, ignore
+				if (col < 0)
+					continue;
+				
+				// Split key and value
+				String key = ln.substring(0, col).trim();
+				String val = ln.substring(col + 1).trim();
+				
+				// Depends on the key
+				switch (key)
+				{
+						// Is this a hairball package?
+					case "hairball-package":
+						throw new Error("TODO");
+						
+						// Dependency
+					case "hairball-depends":
+						throw new Error("TODO");
+					
+						// Unknown
+					default:
+						System.err.println("Bad key " + key);
+						break;
+				}
+			}
+			
+			// Is valid?
+			iv = ishb;
 		}
 		
-		// Failed to read
+		// Failed to read or some other error
 		catch (IOException ioe)
 		{
+			// Only log it if this is not a package
+			if (!(ioe instanceof __NotAPackageException__))
+			{
+				System.err.printf("Could not read package: %s%n",
+					ioe.getCause());
+				ioe.printStackTrace(System.err);
+			}
 		}
 		
 		// Set validity
 		valid = iv;
+		
+		// Set name
+		name = __asciiLowerCase(root.getFileName().toString());
 	}
 	
 	/**
@@ -101,7 +189,7 @@ public class PackageInfo
 	@Override
 	public String toString()
 	{
-		return root.getFileName().toString();
+		return name;
 	}
 	
 	/**
@@ -136,6 +224,16 @@ public class PackageInfo
 		
 		// Build it
 		return sb.toString();
+	}
+	
+	/**
+	 * This is thrown when a given path is not a package.
+	 *
+	 * @since 2016/02/28
+	 */
+	private static class __NotAPackageException__
+		extends IOException
+	{
 	}
 }
 
