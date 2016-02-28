452c7ac5ad637255eae1e9a408fa02c6476f928f
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-02-28 18:08:44 +0000

    base for package list initialization.

--- src/hairball/net/multiphasicapps/hairball/Main.java
+++ src/hairball/net/multiphasicapps/hairball/Main.java
@@ -10,6 +10,11 @@
 
 package net.multiphasicapps.hairball;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 /**
  * Main entry point for hairball.
  *
@@ -25,7 +30,31 @@ public class Main
 	 */
 	public static void main(String... __args)
 	{
-		throw new Error("TODO");
+		// Need two arguments, JAR output directory and the source root
+		if (__args == null || __args.length < 2)
+			throw new IllegalArgumentException("Usage: (Output) (Projects)");
+		
+		// Get locations
+		Path outdir = Paths.get(Objects.<String>requireNonNull(__args[0],
+			"No output directory specified."));
+		Path srcdir = Paths.get(Objects.<String>requireNonNull(__args[1],
+			"No source directory specified."));
+		
+		// Could fail
+		try
+		{
+			// Build package list
+			PackageList pl = new PackageList(outdir, srcdir);
+			System.out.println(pl);
+			
+			throw new Error("TODO");
+		}
+		
+		// Could not read or compile package
+		catch (IOException ioe)
+		{
+			throw new RuntimeException(ioe);
+		}
 	}
 }
 
---
+++ src/hairball/net/multiphasicapps/hairball/PackageInfo.java
@@ -0,0 +1,64 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.hairball;
+
+import java.nio.file.Path;
+
+/**
+ * This contains package information.
+ *
+ * @since 2016/02/28
+ */
+public class PackageInfo
+{
+	/** The package root. */
+	protected final Path root;	
+	
+	/**
+	 * The path to the package root.
+	 *
+	 * @param __p Package root.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/02/28
+	 */
+	public PackageInfo(Path __p)
+		throws NullPointerException
+	{
+		// Check
+		if (__p == null)
+			throw new NullPointerException();
+		
+		// Set
+		root = __p;
+	}
+	
+	/**
+	 * Is this a valid package?
+	 *
+	 * @return {@code true} if it is.
+	 * @since 2016/02/28
+	 */
+	public boolean isValid()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/02/28
+	 */
+	@Override
+	public String toString()
+	{
+		return root.getFileName().toString();
+	}
+}
+
