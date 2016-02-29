75f7ce7774885f3e44cee45af0f90351ae299230
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-02-29 01:35:58 +0000

    Cleanup.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+#     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU Affero General Public License v3, or later.
+# For more information see license.mkd.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Builds SquirrelJME.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
