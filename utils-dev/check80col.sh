bdc638f302cbe01a35e38e9003f0ead1902cde5f
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-03-09 00:38:08 +0000

    Import check80col and checkascii script due to my own strict requirements.

---
+++ utils-dev/check80col.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+#     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU Affero General Public License v3, or later.
+# For more information see license.txt.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Makes sure that no file exceeds 80 columns
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Base directory of code
+__rootdir="."
+
+# Go through all files
+echo "The following files exceed 79 characters" 1>&2
+find "$__rootdir" -type f | while read -r __file
+do
+	# Base file name
+	__name="$(basename -- "$__file")"	
+	
+	# Ignore the fossil file
+	if [ "$__name" = "_FOSSIL_" ]
+	then
+		continue
+	fi
+	
+	# Obtain extension
+	__ext=""
+		
+	# Count number of lines past 80
+	__nl="$(expand -t 4 < "$__file" | sed 's/^\(.\{0,79\}\)$//;/^$/d' | \
+		wc -l)"
+	
+	# File has lines above 80
+	if [ "$__nl" -ge "1" ]
+	then
+		# For line count cheat on stderr
+		echo -n "$__file"
+		
+		# Cheat space on stderr
+		echo -n " ($__nl)" 1>&2
+		
+		# Ending line
+		echo
+	fi
+done
+
---
+++ utils-dev/checkascii.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+#     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU Affero General Public License v3, or later.
+# For more information see license.txt.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Checks for files which do no contain the valid ASCII range.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Base directory of code
+__rootdir="."
+
+# Go through all files
+echo "The following files contain non-ascii characters" 1>&2
+find "$__rootdir" -type f | while read -r __file
+do
+	# Base file name
+	__name="$(basename -- "$__file")"	
+	
+	# Ignore the fossil file
+	if [ "$__name" = "_FOSSIL_" ]
+	then
+		continue
+	fi
+	
+	# Grep it
+	if grep -E '[^\x20-\x7E\t\r\n]' < "$__file" > /dev/null
+	then
+		echo "$__file"
+	fi
+done
+
