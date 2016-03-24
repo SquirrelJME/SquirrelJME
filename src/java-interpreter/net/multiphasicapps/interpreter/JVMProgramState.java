4bd5a3a102fbd5622d885bc469c5761e2a82ce05
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-03-24 20:48:38 +0000

    Base for a JVMProgramState.

---
+++ src/java-interpreter/net/multiphasicapps/interpreter/JVMProgramState.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.interpreter;
+
+/**
+ * This represents the state of a program through all of its operations and
+ * defines operation chains and the type of values stored in local variable
+ * and the stack.
+ *
+ * This is used for optimization.
+ *
+ * @since 2016/03/24
+ */
+public class JVMProgramState
+{
+}
+
