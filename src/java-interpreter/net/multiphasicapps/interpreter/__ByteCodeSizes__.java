ff5bafcd1147f80ad0911cd814fd794fc7d20eae
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-03-30 04:06:26 +0000

    Determine the size of all instructions to determine their placement.

--- src/java-interpreter/net/multiphasicapps/interpreter/__ByteCodeSizes__.java
+++ src/java-interpreter/net/multiphasicapps/interpreter/__ByteCodeSizes__.java
@@ -35,6 +35,29 @@ class __ByteCodeSizes__
 	}
 	
 	/**
+	 * Reads an unsigned integer from the given buffer.
+	 *
+	 * @param __code The byte array.
+	 * @param __pos The position of the integer.
+	 * @return The read value.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/03/29
+	 */
+	private static int __readUInt(byte[] __code, int __pos)
+		throws NullPointerException
+	{
+		// Check
+		if (__code == null)
+			throw new NullPointerException("NARG");
+		
+		// Read and merge the values (big endian)
+		return ((((int)__code[__pos]) & 0xFF) << 24) |
+			((((int)__code[__pos + 1]) & 0xFF) << 16) |
+			((((int)__code[__pos + 2]) & 0xFF) << 8) |
+			((((int)__code[__pos + 3]) & 0xFF));
+	}
+	
+	/**
 	 * This determines the size of the given instruction.
 	 *
 	 * @param __pos The position of the instruction.
@@ -59,7 +82,93 @@ class __ByteCodeSizes__
 			// Read the opcode
 			opcode = ((int)__code[__pos]) & 0xFF;
 			
-			throw new Error("TODO");
+			// Wide operations
+			if (opcode == 196)
+			{
+				// Read a new byte
+				opcode = ((int)__code[__pos + 1]) & 0xFF;
+				
+				// Four bytes
+				if ((opcode >= 21 && opcode <= 25) ||
+					(opcode == 169))
+					return 4;
+				
+				// 6 bytes
+				else if (opcode == 132)
+					return 6;
+			}
+			
+			// Tableswitch
+			else if (opcode == 170)
+			{
+				// Get padded position, which is rounded up to the next byte
+				int ppos = ((__pos + 3) & (~3));
+				
+				// Read the low and high bytes
+				int lo = __readUInt(__code, ppos + 4);
+				int hi = __readUInt(__code, ppos + 8);
+				
+				// Calculate the size
+				return (ppos - __pos) + 12 + (4 * ((hi - lo) + 1));
+			}
+			
+			// Lookup switch
+			else if (opcode == 171)
+			{
+				// Get padded position, which is rounded up to the next byte
+				int ppos = ((__pos + 3) & (~3));
+				
+				// Read the pair count
+				int np = __readUInt(__code, ppos + 4);
+				
+				// Calculate the size
+				return (ppos - __pos) + 8 + (8 * np);
+			}
+			
+			// These are single byte operations
+			else if ((opcode >= 0 && opcode <= 15) ||
+				(opcode >= 26 && opcode <= 53) ||
+				(opcode >= 59 && opcode <= 131) ||
+				(opcode >= 133 && opcode <= 152) ||
+				(opcode >= 172 && opcode <= 177) ||
+				(opcode >= 190 && opcode <= 191) ||
+				(opcode >= 194 && opcode <= 195))
+				return 1;
+			
+			// These are two byte operations
+			else if ((opcode == 18) ||
+				(opcode == 16) ||
+				(opcode >= 21 && opcode <= 25) ||
+				(opcode >= 54 && opcode <= 58) ||
+				(opcode == 169) ||
+				(opcode == 188))
+				return 2;
+			
+			// And three byte operations
+			else if ((opcode == 17) ||
+				(opcode >= 19 && opcode <= 20) ||
+				(opcode == 132) ||
+				(opcode >= 153 && opcode <= 168) ||
+				(opcode >= 178 && opcode <= 183) ||
+				(opcode == 184) ||
+				(opcode == 187) ||
+				(opcode == 189) ||
+				(opcode >= 192 && opcode <= 193) ||
+				(opcode >= 198 && opcode <= 199))
+				return 3;
+			
+			// Four bytes
+			else if (opcode == 197)
+				return 4;
+			
+			// Five bytes
+			else if ((opcode >= 185 && opcode <= 186) ||
+				(opcode >= 200 && opcode <= 201))
+				return 5;
+
+			// If this point is reached then the instruction is not known.
+			throw new JVMVerifyException(String.format(
+				"IN1h %d", opcode));
 		}
 		
 		// Out of bounds instruction
