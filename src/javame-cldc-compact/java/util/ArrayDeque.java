92d4fe47fec9ce79811815b671973a446aa288e0
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-04-10 16:56:51 +0000

    Clear some old code; Add java.util.ArrayDeque.

---
+++ src/javame-cldc-compact/java/util/ArrayDeque.java
@@ -0,0 +1,205 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+public class ArrayDeque<E>
+	extends AbstractCollection<E>
+	implements Deque<E>, Cloneable
+{
+	public ArrayDeque()
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	public ArrayDeque(int __a)
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	public ArrayDeque(Collection<? extends E> __a)
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public boolean add(E __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void addFirst(E __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void addLast(E __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public void clear()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public ArrayDeque<E> clone()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public boolean contains(Object __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public Iterator<E> descendingIterator()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E element()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E getFirst()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E getLast()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public boolean isEmpty()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public Iterator<E> iterator()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean offer(E __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean offerFirst(E __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean offerLast(E __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public E peek()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E peekFirst()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E peekLast()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E poll()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E pollFirst()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E pollLast()
+	{
+		throw new Error("TODO");
+	}
+	
+	public E pop()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void push(E __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public E remove()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public boolean remove(Object __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public E removeFirst()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean removeFirstOccurrence(Object __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public E removeLast()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean removeLastOccurrence(Object __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public int size()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public Object[] toArray()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public <T> T[] toArray(T[] __a)
+	{
+		throw new Error("TODO");
+	}
+}
+
