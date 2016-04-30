40a43db3e61c23d3c88da4907ce9f87643e111bc
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-04-30 15:59:59 +0000

    Pipes will consist of sinks and faucets.

---
+++ src/extra-io-datasink/net/multiphasicapps/io/datasink/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains a datasink which consumes data.
+ *
+ * @since 2016/04/30
+ */
+
+package net.multiphasicapps.io.datasink;
+
