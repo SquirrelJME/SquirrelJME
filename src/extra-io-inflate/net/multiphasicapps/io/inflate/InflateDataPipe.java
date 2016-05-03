df22500e6c9cc366df5f2bb89cb7c7f82a1e87cb
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-03 12:36:21 +0000

    Debug work on window shifting.

--- src/extra-io-inflate/net/multiphasicapps/io/inflate/InflateDataPipe.java
+++ src/extra-io-inflate/net/multiphasicapps/io/inflate/InflateDataPipe.java
@@ -1072,7 +1072,26 @@ public class InflateDataPipe
 			int qwinsz = _qwinsz;
 			if ((qwinsz + __b) >= _QUICK_WINDOW_BITS)
 			{
-				throw new Error("TODO");
+				// Move down
+				int dn = (__b >>> 3) + 1;
+				int sn = (qwinsz >>> 3);
+				int d = 0;
+				for (int s = (qbit >>> 3); s < sn; d++, s++)
+				{
+					System.err.printf("DEBUG -- %d < %d = %02x (%d %d)%n", d,
+						s, qwin[s], qwinsz, __b);
+					qwin[d] = qwin[s];
+				}
+				
+				// Debug
+				for (int x = d; x < qwin.length; x++)
+					qwin[x] = (byte)0xFE;
+				
+				// Crimp inputs
+				qwinsz -= (qbit & (~7));
+				qbit &= 7;
+				_qbit = qbit;
+				_qwinsz = qwinsz;
 			}
 			
 			// Read bytes from the input pipe and place them at the end of the
@@ -1083,6 +1102,20 @@ public class InflateDataPipe
 			
 			// Store
 			_qwinsz = qwinsz;
+			
+			// The window state
+			System.err.printf("DEBUG -- %d/%d (%d):", _qbit, _qbit / 8,
+				_qwinsz);
+			for (int i = 0; i < (_qwinsz >>> 3); i++)
+			{
+				System.err.print(' ');
+				if ((_qbit >>> 3) == i)
+					System.err.print('<');
+				System.err.printf("%02x", _qwin[i]);
+				if ((_qbit >>> 3) == i)
+					System.err.print('>');
+			}
+			System.err.println();
 		}
 	}
 	
