feb6d11ef5be0feac3ab177a9d4bef5af0066c49
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-11 11:22:13 +0000

    Add base exception class.

---
+++ src/native-compiler-byte-code/net/multiphasicapps/narf/bytecode/NBCException.java
@@ -0,0 +1,79 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.narf.bytecode;
+
+import net.multiphasicapps.narf.exception.NARFException;
+
+/**
+ * This represents an exception which may be thrown if the Java byte code is
+ * not valid or does not verify correctly.
+ *
+ * @since 2016/05/11
+ */
+public class NBCException
+	extends NARFException
+{
+	/**
+	 * Initializes the exception with no message or cause.
+	 *
+	 * @since 2016/05/11
+	 */
+	public NBCException(Issue __i)
+	{
+		super(__i);
+	}
+	
+	/**
+	 * Initializes the exception with the given message.
+	 *
+	 * @param __msg The exception message.
+	 * @since 2016/05/11
+	 */
+	public NBCException(Issue __i, String __msg)
+	{
+		super(__i, __msg);
+	}
+	
+	/**
+	 * Initializes the exception with the given message and cause.
+	 *
+	 * @param __msg The exception message.
+	 * @param __c The cause.
+	 * @since 2016/05/11
+	 */
+	public NBCException(Issue __i, String __msg, Throwable __c)
+	{
+		super(__i, __msg, __c);
+	}
+	
+	/**
+	 * Initializes the exception with the given cause and no message.
+	 *
+	 * @param __c The cause of the exception.
+	 * @since 2016/05/11
+	 */
+	public NBCException(Issue __i, Throwable __c)
+	{
+		super(__i, __c);
+	}
+	
+	/**
+	 * This is the reason for failure.
+	 *
+	 * @since 2016/05/11
+	 */
+	public static enum Issue
+		implements BaseIssue
+	{
+		/** End. */
+		;
+	}
+
