50cbc23c44a901ae91272c399bdc8beb3b6f62af
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-14 18:44:40 +0000

    Add base for the base launcher.

---
+++ src/launcher-jvm-javase/net/multiphasicapps/squirreljme/launch/jvm/javase/JVMJavaSELauncher.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.launch.jvm.javase;
+
+import net.multiphasicapps.squirreljme.launch.AbstractLauncher;
+
+/**
+ * This contains the launcher used by the host Java SE system.
+ *
+ * @since 2016/05/14
+ */
+public class JVMJavaSELauncher
+	extends AbstractLauncher
+{
+}
+
---
+++ src/launcher/net/multiphasicapps/squirreljme/launch/AbstractLauncher.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.launch;
+
+/**
+ * This is the base class for the launcher interfaces which are defined by
+ * systems to provide anything that the default launcher does not provide
+ * when it comes to interfaces.
+ *
+ * @since 2016/05/14
+ */
+public abstract class AbstractLauncher
+{
+}
+
