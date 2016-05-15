517969e006e4e4cbf9ad0f9f6ec805a58a678b0e
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-15 12:43:21 +0000

    Base for event queue and event handler.

---
+++ src/launcher/net/multiphasicapps/squirreljme/launch/EventHandler.java
@@ -0,0 +1,37 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.launch;
+
+/**
+ * This is a class which is used as an event handler when one is to be handled.
+ *
+ * The return values of these event handlers allow other events to be
+ * composed and passed through to the next event handler.
+ *
+ * @since 2016/05/15
+ */
+public interface EventHandler
+{
+	/**
+	 * If this is returned from an event handler, the event does not get
+	 * processed through other handlers.
+	 */
+	public static final int DROP_EVENT =
+		Integer.MIN_VALUE;
+	
+	/**
+	 * If this is returned from an event handler, the event is passed through
+	 * to the next handler without being modified.
+	 */
+	public static final int PASS_EVENT =
+		Integer.MIN_VALUE + 1;
+}
+
---
+++ src/launcher/net/multiphasicapps/squirreljme/launch/EventQueue.java
@@ -0,0 +1,37 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.launch;
+
+/**
+ * This is the event queue.
+ *
+ * For reduced memory, the internal event loop is represented by integers which
+ * store the event data. If the queue size is exceeded then it is attempted to
+ * be increased in size, otherwise if that fails then events are dropped.
+ *
+ * @since 2016/05/15
+ */
+public class EventQueue
+{
+	/** Lock. */
+	protected final Object lock =
+		new Object();
+	
+	/** The event queue. */
+	private volatile int[] _queue;
+	
+	/** The read position in the queue. */
+	private volatile int _read;
+	
+	/** The write position in the queue. */
+	private volatile int _write;
+}
+
