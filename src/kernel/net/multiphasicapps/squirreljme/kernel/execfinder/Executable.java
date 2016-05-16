8cafa4bd62b963b385c618e21bacf76e9374905a
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-16 13:10:26 +0000

    Base for Executable and ExecutableFinder.

---
+++ src/kernel/net/multiphasicapps/squirreljme/kernel/execfinder/Executable.java
@@ -0,0 +1,27 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.kernel.execfinder;
+
+/**
+ * This interface describes an executable which binds to a JAR or some other
+ * internal storage for executables. This class describes which kind of program
+ * the executable is and how it is ran. When it comes to execution, the
+ * executable may run differently. For example the executable could be part of
+ * executable ROM and as such starting execution would essentially just be
+ * setting up a state and then setting the program counter to the entry address
+ * of the program
+ *
+ * @since 2016/05/16
+ */
+public interface Executable
+{
+}
+
