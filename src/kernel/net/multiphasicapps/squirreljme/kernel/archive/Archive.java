d397d561872f56eddd03067d049a9523fe9d742e
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-18 13:41:15 +0000

    Notes; Base for archive.

---
+++ src/kernel/net/multiphasicapps/squirreljme/kernel/archive/Archive.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.kernel.archive;
+
+/**
+ * This is the base class for providers which provide archive views of JAR
+ * files, native executables, and potentially other details.
+ *
+ * @since 2016/05/18
+ */
+public abstract class Archive
+{
+}
+
---
+++ src/kernel/net/multiphasicapps/squirreljme/kernel/archive/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This provides an interface over JAR files and native executables so that
+ * they may be built together to form the classpath.
+ *
+ * @since 2016/05/18
+ */
+
+package net.multiphasicapps.squirreljme.kernel.archive;
+
