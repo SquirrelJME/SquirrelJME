c4be96638c7b4b1d8ddca74394c67134e137622d
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-23 19:24:19 +0000

    Base for menu items.

---
+++ src/user-interface/net/multiphasicapps/squirreljme/ui/InternalMenuItem.java
@@ -0,0 +1,34 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.ui;
+
+import java.lang.ref.Reference;
+
+/**
+ * This is an internal representation of a menu item.
+ *
+ * @since 2016/05/23
+ */
+public abstract class InternalMenuItem
+	extends InternalElement<UIMenuItem>
+{
+	/**
+	 * Initializes the internal menu item.
+	 *
+	 * @param __ref The reference to the external menu item.
+	 * @since 2016/05/23
+	 */
+	public InternalMenuItem(Reference<UIMenuItem> __ref)
+	{
+		super(__ref);
+	}
+}
+
---
+++ src/user-interface/net/multiphasicapps/squirreljme/ui/UIMenuItem.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.ui;
+
+/**
+ * This represents a menu item inside of a menu, it has a label and an assigned
+ * label.
+ *
+ * @since 2016/05/23
+ */
+public final class UIMenuItem
+	extends UIElement
+{
+	/**
+	 * Initializes the menu item.
+	 *
+	 * @param __dm The display manager owning this.
+	 * @since 2016/05/23
+	 */
+	UIMenuItem(UIDisplayManager __dm)
+	{
+		super(__dm);
+	}
+}
+
