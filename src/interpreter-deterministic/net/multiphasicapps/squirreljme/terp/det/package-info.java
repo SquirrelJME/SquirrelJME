d400d7a26ecb66a08181c583137129bb06517799
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-29 00:47:05 +0000

    Base for deterministic interpreter package.

---
+++ src/interpreter-deterministic/net/multiphasicapps/squirreljme/terp/det/package-info.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the deterministic interpreter. This interpreter is
+ * generally designed for testing and is not made for speed, however it is
+ * designed for (re-)recording virtual machine sessions along with playing
+ * back code that was executed.
+ *
+ * @since 2016/05/28
+ */
+
+package net.multiphasicapps.squirreljme.terp.det;
+
