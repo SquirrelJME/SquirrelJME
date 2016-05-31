68b1ead3856bf2108ca6655922bc464a70179098
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-05-31 20:42:33 +0000

    Create main class for the launcher.

---
+++ src/launcher/net/multiphasicapps/squirreljme/launcher/Main.java
@@ -0,0 +1,34 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.launcher;
+
+/**
+ * This is the main entry point for the launcher interface, it sets up the
+ * launcher interface and optionally starts executing a program specified on
+ * the command line.
+ *
+ * @since 2016/05/31
+ */
+public class Main
+{
+	/**
+	 * Main entry point for the launcher.
+	 *
+	 * @param __args Launcher arguments (specifically the program to execute
+	 * on launch).
+	 * @since 2016/05/31
+	 */
+	public static void main(String... __args)
+	{
+		throw new Error("TODO");
+	}
+}
+
