a2744ba41ef584b8f91a74365028f293f8242c26
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-01 20:04:10 +0000

    Base for data packets.

--- src/interpreter-rerecord/net/multiphasicapps/squirreljme/terp/rr/RRDataPacket.java
+++ src/interpreter-rerecord/net/multiphasicapps/squirreljme/terp/rr/RRDataPacket.java
@@ -10,12 +10,61 @@
 
 package net.multiphasicapps.squirreljme.terp.rr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * DESCRIBE THIS.
+ * This is a data packet which is recorded and read from an input replay
+ * stream.
+ *
+ * This class is not thread safe and is mutable.
  *
  * @since 2016/06/01
  */
 public class RRDataPacket
 {
+	/** The objects for input and output. */
+	protected final List<Object> data =
+		new ArrayList<>();
+	
+	/** The command packet type. */
+	private volatile RRDataCommand _command;
+	
+	/** The number of elements in the packet. */
+	private volatile int _count;
+	
+	/**
+	 * Initializes the data packet.
+	 *
+	 * @since 2016/06/01
+	 */
+	public RRDataPacket()
+	{
+	}
+	
+	/**
+	 * Returns the data at the specified index.
+	 *
+	 * @param __i The index to get data for.
+	 * @return The data at the specified index.
+	 * @throws IndexOutOfBoundsException If the index it outside of bounds.
+	 * @since 2016/06/01
+	 */
+	public Object get(int __i)
+		throws IndexOutOfBoundsException
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the amount of data elements in the packet.
+	 *
+	 * @return The number of elements in the packet.
+	 * @since 2016/06/01
+	 */
+	public int length()
+	{
+		throw new Error("TODO");
+	}
 }
 
