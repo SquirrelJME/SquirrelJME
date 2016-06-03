9b67b5f6947e303f36b01f9449563eb8f37b4f51
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-03 22:33:47 +0000

    Base for rewriting methods.

--- src/interpreter-rerecord/net/multiphasicapps/squirreljme/terp/rr/RRThread.java
+++ src/interpreter-rerecord/net/multiphasicapps/squirreljme/terp/rr/RRThread.java
@@ -33,7 +33,50 @@ public class RRThread
 	public RRThread(RRInterpreter __terp,
 		RRProcess __ip, CIMethod __m, Object... __args)
 	{
-		super(__terp, __ip, __m, __args);
+		super(__terp, __ip, (__m = __rewriteEntryMethod(__terp, __m)),
+			(__args = __rewriteEntryArgs(__terp, __args)));
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Rewrites the entry method when replaying a stream then writes the
+	 * entry method if recording.
+	 *
+	 * @param __terp The interpreter.
+	 * @param __m The original entry method.
+	 * @return The rewritten entry method.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/03
+	 */
+	private static final CIMethod __rewriteEntryMethod(RRInterpreter __terp,
+		CIMethod __m)
+		throws NullPointerException
+	{
+		// Check
+		if (__terp == null || __m == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Rewrites the entry arguments when replaying a stream then writes the
+	 * entry arguments if recording.
+	 *
+	 * @param __terp The interpreter.
+	 * @param __args The original entry arguments.
+	 * @return The rewritten entry arguments.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/03
+	 */
+	private static final Object[] __rewriteEntryArgs(RRInterpreter __terp,
+		Object... __args)
+		throws NullPointerException
+	{
+		// Check
+		if (__terp == null || __args == null)
+			throw new NullPointerException("NARG");
 		
 		throw new Error("TODO");
 	}
