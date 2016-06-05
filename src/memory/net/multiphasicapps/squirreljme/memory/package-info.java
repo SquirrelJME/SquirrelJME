595d2fa5fb2905c775e5177f78ec764d07c79e8d
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-05 13:09:53 +0000

    Base for memory package.

---
+++ src/memory/net/multiphasicapps/squirreljme/memory/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the memory interfaces which are used to allocate and
+ * manage objects that exist within memory.
+ *
+ * @since 2016/06/05
+ */
+
+package net.multiphasicapps.squirreljme.memory;
+
