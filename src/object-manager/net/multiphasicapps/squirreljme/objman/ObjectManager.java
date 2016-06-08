fbfe7f4f17d2012df0ab380dc2445186047231d2
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-08 18:43:52 +0000

    Add base object manager and the type used pointers.

---
+++ src/object-manager/net/multiphasicapps/squirreljme/objman/ObjectManager.java
@@ -0,0 +1,53 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.objman;
+
+import net.multiphasicapps.squirreljme.memory.MemoryIOException;
+import net.multiphasicapps.squirreljme.memory.MemoryPool;
+import net.multiphasicapps.squirreljme.memory.MemoryPoolManager;
+
+/**
+ * This is the manager which manages all allocated objects.
+ *
+ * This is shared by all implementations of SquirrelJME so that they have a
+ * uniform object layout structure.
+ *
+ * @since 2016/06/08
+ */
+public class ObjectManager
+{
+	/** The used memory pool. */
+	protected final MemoryPoolManager poolman;
+	
+	/** The type used for pointers. */
+	protected final PointerType pointertype;
+	
+	/**
+	 * Intializes the object manager.
+	 *
+	 * @param __pm Initializes the memory pool manager.
+	 * @param __pt The type of values used for pointers.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/08
+	 */
+	public ObjectManager(MemoryPoolManager __pm, PointerType __pt)
+		throws NullPointerException
+	{
+		// Check
+		if (__pm == null || __pm == null)
+			throw new NullPointerException("NARG");
+		
+		// Set
+		this.poolman = __pm;
+		this.pointertype = __pt;
+	}
+}
+
---
+++ src/object-manager/net/multiphasicapps/squirreljme/objman/PointerType.java
@@ -0,0 +1,32 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.objman;
+
+/**
+ * This is the Java type that is used to store pointer values.
+ *
+ * @since 2016/06/08
+ */
+public enum PointerType
+{
+	/** 16-bit pointers. */
+	SHORT,
+	
+	/** 32-bit pointers. */
+	INTEGER,
+	
+	/** 64-bit pointers. */
+	LONG,
+	
+	/** Long. */
+	;
+}
+
