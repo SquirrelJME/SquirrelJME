77e14a9270415ae7ab1aaa2a03e879b1ef5abc2d
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-13 19:39:22 +0000

    Base for memory manager which will own an accessor and an allocator so that allocation can be performed separate of code/data regions in Harvard architectures.

---
+++ src/mmu/net/multiphasicapps/squirreljme/mmu/MemoryManager.java
@@ -0,0 +1,26 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.mmu;
+
+/**
+ * This class is given an allocator and accessor and allows for singular
+ * management in code and data regions of memory.
+ *
+ * This allows Harvard architectures to be given the ability to use just in
+ * time and/or ahead of time compilation. The code and data sections may have
+ * different alignment properties.
+ *
+ * @since 2016/06/13
+ */
+public class MemoryManager
+{
+}
+
