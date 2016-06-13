a08d0f0a0a9f8d316d4ca7a5b2b3c95e8ecab08e
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-13 19:43:38 +0000

    Base work.

--- src/mmu/net/multiphasicapps/squirreljme/mmu/MemoryManager.java
+++ src/mmu/net/multiphasicapps/squirreljme/mmu/MemoryManager.java
@@ -22,5 +22,23 @@ package net.multiphasicapps.squirreljme.mmu;
  */
 public class MemoryManager
 {
+	/**
+	 * This initializes the memory manager which is used to bridge an accessor
+	 * and all allocator for a given memory accessor.
+	 *
+	 * Note that if the allocator allocator access memory other than the
+	 * accessor then it is likely that incorrect results will occur.
+	 *
+	 * @param __ac The accessor for the region of memory.
+	 * @param __al The allocator which is capable of allocating new regions
+	 * within the specified region.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/13
+	 */
+	public MemoryManager()
+		throws NullPointerException
+	{
+		throw new Error("TODO");
+	}
 }
 
