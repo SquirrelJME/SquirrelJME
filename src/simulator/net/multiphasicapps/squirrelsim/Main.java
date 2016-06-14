f3608ba1c8ae5d3ff126f8725325fa14195caf22
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-14 14:35:50 +0000

    Check that both an architecture and OS are specified.

--- src/simulator/net/multiphasicapps/squirrelsim/Main.java
+++ src/simulator/net/multiphasicapps/squirrelsim/Main.java
@@ -10,6 +10,11 @@
 
 package net.multiphasicapps.squirrelsim;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * This is the main entry point for the SquirrelJME simulator.
  *
@@ -25,6 +30,63 @@ public class Main
 	 */
 	public static void main(String... __args)
 	{
+		// Enqueue all arguments for handling
+		Deque<String> aq = new LinkedList<>();
+		for (String a : __args)
+			aq.add(a);
+		
+		// Handle them all
+		String wantcpu = null;
+		String wantos = null;
+		List<String> wantprogram = new ArrayList<>();
+		boolean gotend = false;
+		while (!aq.isEmpty())
+		{
+			// Get next
+			String a = aq.removeFirst();
+			
+			// End of arguments?
+			if (a.equals("--"))
+			{
+				// Correct command line
+				gotend = true;
+				
+				// Fill the desired program arguments
+				while (!aq.isEmpty())
+					wantprogram.add(aq.removeFirst());
+				
+				// Stop
+				break;
+			}
+			
+			// {@squirreljme.cmdline -C(arch) Select CPU to simulate.}
+			if (a.startsWith("-C"))
+				throw new Error("TODO");
+			
+			// {@squirreljme.cmdline -O(os) Select operating system to
+			// simulate.}
+			else if (a.startsWith("-O"))
+				throw new Error("TODO");
+			
+			// {@squirreljme.error BV02 Unknown command line argument. (The
+			// command line switch)}
+			else
+				throw new IllegalArgumentException(String.format("BV02 %s",
+					a));
+		}
+		
+		// {@squirreljme.error Expected to be given the program to launch.}
+		if (!gotend)
+			throw new IllegalArgumentException("BV01");
+		
+		// {@squirreljme.error BV03 No CPU was specified.}
+		if (wantcpu == null)
+			throw new IllegalArgumentException("BV03");
+		
+		// {@squirreljme.error BV04 No operating system was specified.}
+		if (wantos == null)
+			throw new IllegalArgumentException("BV04");
+		
 		throw new Error("TODO");
 	}
 }
