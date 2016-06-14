2c56da44fb18adf079745eb7e4dcb862a4db37a5
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-14 17:32:36 +0000

    Base for simulator configuration class.

---
+++ src/simulator/net/multiphasicapps/squirrelsim/SimulatorConfiguration.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelsim;
+
+/**
+ * This class contains a mutable configuration that the simulator will use
+ * when it comes to initialization.
+ *
+ * @since 2016/06/14
+ */
+public class SimulatorConfiguration
+{
+}
+
