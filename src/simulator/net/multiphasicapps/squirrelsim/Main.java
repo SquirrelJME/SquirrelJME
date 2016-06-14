a590a7cf72b079252c803680bf8f4521df067ced
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-14 15:07:09 +0000

    Add a root option.

--- src/simulator/net/multiphasicapps/squirrelsim/Main.java
+++ src/simulator/net/multiphasicapps/squirrelsim/Main.java
@@ -10,10 +10,14 @@
 
 package net.multiphasicapps.squirrelsim;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This is the main entry point for the SquirrelJME simulator.
@@ -38,6 +42,7 @@ public class Main
 		// Handle them all
 		String wantcpu = null;
 		String wantos = null;
+		Map<String, Path> roots = new LinkedHashMap<>();
 		List<String> wantprogram = new ArrayList<>();
 		boolean gotend = false;
 		while (!aq.isEmpty())
@@ -79,6 +84,28 @@ public class Main
 					throw new IllegalArgumentException("BV06");
 				
 				wantos = a.substring(2);
+			}
+			
+			// {@squirreljme.cmdline -R(name=(mode)=(over)=(path)) Adds a root
+			// filesystem to the simulated machine. The name specifies the name
+			// of the root (such as {@code /} or {@code C}. Not all simulators
+			// support multiple roots or incorrectly named roots. The mode
+			// determines how the root is to be treated.
+			// "{@code w}" makes the root writable, otherwise it is read-only.
+			// The overlay is optional and if it is not used then it must be
+			// blank. The overlay is used so that any modifications to the
+			// filesystem performed by the simulator are performed, they are
+			// placed in the given root instead. If the overlay is not used
+			// then all writes (assuming the filesystem is writable) go
+			// to the actual filesystem.
+			// The path specifies the source of which files make up the given
+			// root filesystem. This may be a ZIP or ISO file so that a common
+			// base for pre-existing files may be used for simpler debugging.
+			// Note that for write support when using ZIP/ISO an overlay must
+			// be specified.}
+			else if (a.startsWith("-R"))
+			{
+				throw new Error("TODO");
 			}
 			
 			// {@squirreljme.error BV02 Unknown command line argument. (The
