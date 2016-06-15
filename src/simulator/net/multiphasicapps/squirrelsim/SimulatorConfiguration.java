b89195db0fc448aaef8a84615141ee00c954a8de
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-15 03:18:58 +0000

    Decode of memory, IPS, and a better set of OS/CPU.

--- src/simulator/net/multiphasicapps/squirrelsim/SimulatorConfiguration.java
+++ src/simulator/net/multiphasicapps/squirrelsim/SimulatorConfiguration.java
@@ -23,6 +23,18 @@ import java.util.ServiceLoader;
  */
 public class SimulatorConfiguration
 {
+	/** The default system hostname. */
+	public static final String DEFAULT_HOSTNAME =
+		"squirreljme";
+	
+	/** The default size of memory. */
+	public static final long DEFAULT_MEMORY_SIZE =
+		25_165_824L;
+	
+	/** Default instructions per second. */
+	public static final long DEFAULT_IPS =
+		133_000_000L;
+	
 	/** CPU providers which are available for usage. */
 	private static final ServiceLoader<CPUProvider> _CPU_SERVICES =
 		ServiceLoader.<CPUProvider>load(CPUProvider.class);
@@ -46,6 +58,18 @@ public class SimulatorConfiguration
 	
 	/** The CPU variant. */
 	private volatile String _cpuvar;
+	
+	/** The amount of memory to use. */
+	private volatile long _memory =
+		DEFAULT_MEMORY_SIZE;
+	
+	/** The number of instruction that execute per second. */
+	private volatile long _ips =
+		DEFAULT_IPS;
+	
+	/** The hostname to use. */
+	private volatile String _hostname =
+		DEFAULT_HOSTNAME;
 	
 	/**
 	 * Initializes the simulator configuration which uses all defaults.
@@ -115,7 +139,7 @@ public class SimulatorConfiguration
 			// Find equal sign
 			int eq = ln.indexOf('=');
 			
-			// {@squirreljme.error Expected equal sign on input line.
+			// {@squirreljme.error BV05 Expected equal sign on input line.
 			// (The input line)}
 			if (eq < 0)
 				throw new IOException(String.format("BV05 %s", ln));
@@ -172,11 +196,7 @@ public class SimulatorConfiguration
 							"BV09 %s", __v));
 					
 					// Set
-					synchronized (lock)
-					{
-						this._cpu = cp;
-						this._cpuvar = xvar;
-					}
+					setCPU(cp, xvar);
 				}
 				break;
 				
@@ -203,25 +223,24 @@ public class SimulatorConfiguration
 							"BV0a %s", __v));
 					
 					// Set
-					synchronized (lock)
-					{
-						this._os = op;
-						this._osvar = xvar;
-					}
+					setOS(op, xvar);
 				}
 				break;
 				
 				// The amount of memory which is available
 			case "system.memory":
-				throw new Error("TODO");
+				setMemorySize(__decodeBytesLong(__v));
+				break;
 				
 				// The instructions per second for the given system
 			case "system.ips":
-				throw new Error("TODO");
+				setIPS(__decodeSiLong(__v));
+				break;
 				
 				// The hostname of the simulated system
 			case "system.hostname":
-				throw new Error("TODO");
+				setHostName(__v);
+				break;
 			
 				// {@squirreljme.error BV06 Cannot set the given key and value
 				// because it is not known. (The key; The value)}
@@ -232,12 +251,251 @@ public class SimulatorConfiguration
 	}
 	
 	/**
-	 * Decode a string which has an si suffix.
+	 * Sets the CPU which is to be simulated.
+	 *
+	 * @param __o The CPU to use.
+	 * @param __v The variant of the CPU.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/14
+	 */
+	public void setCPU(CPUProvider __o, String __v)
+		throws NullPointerException
+	{
+		// Check
+		if (__o == null || __v == null)
+			throw new NullPointerException("NARG");
+		
+		// Lock
+		synchronized (this.lock)
+		{
+			this._cpu = __o;
+			this._cpuvar = __v;
+		}
+	}
+	
+	/**
+	 * Sets the hostname of the system being simulated.
+	 *
+	 * @param __v The simulated hostname.
+	 * @return The old hostname
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/14
+	 */
+	public String setHostName(String __v)
+		throws NullPointerException
+	{
+		// Check
+		if (__v == null)
+			throw new NullPointerException("NARG");
+		
+		// Lock
+		synchronized (this.lock)
+		{
+			String rv = this._hostname;
+			
+			this._hostname = __v;
+			
+			return rv;
+		}
+	}
+	
+	/**
+	 * This sets the logical number of instructions which execute within a
+	 * single second.
+	 *
+	 * @param __v The number of instructions that execute in a second.
+	 * @return The former instructions per second.
+	 * @throws IllegalArgumentException If the requested number of instructions
+	 * to execute in a single second is zero or negative.
+	 * @since 2016/06/14
+	 */
+	public long setIPS(long __v)
+		throws IllegalArgumentException
+	{
+		// {@squirreljme.error BV0f Cannot execute zero or negative number of
+		// instructions per second. (The requested number of instructions per
+		// second)}
+		if (__v <= 0)
+			throw new IllegalArgumentException(String.format("BV0f %d", __v));
+		
+		// Lock
+		synchronized (this.lock)
+		{
+			long rv = this._ips;
+			
+			this._ips = __v;
+			
+			return rv;
+		}
+	}
+	
+	/**
+	 * Sets the number of bytes to use for programs which are running.
+	 *
+	 * @param __b The number of bytes to use per program.
+	 * @return The former memory size.
+	 * @throws IllegalArgumentException If the size of memory is zero or
+	 * negative.
+	 * @since 2016/06/14
+	 */
+	public long setMemorySize(long __b)
+		throws IllegalArgumentException
+	{
+		// {@squirreljme.error BV0e The size of memory cannot be zero or
+		// negative. (The requested size of memory)}
+		if (__b <= 0)
+			throw new IllegalArgumentException(String.format("BV0e %d", __b));
+		
+		// Lock
+		synchronized (this.lock)
+		{
+			long rv = this._memory;
+			
+			this._memory = __b;
+			
+			return rv;
+		}
+	}
+	
+	/**
+	 * Sets the operating which is to be simulated.
+	 *
+	 * @param __o The operating system to use.
+	 * @param __v The variant of the operating system.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/14
+	 */
+	public void setOS(OSProvider __o, String __v)
+		throws NullPointerException
+	{
+		// Check
+		if (__o == null || __v == null)
+			throw new NullPointerException("NARG");
+		
+		// Lock
+		synchronized (this.lock)
+		{
+			this._os = __o;
+			this._osvar = __v;
+		}
+	}
+	
+	/**
+	 * Decodes a byte quantity that could either use SI or binary prefixes.
 	 *
 	 * @param __s The string to decode.
 	 * @return The input value.
 	 * @throws IllegalArgumentException If the specified input is not a number.
 	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/14
+	 */
+	private static long __decodeBytesLong(String __s)
+		throws IllegalArgumentException, NullPointerException
+	{
+		// Check
+		if (__s == null)
+			throw new NullPointerException("NARG");
+		
+		// Could be an illegal number
+		try
+		{
+			// The multiplier and fragment
+			long mul = 1L;
+			String frag;
+			
+			// The length must be at least 3 for there to be a prefix
+			int n = __s.length();
+			if (n >= 2)
+			{
+				// Try prefixes of all 3 sizes (M, Mi, MiB)
+				String got = null;
+				long ful = Long.MIN_VALUE;
+				for (int i = 3; i >= 1; i--)
+				{
+					// Too small?
+					if (n - i <= 0)
+						continue;
+					
+					// Extract prefix
+					String pfx = __s.substring(n - i);
+					
+					// Depends on the prefix
+					switch (pfx)
+					{
+							// Decimal prefixes
+						case "B": ful = 1L; break;
+						case "KB":
+						case "K":
+						case "kB":
+						case "k": ful = 1_000L; break;
+						case "MB":
+						case "M": ful = 1_000_000L; break;
+						case "GB":
+						case "G": ful = 1_000_000_000L; break;
+						case "TB":
+						case "T": ful = 1_000_000_000_000L; break;
+						case "PB":
+						case "P": ful = 1_000_000_000_000_000L; break;
+						case "EB":
+						case "E": ful = 1_000_000_000_000_000_000L; break;
+						
+							// Binary prefixes
+						case "kiB":
+						case "KiB": ful = 1_024L; break;
+						case "MiB": ful = 1_048_576L; break;
+						case "GiB": ful = 1_073_741_824; break;
+						case "TiB": ful = 1_099_511_627_776L; break;
+						case "PiB": ful = 1_125_899_906_842_624L; break;
+						case "EiB": ful = 1_152_921_504_606_846_976L; break;
+						
+							// Ignore for now
+						default:
+							break;
+					}
+					
+					// Found multiplier?
+					if (ful >= 0)
+					{
+						got = pfx;
+						break;
+					}
+				}
+				
+				// {@squirreljme.error BV0d Could not determine the byte
+				// prefix that the input uses. (The input value)}
+				if (ful < 0)
+					throw new IllegalArgumentException(String.format("BV0d %s",
+						__s));
+				
+				// Set
+				mul = ful;
+				frag = __s.substring(0, n - got.length());
+			}
+			
+			else
+				frag = __s;
+			
+			// Decode and multiply
+			return Long.decode(frag) * mul;
+		}
+		
+		// {@squirreljme.error BV0c The input value is not a number. (The
+		// input value)}
+		catch (NumberFormatException e)
+		{
+			throw new IllegalArgumentException(String.format("BV0c %s", __s),
+				e);
+		}
+	}
+	
+	/**
+	 * Decode a string which has an SI prefix.
+	 *
+	 * @param __s The string to decode.
+	 * @return The input value.
+	 * @throws IllegalArgumentException If the specified input is not a number.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/06/14
 	 */
 	private static long __decodeSiLong(String __s)
 		throws IllegalArgumentException, NullPointerException
@@ -284,10 +542,7 @@ public class SimulatorConfiguration
 			
 			// A plain number with no prefix
 			else
-			{
-				mul = 1L;
 				frag = __s;
-			}
 			
 			// Decode and multiply
 			return Long.decode(frag) * mul;
