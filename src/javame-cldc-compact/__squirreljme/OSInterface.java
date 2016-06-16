2eb4cf0547d40bae35e9eb324b8fa683a84f70f8
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-16 01:04:11 +0000

    Base for OS interface.

---
+++ src/javame-cldc-compact/__squirreljme/OSInterface.java
@@ -0,0 +1,38 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package __squirreljme;
+
+/**
+ * This contains the interface to the host operating system.
+ *
+ * Operating systems in general will implement this interface which would then
+ * be used to provide the required native functionality.
+ *
+ * @since 2016/06/15
+ */
+public abstract class OSInterface
+{
+	public static final OSInterface INSTANCE =
+		__getInstance();
+	
+	/**
+	 * The OS interface is magically pre-initialized to a given value, so to
+	 * make it Java compilation friendly the value of the field is returned.
+	 *
+	 * @return {@link #INSTANCE}.
+	 * @since 2016/06/15
+	 */
+	private static OSInterface __getInstance()
+	{
+		return INSTANCE;
+	}
+}
+
