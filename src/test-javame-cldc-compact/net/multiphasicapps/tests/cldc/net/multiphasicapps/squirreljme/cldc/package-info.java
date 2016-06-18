1f924cb1a188119dab17a35fd121ca03f069ce81
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-18 16:46:26 +0000

    Base package to contain the test.

---
+++ src/test-javame-cldc-compact/net/multiphasicapps/tests/cldc/net/multiphasicapps/squirreljme/cldc/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains tests for supporting CLDC methods so that their
+ * proper operation can be tested..
+ *
+ * @since 2016/06/18
+ */
+
+package net.multiphasicapps.tests.cldc.net.multiphasicapps.squirreljme.cldc;
+
