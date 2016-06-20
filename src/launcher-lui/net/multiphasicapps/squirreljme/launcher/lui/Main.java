6651fd9ccf8ff0fefad8833f86b26ef558634fdd
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-20 16:33:47 +0000

    Add actual Main and package-info for the LUI launcher.

---
+++ src/launcher-lui/net/multiphasicapps/squirreljme/launcher/lui/Main.java
@@ -0,0 +1,34 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.launcher.lui;
+
+/**
+ * Ths is the main entry class for the line user interface based launcher, this
+ * should be used as a fallback in the event that the LCDUI based launcher
+ * cannot be used or was not compiled in.
+ *
+ * @since 2016/06/20
+ */
+public class Main
+{
+	/**
+	 * Main entry point for setting up the line based launcher.
+	 *
+	 * @param __args The program arguments (can set the program to initially
+	 * be launched).
+	 * @since 2016/06/20
+	 */
+	public static void main(String... __args)
+	{
+		throw new Error("TODO");
+	}
+}
+
