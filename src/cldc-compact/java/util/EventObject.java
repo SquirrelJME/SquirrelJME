ad66c48ffd04b4b86ca072343764c6f0b0c388cc
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-23 15:11:12 +0000

    add some more missing classes.

---
+++ src/cldc-compact/java/util/EventObject.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+public class EventObject
+{
+	protected transient Object source;
+	
+	public EventObject(Object __a)
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	public Object getSource()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public String toString()
+	{
+		throw new Error("TODO");
+	}
+}
