57a8c22faa1ef467d7f5654732fba9d675557985
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-23 22:07:09 +0000

    When URIs are implemented, the test system can switch to URIs to refer to tests.

---
+++ src/test-util-uri/net/multiphasicapps/tests/util/uri/URITests.java
@@ -0,0 +1,60 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tests.util.uri;
+
+import net.multiphasicapps.tests.InvalidTestException;
+import net.multiphasicapps.tests.TestChecker;
+import net.multiphasicapps.tests.TestInvoker;
+
+/**
+ * This tests that the URI library operates correctly.
+ *
+ * @since 2016/06/23
+ */
+public class URITests
+	implements TestInvoker
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/03/28
+	 */
+	@Override
+	public String invokerName()
+	{
+		return "net.multiphasicapps.util.uri.URI";
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/05/05
+	 */
+	@Override
+	public Iterable<String> invokerTests()
+	{
+		return Arrays.<String>asList();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/03/28
+	 */
+	@Override
+	public void runTest(TestChecker __tc, String __st)
+		throws NullPointerException, Throwable
+	{
+		// Check
+		if (__tc == null || __st == null)
+			throw new NullPointerException();
+		
+		throw new Error("TODO");
+	}
+}
+
