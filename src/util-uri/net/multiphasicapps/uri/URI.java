a986c0d672549f64fd8ae7b92d6998611be638c3
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-23 17:36:59 +0000

    Base for URI class which copies java.net.URI.

---
+++ src/util-uri/net/multiphasicapps/uri/URI.java
@@ -0,0 +1,216 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.uri;
+
+/**
+ * This is a representation of standard URIs and allows information that is
+ * contained within a URI to be obtained and modified.
+ *
+ * This class is immutable and thread safe.
+ *
+ * @since 2016/06/23
+ */
+public final class URI
+	implements Comparable<URI>
+{
+	public URI(String __a)
+		throws URISyntaxException
+	{
+		super();
+		if (false)
+			throw new URISyntaxException(null, null);
+		throw new Error("TODO");
+	}
+	
+	public URI(String __a, String __b, String __c, int __d, String __e,
+		String __f, String __g)
+		throws URISyntaxException
+	{
+		super();
+		if (false)
+			throw new URISyntaxException(null, null);
+		throw new Error("TODO");
+	}
+	
+	public URI(String __a, String __b, String __c, String __d, String __e)
+		throws URISyntaxException
+	{
+		super();
+		if (false)
+			throw new URISyntaxException(null, null);
+		throw new Error("TODO");
+	}
+	
+	public URI(String __a, String __b, String __c, String __d)
+		throws URISyntaxException
+	{
+		super();
+		if (false)
+			throw new URISyntaxException(null, null);
+		throw new Error("TODO");
+	}
+	
+	public URI(String __a, String __b, String __c)
+		throws URISyntaxException
+	{
+		super();
+		if (false)
+			throw new URISyntaxException(null, null);
+		throw new Error("TODO");
+	}
+	
+	public int compareTo(URI __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public boolean equals(Object __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getAuthority()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getFragment()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getHost()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getPath()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getPort()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getQuery()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getRawAuthority()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getRawFragment()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getRawPath()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getRawQuery()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getRawSchemeSpecificPart()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getRawUserInfo()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getScheme()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getSchemeSpecificPart()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getUserInfo()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean isAbsolute()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean isOpaque()
+	{
+		throw new Error("TODO");
+	}
+	
+	public URI normalize()
+	{
+		throw new Error("TODO");
+	}
+	
+	public URI parseServerAuthority()
+		throws URISyntaxException
+	{
+		if (false)
+			throw new URISyntaxException(null, null);
+		throw new Error("TODO");
+	}
+	
+	public URI relativize(URI __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public URI resolve(URI __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public URI resolve(String __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public String toASCIIString()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public String toString()
+	{
+		throw new Error("TODO");
+	}
+	
+	public static URI create(String __a)
+	{
+		throw new Error("TODO");
+	}
+}
+
