549437075ecc77677b329cbd5b9d991b289b17cc
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-24 18:56:57 +0000

    Implement Task.

---
+++ src/meep-swm/javax/microedition/swm/Task.java
@@ -0,0 +1,125 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.swm;
+
+import java.util.Objects;
+
+/**
+ * This describes a task which is currently running on the system. Each task
+ * has a starting point which is a {@link javax.microedition.midlet.MIDlet}.
+ *
+ * System tasks cannot be started or stopped.
+ *
+ * @since 2016/06/24
+ */
+public final class Task
+{
+	/**
+	 * Checks if this task is the same as another task, tasks are equal if
+	 * they share the same name.
+	 *
+	 * @param __o The other object.
+	 * @return {@code true} if this is the same task.
+	 * @sicne 2016/06/24
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		// Must be another task
+		if (!(__o instanceof Task))
+			return false;
+		
+		// Check
+		Task o = (Task)__o;
+		return Objects.equals(getSuite(), o.getSuite()) &&
+			Objects.equals(getName(), o.getName());
+	}
+	
+	/**
+	 * Returns the estimated number of bytes that a task is using.
+	 *
+	 * @return The number of bytes the task is estimated to be using.
+	 * @since 2016/06/24
+	 */
+	public int getHeapUse()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the name of the current task.
+	 *
+	 * @return The name of the task, system tasks always return {@code null}.
+	 * @since 2016/06/24
+	 */
+	public String getName()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the priority of this task.
+	 *
+	 * @return The task priority.
+	 * @since 2016/06/24
+	 */
+	public TaskPriority getPriority()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the status of this task.
+	 *
+	 * @return The task status.
+	 * @since 2016/06/24
+	 */
+	public TaskStatus getStatus()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the suite that this task belongs to.
+	 *
+	 * @return The owning suite.
+	 * @since 2016/06/24
+	 */
+	public Suite getSuite()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the hash code of this task, the hashcode is derived from the
+	 * suite and the name.
+	 *
+	 * @return The hash code.
+	 * @since 2016/06/24
+	 */
+	@Override
+	public int hashCode()
+	{
+		return Objects.hashCode(getSuite()) ^ Objects.hashCode(getName());
+	}
+	
+	/**
+	 * Returns {@code true} if this is a system task.
+	 *
+	 * @return {@code true} if a system task.
+	 * @since 2016/06/24
+	 */
+	public boolean isSystemTask()
+	{
+		throw new Error("TODO");
+	}
+}
+
