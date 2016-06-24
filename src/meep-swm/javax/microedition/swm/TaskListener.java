a299f05a471faeb5905705d38d24d6f1dd6a4707
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-24 17:27:08 +0000

    Implement TaskListener.

---
+++ src/meep-swm/javax/microedition/swm/TaskListener.java
@@ -0,0 +1,29 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.swm;
+
+/**
+ * This is called when a task has changed status.
+ *
+ * @since 2016/06/24
+ */
+public interface TaskListener
+{
+	/**
+	 * This is called when a task has changed status.
+	 *
+	 * @param __t The task which has had its status changed.
+	 * @param __status The new status of the task.
+	 * @since 2016/06/24
+	 */
+	public abstract void notifyStatusUpdate(Task __t, TaskStatus __status);
+}
+
