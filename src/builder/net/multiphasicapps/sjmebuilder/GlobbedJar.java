1bef58d228367b9d01851292bdca18b4bd8854a0
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-25 15:42:57 +0000

    Base for GlobbedJar which will get the natively compiled output along with resources.

---
+++ src/builder/net/multiphasicapps/sjmebuilder/GlobbedJar.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.sjmebuilder;
+
+/**
+ * This represents an entire JAR which is full of class blobs and resource
+ * blobs.
+ *
+ * The output file format of the glob is meant to be compact and simple.
+ *
+ * @since 2016/06/25
+ */
+public class GlobbedJar
+{
+}
+
