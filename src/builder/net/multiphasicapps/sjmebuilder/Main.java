d37f9756beddabff1a0e7652fb5ba716632f2aea
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-06-25 02:40:22 +0000

    Base for builder argument handling.

--- src/builder/net/multiphasicapps/sjmebuilder/Main.java
+++ src/builder/net/multiphasicapps/sjmebuilder/Main.java
@@ -11,8 +11,13 @@
 package net.multiphasicapps.sjmebuilder;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 import net.multiphasicapps.sjmepackages.PackageList;
 
 /**
@@ -25,14 +30,76 @@ public class Main
 	/**
 	 * Main entry point.
 	 *
+	 * @param __args Main program arguments.
 	 * @since 2016/06/24
 	 */
 	public static void main(String... __args)
 	{
+		// Must exist
+		if (__args == null)
+			__args = new String[0];
+		
+		// Fill in queue for argument handling
+		Deque<String> args = new LinkedList<>();
+		for (String s : __args)
+			args.offerLast(s);
+		
+		// Handle arguments
+		boolean dosim = false;
+		String target = null;
+		List<String> simargs = new ArrayList<>();
+		while (!args.isEmpty())
+		{
+			String a = args.removeFirst();
+			
+			// Simulate also?
+			if (a.equals("-S") || a.equals("-s"))
+				dosim = true;
+			
+			// {@squirreljme.error DW02 Unknown command line argument.
+			// (The argument)}
+			else if (a.startsWith("-"))
+				throw new IllegalArgumentException(String.format("DW02 %s",
+					a));
+			
+			// Get the target
+			else
+			{
+				// Set
+				target = a;
+				
+				// {@squirreljme.error DW03 No arguments must follow the
+				// target when not simulating.}
+				if (!dosim && !args.isEmpty())
+					throw new IllegalArgumentException("DW03");
+				
+				// Add remaining arguments to the simulator
+				else
+					while (!args.isEmpty())
+						simargs.add(args.removeFirst());
+				
+				// Stop
+				break;
+			}
+		}
+		
+		// {@squirreljme.error DW04 Usage: [-S] (target) [simulator arguments];
+		// The target is the arch.os.variant to build a native executable for.
+		// The -S switch may
+		// be specified to also simulate the output resulting binary in which
+		// case the arguments to the simulator may be passed following the
+		// target.}
+		if (target == null)
+			throw new IllegalArgumentException("DW04");
+		
+		System.err.printf("DEBUG -- %s %s %s%n", dosim, target, simargs);
+		
 		// Load the package list
+		PrintStream out = System.out;
 		PackageList plist;
 		try
 		{
+			out.println("Loading the package lists...");
 			plist = new PackageList(Paths.get(System.getProperty("user.dir")),
 				null);
 		}
