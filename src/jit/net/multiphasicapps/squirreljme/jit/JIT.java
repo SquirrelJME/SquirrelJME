02a20ec8613b6d51dfb56330cef20807b8368ce2
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-02 22:31:52 +0000

    Base for JIT and 32-bit PowerPC JIT.

---
+++ src/jit/net/multiphasicapps/squirreljme/jit/JIT.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This contains the just in time compiler.
+ *
+ * @since 2016/07/02
+ */
+public abstract class JIT
+{
+}
+
