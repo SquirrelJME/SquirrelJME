b23ffd10200e55bb8713c7179842187f5f1f873b
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-04 15:39:57 +0000

    Will cleanup the simulator.

--- src/simulator/net/multiphasicapps/squirrelsim/Main.java
+++ src/simulator/net/multiphasicapps/squirrelsim/Main.java
@@ -27,6 +27,7 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.ServiceLoader;
 
 /**
  * This is the main entry point for the SquirrelJME simulator.
