f8a04c54b326222820c334ea350d2e2872ba6728
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-08 21:55:50 +0000

    Base for a traditional namespace writer also.

---
+++ src/os-linux-powerpc/net/multiphasicapps/squirreljme/jit/linux/powerpc/LinuxPPCNamespaceWriter.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.linux.powerpc;
+
+import net.multiphasicapps.squirreljme.jit.linux.LinuxNamespaceWriter;
+
+/**
+ * This provides writing namespaces for PowerPC based Linux systems.
+ *
+ * @since 2016/07/08
+ */
+public class LinuxPPCNamespaceWriter
+	extends LinuxNamespaceWriter
+{
+}
+
---
+++ src/os-linux/net/multiphasicapps/squirreljme/jit/linux/LinuxNamespaceWriter.java
@@ -0,0 +1,26 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.linux;
+
+import net.multiphasicapps.squirreljme.jit.traditional.
+	TraditionalNamespaceWriter;
+
+/**
+ * This is the base class for Linux based namespace writers, the code here
+ * must be architecture independent.
+ *
+ * @since 2016/07/08
+ */
+public abstract class LinuxNamespaceWriter
+	extends TraditionalNamespaceWriter
+{
+}
+
