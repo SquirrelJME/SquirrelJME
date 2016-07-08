9ef0fb816567c2421aae33963be485711442e2cf
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-08 17:52:28 +0000

    Add base for JITs that run on traditional CPUs.

---
+++ src/jit-traditional/net/multiphasicapps/squirreljme/jit/traditional/package-info.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This contains the base classes for traditional CPUs which have been used on
+ * Earth since the general appearance of mainstream computers. Traditional
+ * computers execute machine code and have a binary memory space which is split
+ * into bytes. Generally all CPUs of this kind use registers for a small set of
+ * quickly accessible variables.
+ *
+ * @since 2016/07/08
+ */
+
+package net.multiphasicapps.squirreljme.jit.traditional;
+
