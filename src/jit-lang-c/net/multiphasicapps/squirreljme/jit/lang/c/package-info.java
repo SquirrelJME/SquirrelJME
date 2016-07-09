87549651e8c8a57d4ad1924a124b4abd5ea9a78e
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-09 17:30:01 +0000

    Base for language outputs.

---
+++ src/jit-lang-c/net/multiphasicapps/squirreljme/jit/lang/c/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This is a JIT which outputs C source code which can be manually compiled and
+ * used.
+ *
+ * @since 2016/07/09
+ */
+
+package net.multiphasicapps.squirreljme.jit.lang.c;
+
---
+++ src/jit-lang/net/multiphasicapps/squirreljme/jit/lang/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package provides the base for JITs which target programming languages
+ * and not actual machines.
+ *
+ * @since 2016/07/09
+ */
+
+package net.multiphasicapps.squirreljme.jit.lang;
+
