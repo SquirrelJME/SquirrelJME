272f75507ab18e7fd6928a207558207bc94b24a6
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-09 23:19:06 +0000

    Notes; More base class.

--- src/zip-writer-stream/net/multiphasicapps/zip/streamwriter/ZipStreamWriter.java
+++ src/zip-writer-stream/net/multiphasicapps/zip/streamwriter/ZipStreamWriter.java
@@ -10,12 +10,48 @@
 
 package net.multiphasicapps.zip.streamwriter;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
- * DESCRIBE THIS.
+ * This class is used to write to ZIP files in an unknown and stream based
+ * manner where the size of the contents is completely unknown.
+ *
+ * When the stream is closed, the central directory of the ZIP file will be
+ * written to the end of the file.
  *
  * @since 2016/07/09
  */
 public class ZipStreamWriter
+	implements Closeable
 {
+	/**
+	 * This initializes the stream for writing ZIP file data.
+	 *
+	 * @param __os The output stream to write to.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/07/09
+	 */
+	public ZipStreamWriter(OutputStream __os)
+		throws NullPointerException
+	{
+		// Check
+		if (__os == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/07/09
+	 */
+	@Override
+	public void close()
+		throws IOException
+	{
+		throw new Error("TODO");
+	}
 }
 
