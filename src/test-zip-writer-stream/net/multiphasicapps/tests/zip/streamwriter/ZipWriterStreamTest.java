9734c4deb66034d882763ba65352a637b1d02db9
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-10 17:25:12 +0000

    Base for ZIP writer tests.

---
+++ src/test-zip-writer-stream/net/multiphasicapps/tests/zip/streamwriter/ZipWriterStreamTest.java
@@ -0,0 +1,69 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU Affero General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tests.zip.streamwriter;
+
+import java.util.Arrays;
+import java.util.Random;
+import net.multiphasicapps.tests.InvalidTestException;
+import net.multiphasicapps.tests.TestChecker;
+import net.multiphasicapps.tests.TestInvoker;
+import net.multiphasicapps.zip.streamwriter.ZipStreamWriter;
+
+/**
+ * This tests that ZIP files are correctly streamed and that any output ZIP
+ * file can be read by the already existing ZIP code.
+ *
+ * @since 2016/07/10
+ */
+public class ZipWriterStreamTest
+	implements TestInvoker
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/07/10
+	 */
+	@Override
+	public String invokerName()
+	{
+		return "net.multiphasicapps.zip.streamwriter.ZipStreamWriter";
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/07/10
+	 */
+	@Override
+	public Iterable<String> invokerTests()
+	{
+		// Generate some random seeds
+		Random rand = new Random(0x1989_07_06);
+		
+		return Arrays.<String>asList(Long.toString(rand.nextInt()),
+			Long.toString(rand.nextInt()),
+			Long.toString(rand.nextInt()));
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/07/10
+	 */
+	@Override
+	public void runTest(TestChecker __tc, String __st)
+		throws NullPointerException, Throwable
+	{
+		// Check
+		if (__tc == null || __st == null)
+			throw new NullPointerException();
+		
+		throw new Error("TODO");
+	}
+}
+
