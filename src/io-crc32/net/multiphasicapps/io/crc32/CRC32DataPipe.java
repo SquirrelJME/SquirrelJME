f6c8bb1cffd39505e40ace6d245d036137457268
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-16 15:27:44 +0000

    Base for CRC32 data pipe.

---
+++ src/io-crc32/net/multiphasicapps/io/crc32/CRC32DataPipe.java
@@ -0,0 +1,35 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.io.crc32;
+
+import net.multiphasicapps.io.datapipe.DataPipe;
+import net.multiphasicapps.io.datapipe.PipeProcessException;
+import net.multiphasicapps.io.datapipe.PipeStalledException;
+
+/**
+ * This is a data pipe which supports the CRC 32 algorithm.
+ *
+ * @since 2016/07/16
+ */
+public class CRC32DataPipe
+	extends DataPipe
+{
+	/**
+	 * Initializes the CRC-32 data pipe.
+	 *
+	 * @since 2016/07/16
+	 */
+	public CRC32DataPipe()
+	{
+		throw new Error("TODO");
+	}
+}
+
