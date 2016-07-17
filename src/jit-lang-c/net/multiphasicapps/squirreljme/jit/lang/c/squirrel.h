c294cf479943139aaeadf70e8dd1fd8fd37504d6
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-17 21:21:30 +0000

    Base squirrel.h which is included by all C output.

---
+++ src/jit-lang-c/net/multiphasicapps/squirreljme/jit/lang/c/squirrel.h
@@ -0,0 +1,47 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// --------------------------------------------------------------------------*/
+
+/**
+ * This is the core header which is used by the SquirrelJME C output JIT
+ * compiler.
+ *
+ * @since 2016/07/17
+ */
+
+/** Header guard. */
+#ifndef SJME_hGNETMULTIPHASIGCSQUIRRELH
+#define SJME_hGNETMULTIPHASIGCSQUIRRELH
+
+/** Anti-C++. */
+#ifdef _cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_cXNETMULTIPHASIGCSQUIRRELH
+extern "C"
+{
+#endif /** #ifdef SJME_CXX_IS_EXTERNED */
+#endif /** #ifdef __cplusplus */
+
+/****************************************************************************/
+
+/****************************************************************************/
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_cXNETMULTIPHASIGCSQUIRRELH
+}
+#undef SJME_cXNETMULTIPHASIGCSQUIRRELH
+#undef SJME_CXX_IS_EXTERNED
+#endif /** #ifdef SJME_cXNETMULTIPHASIGCSQUIRRELH */
+#endif /** #ifdef __cplusplus */
+
+/** Header guard. */
+#endif /* #ifndef SJME_hGNETMULTIPHASIGCSQUIRRELH */
+
