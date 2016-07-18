e45cc80b5212707286ba305e22c12b9894acf800
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-18 21:59:32 +0000

    Base class which would provide access to the basic assets.

---
+++ src/basic-assets/net/multiphasicapps/squirreljme/basicassets/BasicAsset.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.basicassets;
+
+import java.io.IOException;
+
+/**
+ * This class provides access to the basic assets to be included in all
+ * distributions of SquirrelJME.
+ *
+ * @since 2016/07/18
+ */
+public class BasicAsset
+{
+}
+
