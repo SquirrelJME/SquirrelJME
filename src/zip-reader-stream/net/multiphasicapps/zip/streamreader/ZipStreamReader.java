e43702a8222722af38c48888e25315d51dad5216
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-19 17:20:07 +0000

    Basic scanning of the local file header.

--- src/zip-reader-stream/net/multiphasicapps/zip/streamreader/ZipStreamReader.java
+++ src/zip-reader-stream/net/multiphasicapps/zip/streamreader/ZipStreamReader.java
@@ -26,11 +26,29 @@ import net.multiphasicapps.zip.ZipCompressionType;
 public class ZipStreamReader
 	implements Closeable
 {
+	/** The minumum size of the local header. */
+	private static final int _MINIMUM_HEADER_SIZE =
+		30;
+	
+	/** Lock. */
+	protected final Object lock =
+		new Object();
+	
 	/** The dynamic history stream. */
 	protected final DynamicHistoryInputStream input;
 	
 	/** This is used after an input structure is detected. */
 	protected final ExtendedDataInputStream data;
+	
+	/** This can hold the local header except for the comment and filename. */
+	private final byte[] _localheader =
+		new byte[_MINIMUM_HEADER_SIZE];
+	
+	/** The current entry being read, cannot next entry if this is the case. */
+	private volatile ZipStreamEntry _entry;
+	
+	/** End of file reached? */
+	private volatile boolean _eof;
 	
 	/**
 	 * Initializes the reader for input ZIP file data.
@@ -60,7 +78,16 @@ public class ZipStreamReader
 	public void close()
 		throws IOException
 	{
-		throw new Error("TODO");
+		// Lock
+		synchronized (this.lock)
+		{
+			// Mark EOF
+			this._eof = true;
+			
+			// Close the source
+			this.input.close();
+			this.data.close();
+		}
 	}
 	
 	/**
@@ -74,7 +101,62 @@ public class ZipStreamReader
 	public ZipStreamEntry nextEntry()
 		throws IOException
 	{
-		throw new Error("TODO");
+		// Lock
+		synchronized (this.lock)
+		{
+			// {@squirreljme.error BG01 An entry is currently being read, it
+			// must first be closed.}
+			if (this._entry != null)
+				throw new IOException("BG01");
+			
+			// End of file reached?
+			if (this._eof)
+				return null;
+			
+			// Read until an entry is found
+			DynamicHistoryInputStream input = this.input;
+			byte[] localheader = this._localheader;
+			for (;;)
+			{
+				// Peek the magic number
+				int rhcount = input.peek(0, localheader, 0, 4);
+			
+				// Could not fit the magic number, treat as EOF
+				if (rhcount < 4)
+				{
+					this._eof = true;
+					return null;
+				}
+			
+				// Does not match the magic number for local headers
+				byte lha = localheader[0], lhb = localheader[1],
+					lhc = localheader[2], lhd = localheader[3];
+				if (lha != 0x50 && lhb != 0x4B &&
+					lhc != 0x03 && lhd != 0x04)
+				{
+					// The header could just be a few bytes away, so instead
+					// of looping on single bytes skip a few if it is known
+					// there will never be a local header there.
+					input.read(localheader, 0, (lhb == 0x50 ? 1 :
+						(lhc == 0x50 ? 2 : lhd == 0x50 ? 3 : 4)));
+					continue;
+				}
+				
+				// Read the rest of the header
+				rhcount = input.peek(0, localheader);
+				
+				// EOF reached (cannot fit a local header in this many bytes)
+				// Ignore the somewhat malformed ZIP since it could be part of
+				// another file structure due to polyglots
+				if (rhcount < _MINIMUM_HEADER_SIZE)
+				{
+					this._eof = true;
+					return null;
+				}
+				
+				throw new Error("TODO");
+			}
+		}
 	}
 }
 
