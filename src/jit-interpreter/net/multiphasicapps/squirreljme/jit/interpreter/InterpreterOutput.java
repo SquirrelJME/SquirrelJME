42761122eb167c3b5eace39a6402cbf8c86e1ae8
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-22 23:27:37 +0000

    Base class for output.

---
+++ src/jit-interpreter/net/multiphasicapps/squirreljme/jit/interpreter/InterpreterOutput.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.interpreter;
+
+import net.multiphasicapps.squirreljme.jit.JITException;
+import net.multiphasicapps.squirreljme.jit.JITOutput;
+
+/**
+ * This is the output for the interpreter.
+ *
+ * @since 2016/07/22
+ */
+public class InterpreterOutput
+	implements JITOutput
+{
+}
+
