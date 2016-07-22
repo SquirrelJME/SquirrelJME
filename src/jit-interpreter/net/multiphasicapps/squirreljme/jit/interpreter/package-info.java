91246952b44de00791d650d8105abe4ac4982e57
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-22 23:22:44 +0000

    Base for jit-interpreter which will contain the JIT output used by the interpreter.

---
+++ src/jit-interpreter/net/multiphasicapps/squirreljme/jit/interpreter/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the JIT for the interpreter which just outputs the
+ * input JIT stream logic in a linear form later parsed by the interpreter.
+ *
+ * @since 2016/07/22
+ */
+
+package net.multiphasicapps.squirreljme.jit.interpreter;
+
