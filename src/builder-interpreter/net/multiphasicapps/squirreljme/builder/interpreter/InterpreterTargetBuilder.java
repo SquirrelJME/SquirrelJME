10f42081793e6640b425749b8202095ce8c2bfe7
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-22 20:49:00 +0000

    Base for TargetBuilder and InterpreterTargetBuilder.

---
+++ src/builder-interpreter/net/multiphasicapps/squirreljme/builder/interpreter/InterpreterTargetBuilder.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.builder.interpreter;
+
+import net.multiphasicapps.squirreljme.builder.TargetBuilder;
+
+/**
+ * This is the builder which can target the interpreter.
+ *
+ * @since 2016/07/22
+ */
+public class InterpreterTargetBuilder
+	extends TargetBuilder
+{
+}
+
---
+++ src/builder/net/multiphasicapps/squirreljme/builder/TargetBuilder.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.builder;
+
+import java.util.ServiceLoader;
+
+/**
+ * This is the base class for builders which can generate binaries for a given
+ * target.
+ *
+ * @since 2016/07/22
+ */
+public abstract class TargetBuilder
+{
+}
+
