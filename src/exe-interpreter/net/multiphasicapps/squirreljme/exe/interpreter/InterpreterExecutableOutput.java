778af214cb2853dcca532438289f7d9e07345d7d
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-24 15:25:06 +0000

    Base for interpreter executable output.

---
+++ src/exe-interpreter/net/multiphasicapps/squirreljme/exe/interpreter/InterpreterExecutableOutput.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.exe.interpreter;
+
+import net.multiphasicapps.squirreljme.exe.ExecutableOutput;
+
+/**
+ * This is able to link the blobs built for the interpreter as a single binary
+ * which can then be run on the interpreter.
+ *
+ * @since 2016/07/24
+ */
+public class InterpreterExecutableOutput
+	implements ExecutableOutput
+{
+}
+
