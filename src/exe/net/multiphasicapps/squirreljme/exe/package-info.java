5706ebda09363e34f355c9496dffa58f5162204c
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-24 01:25:47 +0000

    Base for ExecutableOutput interface.

---
+++ src/exe/net/multiphasicapps/squirreljme/exe/package-info.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the base classes for outputting linked executables.
+ *
+ * Due to the wide variety of executable formats for various systems, these
+ * interfaces will be very basic.
+ *
+ * @since 2016/07/23
+ */
+
+package net.multiphasicapps.squirreljme.exe;
+
