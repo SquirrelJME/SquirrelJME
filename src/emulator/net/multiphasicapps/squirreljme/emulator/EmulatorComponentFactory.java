5a3b5eff99b458d35f9e7a5e88fd50d532fdceee
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-26 20:58:40 +0000

    Base for factory interface.

---
+++ src/emulator/net/multiphasicapps/squirreljme/emulator/EmulatorComponentFactory.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.emulator;
+
+/**
+ * This is a factory which is exposed by the {@link java.util.ServiceLoader}
+ * and is used to initialize new components to be added to systems when
+ * requested.
+ *
+ * @since 2016/07/26
+ */
+public interface EmulatorComponentFactory
+{
+}
+
