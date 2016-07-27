30e0783b1dbd7b77d978a93d930ea163a0b56847
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-27 02:24:28 +0000

    Base for output also.

--- src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/GenericOutput.java
+++ src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/GenericOutput.java
@@ -13,42 +13,27 @@ package net.multiphasicapps.squirreljme.jit.generic;
 import net.multiphasicapps.squirreljme.jit.base.JITException;
 import net.multiphasicapps.squirreljme.jit.base.JITTriplet;
 import net.multiphasicapps.squirreljme.jit.JITOutput;
-import net.multiphasicapps.squirreljme.jit.JITOutputConfig;
 import net.multiphasicapps.squirreljme.jit.JITOutputFactory;
 
 /**
- * This is the base class for all factories which JIT compile byte code into
- * standard blobs shared by many architectures.
+ * This is the base class for generic blob output which is shared across
+ * modern CPU architectures.
  *
  * @since 2016/07/26
  */
-public abstract class GenericOutputFactory
-	extends JITOutputFactory
+public abstract class GenericOutput
+	implements JITOutput
 {
 	/**
 	 * {@inheritDoc}
 	 * @since 2016/07/26
 	 */
 	@Override
-	public final JITOutput create(JITOutputConfig.Immutable __config)
+	public JITNamespaceWriter beginNamespace(String __ns)
 		throws JITException, NullPointerException
 	{
 		// Check
-		if (__config == null)
-			throw new NullPointerException("NARG");
-		
-		throw new Error("TODO");
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 * @since 2016/07/26
-	 */
-	@Override
-	public final boolean supportsConfig(JITOutputConfig.Immutable __config)
-	{
-		// Check
-		if (__config == null)
+		if (__ns == null)
 			throw new NullPointerException("NARG");
 		
 		throw new Error("TODO");
