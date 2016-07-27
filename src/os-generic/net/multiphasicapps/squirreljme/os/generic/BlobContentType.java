ce8cb7413d8487e8297f15f5fce78fe3819d6c04
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-27 19:02:22 +0000

    Add BlobContentType class.

---
+++ src/os-generic/net/multiphasicapps/squirreljme/os/generic/BlobContentType.java
@@ -0,0 +1,29 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.os.generic;
+
+/**
+ * This represents the type that a content is within the blob.
+ *
+ * @since 2016/07/27
+ */
+public enum BlobContentType
+{
+	/** A class. */
+	CLASS,
+	
+	/** A resource. */
+	RESOURCE,
+	
+	/** End. */
+	;
+}
+
