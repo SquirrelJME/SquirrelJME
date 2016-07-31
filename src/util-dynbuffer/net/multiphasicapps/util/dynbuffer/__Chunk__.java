1a2946a8e29b49e5667588e0e4efbbaceaae1a78
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-07-31 14:51:12 +0000

    Notes.

---
+++ src/util-dynbuffer/net/multiphasicapps/util/dynbuffer/__Chunk__.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.util.dynbuffer;
+
+/**
+ * This represents a chunk of data within a buffer.
+ *
+ * @since 2016/07/31
+ */
+final class __Chunk__
+{
+}
+
