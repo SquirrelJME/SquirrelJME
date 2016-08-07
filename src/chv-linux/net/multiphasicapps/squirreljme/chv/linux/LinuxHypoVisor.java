ec464d9055a02991f873d9086caae02add891fc7
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-07 01:47:57 +0000

    Base for spawn method.

--- src/chv-linux/net/multiphasicapps/squirreljme/chv/linux/LinuxHypoVisor.java
+++ src/chv-linux/net/multiphasicapps/squirreljme/chv/linux/LinuxHypoVisor.java
@@ -25,10 +25,13 @@ public abstract class LinuxHypoVisor
 	implements HypoVisor
 {
 	/** The initial ELF binary. */
-	protected volatile byte[] _initelf;
+	private volatile byte[] _initelf;
 	
 	/** The arguments to the starting program. */
-	protected volatile String[] _initargs;
+	private volatile String[] _initargs;
+	
+	/** The owning emulator. */
+	private volatile Emulator _emulator;
 	
 	/**
 	 * {@inheritDoc}
@@ -42,16 +45,17 @@ public abstract class LinuxHypoVisor
 		if (__e == null)
 			throw new NullPointerException("NARG");
 		
-		// Create initial CPU state
-		CPUState cs = __e.createCPUState();
+		// Set emulator
+		this._emulator = __e;
 		
-		// {@squirreljme.error CB01 Linux requires a CPU which has a Von
-		// Neumann architecture (data and instructions are in the same
-		// address space).}
-		if (!(cs instanceof VonNeumannAddressing))
-			throw new IllegalStateException("CB01");
+		// {@squirreljme.error CB02 Initial program has not been set.}
+		byte[] initelf = this._initelf;
+		String[] initargs = this._initargs;
+		if (initelf == null || initargs == null)
+			throw new IllegalStateException("CB02");
 		
-		throw new Error("TODO");
+		// Spawn program (not a fork)
+		spawn(initelf, initargs, new String[0]);
 	}
 	
 	/**
@@ -73,6 +77,42 @@ public abstract class LinuxHypoVisor
 		// Copy
 		this._initelf = __elf.clone();
 		this._initargs = __args.clone();
+	}
+	
+	/**
+	 * Spawns the specified process using the given binary and arguments.
+	 *
+	 * @param __elf The executable to load and execute.
+	 * @param __args The arguments to the program.
+	 * @param __env The starting environment.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/08/06
+	 */
+	public final LinuxProcess spawn(byte[] __elf, String[] __args,
+		String[] __env)
+		throws NullPointerException
+	{
+		// Check
+		if (__elf == null || __args == null || __env == null)
+			throw new NullPointerException("NARG");
+		
+		// {@squirreljme.error CB03 Cannot spawn a process because an emulator
+		// was not set.}
+		Emulator emulator = this._emulator;
+		if (emulator == null)
+			throw new IllegalStateException("CB03");
+		
+		// Create initial CPU state
+		CPUState cs = emulator.createCPUState();
+		
+		// {@squirreljme.error CB01 Linux requires a CPU which has a Von
+		// Neumann architecture (data and instructions are in the same
+		// address space).}
+		if (!(cs instanceof VonNeumannAddressing))
+			throw new IllegalStateException("CB01");
+		VonNeumannAddressing vna = (VonNeumannAddressing)cs;
+		
+		throw new Error("TODO");
 	}
 }
 
