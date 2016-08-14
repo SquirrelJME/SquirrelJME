585879662f961414ee574b84befaf42dd9f13744
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-14 19:30:00 +0000

    Add base for the constant pool writer.

---
+++ src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/__PoolWriter__.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.generic;
+
+/**
+ * This writes the constant pool.
+ *
+ * @since 2016/08/14
+ */
+class __PoolWriter__
+{
+	/**
+	 * Initializes the string writer.
+	 *
+	 * @param __pool The constant pool to use.
+	 * @since 2016/08/14
+	 */
+	__PoolWriter__()
+	{
+	}
+}
+
