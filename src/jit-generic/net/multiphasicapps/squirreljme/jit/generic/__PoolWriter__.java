91a579474e683e8541c9ee6e2693c6b2823a4b63
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-14 20:01:09 +0000

    Base for string table determination.

--- src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/__PoolWriter__.java
+++ src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/__PoolWriter__.java
@@ -11,6 +11,8 @@
 package net.multiphasicapps.squirreljme.jit.generic;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import net.multiphasicapps.io.data.ExtendedDataOutputStream;
 import net.multiphasicapps.squirreljme.jit.JITConstantPool;
 
@@ -23,6 +25,10 @@ class __PoolWriter__
 {
 	/** The pool to write. */
 	protected final JITConstantPool pool;
+	
+	/** The mapping of strings. */
+	protected final Map<String, __String__> strings =
+		new HashMap<>();
 	
 	/**
 	 * Initializes the pool writer.
@@ -39,6 +45,29 @@ class __PoolWriter__
 		
 		// Set
 		this.pool = __pool;
+		
+		// Go through all pool entries and extract strings from them to use
+		int n = __pool.size();
+		for (int i = 0; i < n; i++)
+		{
+			// Get tag here
+			int tag = __pool.tag(i);
+			
+			// Depends on the tag
+			switch (tag)
+			{
+					// No string data
+				case JITConstantPool.TAG_INTEGER:
+				case JITConstantPool.TAG_FLOAT:
+				case JITConstantPool.TAG_LONG:
+				case JITConstantPool.TAG_DOUBLE:
+					continue;
+				
+					// Unknown
+				default:
+					throw new RuntimeException("OOPS");
+			}
+		}
 	}
 	
 	/**
@@ -57,6 +86,43 @@ class __PoolWriter__
 			throw new NullPointerException("NARG");
 		
 		throw new Error("TODO");
+	}
+	
+	/**
+	 * String storage holder.
+	 *
+	 * @since 2016/08/14
+	 */
+	private class __String__
+	{
+		/** The string to use. */
+		final String _string;
+		
+		/** The index of the string in the table. */
+		final int _index;
+		
+		/** The position from the start where the string data is. */
+		volatile int _position;
+		
+		/**
+		 * Initializes the string 
+		 *
+		 * @param __s The string which was used.
+		 * @param __dx The index of the string.
+		 * @throws NullPointerException On null arguments.
+		 * @since 2016/08/14
+		 */
+		private __String__(String __s, int __dx)
+			throws NullPointerException
+		{
+			// Check
+			if (__s == null)
+				throw new NullPointerException("NARG");
+			
+			// Set
+			this._string = __s;
+			this._index = __dx;
+		}
 	}
 }
 
