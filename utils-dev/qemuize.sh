8a43aa489a232943041294b6d06d8a7673de10fa
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-15 14:05:04 +0000

    Add a script to extract and run the binary.

---
+++ utils-dev/qemuize.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+#     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3, or later.
+# For more information see license.mkd.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Likely a Linux only script, run the build to a given ZIP then
+# extract it and try running it.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Perform the build and run it
+rm -f "$$.zip"
+if "$__exedir/../build.sh" $* "$$.zip"
+then
+	# Unzip then delete the ZIP
+	unzip -o "$$.zip" squirreljme
+	rm -f "$$.zip"
+	
+	# Run it
+	chmod +x squirreljme
+	./squirreljme
+	exit $?
+else
+	# Failed, so fail
+	rm -f "$$.zip"
+	echo "Failed build..."
+	exit 1
+fi
+
