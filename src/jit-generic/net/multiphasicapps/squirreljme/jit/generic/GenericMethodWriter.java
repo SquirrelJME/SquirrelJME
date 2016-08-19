b62c653dd53ef50051ac79dd20ab10b449e0cca7
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-19 18:09:13 +0000

    Add base for a generic method writer.

---
+++ src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/GenericMethodWriter.java
@@ -0,0 +1,26 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.generic;
+
+import net.multiphasicapps.squirreljme.jit.JITMethodWriter;
+
+/**
+ * This is the base class for the native machine code generator which is common
+ * to many architectures. Support for architectures extends this class which
+ * is used to create the actual machine code.
+ *
+ * @since 2016/08/19
+ */
+public abstract class GenericMethodWriter
+	implements JITMethodWriter
+{
+}
+
