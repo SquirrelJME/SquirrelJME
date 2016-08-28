ac021a0cb080740fc0b8bfbec10ce8f4409223eb
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-28 22:13:20 +0000

    Add base class for stack types.

---
+++ src/jit/net/multiphasicapps/squirreljme/jit/__SMTType__.java
@@ -0,0 +1,43 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This represents the type of value which is stored in a variable either on
+ * the stack or in a local.
+ *
+ * @since 2016/05/12
+ */
+enum __SMTType__
+{
+	/** 32-bit Integer. */
+	INTEGER,
+	
+	/** 64-bit Integer. */
+	LONG,
+	
+	/** 32-bit Float. */
+	FLOAT,
+	
+	/** 64-bit Double. */
+	DOUBLE,
+	
+	/** Object. */
+	OBJECT,
+	
+	/** The top of a long or double. */
+	TOP,
+	
+	/** End. */
+	;
+}
+
+
