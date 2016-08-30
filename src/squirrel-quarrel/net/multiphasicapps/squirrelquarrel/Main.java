bc020cc69d922f08c775ec1bddd435999352a525
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-30 17:03:54 +0000

    Add a main to Squirrel Quarrel.

---
+++ src/squirrel-quarrel/net/multiphasicapps/squirrelquarrel/Main.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel;
+
+/**
+ * Main entry point for standard Java code.
+ *
+ * @since 2016/08/30
+ */
+public class Main
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2016/08/30
+	 */
+	public static void main(String... __args)
+	{
+		throw new Error("TODO");
+	}
+}
+
