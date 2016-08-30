58b9e944d351fa26208ef04f4f11650147d89160
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-30 16:28:12 +0000

    Squirrel Quarrel basework.

---
+++ src/squirrel-quarrel/net/multiphasicapps/squirrelquarrel/unit/UnitType.java
@@ -0,0 +1,26 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel.unit;
+
+/**
+ * This is the type of data that a unit is.
+ *
+ * @since 2016/08/30
+ */
+public enum UnitType
+{
+	/** Soldier. */
+	SQ_GROUND_SOLDIER,
+	
+	/** End. */
+	;
+}
+
