17205ca90c767ad1f2b044122e6c6004be3da7b8
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-08-31 13:23:14 +0000

    Base for allocator state.

---
+++ src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/GenericAllocatorState.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.generic;
+
+/**
+ * This contains a state for the register allocator which is used in the
+ * branch processor so that the state that has been previously set can be
+ * matched when jumping to that location.
+ *
+ * @since 2016/08/31
+ */
+public class GenericAllocatorState
+{
+}
+
