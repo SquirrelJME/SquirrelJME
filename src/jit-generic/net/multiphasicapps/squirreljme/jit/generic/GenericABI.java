8b0a6d1192242522b08715368f48227c8fda02c3
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-01 23:52:25 +0000

    Add methods for register ABI usage.

--- src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/GenericABI.java
+++ src/jit-generic/net/multiphasicapps/squirreljme/jit/generic/GenericABI.java
@@ -10,7 +10,10 @@
 
 package net.multiphasicapps.squirreljme.jit.generic;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.multiphasicapps.squirreljme.jit.JITObjectProperties;
+import net.multiphasicapps.util.unmodifiable.UnmodifiableList;
 
 /**
  * This contains the information on a given ABI such as which registers are
@@ -39,12 +42,124 @@ public final class GenericABI
 	}
 	
 	/**
+	 * Returns the list of registers that are used for passing method
+	 * arguments.
+	 *
+	 * @param __k The kind of registers used.
+	 * @return The list of argument registers.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/01
+	 */
+	public final List<GenericRegister> arguments(GenericRegisterKind __k)
+		throws NullPointerException
+	{
+		// Check
+		if (__k == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Checks if the specified register is a callee saved register.
+	 *
+	 * @param __r The register to check if it is callee saved.
+	 * @return {@code true} then it is calle saved.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/01
+	 */
+	public boolean isSaved(GenericRegister __r)
+		throws NullPointerException
+	{
+		// Check
+		if (__r == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Checks if the specified register is a caller saved register.
+	 *
+	 * @param __r The register to check if it is caller saved.
+	 * @return {@code true} then it is caller saved.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/01
+	 */
+	public boolean isTemporary(GenericRegister __r)
+		throws NullPointerException
+	{
+		// Check
+		if (__r == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
 	 * {@inheritDoc}
 	 * @since 2016/09/01
 	 */
 	@Override
-	public String[] properties()
+	public final String[] properties()
 	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the list of registers which are used to store the return value
+	 * for when a method is called.
+	 *
+	 * @param __k The kinds of registers to get.
+	 * @return The list of return value registers.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/01
+	 */
+	public final List<GenericRegister> result(GenericRegisterKind __k)
+		throws NullPointerException
+	{
+		// Check
+		if (__k == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the list of saved registers which must be preserved across
+	 * method calls. These registers are callee saved.
+	 *
+	 * @param __k The kinds of registers to get.
+	 * @return The list of saved registers.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/01
+	 */
+	public final List<GenericRegister> saved(GenericRegisterKind __k)
+		throws NullPointerException
+	{
+		// Check
+		if (__k == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the list of registers which are considered temporary and are not
+	 * saved across method calls. These registers are caller saved.
+	 *
+	 * @param __k The kind of registers used.
+	 * @return The list of temporary registers.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/01
+	 */
+	public final List<GenericRegister> temporary(GenericRegisterKind __k)
+		throws NullPointerException
+	{
+		// Check
+		if (__k == null)
+			throw new NullPointerException("NARG");
+		
 		throw new Error("TODO");
 	}
 }
