a250cba5de4bea7bcd2ba4d81c0af5b6eaa97b06
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-04 02:34:32 +0000

    Add script which uses a very small stack size and max memory size for testing (so I do not have to type it all the time anymore).

---
+++ utils-dev/skrimp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+#     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3, or later.
+# For more information see license.mkd.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Sets the Java heap to use not much memory at all
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Wrap
+JAVA_OPTIONS="-Xms2M -Xmx2M -Xss64K" "$__exedir/../build.sh" $*
+exit $?
+
