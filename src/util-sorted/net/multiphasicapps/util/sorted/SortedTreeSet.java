96790e8c0aefa09213b110350b04c960c0adbcfd
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-06 22:55:04 +0000

    Base for add of value.

--- src/util-sorted/net/multiphasicapps/util/sorted/SortedTreeSet.java
+++ src/util-sorted/net/multiphasicapps/util/sorted/SortedTreeSet.java
@@ -30,7 +30,7 @@ public class SortedTreeSet<V>
 	extends AbstractSet<V>
 {
 	/** The comparison method to use. */
-	private final Comparator<? extends V> _compare;
+	private final Comparator<V> _compare;
 	
 	/** The root node. */
 	volatile __Node__<V> _root;
@@ -67,6 +67,7 @@ public class SortedTreeSet<V>
 	 * @throws NullPointerException On null arguments.
 	 * @since 2016/09/06
 	 */
+	@SuppressWarnings({"unchecked"})
 	public SortedTreeSet(Comparator<? extends V> __comp)
 		throws NullPointerException
 	{
@@ -75,7 +76,7 @@ public class SortedTreeSet<V>
 			throw new NullPointerException("NARG");
 		
 		// Set
-		this._compare = __comp;
+		this._compare = (Comparator<V>)__comp;
 	}
 	
 	/**
@@ -87,6 +88,7 @@ public class SortedTreeSet<V>
 	 * @throws NullPointerException On null arguments.
 	 * @since 2016/09/06
 	 */
+	@SuppressWarnings({"unchecked"})
 	public SortedTreeSet(Comparator<? extends V> __comp,
 		Collection<? extends V> __s)
 		throws NullPointerException
@@ -96,10 +98,26 @@ public class SortedTreeSet<V>
 			throw new NullPointerException("NARG");
 		
 		// Set
-		this._compare = __comp;
+		this._compare = (Comparator<V>)__comp;
 		
 		// Just call add all from collection
 		addAll(__s);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/09/06
+	 */
+	@Override
+	public boolean add(V __v)
+	{
+		// Replace the root
+		__Node__<V> n = __insert(this._root, __v);
+		this._root = n;
+		n._color = __Color__.BLACK;
+		
+		// Did add?
+		throw new Error("TODO");
 	}
 	
 	/**
@@ -128,9 +146,71 @@ public class SortedTreeSet<V>
 	 * @since 2016/09/06
 	 */
 	@Override
+	public boolean remove(Object __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/09/06
+	 */
+	@Override
 	public int size()
 	{
 		throw new Error("TODO");
+	}
+	
+	/**
+	 * Inserts a node into the tree.
+	 *
+	 * @param __at The current node the algorithm is at.
+	 * @param __v The value to insert.
+	 * @return The newly created node.
+	 * @since 2016/09/06
+	 */
+	private final __Node__<V> __insert(__Node__<V> __at, V __v)
+	{
+		// The tree is empty, adding an element is trivial
+		if (__at == null)
+			return new __Node__<V>(__v);
+		
+		// Compare node and value
+		Comparator<V> compare = this._compare;
+		V existval = __at._value;
+		int res = compare.compare(__v, existval);
+		
+		// If replacing, do nothing because only a single value may exist
+		// at a time.
+		if (res == 0)
+			return __at;
+		
+		// Insert on left side
+		else if (res < 0)
+			__at._left = __insert(__at._left, __v);
+		
+		// Insert on right side
+		else if (res > 0)
+			__at._right = __insert(__at._right, __v);
+		
+		// If the right side is red then rotate left
+		if (__at.isSideColorRed(true))
+			__at = __rotate(__at, false);
+		
+		// If the left node is red and it has another left node which is also
+		// red then rotate right
+		__Node__<V> atleft = __at._left;
+		if (atleft != null && __at.isSideColorRed(false) &&
+			atleft.isSideColorRed(false))
+			__at = __rotate(__at, true);
+		
+		// If both sides are red then they must become black while the node
+		// above becomes red
+		if (__at.isSideColorRed(false) && __at.isSideColorRed(true))
+			__flipColor(__at);
+		
+		// Return the node, which may have been rotated
+		return __at;
 	}
 	
 	/**
@@ -146,6 +226,44 @@ public class SortedTreeSet<V>
 		__Node__<V> root = this._root;
 		if (root == null)
 			return null;
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Flips the color of the sub-nodes so that red becomes black and black
+	 * becomes red.
+	 *
+	 * @param __at The node to flip.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/06
+	 */
+	private final void __flipColor(__Node__<V> __at)
+		throws NullPointerException
+	{
+		// Check
+		if (__at == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Rotates a node a given direction.
+	 *
+	 * @param __at The node to rotate.
+	 * @param __r If {@code true} then the node is rotated right, otherwise
+	 * it is rotated to the left.
+	 * @return The resulting node to place in the tree.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/06
+	 */
+	private final __Node__<V> __rotate(__Node__<V> __at, boolean __r)
+		throws NullPointerException
+	{
+		// Check
+		if (__at == null)
+			throw new NullPointerException("NARG");
 		
 		throw new Error("TODO");
 	}
--- src/util-sorted/net/multiphasicapps/util/sorted/__Node__.java
+++ src/util-sorted/net/multiphasicapps/util/sorted/__Node__.java
@@ -28,5 +28,64 @@ class __Node__<V>
 	
 	/** The node to the right. */
 	volatile __Node__<V> _right;
+	
+	/**
+	 * Initializes a node with no value.
+	 *
+	 * @since 2016/09/06
+	 */
+	__Node__()
+	{
+	}
+	
+	/**
+	 * Initializes a node with the given value.
+	 *
+	 * @param __v The initial value.
+	 * @since 2016/09/06
+	 */
+	__Node__(V __v)
+	{
+		this._value = __v;
+	}
+	
+	/**
+	 * Checks if a node on a given side is of the specified color.
+	 *
+	 * @param __r If {@code true} then the right side is checked, otherwise
+	 * the left.
+	 * @param __c The color to check.
+	 * @return {@code true} if the side is the given color.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/06
+	 */
+	final boolean isSideColor(boolean __r, __Color__ __c)
+		throws NullPointerException
+	{
+		// Check
+		if (__c == null)
+			throw new NullPointerException("NARG");
+		
+		// Get node, if there is no node there then never a color
+		__Node__<V> n = (__r ? this._right : this._left);
+		if (n == null)
+			return false;
+		
+		// Check
+		return __c == n._color;
+	}
+	
+	/**
+	 * Checks if a node on a given side is of the red color.
+	 *
+	 * @param __r If {@code true} then the right side is checked, otherwise
+	 * the left.
+	 * @return {@code true} if the side is red.
+	 * @since 2016/09/06
+	 */
+	final boolean isSideColorRed(boolean __r)
+	{
+		return isSideColor(__r, __Color__.RED);
+	}
 }
 
