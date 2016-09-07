d0df3dd2d44e28f35c5aa71fde1e6775711ca972
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-07 22:50:48 +0000

    Add base for a StartMIDlet class.

---
+++ src/meep-midlet/net/multiphasicapps/squirreljme/meep/midlet/StartMIDlet.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.meep.midlet;
+
+/**
+ * This class contains the main entry point which is used to enter the MIDlet
+ * main entry point from classical Java applications which enter via
+ * {@code main}.
+ *
+ * @since 2016/09/07
+ */
+public class StartMIDlet
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2016/09/07
+	 */
+	public static void main(String[] __args)
+	{
+		throw new Error("TODO");
+	}
+}
+
