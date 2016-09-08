85eca6a875a2d5b92b3b814fbb461c4a7e0dd970
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-08 22:43:16 +0000

    Add base for a UUID generator; Also add a random number generator script (fallback to a horrible algorithm if /dev/*random is missing).

---
+++ utils-dev/random.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+#     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3, or later.
+# For more information see license.mkd.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Outputs some random bytes as hexadecimal
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Need count?
+if [ "$#" -le "0" ]
+then
+	echo "Usage: $0 (count)"
+	exit 1
+fi
+
+# If outputting no bytes, ignore
+__count="$1"
+if [ "$__count" -le "0" ]
+then
+	exit 0
+fi
+
+# Real random source, if it exists
+if [ -c /dev/urandom ] || [ -c /dev/random ]
+then
+	# Unblocked
+	(if [ -c /dev/urandom ]
+	then
+		dd if=/dev/urandom "bs=$__count" count=1
+		
+	# Normal
+	else
+		dd if=/dev/random "bs=$__count" count=1
+	fi) 2> /dev/null | hexdump -x |
+		sed 's/^[0-9a-fA-F]\{1,\}//g' | tr -d ' ' | tr -d '\n' | tr -d '\t'
+	
+	# End with newline
+	echo ""
+
+# Not really random, but something
+else
+	# Seed the random number generator and get a value
+	__seed="$( (echo -n $$ & echo -n $!; date +%s & echo -n $!) |
+		tr -d ' ' | tr -d '\n' | sed 's/[^0-9]*//g' | cut -c 1-8)"
+	
+	# Loop
+	__i=0
+	while [ "$__i" -lt "$__count" ]
+	do
+		# Calculate a value
+		__a="$(expr $__seed '*' 1337 | cut -c 1-8)"
+		
+		# Set new seed
+		echo -n "" &
+		__pid="$!"
+		__seed="$__a$__pid"
+		
+		# Add all of the seed values together
+		__rest="$__seed"
+		__xa="0"
+		__xb="1"
+		__j="0"
+		while [ -n "$__rest" ]
+		do
+			__char="$(echo "$__rest" | cut -c 1)"
+			
+			# Add to A or B?
+			if [ "$__j" -eq "0" ]
+			then
+				__xa="$(expr "$__xa" + "$__char")"
+				__j="1"
+			else
+				__xb="$(expr "$__xb" + "$__char")"
+				__j="0"
+			fi
+			
+			# Cut character
+			__rest="$(echo "$__rest" | cut -c 2-)"
+		done
+		
+		# Modulo 16 for hex
+		__ya="$(expr $__xa % 16)"
+		__yb="$(expr $__xb % 16)"
+		
+		# Echo individual hex characters
+		for __q in $__ya $__yb
+		do
+			case $__q in
+				10) echo -n "a";;
+				11) echo -n "b";;
+				12) echo -n "c";;
+				13) echo -n "d";;
+				14) echo -n "e";;
+				15) echo -n "f";;
+				*) echo -n "$__q";;
+			esac
+		done
+		
+		# Increment
+		__i="$(expr $__i + 1)"
+	done
+	
+	# End with newline
+	echo ""
+fi
+
---
+++ utils-dev/uuid.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+#     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3, or later.
+# For more information see license.mkd.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Creates a UUID.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# One UUID command?
+if false && which uuidgen 2>&1 > /dev/null
+then
+	uuidgen
+
+# Another command
+elif false && which uuid 2>&1 > /dev/null
+then
+	uuid
+
+# Otherwise make something up, use random data source
+else
+	__num="$("$__exedir/random.sh" 16)"
+	echo "$__num" | \
+		sed 's/\(.\{8\}\)\(....\)\(....\)\(....\)\(.\{12\}\)/\1-\2-\3-\4-\5/g'
+fi
