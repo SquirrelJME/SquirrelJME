0139b425219396532d7d985056ae1b0336f792c2
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-11 00:42:09 +0000

    Add base for native code output factories.

---
+++ src/native-code-mips/net/multiphasicapps/squirreljme/nativecode/mips/MIPSWriterFactory.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.nativecode.mips;
+
+import net.multiphasicapps.squirreljme.nativecode.NativeCodeWriterFactory;
+
+/**
+ * This is a factory which creates native code writers for MIPS based
+ * systems.
+ *
+ * @since 2016/09/10
+ */
+public class MIPSWriterFactory
+	implements NativeCodeWriterFactory
+{
+}
+
