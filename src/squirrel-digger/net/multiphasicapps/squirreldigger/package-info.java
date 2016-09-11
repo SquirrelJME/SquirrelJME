def3a3afee104eda417ef00a824e734618694171
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-11 13:00:02 +0000

    Add base Squirrel Digger.

---
+++ src/squirrel-digger/net/multiphasicapps/squirreldigger/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the base for Squirrel Digger, a digging game.
+ *
+ * @since 2016/09/11
+ */
+
+package net.multiphasicapps.squirreldigger;
+
