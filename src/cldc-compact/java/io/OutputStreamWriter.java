0115d8bc58cd4b978d7121090c37add32ed02cf6
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-14 01:54:39 +0000

    Add java.io.OutputStreamWriter.

---
+++ src/cldc-compact/java/io/OutputStreamWriter.java
@@ -0,0 +1,81 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package java.io;
+
+public class OutputStreamWriter
+	extends Writer
+{
+	public OutputStreamWriter(OutputStream __a, String __b)
+		throws UnsupportedEncodingException
+	{
+		super();
+		if (false)
+			throw new UnsupportedEncodingException();
+		throw new Error("TODO");
+	}
+	
+	public OutputStreamWriter(OutputStream __a)
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public void close()
+		throws IOException
+	{
+		if (false)
+			throw new IOException();
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public void flush()
+		throws IOException
+	{
+		if (false)
+			throw new IOException();
+		throw new Error("TODO");
+	}
+	
+	public String getEncoding()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public void write(int __a)
+		throws IOException
+	{
+		if (false)
+			throw new IOException();
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public void write(char[] __a, int __b, int __c)
+		throws IOException
+	{
+		if (false)
+			throw new IOException();
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public void write(String __a, int __b, int __c)
+		throws IOException
+	{
+		if (false)
+			throw new IOException();
+		throw new Error("TODO");
+	}
+}
+
