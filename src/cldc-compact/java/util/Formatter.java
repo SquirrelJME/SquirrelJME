50c36680e248f4821e8dc2fcd229d050e0fca215
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-14 01:38:40 +0000

    Add java.util.Formatter.

---
+++ src/cldc-compact/java/util/Formatter.java
@@ -0,0 +1,72 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+public final class Formatter
+	implements Closeable
+{
+	public Formatter()
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	public Formatter(Appendable __a)
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	public Formatter(PrintStream __a)
+	{
+		super();
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public void close()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void flush()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Formatter format(String __a, Object... __b)
+	{
+		throw new Error("TODO");
+	}
+	
+	public IOException ioException()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Appendable out()
+	{
+		throw new Error("TODO");
+	}
+	
+	@Override
+	public String toString()
+	{
+		throw new Error("TODO");
+	}
+}
+
