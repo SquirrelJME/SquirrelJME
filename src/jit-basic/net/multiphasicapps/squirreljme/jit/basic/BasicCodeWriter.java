86e277ef1e8d96cfef4cc8df6d01363f8dc60581
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-15 18:25:03 +0000

    Base for argument type conversion.

--- src/jit-basic/net/multiphasicapps/squirreljme/jit/basic/BasicCodeWriter.java
+++ src/jit-basic/net/multiphasicapps/squirreljme/jit/basic/BasicCodeWriter.java
@@ -10,16 +10,24 @@
 
 package net.multiphasicapps.squirreljme.jit.basic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import net.multiphasicapps.io.data.ExtendedDataOutputStream;
 import net.multiphasicapps.squirreljme.classformat.StackMapType;
 import net.multiphasicapps.squirreljme.jit.base.JITException;
+import net.multiphasicapps.squirreljme.jit.base.JITTriplet;
 import net.multiphasicapps.squirreljme.jit.JITCodeWriter;
+import net.multiphasicapps.squirreljme.jit.JITConfig;
+import net.multiphasicapps.squirreljme.nativecode.base.NativeFloatType;
 import net.multiphasicapps.squirreljme.nativecode.NativeAllocation;
 import net.multiphasicapps.squirreljme.nativecode.NativeAllocator;
 import net.multiphasicapps.squirreljme.nativecode.NativeCodeWriter;
 import net.multiphasicapps.squirreljme.nativecode.NativeCodeWriterFactory;
 import net.multiphasicapps.squirreljme.nativecode.NativeCodeWriterOptions;
+import net.multiphasicapps.squirreljme.nativecode.NativeRegisterFloatType;
+import net.multiphasicapps.squirreljme.nativecode.NativeRegisterIntegerType;
+import net.multiphasicapps.squirreljme.nativecode.NativeRegisterType;
 
 /**
  * This is used to parse stack cached Java byte code to produce native machine
@@ -48,6 +56,9 @@ public class BasicCodeWriter
 	/** Native register and stack allocator. */
 	protected final NativeAllocator allocator;
 	
+	/** The JIT configuration .*/
+	protected final JITConfig config;
+	
 	/** The current piece of code being written. */
 	final __Code__ _code;
 	
@@ -72,6 +83,7 @@ public class BasicCodeWriter
 		this.namespace = __ns;
 		this.output = __o;
 		this._code = __c;
+		this.config = __ns._config;
 		this.pool = __ns._pool;
 		
 		// Create code writer instance
@@ -128,11 +140,82 @@ public class BasicCodeWriter
 		if (__t == null)
 			throw new NullPointerException("NARG");
 		
+		// Convert input arguments to types
+		List<NativeRegisterType> args = new ArrayList<>();
+		for (StackMapType t : __t)
+			args.add(stackMapToRegisterType(t));
+			
 		// Debug
-		System.err.printf("DEBUG -- Primed args: %s%n",
-			Arrays.<StackMapType>asList(__t));
+		System.err.printf("DEBUG -- Primed args in: %s -> %s%n",
+			Arrays.<StackMapType>asList(__t), args);
+		
+		// Allocate the register
+		NativeAllocation[] allocs = this.allocator.argumentAllocate(
+			args.<NativeRegisterType>toArray(
+				new NativeRegisterType[args.size()]));
+		
+		// Debug
+		System.err.printf("DEBUG -- Primed args out: %s%n",
+			Arrays.<NativeAllocation>asList(allocs));
 		
 		throw new Error("TODO");
+	}
+	
+	/**
+	 * Converts a Java stack map type to a native register type.
+	 *
+	 * @param __t The type to convert.
+	 * @return The register type for the given register.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/09/15
+	 */
+	public NativeRegisterType stackMapToRegisterType(StackMapType __t)
+		throws NullPointerException
+	{
+		// Check
+		if (__t == null)
+			throw new NullPointerException("NARG");
+		
+		// Depends
+		JITConfig config = this.config;
+		JITTriplet triplet = config.triplet();
+		NativeFloatType floating = triplet.floatingPoint();
+		switch (__t)
+		{
+				// As-is
+			case INTEGER: return NativeRegisterIntegerType.INTEGER;
+			case LONG: return NativeRegisterIntegerType.LONG;
+			
+				// Depends on the pointer size
+			case OBJECT:
+				switch (triplet.bits())
+				{
+					case 8: return NativeRegisterIntegerType.BYTE;
+					case 16: return NativeRegisterIntegerType.SHORT;
+					case 32: return NativeRegisterIntegerType.INTEGER;
+					case 64: return NativeRegisterIntegerType.LONG;
+					
+						// Unknown
+					default:
+						throw new RuntimeException("OOPS");
+				}
+				
+				// Could be in software
+			case FLOAT:
+				if (floating.isHardwareFloat())
+					return NativeRegisterFloatType.FLOAT;
+				return NativeRegisterIntegerType.INTEGER;
+			
+				// Could also be in software
+			case DOUBLE:
+				if (floating.isHardwareDouble())
+					return NativeRegisterFloatType.DOUBLE;
+				return NativeRegisterIntegerType.LONG;
+			
+				// Unknown
+			default:
+				throw new RuntimeException("OOPS");
+		}
 	}
 	
 	/**
