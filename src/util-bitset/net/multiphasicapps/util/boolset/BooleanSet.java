55bca9d19c8591d622fdc3fd2660421c7189864d
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-16 13:12:20 +0000

    Implement set and size for FSBS.

--- src/util-bitset/net/multiphasicapps/util/boolset/BooleanSet.java
+++ src/util-bitset/net/multiphasicapps/util/boolset/BooleanSet.java
@@ -13,9 +13,31 @@ package net.multiphasicapps.util.boolset;
 /**
  * This interface describes a boolean set.
  *
+ * Implementations of this interface are not required to be thread safe.
+ *
  * @since 2016/09/16
  */
 public interface BooleanSet
 {
+	/**
+	 * Sets the specified bit to the given state.
+	 *
+	 * @param __i The bit to set.
+	 * @param __v The state of the bit.
+	 * @return The old value of the bit.
+	 * @throws IndexOutOfBoundsException If the bit is negative or exceeds
+	 * the capacity of the set.
+	 * @since 2016/09/16
+	 */
+	public abstract boolean set(int __i, boolean __v)
+		throws IndexOutOfBoundsException;
+	
+	/**
+	 * Returns the number of bits that are in the set.
+	 *
+	 * @return The number of bits in the set.
+	 * @since 2016/09/16
+	 */
+	public abstract int size();
 }
 
--- src/util-bitset/net/multiphasicapps/util/boolset/FixedSizeBooleanSet.java
+++ src/util-bitset/net/multiphasicapps/util/boolset/FixedSizeBooleanSet.java
@@ -14,11 +14,19 @@ package net.multiphasicapps.util.boolset;
  * This is a {@link BooleanSet} which is of a fixed size, it only permits
  * reading and writing from bits that are valid within the set.
  *
+ * This class is not thread safe.
+ *
  * @since 2016/09/16
  */
 public class FixedSizeBooleanSet
 	implements BooleanSet
 {
+	/** The number of bits in the set. */
+	protected final int size;
+	
+	/** Raw boolean data. */
+	private final byte[] _bits;
+	
 	/**
 	 * Initializes the fixed size boolean set.
 	 *
@@ -34,7 +42,49 @@ public class FixedSizeBooleanSet
 		if (__n < 0)
 			throw new IndexOutOfBoundsException("CH01");
 		
-		throw new Error("TODO");
+		// Set and allocate
+		this.size = __n;
+		this._bits = new byte[__n >>> 3];
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/09/16
+	 */
+	@Override
+	public boolean set(int __i, boolean __v)
+		throws IndexOutOfBoundsException
+	{
+		// {@squirreljme.error CH02 Cannot set a bit which is outside the
+		// bounds of the set.}
+		if (__i < 0 || __i >= this.size)
+			throw new IndexOutOfBoundsException("CH02");
+		
+		// Determine the location and mask of the bit
+		int lo = __i >>> 3;
+		int mk = 1 << (__i & 7);
+		
+		// Get the old value
+		byte[] bits = this._bits;
+		byte bye = bits[lo];
+		boolean rv = (0 != (bye & mk));
+		
+		// If the value is different, flip the bit
+		if (rv != __v)
+			bits[lo] = (byte)(bye ^ mk);
+		
+		// Old value
+		return rv;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/09/16
+	 */
+	@Override
+	public int size()
+	{
+		return this.size;
 	}
 }
 
