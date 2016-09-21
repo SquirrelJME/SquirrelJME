ef679963693ea5a77801977613f04256e19786eb
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-21 10:26:16 +0000

    Base for setting up the class loader and invoking the requested main method, however the custom class loader is not yet implemented.

--- src/bootstrap-javase-bridge/net/multiphasicapps/squirreljme/bootstrap/javase/BridgedLauncher.java
+++ src/bootstrap-javase-bridge/net/multiphasicapps/squirreljme/bootstrap/javase/BridgedLauncher.java
@@ -10,6 +10,8 @@
 
 package net.multiphasicapps.squirreljme.bootstrap.javase;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import net.multiphasicapps.squirreljme.bootstrap.base.launcher.BootLauncher;
 import net.multiphasicapps.squirreljme.bootstrap.base.launcher.
 	ResourceAccessor;
@@ -35,7 +37,92 @@ public class BridgedLauncher
 		if (__ra == null || __main == null || __args == null)
 			throw new NullPointerException("NARG");
 		
-		throw new Error("TODO");
+		// Setup class loader
+		__Loader__ ld = new __Loader__(__ra);
+		
+		try
+		{
+			// Find the main class
+			Class<?> cl = Class.forName(__main, true, ld);
+			
+			// Find the main method
+			Method m = cl.getDeclaredMethod("main", String[].class);
+			
+			// Invoke it with the arguments
+			m.invoke(null, (Object)__args);
+			
+			// Ok
+			return true;
+		}
+		
+		// Invocation failed
+		catch (InvocationTargetException e)
+		{
+			Throwable t = e.getCause();
+			
+			if (t instanceof Error)
+				throw (Error)t;
+			if (t instanceof RuntimeException)
+				throw (RuntimeException)t;
+			
+			// {@squirreljme.error DE08 Exception was thrown in the launched
+			// program.}
+			throw new RuntimeException("DE08", e);
+		}
+		
+		// {@squirreljme.error DE07 Could not launch the the specified
+		// program.}
+		catch (ReflectiveOperationException e)
+		{
+			throw new RuntimeException("DE07", e);
+		}
+	}
+	
+	/**
+	 * This returns the class loader used by the current thread if it has
+	 * been set, otherwise the system class loader.
+	 *
+	 * @return The context class loader or the system one if it is not set.
+	 * @since 2016/09/21
+	 */
+	private static ClassLoader __contextLoader()
+	{
+		ClassLoader rv = Thread.currentThread().getContextClassLoader();
+		if (rv != null)
+			return null;
+		return ClassLoader.getSystemClassLoader();
+	}
+	
+	/**
+	 * This is the internal class loader which
+	 *
+	 * @since 2016/09/21
+	 */
+	private final class __Loader__
+		extends ClassLoader
+	{
+		/** The accessor for resources. */
+		protected final ResourceAccessor accessor;
+		
+		/**
+		 * Initializes the class loader.
+		 *
+		 * @param __ra The accessor for resources.
+		 * @throws NullPointerException On null arguments.
+		 * @since 2016/09/21
+		 */
+		private __Loader__(ResourceAccessor __ra)
+			throws NullPointerException
+		{
+			super(__contextLoader());
+			
+			// Check
+			if (__ra == null)
+				throw new NullPointerException("NARG");
+			
+			// Set
+			this.accessor = __ra;
+		}
 	}
 }
 
