103994482ba3c8d43ca72c16c79610913cadfbf5
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-27 16:53:39 +0000

    Add a method to get the offset for a variable at the top of the stack (the stack pointer) and make the base from the frame pointer instead.

--- src/native-code/net/multiphasicapps/squirreljme/nativecode/NativeStackDirection.java
+++ src/native-code/net/multiphasicapps/squirreljme/nativecode/NativeStackDirection.java
@@ -27,8 +27,9 @@ public enum NativeStackDirection
 	;
 	
 	/**
-	 * Determines the base address at which to start writing values to on the
-	 * stack.
+	 * Determines the address of the value with the given length as if it were
+	 * accessed from the base of the stack (the frame pointer) which is at the
+	 * low end of the stack.
 	 *
 	 * @param __soff The offset from the base of the stack.
 	 * @param __len The number of bytes that make up the value.
@@ -80,6 +81,50 @@ public enum NativeStackDirection
 				// Known
 			case LOW_TO_HIGH: return __d;
 			case HIGH_TO_LOW: return -__d;
+			
+				// Unknown
+			default:
+				throw new RuntimeException("OOPS");
+		}
+	}
+	
+	/**
+	 * This calculates the position of the specified entry from the top of the
+	 * stack (the stack pointer).
+	 *
+	 * Since this is from the top of the stack, the size of the stack must also
+	 * be passed.
+	 *
+	 * @param __soff The offset of the entry on the stack, from the base of
+	 * the stack.
+	 * @param __len The length of the entry.
+	 * @param __ss The current size of the stack.
+	 * @return The offset from the top of the stack, where the entry is
+	 * located.
+	 * @throws IllegalArgumentException If the length is zero or negative, or
+	 * the stack size is negative.
+	 * @since 2016/09/27
+	 */
+	public final int top(int __soff, int __len, int __ss)
+		throws IllegalArgumentException
+	{
+		// {@squirreljme.error AR0d The size of the stack is negative.}
+		if (__ss < 0)
+			throw new IllegalArgumentException("AR0d");
+		
+		// {@squirreljme.error AR0e Illegal stack offset and/or length, the
+		// length is zero, or the combined values overflow. (The stack offset;
+		// The length)}
+		if (__len <= 0 || (__soff + __len) < __soff)
+			throw new IllegalArgumentException(String.format("AR0e %d %d",
+				__soff, __len));
+		
+		// Depends
+		switch (this)
+		{
+				// Known
+			case LOW_TO_HIGH: return (__ss - __soff);
+			case HIGH_TO_LOW: return __ss - (__soff + __len);
 			
 				// Unknown
 			default:
