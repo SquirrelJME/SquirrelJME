bbe2686d0c010b71602e7e06e27ac0280b8d77d9
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-09-28 14:38:47 +0000

    Add some base methods to the output interface.

--- src/exe/net/multiphasicapps/squirreljme/exe/ExecutableOutput.java
+++ src/exe/net/multiphasicapps/squirreljme/exe/ExecutableOutput.java
@@ -10,12 +10,18 @@
 
 package net.multiphasicapps.squirreljme.exe;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import net.multiphasicapps.squirreljme.jit.base.JITConfig;
 
 /**
  * This interface describes a means for which an executable that contains
  * native code, class definitions, and resources is written. The output class
  * handles linkage, the layout of the resulting binary, and other details.
+ *
+ * It is up to the implementation of the output how caching of the resulting
+ * binary is to be handled, it may store the data in memory or use a temporary
+ * file on the disk.
  *
  * @since 2016/09/28
  */
@@ -28,5 +34,20 @@ public interface ExecutableOutput
 	 * @since 2016/09/28
 	 */
 	public abstract JITConfig config();
+	
+	/**
+	 * Writes the resultant binary to the output stream.
+	 *
+	 * This method should cause linking to occur if needed.
+	 *
+	 * This method must be able to be called multiple times, if there are no
+	 * changes then it should output the same exact binary.
+	 *
+	 * @param __os The stream to write binary data to.
+	 * @throws IOException On read/write errors.
+	 * @since 2016/09/28
+	 */
+	public abstract void writeOutput(OutputStream __os)
+		throws IOException;
 }
 
