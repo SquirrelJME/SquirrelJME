7ae4eaee4fa866a8641986d003a3b9f6df9cfaa6
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-06 21:31:49 +0000

    Base for Squirrel Digger.

---
+++ src/squirrel-digger/net/multiphasicapps/squirreldigger/DefaultMain.java
@@ -0,0 +1,35 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreldigger;
+
+/**
+ * This is the classical main entry point.
+ *
+ * @since 2016/10/06
+ */
+public class DefaultMain
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2016/10/06
+	 */
+	public static void main(String... __args)
+	{
+		// Force to exist
+		if (__args == null)
+			__args = new String[0];
+		
+		throw new Error("TODO");
+	}
+}
+
