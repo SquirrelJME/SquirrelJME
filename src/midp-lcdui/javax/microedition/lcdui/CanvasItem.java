c7b782edc8652f3ace630659de2e26f832eb2aa6
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 19:44:05 +0000

    Add CanvasItem.

---
+++ src/midp-lcdui/javax/microedition/lcdui/CanvasItem.java
@@ -0,0 +1,75 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public abstract class CanvasItem
+{
+	public int getHeight()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Object getParent()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getPositionX()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getPositionY()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getWidth()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getZPosition()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setParent(Object __p)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setPositionX(int __x)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setPositionY(int __y)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setSize(int __w, int __h)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setVisible(boolean __v)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setZPosition(int __z)
+	{
+		throw new Error("TODO");
+	}
+}
+
