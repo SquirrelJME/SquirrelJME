7a83289c7faf216d4f15928057f14b9ddc4e6a6d
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 20:29:12 +0000

    Add IdleItem.

---
+++ src/midp-lcdui/javax/microedition/lcdui/IdleItem.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public abstract class IdleItem
+	extends CustomItem
+{
+	protected IdleItem(String __label)
+	{
+		throw new Error("TODO");
+	}
+	
+	protected void addedToDisplay(Display __d)
+	{
+		throw new Error("TODO");
+	}
+	
+	protected void removedFromDisplay(Display __d)
+	{
+		throw new Error("TODO");
+	}
+}
+
