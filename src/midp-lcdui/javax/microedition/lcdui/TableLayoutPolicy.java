170ff453f244a3c2718c8c0564991ed09048978a
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 20:54:56 +0000

    Add TableLayoutPolicy.

---
+++ src/midp-lcdui/javax/microedition/lcdui/TableLayoutPolicy.java
@@ -0,0 +1,26 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public final class TableLayoutPolicy
+	extends FormLayoutPolicy
+{
+	public TableLayoutPolicy(Form __f, int __cols)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getColumns()
+	{
+		throw new Error("TODO");
+	}
+}
+
