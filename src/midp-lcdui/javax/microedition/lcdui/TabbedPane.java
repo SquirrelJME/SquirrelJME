afb5a696ca410c2868f575947794fb8b43d0eca1
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 20:53:17 +0000

    Add TabbedPane.

---
+++ src/midp-lcdui/javax/microedition/lcdui/TabbedPane.java
@@ -0,0 +1,71 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public class TabbedPane
+	extends Screen
+{
+	public TabbedPane(String __title, boolean __stringtab, boolean __suptitle)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void addTab(Screen __t, Image __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void addTabListener(TabListener __tl)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getCount()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Screen getScreen(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getSelectedIndex()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Image getTabIcon(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void insertTab(int __i, Screen __t, Image __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void removeTab(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setFocus(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setTabIcon(int __i, Image __icon)
+	{
+		throw new Error("TODO");
+	}
+}
+
