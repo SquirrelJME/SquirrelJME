9715c542f1f9b302ab46a0e914fc2afee27e720e
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 20:49:30 +0000

    Add ScalableImage.

---
+++ src/midp-lcdui/javax/microedition/lcdui/ScalableImage.java
@@ -0,0 +1,46 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public class ScalableImage
+	extends Image
+{
+	public int getViewportHeight()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getViewportWidth()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setHeight(int __h, boolean __mar)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setWidth(int __w, boolean __mar)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void unbind()
+	{
+		throw new Error("TODO");
+	}
+	
+	public static ScalableImage bind(Object __esi)
+	{
+		throw new Error("TODO");
+	}
+}
+
