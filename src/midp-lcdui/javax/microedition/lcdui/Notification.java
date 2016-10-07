75cf11a1b2b8c8e9d9d456ca8016fbbfa9118f57
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 20:42:16 +0000

    Add Notification.

---
+++ src/midp-lcdui/javax/microedition/lcdui/Notification.java
@@ -0,0 +1,70 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public class Notification
+{
+	public Notification(NotificationType __t)
+	{
+		throw new Error("TODO");
+	}
+	
+	public Notification(NotificationType __t, String __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public Notification(NotificationType __t, String __l, Image __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public long getTimeStamp()
+	{
+		throw new Error("TODO");
+	}
+	
+	public NotificationType getType()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void post(boolean __s)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void post(boolean __s, int __dur)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void remove()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setImage(Image __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setLabel(String __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setListener(NotificationListener __nl)
+	{
+		throw new Error("TODO");
+	}
+}
+
