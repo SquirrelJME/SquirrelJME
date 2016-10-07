b957ec99a1b5dcd456f2280ef05d41784f32c049
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 21:06:18 +0000

    Add Text.

---
+++ src/midp-lcdui/javax/microedition/lcdui/Text.java
@@ -0,0 +1,264 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public class Text
+{
+	public static final int ALIGN_CENTER =
+		1;
+	
+	public static final int ALIGN_DEFAULT =
+		4;
+	
+	public static final int ALIGN_JUSTIFY =
+		3;
+	
+	public static final int ALIGN_LEFT =
+		0;
+	
+	public static final int ALIGN_RIGHT =
+		2;
+	
+	public static final int DIRECTION_LTR =
+		10;
+	
+	public static final int DIRECTION_NEUTRAL =
+		12;
+	
+	public static final int DIRECTION_RTL =
+		11;
+	
+	public Text()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Text(String __c, int __w, int __h)
+	{
+		throw new Error("TODO");
+	}
+	
+	public Text(String __c, int __o, int __l, int __w, int __h)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void delete(int __i, int __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getAlignment()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getBackgroundColor()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getCaret()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void getCharExtent(int __i, int[] __ext)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getCharIndex(int __x, int __y)
+	{
+		throw new Error("TODO");
+	}
+	
+	public Font getFont()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Font getFont(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getForegroundColor()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getForegroundColor(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getHeight()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getHighlightIndex()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getHighlightLength()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getIndent()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getInitialDirection()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getRequiredHeight()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getRequiredLineCount()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getScrollOffset()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getSpaceAbove()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getSpaceBelow()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getText(int __i, int __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getTextLength()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getWidth()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void insert(int __i, String __c)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int lastRenderedIndex()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void moveCaret(int __nl)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setAlignment(int __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setBackgroundColor(int __argb)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setCaret(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setFont(Font __f)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setFont(Font __f, int __i, int __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setForegroundColor(int __argb)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setForegroundColor(int __argb, int __i, int __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setHeight(int __h)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setHighlight(int __i, int __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setIndent(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setInitialDirection(int __dir)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setScrollOffset(int __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setSpaceAbove(int __sa)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setSpaceBelow(int __sb)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setWidth(int __w)
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean textFits()
+	{
+		throw new Error("TODO");
+	}
+}
+
