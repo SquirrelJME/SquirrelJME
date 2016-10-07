762d8e57836e3482fdd8a477fb4193df8d4c3823
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 20:39:35 +0000

    Add Menu.

---
+++ src/midp-lcdui/javax/microedition/lcdui/Menu.java
@@ -0,0 +1,140 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public class Menu
+{
+	public Menu(String __short, String __long, Image __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int append(Command __c)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int append(Menu __m)
+	{
+		throw new Error("TODO");
+	}
+	
+	public Command getCommand(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public Font getFont()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Image getImage()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getLabel()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getLongLabel()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Menu getMenu(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getMenuDepth()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void insert(int __i, Command __c)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void insert(int __i, Menu __m)
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean isCommand(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean isEnabled()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean isVisible()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void onParentEnabled(boolean __e)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void remove(Command __c)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void remove(Menu __m)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setEnabled(boolean __e)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setFont(Font __f)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setImage(Image __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setLabel(String __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setLongLabel(String __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int size()
+	{
+		throw new Error("TODO");
+	}
+	
+	public static int getMaxMenuDepth()
+	{
+		throw new Error("TODO");
+	}
+}
+
