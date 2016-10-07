b1d59de4d690ae1337b570d5cf97cbd9dfc25da2
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-07 22:49:22 +0000

    Add TextEditor.

---
+++ src/midp-lcdui/javax/microedition/lcdui/TextEditor.java
@@ -0,0 +1,171 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.lcdui;
+
+public class TextEditor
+	extends CanvasItem
+{
+	public TextEditor(String __text, int __maxsz, int __cons, int __w, int __h)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void delete(int __o, int __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getBackgroundColor()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getCaretPosition()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getConstraints()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getContentHeight()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean getFocus()
+	{
+		throw new Error("TODO");
+	}
+	
+	public Font getFont()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getForegroundColor()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getHighlightColor()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getLineMarginHeight()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getMaxSize()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getSelection()
+	{
+		throw new Error("TODO");
+	}
+	
+	public String getString()
+	{
+		throw new Error("TODO");
+	}
+	
+	public boolean getVisible()
+	{
+		throw new Error("TODO");
+	}
+	
+	public int getVisibleContentPosition()
+	{
+		throw new Error("TODO");
+	}
+	
+	public void insert(String __t, int __p)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setBackgroundColor(int __a, int __r, int __g, int __b)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setCaretPosition(int __i)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setConstraints(int __cons)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setFocus(boolean __f)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setFont(Font __f)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setForegroundColor(int __a, int __r, int __g, int __b)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setHighlightColor(int __a, int __r, int __g, int __b)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setInitialInputMode(String __cs)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int setMaxSize(int __sz)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setSelection(int __i, int __l)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setString(String __s)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setTextEditorListener(TextEditorChangeListener __cl)
+	{
+		throw new Error("TODO");
+	}
+	
+	public void setVisible(boolean __v)
+	{
+		throw new Error("TODO");
+	}
+	
+	public int size()
+	{
+		throw new Error("TODO");
+	}
+}
+
