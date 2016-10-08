e577237cd7e877435162842e52ed7b16ea33b5e4
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-08 16:34:53 +0000

    Base for JavaSE backed framebuffer.

---
+++ src/bui-framebuffer-javase/net/multiphasicapps/squirreljme/bui/framebuffer/javase/JavaSEFrameBufferProvider.java
@@ -0,0 +1,26 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.bui.framebuffer.javase;
+
+import net.multiphasicapps.squirreljme.bui.framebuffer.Framebuffer;
+import net.multiphasicapps.squirreljme.bui.framebuffer.FramebufferProvider;
+
+/**
+ * This is a provider that provides Java SE backed framebuffer that utilize
+ * Swing to provide a common user interface.
+ *
+ * @since 2016/10/08
+ */
+public class JavaSEFrameBufferProvider
+	implements FramebufferProvider
+{
+}
+
