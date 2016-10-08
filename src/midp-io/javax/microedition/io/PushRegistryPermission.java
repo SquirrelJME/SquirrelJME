c0d55457fae41e987cbc106d5a045ebb79789453
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-08 01:18:44 +0000

    Add IMCServerConnection and PushRegistryPermission.

---
+++ src/midp-io/javax/microedition/io/PushRegistryPermission.java
@@ -0,0 +1,64 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.io;
+
+import java.security.Permission;
+
+public class PushRegistryPermission
+	extends Permission
+{
+	public PushRegistryPermission(String __uri, String __act)
+	{
+		super(__uri, __act);
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/07
+	 */
+	@Override
+	public boolean equals(Object __a)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/07
+	 */
+	@Override
+	public String getActions()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/07
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/07
+	 */
+	@Override
+	public boolean implies(Permission __a)
+	{
+		throw new Error("TODO");
+	}
+}
+
