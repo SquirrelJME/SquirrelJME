ad32d1ea5bcee91f98dfc556af980be178c240bc
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-11 17:19:11 +0000

    Add base for the auto interpreter.

---
+++ src/auto-interpreter/net/multiphasicapps/squirreljme/autointerpreter/Main.java
@@ -0,0 +1,35 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.autointerpreter;
+
+/**
+ * This is the main entry point for the auto-interpreter.
+ *
+ * @since 2016/10/11
+ */
+public class Main
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2016/10/11
+	 */
+	public static void main(String... __args)
+	{
+		// Force to exist
+		if (__args == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+}
+
---
+++ src/auto-interpreter/net/multiphasicapps/squirreljme/autointerpreter/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the auto interpreter which is used to setup and
+ * initialize a virtualized SquirrelJME environment.
+ *
+ * @since 2016/10/11
+ */
+
+package net.multiphasicapps.squirreljme.autointerpreter;
+
