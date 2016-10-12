06ed6b779e20239ebc292b2151beba3d32b49bfa
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-12 20:49:18 +0000

    Add base for midlet naming details.

---
+++ src/midlet-identity/net/multiphasicapps/squirreljme/midletid/MidletSuiteID.java
@@ -0,0 +1,81 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.midletid;
+
+/**
+ * This is the identity for a midlet suite which contains a name, vendor,
+ * and version.
+ *
+ * @since 2016/10/12
+ */
+public final class MidletSuiteID
+	implements Comparable<MidletSuiteID>
+{
+	/**
+	 * Initializes the suite identifier.
+	 *
+	 * @param __name The name of the suite.
+	 * @param __vendor The vendor of the suite.
+	 * @param __ver The version of the suite.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/10/12
+	 */
+	public MidletUID(MidletSuiteName __name, MidletSuiteVendor __ven,
+		MidletVersion __ver)
+		throws NullPointerException
+	{
+		// Check
+		if (__name == null || __ven == null || __ver == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public int compareTo(MidletSuiteID __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public String toString()
+	{
+		throw new Error("TODO");
+	}
+}
+
---
+++ src/midlet-identity/net/multiphasicapps/squirreljme/midletid/MidletVersion.java
@@ -0,0 +1,79 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.midletid;
+
+/**
+ * This represents a midlet version.
+ *
+ * @since 2016/10/12
+ */
+public final class MidletVersion
+	implements Comparable<MidletVersion>
+{
+	/**
+	 * Initializes the version.
+	 *
+	 * @param __v The value to parse.
+	 * @throws IllegalArgumentException If the input is not valid.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/10/12
+	 */
+	public MidletVersion(String __v)
+		throws IllegalArgumentException, NullPointerException
+	{
+		// Check
+		if (__v == null)
+			throw new NullPointerException("NARG");
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public int compareTo(MidletVersion __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/12
+	 */
+	@Override
+	public String toString()
+	{
+		throw new Error("TODO");
+	}
+}
+
