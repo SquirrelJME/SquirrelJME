737db3f438dcb767a1d0932510edd7e4a69db5fd
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-12 02:48:46 +0000

    Add an unsafe that is in the Java SE bridge since it will be needed by sockets.

---
+++ src/bootstrap-javase-bridge/net/multiphasicapps/squirreljme/unsafe/SquirrelJME.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.unsafe;
+
+/**
+ * This class contains implementations of unsafe SquirrelJME operations which
+ * are not part of the bootstrap's external build dependencies. This in the
+ * general case will only have socket related code.
+ *
+ * @since 2016/10/11
+ */
+public final class SquirrelJME
+{
+}
+
