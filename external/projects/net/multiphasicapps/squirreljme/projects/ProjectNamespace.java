5ee747429ca25f8591abd0a849ecfba412c49775
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-16 19:11:55 +0000

    Implement ProjectNamespace which contains the name of a namespace.

--- external/projects/net/multiphasicapps/squirreljme/projects/ProjectNamespace.java
+++ external/projects/net/multiphasicapps/squirreljme/projects/ProjectNamespace.java
@@ -11,11 +11,105 @@
 package net.multiphasicapps.squirreljme.projects;
 
 /**
- * DESCRIBE THIS.
+ * This represents the namespace name of a project.
  *
  * @since 2016/10/16
  */
-public class ProjectNamespace
+public final class ProjectNamespace
+	implements Comparable<ProjectNamespace>
 {
+	/** The name of the namespace. */
+	protected final String string;
+	
+	/**
+	 * Initializes the namespace name.
+	 *
+	 * @param __n The name of the namespace.
+	 * @throws InvalidProjectException If the name is not valid.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2016/10/16
+	 */
+	public ProjectNamespace(String __n)
+		throws InvalidProjectException, NullPointerException
+	{
+		// Check
+		if (__n == null)
+			throw new NullPointerException("NARG");
+		
+		// Setup
+		StringBuilder sb = new StringBuilder();
+		int n = __n.length();
+		for (int i = 0; i < n; i++)
+		{
+			char c = __n.charAt(i);
+			
+			// Keep as is
+			if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-')
+				sb.append(c);
+			
+			// Lowercase
+			else if (c >= 'A' && c <= 'Z')
+				sb.append((char)('a' + (c - 'A')));
+			
+			// {@squirreljme.error CI0h The specified namespace contains an
+			// illegal character. (The specified namespace)}
+			else
+				throw new InvalidProjectException(String.format("CI0h %s",
+					__n));
+		}
+		
+		// Use it
+		this.string = sb.toString();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/16
+	 */
+	@Override
+	public String compareTo(ProjectNamespace __o)
+		throws NullPointerException
+	{
+		// Check
+		if (__o == null)
+			throw new NullPointerException("NARG");
+		
+		return this.string.compareTo(__o.string);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/16
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		// Check
+		if (!(__o instanceof ProjectNamespace))
+			return false;
+		
+		// Compare
+		return this.string.equals(((ProjectNamespace)__o).string);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/16
+	 */
+	@Override
+	public int hashCode()
+	{
+		return this.string.hashCode();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2016/10/16
+	 */
+	@Override
+	public String toString()
+	{
+		return this.string;
+	}
 }
 
