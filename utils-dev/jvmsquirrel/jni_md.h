05fd2f58c068a36791fb4032f36154c2600f1c30
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-19 20:27:30 +0000

    Add base for a libjvm.so virtual machine.

---
+++ utils-dev/jvmsquirrel/jni_md.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// --------------------------------------------------------------------------*/
+
+/**
+ * DESCRIBE THIS.
+ *
+ * @since 2016/10/19
+ */
+
+/** Header guard. */
+#ifndef SJME_hGJNI_MDH
+#define SJME_hGJNI_MDH
+
+/** Anti-C++. */
+#ifdef _cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_cXJNI_MDH
+extern "C"
+{
+#endif /** #ifdef SJME_CXX_IS_EXTERNED */
+#endif /** #ifdef __cplusplus */
+
+/****************************************************************************/
+
+/****************************************************************************/
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_cXJNI_MDH
+}
+#undef SJME_cXJNI_MDH
+#undef SJME_CXX_IS_EXTERNED
+#endif /** #ifdef SJME_cXJNI_MDH */
+#endif /** #ifdef __cplusplus */
+
+/** Header guard. */
+#endif /* #ifndef SJME_hGJNI_MDH */
+
---
+++ utils-dev/jvmsquirrel/jvmsquirrel.c
@@ -0,0 +1,14 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+// This is a miniature Java VM virtual machine for simplistic sanity purposes.
+// Download: <http://hg.openjdk.java.net/jdk8/jdk8/jdk/archive/tip.tar.gz>
+// In
+// -------------------------------------------------------------------------*/
+
