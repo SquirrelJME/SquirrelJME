e2e8f89d7a6ef6870ba6deca5afa82f6bf1d579f
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-10-28 16:11:18 +0000

    Base for build base package.

---
+++ build/build-base/net/multiphasicapps/squirreljme/build/base/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the interfaces which are used by the build system so
+ * that it may operate correctly.
+ *
+ * @since 2016/10/28
+ */
+
+package net.multiphasicapps.squirreljme.build.base;
+
