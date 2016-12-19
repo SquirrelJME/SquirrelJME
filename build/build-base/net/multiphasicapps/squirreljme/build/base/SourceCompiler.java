88dc52494705df1ba4ae6b02ed68bdd67117e91c
Stephanie Gawroriski <xer@multiphasicapps.net>
2016-12-19 19:45:45 +0000

    Add base interface and provider for the source code compiler.

---
+++ build/build-base/net/multiphasicapps/squirreljme/build/base/SourceCompiler.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.build.base;
+
+/**
+ * This interfaced is used to describe a compiler which is used to compile
+ * Java source code into Java class files.
+ *
+ * @since 2016/12/19
+ */
+public interface SourceCompiler
+{
+}
+
---
+++ build/build-base/net/multiphasicapps/squirreljme/build/base/SourceCompilerProvider.java
@@ -0,0 +1,64 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// For more information see license.mkd.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.build.base;
+
+import java.util.ServiceLoader;
+
+/**
+ * This class is used to create instances of source code compilers so that
+ * each compilation instance is standalone. This class is used via the
+ * service loader.
+ *
+ * @since 2016/12/19
+ */
+public abstract class SourceCompilerProvider
+{
+	/** Service lookup. */
+	private static final ServiceLoader<SourceCompilerProvider> _SERVICES =
+		ServiceLoader.<SourceCompilerProvider>load(
+		SourceCompilerProvider.class);
+	
+	/**
+	 * Creates a new instance of the source compiler.
+	 *
+	 * @return The compiler instance.
+	 * @since 2016/12/19
+	 */
+	public abstract SourceCompiler newCompilerInstance();
+	
+	/**
+	 * Creates a new instance of the source compiler.
+	 *
+	 * @return The compiler instance.
+	 * @throws RuntimeException If no compiler is available.
+	 * @since 2016/12/19
+	 */
+	public static SourceCompiler newInstance()
+		throws RuntimeException
+	{
+		// Lock
+		ServiceLoader<SourceCompilerProvider> services = _SERVICES;
+		synchronized (services)
+		{
+			// Use the first service, whatever that may be
+			for (SourceCompilerProvider p : services)
+			{
+				SourceCompiler rv = p.newCompilerInstance();
+				if (rv != null)
+					return rv;
+			}
+		}
+		
+		// {@squirreljme.error AU01 No compiler is available for usage.}
+		throw new RuntimeException("AU01");
+	}
+}
+
