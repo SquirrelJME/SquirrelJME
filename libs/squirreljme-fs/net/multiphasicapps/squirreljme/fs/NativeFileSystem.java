8e5e3ea96eb9fe3b154e5bf1a2fe553a2dc61390
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-01-01 14:53:17 +0000

    Update copyright statement.

--- libs/squirreljme-fs/net/multiphasicapps/squirreljme/fs/NativeFileSystem.java
+++ libs/squirreljme-fs/net/multiphasicapps/squirreljme/fs/NativeFileSystem.java
@@ -1,11 +1,11 @@
 // -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
 // ---------------------------------------------------------------------------
 // Multi-Phasic Applications: SquirrelJME
-//     Copyright (C) 2013-2016 Steven Gawroriski <steven@multiphasicapps.net>
-//     Copyright (C) 2013-2016 Multi-Phasic Applications <multiphasicapps.net>
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
 // ---------------------------------------------------------------------------
 // SquirrelJME is under the GNU General Public License v3+, or later.
-// For more information see license.mkd.
+// See license.mkd for licensing and copyright information.
 // ---------------------------------------------------------------------------
 
 package net.multiphasicapps.squirreljme.fs;
