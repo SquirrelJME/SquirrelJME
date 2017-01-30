6724bfe20e0dc9985b011debeea8096f67976077
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-01-30 22:09:00 +0000

    Add base for translation engine.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/TranslationEngine.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This is the base class for the JIT translation engine and is used to
+ * translate class files to {@link ExecutableClass}es which may be executed
+ * directly by the system, cached for later, or act as part of the kernel build
+ * step.
+ *
+ * @since 2017/01/30
+ */
+public abstract class TranslationEngine
+{
+}
+
---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/TranslationEngineProvider.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This provides a single instance creation of a translation engine which is
+ * used by the JIT to translate byte code into an {@link ExecutableClass}.
+ *
+ * @since 2017/01/30
+ */
+public interface TranslationEngineProvider
+{
+	/**
+	 * Creates a new instance of the translation engine for use during JIT
+	 * compilation.
+	 *
+	 * @return The translation engine.
+	 * @since 2017/01/30
+	 */
+	public abstract TranslationEngine createEngin();
+}
+
