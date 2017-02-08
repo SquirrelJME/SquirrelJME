239d0ef5c795e83eb8ab479610ca29faf7e4ecb3
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-08 19:57:00 +0000

    Add base for Squirrel Quarrel package.

---
+++ libs/squirrel-quarrel-core/net/multiphasicapps/squirrelquarrel/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the implementation of the game, it is used as a
+ * library which is implemented by front-ends.
+ *
+ * @since 2017/02/08
+ */
+
+package net.multiphasicapps.squirrelquarrel;
+
