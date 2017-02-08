706fbb060311bda24a7480553e44d978ac39fda4
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-08 17:07:14 +0000

    Add DataType enumeration.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/DataType.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This represents the type of data which could be stored at a given memory
+ * address, these map to primitive types.
+ *
+ * @since 2017/02/08
+ */
+public enum DataType
+{
+	/** Byte. */
+	BYTE,
+	
+	/** Short. */
+	SHORT,
+	
+	/** Integer. */
+	INTEGER,
+	
+	/** Long. */
+	LONG,
+	
+	/** Float. */
+	FLOAT,
+	
+	/** Double. */
+	DOUBLE,
+	
+	/** End. */
+	;
+}
+
