d99e8240df316632a01c819aaf9f5320093ff6a0
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-10 01:42:35 +0000

    Add base for MegaTile and Level classes.

---
+++ libs/squirrel-quarrel-core/net/multiphasicapps/squirrelquarrel/Level.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel;
+
+/**
+ * This stores the mega tiles within the level along with storing level related
+ * data structures.
+ *
+ * @since 2017/02/09
+ */
+public class Level
+{
+}
+
---
+++ libs/squirrel-quarrel-core/net/multiphasicapps/squirrelquarrel/MegaTile.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel;
+
+/**
+ * This represents a single mega tile which represents a small region in the
+ * level.
+ *
+ * @since 2017/02/09
+ */
+public class MegaTile
+{
+}
+
