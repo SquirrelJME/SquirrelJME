7286ee91112998a04c00af7deb6065dd3ccc6fc5
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-11 02:07:41 +0000

    Import and adapt the old XPM image reader.

---
+++ libs/image-xpm-reader/net/multiphasicapps/xpm/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the reader for XPM images.
+ *
+ * @since 2017/02/10
+ */
+
+package net.multiphasicapps.xpm;
+
