611a2f824cbf6d114d60c27432a67e547e04d887
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-11 19:41:33 +0000

    Implement primitiveRGBSlice drawing into images.

--- apis/midp-lcdui/javax/microedition/lcdui/Image.java
+++ apis/midp-lcdui/javax/microedition/lcdui/Image.java
@@ -454,7 +454,48 @@ public class Image
 		protected void primitiveRGBSlice(int[] __b, int __o, int __l,
 			int __x, int __y, boolean __blend)
 		{
-			throw new Error("TODO");
+			int[] data = Image.this._data;
+			int iw = Image.this._width;
+			int ih = Image.this._height;
+			
+			// Calculate destination
+			int dest = (__y * iw) + __x;
+			int end = __o + __l;
+			
+			// Blending?
+			if (__blend)
+				for (int src = __o; src < end; src++)
+				{
+					// Get both src and dest colors
+					int sc = __b[src],
+						dc = data[dest];
+					
+					// Split into RGB
+					int sa = (sc >> 24) & 0xFF,
+						sr = (sc >> 16) & 0xFF,
+						sg = (sc >> 8) & 0xFF,
+						sb = (sc) & 0xFF,
+						da = (dc >> 24) & 0xFF,
+						dr = (dc >> 16) & 0xFF,
+						dg = (dc >> 8) & 0xFF,
+						db = (dc) & 0xFF;
+					
+					// Perform blending
+					int qq = 255 - sa;
+					int xa = sa + da - ((sa * da) / 255),
+						xr = ((sr * sa) / 255) + ((dr * qq) / 255),
+						xg = ((sg * sa) / 255) + ((dg * qq) / 255),
+						xb = ((sb * sa) / 255) + ((db * qq) / 255);
+					
+					// Recompile
+					int xc = (xa << 24) | (xr << 16) | (xg << 8) | xb;
+					data[dest++] = xc;
+				}
+			
+			// Not blending
+			else
+				for (int src = __o; src < end; src++)
+					data[dest++] = __b[src] | 0xFF000000;
 		}
 	}
 }
