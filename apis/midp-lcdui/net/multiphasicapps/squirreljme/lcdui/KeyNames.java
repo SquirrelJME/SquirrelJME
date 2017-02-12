7b7fe925dcbf8159a60feddc2f72ca42802807ba
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-12 18:08:56 +0000

    Implement key names.

--- apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/KeyNames.java
+++ apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/KeyNames.java
@@ -39,7 +39,58 @@ public final class KeyNames
 	public static String getKeyName(int __c)
 		throws IllegalArgumentException
 	{
-		throw new Error("TODO");
+		switch (__c)
+		{
+				// Less friendly names
+			case Canvas.KEY_BACKSPACE:	return "Backspace";
+			case Canvas.KEY_DELETE:		return "Delete";
+			case Canvas.KEY_DOWN:		return "Down";
+			case Canvas.KEY_ENTER:		return "Enter";
+			case Canvas.KEY_ESCAPE:		return "Escape";
+			case Canvas.KEY_LEFT:		return "Left";
+			case Canvas.KEY_RIGHT:		return "Right";
+			case Canvas.KEY_SELECT:		return "Select";
+			case Canvas.KEY_SPACE:		return "Space";
+			case Canvas.KEY_TAB:		return "Tab";
+			case Canvas.KEY_UP:			return "Up";
+			
+				// Function keys
+			case NonStandardKey.F1:		return "F1";
+			case NonStandardKey.F2:		return "F2";
+			case NonStandardKey.F3:		return "F3";
+			case NonStandardKey.F4:		return "F4";
+			case NonStandardKey.F5:		return "F5";
+			case NonStandardKey.F6:		return "F6";
+			case NonStandardKey.F7:		return "F7";
+			case NonStandardKey.F8:		return "F8";
+			case NonStandardKey.F9:		return "F9";
+			case NonStandardKey.F10:	return "F10";
+			case NonStandardKey.F11:	return "F11";
+			case NonStandardKey.F12:	return "F12";
+			case NonStandardKey.F13:	return "F13";
+			case NonStandardKey.F14:	return "F14";
+			case NonStandardKey.F15:	return "F15";
+			case NonStandardKey.F16:	return "F16";
+			case NonStandardKey.F17:	return "F17";
+			case NonStandardKey.F18:	return "F18";
+			case NonStandardKey.F19:	return "F19";
+			case NonStandardKey.F20:	return "F20";
+			case NonStandardKey.F21:	return "F21";
+			case NonStandardKey.F22:	return "F22";
+			case NonStandardKey.F23:	return "F23";
+			case NonStandardKey.F24:	return "F24";
+			
+				// Unknown or default
+			default:
+				// Default ASCII character, use the glyph name
+				if (__c >= 0x21 && __c <= 0x7E)
+					return Character.valueOf((char)__c).toString();
+				
+				// {@squirreljme.error EB0o Cannot get the name for the given
+				// key code because it is not known. (The key code)}
+				throw new IllegalArgumentException(String.format("EB0o %d",
+					__c));
+		}
 	}
 }
 
