d553eb7b778c0aaa67e4ca7f2fa0c36297ef7aad
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-14 22:48:52 +0000

    Add species enumeration.

---
+++ libs/squirrel-quarrel-core/net/multiphasicapps/squirrelquarrel/Species.java
@@ -0,0 +1,29 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel;
+
+/**
+ * The represents the species which are available in the game.
+ *
+ * @since 2017/02/14
+ */
+public enum Species
+{
+	/** Not really a major species. */
+	NEUTRAL,
+	
+	/** Squirrels. */
+	SQUIRREL,
+	
+	/** End. */
+	;
+}
+
