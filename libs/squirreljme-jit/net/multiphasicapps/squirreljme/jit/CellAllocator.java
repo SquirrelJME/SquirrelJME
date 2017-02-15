71ba63535292db84e36d72b1d217bc2f5976dca2
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-15 15:01:56 +0000

    Remove RegisterPurpose because it serves no purpose.

--- libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/CellAllocator.java
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/CellAllocator.java
@@ -21,25 +21,5 @@ package net.multiphasicapps.squirreljme.jit;
  */
 public abstract class CellAllocator
 {
-	/**
-	 * This is used to grant a single purpose or multiple purposes to the
-	 * specified register. This tells the allocator how the registers should
-	 * be handled by the compiler.
-	 *
-	 * @param __r The register to define a purpose for.
-	 * @param __p The purposes to give the specified register.
-	 * @throws JITException If the register could not be defined.
-	 * @throws NullPointerException On null arguments.
-	 * @since 2017/02/13
-	 */
-	protected final void defineRegister(Register __r, RegisterPurpose... __p)
-		throws JITException, NullPointerException
-	{
-		// Check
-		if (__r == null || __p == null || __p.length <= 0)
-			throw new NullPointerException("NARG");
-		
-		throw new Error("TODO");
-	}
 }
 
