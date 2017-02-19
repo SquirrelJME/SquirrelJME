fbc31dab46f1d5833ab9b5e35ed6b804f7045364
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-19 11:48:41 +0000

    Add base for MIPSBinding.

---
+++ libs/squirreljme-jit-mips/net/multiphasicapps/squirreljme/jit/mips/MIPSBinding.java
@@ -0,0 +1,53 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.mips;
+
+import net.multiphasicapps.squirreljme.jit.Binding;
+
+/**
+ * This is a binding for .
+ *
+ * @since 2017/02/19
+ */
+public class MIPSBinding
+	implements Binding
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/02/19
+	 */
+	@Override
+	public MIPSBinding copy()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/02/19
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/02/19
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new Error("TODO");
+	}
+}
+
