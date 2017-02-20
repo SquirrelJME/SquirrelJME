2a6acfb2051c6e3d2ad4a6182de3f22ea290c367
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-20 14:21:01 +0000

    Add MIPSGlobalBinding.

---
+++ libs/squirreljme-jit-mips/net/multiphasicapps/squirreljme/jit/mips/MIPSGlobalBinding.java
@@ -0,0 +1,53 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.mips;
+
+import net.multiphasicapps.squirreljme.jit.GlobalBinding;
+
+/**
+ * Global binding for MIPS based systems.
+ *
+ * @since 2017/02/20
+ */
+public class MIPSGlobalBinding
+	implements GlobalBinding
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/02/20
+	 */
+	@Override
+	public MIPSGlobalBinding copy()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/02/20
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/02/20
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new Error("TODO");
+	}
+}
+
