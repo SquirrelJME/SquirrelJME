22e2460286a22e4a6afc974763e2101bb5f6ab13
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-26 20:30:55 +0000

    Add RecordStoreInfo.

---
+++ apis/meep-rms/javax/microedition/rms/RecordStoreInfo.java
@@ -0,0 +1,102 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.rms;
+
+/**
+ * This stores information on a record store.
+ *
+ * @since 2017/02/26
+ */
+public final class RecordStoreInfo
+{
+	/**
+	 * Used internally.
+	 *
+	 * @since 2017/02/26
+	 */
+	RecordStoreInfo()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Gets the authorization mode of the associated {@link RecordStore}.
+	 *
+	 * @return The authorization mode, one of
+	 * {@link RecordStore#AUTHMODE_PRIVATE} or
+	 * {@link RecordStore#AUTHMODE_ANY}.
+	 * @throws RecordStoreNotOpenException If the record store is not open.
+	 * @since 2017/02/26
+	 */
+	public int getAuthMode()
+		throws RecordStoreNotOpenException
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the size of the record store including any overhead it may
+	 * have.
+	 *
+	 * @return The number of bytes the record store consumes, not to exceed
+	 * {@link Long#MAX_VALUE}.
+	 * @throws RecordStoreNotOpenException If the record store is not open.
+	 * @since 2017/02/26
+	 */
+	public long getSize()
+		throws RecordStoreNotOpenException
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns the number of bytes which may be available for storage. This
+	 * is only an estimate and the actual number may be higher or lower
+	 * depending on overhead and storage requirements.
+	 *
+	 * @return The estimated number of bytes available for storage, not to
+	 * exceed {@link Long#MAX_VALUE}.
+	 * @throws RecordStoreNotOpenException If the record store is not open.
+	 * @since 2017/02/26
+	 */
+	public long getSizeAvailable()
+		throws RecordStoreNotOpenException
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns {@code true} if the record store is encrypted.
+	 *
+	 * @return {@code true} if the record store is encrypted.
+	 * @throws RecordStoreNotOpenException If the record store is not open.
+	 * @since 2017/02/26
+	 */
+	public boolean isEncrypted()
+		throws RecordStoreNotOpenException
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * Returns {@code true} if the record store can be written to.
+	 *
+	 * @return {@code true} if the record store can be written to.
+	 * @throws RecordStoreNotOpenException If the record store is not open.
+	 * @since 2017/02/26
+	 */
+	public boolean isWriteable()
+		throws RecordStoreNotOpenException
+	{
+		throw new Error("TODO");
+	}
+}
+
