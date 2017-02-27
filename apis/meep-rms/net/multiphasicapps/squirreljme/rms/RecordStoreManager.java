dc5bc99c40f6adb220fd0677855277180b604778
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-02-27 14:07:13 +0000

    Add base for RecordStoreManager.

---
+++ apis/meep-rms/net/multiphasicapps/squirreljme/rms/RecordStoreManager.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.rms;
+
+/**
+ * This is the base class for a manager which provides access to a record
+ * store.
+ *
+ * @since 2017/02/27
+ */
+public abstract class RecordStoreManager
+{
+}
+
