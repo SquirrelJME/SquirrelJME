5a36be4ac7f6c0017d8a91055c8ab1e9b4a2ea18
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-03-05 02:10:25 +0000

    Basic reading of Zlib header blocks.

--- libs/zlib-decompression/net/multiphasicapps/io/zlibdecompression/ZLibDecompressor.java
+++ libs/zlib-decompression/net/multiphasicapps/io/zlibdecompression/ZLibDecompressor.java
@@ -10,6 +10,7 @@
 
 package net.multiphasicapps.io.zlibdecompression;
 
+import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import net.multiphasicapps.io.inflate.InflaterInputStream;
@@ -27,8 +28,28 @@ import net.multiphasicapps.io.inflate.InflaterInputStream;
 public class ZLibDecompressor
 	extends InputStream
 {
+	/** Compression method mask. */
+	private static final int _CMF_COMPRESSION_METHOD_MASK =
+		0b00001111;
+	
+	/** Compression info mask. */
+	private static final int _CMF_COMPRESSION_INFO_MASK =
+		0b11110000;
+	
+	/** Compression info shift. */
+	private static final int _CMF_COMPRESSION_INFO_SHIFT =
+		4;
+	
+	/** Deflate compression method. */
+	private static final int _CMF_METHOD_DEFLATE =
+		8;
+	
+	/** Is a preset dictionary being used? */
+	private static final int _FLAG_PRESET_DICTIONARY =
+		1 << 5;
+	
 	/** The source stream. */
-	protected final InputStream in;
+	protected final DataInputStream in;
 	
 	/** Single byte array to be shared for single reads. */
 	private final byte[] _solo =
@@ -55,7 +76,7 @@ public class ZLibDecompressor
 			throw new NullPointerException("NARG");
 		
 		// Set
-		this.in = __in;
+		this.in = new DataInputStream(__in);
 	}
 	
 	/**
@@ -141,10 +162,101 @@ public class ZLibDecompressor
 		// Try to fill the buffer up as much as possible
 		int rv = 0;
 		boolean eof = false;
+		DataInputStream in = this.in;
 		InputStream current = this._current;
-		while (rv < __l)
+		for (int at = __o, rest = __l; rv < __l;)
 		{
-			throw new todo.TODO();
+			// There is a current stream being read
+			if (current != null)
+			{
+				int rc = current.read(__b, at, rest);
+				
+				// If EOF is reached then the checksum must be checked
+				if (rc < 0)
+				{
+					// Read checksums
+					int want = in.readInt(),
+						was = 0;
+					if (true)
+						throw new todo.TODO();
+					
+					// {@squirreljme.error BT05 The checksum for the ZLib
+					// stream is not valid. (The desired checksum; The actual
+					// checksum)}
+					if (want != was)
+						throw new IOException(String.format("BT05 %08x %08x",
+							want, was));
+					
+					// Clear current because no more data can be read from
+					// it
+					current = null;
+					this._current = null;
+				}
+				
+				// Otherwise consume those bytes
+				else
+				{
+					at += rc;
+					rest -= rc;
+					rv += rc;
+				}
+			}
+			
+			// Otherwise try to see if there is data to be read
+			else
+			{
+				// Compression method and flags
+				int cmf = in.read();
+				
+				// The end of stream could be at this point at which point it
+				// is acceptable to stop reading data
+				if (cmf < 0)
+				{
+					eof = true;
+					this._eof = true;
+					break;
+				}
+				
+				// {@squirreljme.error BT02 Only deflate compressed ZLib
+				// streams are supported. (The compression method used)}
+				int method = (cmf & _CMF_COMPRESSION_METHOD_MASK);
+				if (_CMF_METHOD_DEFLATE != method)
+					throw new IOException(String.format("BT02 %d", method));
+				
+				// {@squirreljme.error BT03 The specified binary logarithm
+				// specified for the sliding window is not valid. (The binary
+				// logarithm of the sliding window)}
+				// The specification says that higher sliding windows are not
+				// allowed, but skirt that requirement
+				int slwin = ((cmf & _CMF_COMPRESSION_INFO_MASK) >>>
+					_CMF_COMPRESSION_INFO_SHIFT) + 8;
+				if (slwin < 0 || slwin > 30)
+					throw new IOException(String.format("BT03 %d", slwin));
+				
+				// Shift up
+				slwin = 1 << slwin;
+				
+				// Read more flags
+				int mf = in.readUnsignedByte();
+				
+				// {@squirreljme.error BT04 The checksum for the starting
+				// ZLib header is not a multiple of 31. (The checksum
+				// remainder)}
+				// This is a basic check to ensure that in most cases that the
+				// header of the ZLib chunk is not corrupt.
+				int was = ((cmf * 256) + mf) % 31;
+				if (was != 0)
+					throw new IOException(String.format("BT04 %d", was));
+				
+				// {@squirreljme.error BT01 Preset dictionaries in ZLib
+				// streams are not supported.}
+				if ((mf & _FLAG_PRESET_DICTIONARY) != 0)
+					throw new IOException("BT01");
+				
+				// Setup inflate stream
+				current = new InflaterInputStream(in, slwin);
+				this._current = current;
+			}
 		}
 		
 		// Return EOF or the read count
