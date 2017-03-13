8b46de8df9f082f69fd64999ccc9af70649039e0
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-03-13 20:10:15 +0000

    Add webdemo HTML template page.

---
+++ libs/squirreljme-jit-webdemo/net/multiphasicapps/squirreljme/jit/webdemo/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the JIT for the web demo.
+ *
+ * @since 2017/03/13
+ */
+
+package net.multiphasicapps.squirreljme.jit.webdemo;
+
