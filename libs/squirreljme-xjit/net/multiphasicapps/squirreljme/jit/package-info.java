1b5c46ca1b141eac43389857530b61d9c44584cf
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-04-02 12:57:12 +0000

    Add base for the new JIT package, since it would be easier to write the new code there instead of mixing it with the old code.

---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/package-info.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the JIT for SquirrelJME. It handles parsing of the
+ * class file format and translating byte code instructions into native machine
+ * code.
+ *
+ * @since 2017/04/02
+ */
+
+package net.multiphasicapps.squirreljme.jit;
+
