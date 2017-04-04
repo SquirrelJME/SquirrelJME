e51c3e25c43df6e581489447cf6c89d9d3607136
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-04-04 20:54:03 +0000

    Base for exported fields and members.

---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/ExportedField.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This contains a field that is exported by a class.
+ *
+ * @since 2017/04/04
+ */
+public class ExportedField
+	extends ExportedMember
+{
+}
+
---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/ExportedMember.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+import net.multiphasicapps.squirreljme.linkage.Export;
+
+/**
+ * This represents a member that is exported.
+ *
+ * @since 2017/04/04
+ */
+public abstract class ExportedMember
+	implements Export
+{
+}
+
