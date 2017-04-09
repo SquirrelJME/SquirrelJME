043b2e8d138e3dbaee857fcf15a0e43405cb1789
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-04-09 23:15:19 +0000

    Base class for handling the code attribute for methods.

---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/__Code__.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This handles the code attribute for a method.
+ *
+ * @since 2017/04/09
+ */
+class __Code__
+{
+}
+
