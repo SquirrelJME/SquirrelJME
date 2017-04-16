8efcea0cf478cf3d7cbffcafbc1c817f3ec6bfd7
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-04-16 15:42:44 +0000

    Add base for __RegisterCounts__ to use the least used register set.

---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/__RegisterCounts__.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This keeps track of how many times a register has been requested for
+ * allocation. This is used as a heuristic to choose the least active
+ * registers when choosing registers for allocation.
+ *
+ * @since 2017/04/16
+ */
+class __RegisterCounts__
+{
+}
+
