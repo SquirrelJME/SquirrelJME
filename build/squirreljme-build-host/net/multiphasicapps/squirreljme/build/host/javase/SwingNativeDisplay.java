97401f4ddff10647eca5917a34e6a7a236044157
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-05-23 21:38:48 +0000

    Add SwingNativeDisplay used to wrap Swing with the LCDUI system.

---
+++ build/squirreljme-build-host/net/multiphasicapps/squirreljme/build/host/javase/SwingNativeDisplay.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.build.host.javase;
+
+import net.multiphasicapps.squirreljme.lcdui.NativeDisplay;
+
+/**
+ * This is a display which implements the native display interface on top of
+ * Java's Swing interface.
+ *
+ * @since 2017/05/23
+ */
+public class SwingNativeDisplay
+	extends NativeDisplay
+{
+}
+
