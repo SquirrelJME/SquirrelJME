60eddb637dcd9e606f9decd0c550671c3fadd191
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-05-26 01:47:39 +0000

    Add base for VariableState family of classes.

---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/ImmutableVariableState.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This is an immutable variable state which contains one which never changes
+ * once it is created.
+ *
+ * @since 2017/05/25
+ */
+public final class ImmutableVariableState
+	extends VariableState
+{
+}
+
---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/VariableState.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This is the base class which is used to represent the state of variables
+ * within a basic block.
+ *
+ * @see ImmutableVariableState
+ * @see MutableVariableState
+ * @since 2017/05/25
+ */
+public abstract class VariableState
+{
+}
+
