c2ee2509d2ef7b0fac8c0db53034819395d3a7b0
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-05-29 18:29:44 +0000

    Add CompiledClass class which will contain the final output for a class.

---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/CompiledClass.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This represents a single compiled class which contains fields and methods
+ * for exporting to native executables or to active memory. This is the end
+ * result of all JIT operations for a single class.
+ *
+ * @since 2017/05/29
+ */
+public class CompiledClass
+{
+}
+
