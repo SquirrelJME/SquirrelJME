b4b501abde4445da514a07c47a52ee349505acb1
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-05-29 19:41:32 +0000

    Move and make NativeType an interface so that what is represents is machine specific.

---
+++ libs/squirreljme-xjit/net/multiphasicapps/squirreljme/jit/target/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the target JIT and anything that is needed to support
+ * compilation to targets.
+ *
+ * @since 2017/05/29
+ */
+
+package net.multiphasicapps.squirreljme.jit.target;
+
