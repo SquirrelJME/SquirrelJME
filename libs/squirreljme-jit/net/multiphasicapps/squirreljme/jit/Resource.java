c5dba25c7d99fb713defea04ea9530a05261fad4
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-06-08 15:46:31 +0000

    Storing of resource data.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/Resource.java
@@ -0,0 +1,57 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit;
+
+/**
+ * This contains the data for a single resource.
+ *
+ * @since 2017/06/08
+ */
+public final class Resource
+{
+	/** The name of the resource. */
+	protected final String name;
+	
+	/** The resource data. */
+	private final byte[] _data;
+	
+	/**
+	 * Initializes the resource.
+	 *
+	 * @param __n The name of the resource.
+	 * @param __d The resource data, this is used directly and is not copied.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/06/08
+	 */
+	Resource(String __n, byte[] __d)
+		throws NullPointerException
+	{
+		// Check
+		if (__n == null || __d == null)
+			throw new NullPointerException("NARG");
+		
+		// Set
+		this.name = __n;
+		this._data = __d;
+	}
+	
+	/**
+	 * Returns the name of the resource.
+	 *
+	 * @return The resource name.
+	 * @since 2017/06/08
+	 */
+	public final String name()
+	{
+		return this.name();
+	}
+}
+
--- libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/ResourceCluster.java
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/ResourceCluster.java
@@ -10,8 +10,11 @@
 
 package net.multiphasicapps.squirreljme.jit;
 
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.IOException;
+import java.util.Map;
+import net.multiphasicapps.util.sorted.SortedTreeMap;
 
 /**
  * This class is associated with a link table and specifies resources which
@@ -23,6 +26,10 @@ public final class ResourceCluster
 {
 	/** The identifier for this cluster. */
 	protected final ClusterIdentifier id;
+	
+	/** Resources which exist within this cluster. */
+	private final Map<String, Resource> _resources =
+		new SortedTreeMap<>();
 	
 	/**
 	 * Initializes the resource cluster.
@@ -69,7 +76,100 @@ public final class ResourceCluster
 		if (__n == null)
 			throw new NullPointerException("NARG");
 		
-		throw new todo.TODO();
+		return new __Output__(__n);
+	}
+	
+	/**
+	 * This is used to write 
+	 *
+	 * @since 2017/05/08
+	 */
+	private final class __Output__
+		extends OutputStream
+	{
+		/** The output byte array. */
+		protected final ByteArrayOutputStream out =
+			new ByteArrayOutputStream();
+		
+		/** The name of the resource. */
+		protected final String name;
+		
+		/** Has the resource been added? */
+		private volatile boolean _added;
+		
+		/**
+		 * Initializes the resource output.
+		 *
+		 * @param __n The name of the resource to add.
+		 * @throws NullPointerException On null arguments.
+		 * @since 2017/06/08
+		 */
+		private __Output__(String __n)
+			throws NullPointerException
+		{
+			// Check
+			if (__n == null)
+				throw new NullPointerException("NARG");
+			
+			// Set
+			this.name = __n;
+		}
+		
+		/**
+		 * {@inheritDoc}
+		 * @since 2017/06/08
+		 */
+		@Override
+		public void close()
+			throws IOException
+		{
+			// Only add resources once
+			if (this._added)
+				return;
+			this._added = true;
+			
+			// {@squirreljme.error JI05 Cannot add the specified resource
+			// because it already exists in the cluster. (The resource name)}
+			Map<String, Resource> resources = ResourceCluster.this._resources;
+			String name = this.name;
+			if (null != resources.get(name))
+				throw new IOException(String.format("JI05 %s", name));
+			
+			// Store it
+			resources.put(name, new Resource(name, this.out.toByteArray()));
+		}
+		
+		/**
+		 * {@inheritDoc}
+		 * @since 2017/06/08
+		 */
+		@Override
+		public void write(int __b)
+			throws IOException
+		{
+			// {@squirreljme.error JI03 Cannot write single byte to the resource
+			// because it has already been added.}
+			if (this._added)
+				throw new IOException("JI03");
+			
+			this.out.write(__b);
+		}
+		
+		/**
+		 * {@inheritDoc}
+		 * @since 2017/06/08
+		 */
+		@Override
+		public void write(byte[] __b, int __o, int __l)
+			throws IOException
+		{
+			// {@squirreljme.error JI04 Cannot write multiple bytes to the
+			// resource because it has already been added.}
+			if (this._added)
+				throw new IOException("JI04");
+			
+			this.out.write(__b, __o, __l);
+		}
 	}
 }
 
