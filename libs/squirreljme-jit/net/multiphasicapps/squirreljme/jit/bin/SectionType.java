99430a078d6e632e961338d6f30f701fff2a844f
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-06-27 22:21:38 +0000

    SectionBuilder is FragmentBuilder; Add base for Fragment which is contained by Section; Base for building fragments.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/SectionType.java
@@ -0,0 +1,29 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.bin;
+
+/**
+ * This represents the type of sections which exist.
+ *
+ * @since 2017/06/27
+ */
+public enum SectionType
+{
+	/** The text section. */
+	TEXT,
+	
+	/** The data section. */
+	DATA,
+	
+	/** End. */
+	;
+}
+
