8ecc2df9cb5320fae8a2bf5eecdc1ebfe6cfdccd
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-06-27 19:13:01 +0000

    Copy bytes from resources to the section builder.

--- libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/SectionBuilder.java
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/SectionBuilder.java
@@ -37,5 +37,39 @@ public class SectionBuilder
 	{
 		super(__ls);
 	}
+	
+	/**
+	 * Appends a single non-dynamic byte to the output section.
+	 *
+	 * @param __v The byte to add.
+	 * @since 2017/06/27
+	 */
+	public void append(byte __v)
+	{
+		this.bytes.addLast(__v);
+	}
+	
+	/**
+	 * Appends multiple non-dynamic bytes to the output section.
+	 *
+	 * @param __v The bytes to add.
+	 * @param __o The offset into the array.
+	 * @param __l The number of bytes to add.
+	 * @throws IndexOutOfBoundsException If the offset and/or length are
+	 * negative or exceeds the array bounds.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/06/27
+	 */
+	public void append(byte[] __v, int __o, int __l)
+		throws IndexOutOfBoundsException, NullPointerException
+	{
+		// Check
+		if (__v == null)
+			throw new NullPointerException("NARG");
+		if (__o < 0 || __l < 0 || (__o + __l) > __v.length)
+			throw new IndexOutOfBoundsException("IOOB");
+		
+		this.bytes.addLast(__v, __o, __l);
+	}
 }
 
