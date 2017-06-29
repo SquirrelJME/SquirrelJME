b2f0035dff76d57a6511e0891da11b04c999691b
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-06-29 00:50:21 +0000

    Add base for a SectionCounter which will be used to determine the section text/data goes in (needed by Palm OS).

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/FlatSectionCounter.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.bin;
+
+/**
+ * This is a section counter which only uses a single indexed text and data
+ * section.
+ *
+ * @since 2017/06/28
+ */
+public class FlatSectionCounter
+	implements SectionCounter
+{
+}
+
