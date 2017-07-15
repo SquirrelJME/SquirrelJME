a645d236257d1cc40d58b0db0bc73c37ce6d46b8
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-07-15 15:49:31 +0000

    Add base for StackMapTable class.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/java/StackMapTable.java
@@ -0,0 +1,27 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.java;
+
+/**
+ * This is the stack map table which is used to verify the types of local and
+ * stack variables along with the stack length are valid for certain
+ * positions within the instruction.
+ *
+ * This just contains the type and potential initialization information for
+ * instructions which are targets of jumps.
+ *
+ * @since 2017/07/15
+ */
+public final class StackMapTable
+{
+	
+}
+
