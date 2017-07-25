1be1f9070f60a9fd9053ca699b0b2ea6f4e05c8a
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-07-25 02:45:59 +0000

    Add base for ExtraMath which will contain some functions such as logarithms.

---
+++ libs/math/net/multiphasicapps/math/ExtraMath.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.math;
+
+/**
+ * This class contains the extra math library and contains functions which
+ * should be in {@link Math} but are not there.
+ *
+ * @since 2017/07/24
+ */
+public class ExtraMath
+{
+}
+
