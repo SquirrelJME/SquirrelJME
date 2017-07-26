6b2b86fb0c185a71f4c67aa4ca5d85df058de8b0
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-07-26 15:03:46 +0000

    Add base for StackMapType.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/java/StackMapType.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.java;
+
+/**
+ * This represents a type within the stack map table which maps to a type used
+ * within the running virtual machine. Types in the stack map will usually be
+ * initialized but they can be uninitialized in the event of {@code new}
+ * operations being performed.
+ *
+ * @since 2017/07/26
+ */
+public final class StackMapType
+{
+}
+
