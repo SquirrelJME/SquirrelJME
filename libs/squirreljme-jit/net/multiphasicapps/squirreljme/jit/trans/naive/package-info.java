ff918cf8d25519c089c5416d768e09bc36d25f1c
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-01 19:13:23 +0000

    Add base for other packages; Allow instruction lookup by index in the byte code rather than just by address.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/trans/naive/package-info.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Steven Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the naive translator which operates in a very
+ * simplistic fashion, it does not perform any operations and for the most
+ * part translates the expanded instruction set virtually as is.
+ *
+ * @since 2017/08/01
+ */
+
+package net.multiphasicapps.squirreljme.jit.trans.naive;
+
