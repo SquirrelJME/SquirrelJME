65b2ced8368b2626370992de39af6644cee21e5b
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-01 22:28:24 +0000

    Some script adjustments.

--- utils-dev/__xertostephanieblog.sh
+++ utils-dev/__xertostephanieblog.sh
@@ -7,7 +7,7 @@
 # SquirrelJME is under the GNU General Public License v3+, or later.
 # See license.mkd for licensing and copyright information.
 # ---------------------------------------------------------------------------
-# DESCRIPTION: Maps author to a name.
+# DESCRIPTION: Convert Steven to Xer in the blog.
 
 # Force C locale
 export LC_ALL=C
@@ -15,20 +15,30 @@ export LC_ALL=C
 # Directory of this script
 __exedir="$(dirname -- "$0")"
 
-case "$1" in
-	squirreljme)
-		echo "SquirrelJME";
-		;;
-	
-	steven.gawroriski)		echo "Stephanie Gawroriski" ;;
-	steven-gawroriski)		echo "Stephanie Gawroriski" ;;
-	stephanie.gawroriski)	echo "Stephanie Gawroriski" ;;
-	stephanie-gawroriski)	echo "Stephanie Gawroriski" ;;
-	xer)					echo "Stephanie Gawroriski" ;;
-		
-	*)
-		echo "Unknown";
-		;;
-esac
+# LS locks the database sadly
+fossil uv ls > /tmp/$$.ls
 
+# Go through all UV entries
+cat /tmp/$$.ls | while read __line
+do
+	# Only accept notes from old name
+	if ! echo "$__line" | grep '^developer-notes\/xer\/' > /dev/null
+	then
+		continue
+	fi
+	
+	# New path
+	__new="$(echo "$__line" | sed 's/xer/stephanie-gawroriski/g')"
+	
+	echo "## $__line -> $__new"
+	
+	# Extract it, copy, then remove it
+	if fossil uv cat "$__line" > /tmp/$$
+	then
+		fossil uv add /tmp/$$ --as "$__new"
+		fossil uv rm "$__line"
+	fi
+done
+
+rm -f /tmp/$$ /tmp/$$.ls
 
