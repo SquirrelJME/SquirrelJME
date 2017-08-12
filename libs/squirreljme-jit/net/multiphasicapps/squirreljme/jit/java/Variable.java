c0d1ec23fa948c0d9b537e2b790dacba243aa8f5
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-12 13:21:17 +0000

    Add base for Variable and VariableState.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/java/Variable.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.java;
+
+/**
+ * This represents a variable which may in locals or on the stack.
+ *
+ * @since 2017/08/12
+ */
+public final class Variable
+{
+}
+
