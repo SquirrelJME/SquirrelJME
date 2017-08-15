1a2cc76c1749f63353b8737ae93da6f222231ebf
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-15 04:21:54 +0000

    Add methods for appending primitive types.

--- libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/FragmentBuilder.java
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/FragmentBuilder.java
@@ -29,6 +29,10 @@ public class FragmentBuilder
 	private final ByteDeque _bytes =
 		new ByteDeque();
 	
+	/** Buffer for multi-byte data. */
+	private final byte[] _buf =
+		new byte[8];
+	
 	/** Has the fragment been finished? */
 	private volatile boolean _finished;
 	
@@ -88,6 +92,88 @@ public class FragmentBuilder
 			throw new IllegalStateException("JI0y");
 		
 		this._bytes.addLast(__v, __o, __l);
+	}
+	
+	/**
+	 * Appends a double to the fragment.
+	 *
+	 * @param __v The value to append.
+	 * @throws IllegalStateException If the fragment builder is finished.
+	 * @since 2017/09/15
+	 */
+	public final void appendDouble(double __v)
+		throws IllegalStateException
+	{
+		appendLong(Double.doubleToRawLongBits(__v));
+	}
+	
+	/**
+	 * Appends a float to the fragment.
+	 *
+	 * @param __v The value to append.
+	 * @throws IllegalStateException If the fragment builder is finished.
+	 * @since 2017/09/15
+	 */
+	public final void appendFloat(float __v)
+		throws IllegalStateException
+	{
+		appendInteger(Float.floatToRawIntBits(__v));
+	}
+	
+	/**
+	 * Appends an integer to the fragment.
+	 *
+	 * @param __v The value to append.
+	 * @throws IllegalStateException If the fragment builder is finished.
+	 * @since 2017/09/15
+	 */
+	public final void appendInteger(int __v)
+		throws IllegalStateException
+	{
+		byte[] buf = this._buf;
+		buf[0] = (byte)(__v >>> 24);
+		buf[1] = (byte)(__v >>> 16);
+		buf[2] = (byte)(__v >>> 8);
+		buf[3] = (byte)(__v);
+		append(buf, 0, 4);
+	}
+	
+	/**
+	 * Appends a long to the fragment.
+	 *
+	 * @param __v The value to append.
+	 * @throws IllegalStateException If the fragment builder is finished.
+	 * @since 2017/09/15
+	 */
+	public final void appendLong(long __v)
+		throws IllegalStateException
+	{
+		byte[] buf = this._buf;
+		buf[0] = (byte)(__v >>> 56);
+		buf[1] = (byte)(__v >>> 48);
+		buf[2] = (byte)(__v >>> 40);
+		buf[3] = (byte)(__v >>> 32);
+		buf[4] = (byte)(__v >>> 24);
+		buf[5] = (byte)(__v >>> 16);
+		buf[6] = (byte)(__v >>> 8);
+		buf[7] = (byte)(__v);
+		append(buf, 0, 8);
+	}
+	
+	/**
+	 * Appends a short to the fragment.
+	 *
+	 * @param __v The value to append.
+	 * @throws IllegalStateException If the fragment builder is finished.
+	 * @since 2017/09/15
+	 */
+	public final void appendShort(short __v)
+		throws IllegalStateException
+	{
+		byte[] buf = this._buf;
+		buf[0] = (byte)(__v >>> 8);
+		buf[1] = (byte)(__v);
+		append(buf, 0, 2);
 	}
 	
 	/**
