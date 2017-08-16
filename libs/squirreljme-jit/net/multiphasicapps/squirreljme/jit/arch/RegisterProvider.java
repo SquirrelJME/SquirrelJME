196c3af3e8998bf07aaad6cc7d1ed4dca367994a
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-16 11:46:15 +0000

    Add a RegisterProvider which will be used to give access to registers.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/arch/RegisterProvider.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.arch;
+
+/**
+ * This class is used to provide access to the native registers used for the
+ * native machine target.
+ *
+ * @since 2017/08/16
+ */
+public abstract class RegisterProvider
+{
+}
+
