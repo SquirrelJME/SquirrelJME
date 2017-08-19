db83fd2032248ff68d84b7e21c4f08a53aa60723
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-19 23:52:17 +0000

    Move graphics related classes to a new sub-package.

--- apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/gfx/BasicGraphics.java
+++ apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/gfx/BasicGraphics.java
@@ -8,7 +8,7 @@
 // See license.mkd for licensing and copyright information.
 // ---------------------------------------------------------------------------
 
-package net.multiphasicapps.squirreljme.lcdui;
+package net.multiphasicapps.squirreljme.lcdui.gfx;
 
 import javax.microedition.lcdui.Font;
 import javax.microedition.lcdui.Graphics;
