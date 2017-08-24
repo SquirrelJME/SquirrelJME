38a3a159712b17ab60b3d388812b5e3a3f8156d4
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-24 01:05:16 +0000

    Add base for the verifier package.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/verifier/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the verifier which ensures that the class files are
+ * correct and are valid within the Java specification.
+ *
+ * @since 2017/08/23
+ */
+
+package net.multiphasicapps.squirreljme.jit.verifier;
+
