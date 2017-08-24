f8be9cd84c8a2b9b1115a3e2e402d28ec2d3b4af
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-24 05:20:32 +0000

    Add base for new binary interface.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/TemporaryBinary.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.bin;
+
+/**
+ * This class represents a temporary binary which is used to contain the
+ * compiled information that is to be placed in the text and data sections
+ * for a given executable.
+ *
+ * @since 2017/08/24
+ */
+public class TemporaryBinary
+{
+}
+
