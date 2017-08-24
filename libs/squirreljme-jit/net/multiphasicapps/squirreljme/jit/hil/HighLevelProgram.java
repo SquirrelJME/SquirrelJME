69064fedd68bdcd72350308b5985cce9e638a2ef
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-24 05:51:30 +0000

    Add base for high level program.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/hil/HighLevelProgram.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.hil;
+
+/**
+ * This class represents a high level program which has been generated from
+ * the Java byte code.
+ *
+ * @since 2017/08/24
+ */
+public class HighLevelProgram
+{
+}
+
