2ed8843e6bb51973cef4dbbe29444c5f4d6349da
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-24 05:22:30 +0000

    Add some more temporary binary support classes.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/TemporaryFragment.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.bin;
+
+/**
+ * This represents a single fragment within a section which is used to
+ * store compiled machine code or data temporarily before the binary is fully
+ * formed.
+ *
+ * @see TemporarySection
+ * @since 2017/08/24
+ */
+public class TemporaryFragment
+{
+}
+
