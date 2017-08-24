72cc3f1407c237437741274f5a9cc158f1cd2064
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-24 20:08:51 +0000

    Add symbol storage package and class.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/symbols/ClassStructure.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.symbols;
+
+/**
+ * The contains information on the structure of the class.
+ *
+ * @since 2017/08/24
+ */
+public class ClassStructure
+{
+}
+
