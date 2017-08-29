b4c542fc2795436c3ef130408f813ac0c1437d89
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-08-29 21:32:52 +0000

    Add base for resource linking points.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bin/ResourceLinkingPoint.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.bin;
+
+/**
+ * This is a linking point which represents a single resource that exists
+ * within the virtual machine.
+ *
+ * @since 2017/08/29
+ */
+public final class ResourceLinkingPoint
+	implements LinkingPoint
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/08/29
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/08/29
+	 */
+	@Override
+	public final int hashCode()
+	{
+		throw new todo.TODO();
+	}
+}
+
