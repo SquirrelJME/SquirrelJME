bb5c08015d00c59842f47ebbc6ad93a717ccc018
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-09-04 16:36:56 +0000

    Add base for java-source-structure project which is used to parse Java source code files.

---
+++ libs/java-source-structure/net/multiphasicapps/javac/struct/TokenType.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.struct;
+
+/**
+ * This represents the type of token which was parsed.
+ *
+ * @since 2017/09/04
+ */
+public enum TokenType
+{
+	/** End. */
+	;
+}
+
