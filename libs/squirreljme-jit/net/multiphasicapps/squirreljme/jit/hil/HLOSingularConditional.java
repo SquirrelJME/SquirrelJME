08acd55cec20e405fba0b8c6925d640e6148e822
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-09-22 17:09:17 +0000

    Add HLOJumpOnNotNull; Add some HLO interfaces.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/hil/HLOSingularConditional.java
@@ -0,0 +1,32 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.hil;
+
+import net.multiphasicapps.squirreljme.jit.java.TypedVariable;
+
+/**
+ * This describes an operation which uses a single variable for its
+ * condition check.
+ *
+ * @since 2017/09/22
+ */
+public interface HLOSingularConditional
+	extends HLO
+{
+	/**
+	 * The variable to check for the condition.
+	 *
+	 * @return The variable to be checked.
+	 * @since 2017/09/22
+	 */
+	public abstract TypedVariable variable();
+}
+
