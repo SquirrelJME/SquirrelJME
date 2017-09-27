6e32a238ae7b0410368712f1b8609539b4d0ca92
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-09-27 13:17:47 +0000

    Add base for groups, resources, and class names.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/cff/ClassName.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.cff;
+
+/**
+ * This represents the name of a class or array within the virtual machine.
+ *
+ * This class is immutable.
+ *
+ * @since 2017/09/27
+ */
+public class ClassName
+	implements Comparable<ClassName>
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/09/27
+	 */
+	@Override
+	public int compareTo(ClassName __b)
+	{
+		throw new todo.TODO();
+	}
+}
+
