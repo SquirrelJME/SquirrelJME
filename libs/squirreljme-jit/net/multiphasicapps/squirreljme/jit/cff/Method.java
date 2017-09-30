ecc191c6372afc566dd6dc986adc207f8d9895c8
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-09-30 13:45:35 +0000

    Add base for decoding members within classes.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/cff/Method.java
@@ -0,0 +1,48 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.cff;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ * This represents a method which is used to execute byte code.
+ *
+ * @since 2017/09/30
+ */
+public final class Method
+	extends Member
+{
+	/**
+	 * Decodes all methods from the input class data.
+	 *
+	 * @param __ver The version of the class.
+	 * @param __tn The name of the owning class.
+	 * @param __cf The flags for the owning class.
+	 * @param __pool The constant pool for the class.
+	 * @throws InvalidClassFormatException If the class is not formatted
+	 * correctly.
+	 * @throws IOException On read errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/09/30
+	 */
+	public static Method[] decode(ClassVersion __ver, ClassName __tn,
+		ClassFlags __cf, Pool __pool, DataInputStream __in)
+		throws InvalidClassFormatException, IOException, NullPointerException
+	{
+		if (__ver == null || __tn == null || __cf == null || __pool == null ||
+			__in == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+}
+
