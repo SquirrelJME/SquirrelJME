e1e9bbaf039e459bfeb79e910892c4c505b7ae43
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-01 14:04:07 +0000

    Base for hardware state, if it is disabled/absent then force in the background.

--- apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/DisplayHead.java
+++ apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/DisplayHead.java
@@ -18,6 +18,27 @@ package net.multiphasicapps.squirreljme.lcdui;
  */
 public abstract class DisplayHead
 {
+	/** The current hardware state of this head. */
+	private volatile DisplayHardwareState _hwstate;
+	
+	/**
+	 * Returns the current hardware state.
+	 *
+	 * @return The hardware state.
+	 * @throws RuntimeException If the hardware state was not set.
+	 * @since 2017/10/01
+	 */
+	public final DisplayHardwareState getHardwareState()
+		throws RuntimeException
+	{
+		// {@squirreljme.error EB1n The hardware state has not been set by
+		// the display driver.}
+		DisplayHardwareState rv = this._hwstate;
+		if (rv == null)
+			throw new RuntimeException("EB1n");
+		return rv;
+	}
+	
 	/**
 	 * Sets the current state of the display.
 	 *
@@ -31,17 +52,47 @@ public abstract class DisplayHead
 		if (__ds == null)
 			throw new NullPointerException("NARG");
 		
-		// If there is no change in state, then do nothing
-		if (true)
-			throw new todo.TODO();
+		// Modifies global state
+		synchronized (DisplayManager.GLOBAL_LOCK)
+		{
+			// If the hardware state is in disabled mode, then force the
+			// display to be disabled
+			DisplayHardwareState hws = getHardwareState();
+			if (hws.forceDisabled())
+				__ds = DisplayState.BACKGROUND;
+			
+			// If there is no change in state, then do nothing
+			if (true)
+				throw new todo.TODO();
 		
-		// If the hardware state of this display is in the background or
-		// disabled then the only valid display state is in the background
-		// and it will never be the foreground
-		if (true)
-			throw new todo.TODO();
+			// If the hardware state of this display is in the background or
+			// disabled then the only valid display state is in the background
+			// and it will never be the foreground
+			if (true)
+				throw new todo.TODO();
 		
-		throw new todo.TODO();
-	} 
+			throw new todo.TODO();
+		}
+	}
+	
+	/**
+	 * Sets the hardware state.
+	 *
+	 * @param __s The hardware state to set.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/10/01
+	 */
+	protected final void setHardwareState(DisplayHardwareState __s)
+		throws NullPointerException
+	{
+		if (__s == null)
+			throw new NullPointerException("NARG");
+		
+		// Modifies global state
+		synchronized (DisplayManager.GLOBAL_LOCK)
+		{
+			throw new todo.TODO();
+		}
+	}
 }
 
