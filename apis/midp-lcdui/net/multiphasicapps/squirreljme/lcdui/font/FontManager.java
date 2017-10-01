6f3525cf1bd7b4415d0310b2e2ec364c7aa81811
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-01 18:10:17 +0000

    Add base for font package and the FontManager.

---
+++ apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/font/FontManager.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.lcdui.font;
+
+/**
+ * This class is used as a service and provides management for fonts to be
+ * provided to the LCDUI interface.
+ *
+ * @since 2017/10/01
+ */
+public abstract class FontManager
+{
+}
+
