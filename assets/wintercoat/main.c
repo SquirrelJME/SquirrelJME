33e03d84d4e417d55579ec030c17ffd854fb7b23
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-05 20:17:42 +0000

    Base wintercoat main entry point.

---
+++ assets/wintercoat/main.c
@@ -0,0 +1,21 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// --------------------------------------------------------------------------*/
+
+/**
+ * The main entry point for Wintercoat.
+ *
+ * @since 2017/10/05
+ */
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
+
