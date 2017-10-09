175bf887ec381bacbec95aa6827fe2884ca0d025
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-09 13:14:25 +0000

    Classes cannot extend final and interface classes.

--- libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/verifier/__NodeBuilder__.java
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/verifier/__NodeBuilder__.java
@@ -30,6 +30,9 @@ final class __NodeBuilder__
 	/** Reference to the owning tree. */
 	private final Reference<__TreeBuilder__> _treeref;
 	
+	/** Has this finished the inheritence stage? */
+	private volatile boolean _didinherits;
+	
 	/**
 	 * Initializes the node builder for the tree data.
 	 *
@@ -37,11 +40,12 @@ final class __NodeBuilder__
 	 * @param __m The map of nodes to be pre-placed into.
 	 * @param __f The class to verify.
 	 * @throws NullPointerException On null arguments.
+	 * @throws VerificationException If the class could not be verified.
 	 * @since 2017/10/09
 	 */
 	__NodeBuilder__(Reference<__TreeBuilder__> __tr,
 		Map<ClassName, __NodeBuilder__> __m, ClassFile __f)
-		throws NullPointerException
+		throws NullPointerException, VerificationException
 	{
 		if (__tr == null || __m == null || __f == null)
 			throw new NullPointerException("NARG");
@@ -63,7 +67,41 @@ final class __NodeBuilder__
 		// Will need to work with the owning tree
 		__TreeBuilder__ tree = __treeBuilder();
 		
+		// Check to make sure the super class is valid and has no
+		// circular inheritence to it
+		ClassName supername = __f.superName();
+		__NodeBuilder__ supernode;
+		if (supername != null)
+		{
+			supernode = tree.get(supername);
+			
+			// {@squirreljme.error JI37 The specified class cannot extend the
+			// other class because it has the incorrect flags. (The name of
+			// this class; The name of the super class; The super class flags)}
+			ClassFlags superflags = supernode.flags();
+			if (superflags.isFinal() || superflags.isInterface())
+				throw new VerificationException(String.format("JI37 %s %s %s",
+					thisname, supername, superflags));
+			
+			throw new todo.TODO();
+		}
+		
+		// No super node, set this because it is used later to determine the
+		// method lookup
+		else
+			supernode = null;
+		
+		if (true)
+			throw new todo.TODO();
+		
+		// The initial inheritence stage has been completed for this node
+		// This flag should never normally be read by the inheritence check
+		// because if it does, it means that there is a circular reference
+		// for classes
+		this._didinherits = true;
+		
 		throw new todo.TODO();
+		
 	}
 	
 	/**
