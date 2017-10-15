a5584c5085a10285e530358c77b2ddad83d110c1
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-15 18:46:06 +0000

    Add base for an instruction parser.

---
+++ libs/squirreljme-jit/net/multiphasicapps/squirreljme/jit/bvm/InstructionParser.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.jit.bvm;
+
+/**
+ * This class is used to parse instructions for individual methods within the
+ * virtual machine. Each parser remembers its own state as to what is occurring
+ * within the virtual machine.
+ *
+ * @since 2017/10/15
+ */
+public class InstructionParser
+{
+}
+
