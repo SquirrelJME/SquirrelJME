767be50491dbbfe2d1092ad5b9863b15dc513ba9
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-21 00:25:23 +0000

    Add base for a font manager; Also a built-in monospace font will be provided initially for fallback usage.

---
+++ apis/midp-lcdui/net/multiphasicapps/squirreljme/lcdui/font/DefaultFontManager.java
@@ -0,0 +1,40 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.lcdui.font;
+
+import javax.microedition.lcdui.Font;
+
+/**
+ * This is a font manager which acts as a provider for a default set of fonts.
+ * The default fonts are quite primitive and are mostly intended to be used
+ * as a fallback when no native fonts are provided.
+ *
+ * @since 2017/10/20
+ */
+public class DefaultFontManager
+	extends FontManager
+{
+	/** Instance of the built-in monospace font. */
+	protected final BuiltInMonospaceFont monospace =
+		new BuiltInMonospaceFont();
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/10/20
+	 */
+	@Override
+	public PrimitiveFont getPrimitiveFont(int __face)
+	{
+		// Always just return the monospace font
+		return this.monospace;
+	}
+}
+
