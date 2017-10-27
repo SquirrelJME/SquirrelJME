717b78b7bdcb369d0a071ae6395e9d93b6e3dcab
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-27 02:42:54 +0000

    Add base for a script which will run through a directory to reorder error codes so that they are cleaned.

---
+++ utils-dev/reordererrors.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+#     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3+, or later.
+# See license.mkd for licensing and copyright information.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: This runs the error reordering script.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Need to compile the program
+__cls="ReorderErrors"
+__src="$__exedir/$__cls.java"
+__odr="/tmp"
+__out="$__odr/$__cls.class"
+
+# Need to compile?
+if [ ! -f "$__out" ] || [ "$__src" -nt "$__out" ]
+then
+	if ! javac -d "$__odr" "$__src"
+	then
+		echo "Failed to compile"
+		exit 1
+	fi
+fi
+
+# Execute it
+java -classpath "$__odr" "$__cls" "$*"
+exit $?
+
