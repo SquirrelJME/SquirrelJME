dcc66cdfa2c92ba9d74daaf2926c921a398481ef
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-10-31 03:45:47 +0000

    Add base for JITPipe.

---
+++ libs/squirreljme-build-system/net/multiphasicapps/squirreljme/build/system/JITPipe.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.build.system;
+
+/**
+ * This class is used to specify the files and projects which are to be
+ * compiled as input for the JIT with produced output for targets.
+ *
+ * @since 2017/10/30
+ */
+public class JITPipe
+{
+}
+
