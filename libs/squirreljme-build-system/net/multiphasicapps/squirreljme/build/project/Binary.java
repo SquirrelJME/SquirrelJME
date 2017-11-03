ca9662cbc2b6a1b6b690115c1704f3e75732a5fb
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-11-03 02:24:02 +0000

    Add base for initializing binaries.

--- libs/squirreljme-build-system/net/multiphasicapps/squirreljme/build/project/Binary.java
+++ libs/squirreljme-build-system/net/multiphasicapps/squirreljme/build/project/Binary.java
@@ -10,6 +10,10 @@
 
 package net.multiphasicapps.squirreljme.build.project;
 
+import java.lang.ref.Reference;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * This class represents a binary which has been loaded by the binary manager.
  *
@@ -17,5 +21,30 @@ package net.multiphasicapps.squirreljme.build.project;
  */
 public final class Binary
 {
+	/**
+	 * Initializes the binary.
+	 *
+	 * @param __ref The reference to the binary manager, to find other binaries
+	 * such as for dependencies.
+	 * @param __name The name of this binary.
+	 * @param __source The source of this binary, may be {@code null} if there
+	 * is no source.
+	 * @
+	 */
+	Binary(Reference<BinaryManager> __ref, SourceName __name, Source __source,
+		Path __path)
+		throws NoSuchBinaryException, NullPointerException
+	{
+		if (__ref == null || __name == null || __path == null)
+			throw new NullPointerException("NARG");
+		
+		// {@squirreljme.error AO0i Cannot open the specified path as a project
+		// because it does not exist and there is no base source code. (The
+		// path to open as a binary)}
+		if (__source == null && !Files.exists(__path))
+			throw new NoSuchBinaryException(String.format("AO0i %s", __path));
+		
+		throw new todo.TODO();
+	}
 }
 
---
+++ libs/squirreljme-build-system/net/multiphasicapps/squirreljme/build/project/NoSuchBinaryException.java
@@ -0,0 +1,64 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.build.project;
+
+/**
+ * This is thrown when the specified binary does not exist.
+ *
+ * @since 2017/11/02
+ */
+public class NoSuchBinaryException
+	extends ProjectException
+{
+	/**
+	 * Initialize the exception with no message or cause.
+	 *
+	 * @since 2017/11/02
+	 */
+	public NoSuchBinaryException()
+	{
+	}
+	
+	/**
+	 * Initialize the exception with a message and no cause.
+	 *
+	 * @param __m The message.
+	 * @since 2017/11/02
+	 */
+	public NoSuchBinaryException(String __m)
+	{
+		super(__m);
+	}
+	
+	/**
+	 * Initialize the exception with a message and cause.
+	 *
+	 * @param __m The message.
+	 * @param __c The cause.
+	 * @since 2017/11/02
+	 */
+	public NoSuchBinaryException(String __m, Throwable __c)
+	{
+		super(__m, __c);
+	}
+	
+	/**
+	 * Initialize the exception with no message and with a cause.
+	 *
+	 * @param __c The cause.
+	 * @since 2017/11/02
+	 */
+	public NoSuchBinaryException(Throwable __c)
+	{
+		super(__c);
+	}
+}
+
