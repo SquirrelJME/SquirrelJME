b39f83dc4e2bfa549ede1688ec2ef29472649229
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-11-09 16:42:25 +0000

    Add some base workup for code.

---
+++ bldt/mids/builder/net/multiphasicapps/squirreljme/builder/entry/BuilderMain.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.builder.entry;
+
+/**
+ * Main entry point for the builder.
+ *
+ * @since 2017/11/09
+ */
+public class BuilderMain
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2017/11/09
+	 */
+	public static void main(String... __args)
+	{
+		throw new todo.TODO();
+	}
+}
+
