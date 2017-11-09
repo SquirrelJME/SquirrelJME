3728e4c15d710ef326f335dcd27fb1b73e3bd9dc
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-11-09 18:23:16 +0000

    Add lsprojects.sh script to print the relative from the project base paths for all projects.

---
+++ utils-dev/lsprojects.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+#     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3+, or later.
+# See license.mkd for licensing and copyright information.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: List all relative projects from the base of the source code.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Go through all namespaces
+__root="$__exedir/.."
+"$__exedir/lsnamespaces.sh" | while read __dir
+do
+	# And directories within the namespaces
+	for __file in "$__root/$__dir/"*
+	do
+		# Ignore non-directories
+		if [ ! -d "$__file" ]
+		then
+			continue
+		fi
+		
+		# If there is a project here, print that directory
+		if [ -f "$__file/META-INF/MANIFEST.MF" ]
+		then
+			"$__exedir/relative.sh" "$__root" "$__file"
+		fi
+	done
+done
+
