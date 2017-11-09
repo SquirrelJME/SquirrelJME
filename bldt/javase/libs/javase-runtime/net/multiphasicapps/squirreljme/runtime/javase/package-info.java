da8d4e275c20e1d1ec7f527b6894e1997646ce5b
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-11-09 17:13:23 +0000

    Move the Swing code to a specific sub-package.

---
+++ bldt/javase/libs/javase-runtime/net/multiphasicapps/squirreljme/runtime/javase/package-info.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the support for SquirrelJME which runs on an existing
+ * Java SE run-time by providing basic support for implemented classes and
+ * such.
+ *
+ * @since 2017/11/09
+ */
+
+package net.multiphasicapps.squirreljme.runtime.javase;
+
