ef7d9d37efd02cbd9b08d181c79f366de8c1062a
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-11-28 18:27:32 +0000

    Make Compiler an abstract class because most of it will be duplicated needlessly anyway.

--- bldt/javase/libs/builder-implementation/net/multiphasicapps/squirreljme/builder/javase/javac/HostCompiler.java
+++ bldt/javase/libs/builder-implementation/net/multiphasicapps/squirreljme/builder/javase/javac/HostCompiler.java
@@ -8,8 +8,9 @@
 // See license.mkd for licensing and copyright information.
 // ---------------------------------------------------------------------------
 
-package net.multiphasicapps.javac.cute;
+package net.multiphasicapps.squirreljme.builder.javase.javac;
 
+import javax.tools.JavaCompiler;
 import net.multiphasicapps.javac.Compiler;
 import net.multiphasicapps.javac.CompilerException;
 import net.multiphasicapps.javac.CompilerInput;
@@ -19,12 +20,32 @@ import net.multiphasicapps.javac.CompilerOutput;
 import net.multiphasicapps.javac.CompilerPathSet;
 
 /**
- * This is used to create instances of the compiler.
+ * This is the compiler which utilizes the system Java compiler, if one is
+ * available.
  *
  * @since 2017/11/28
  */
-public class CuteCompiler
+public class HostCompiler
 	extends Compiler
 {
+	/** The real Java compiler to use. */
+	protected final JavaCompiler javac;
+	
+	/**
+	 * Initializes the host compiler which uses the given Java compiler.
+	 *
+	 * @param __javac The Java compiler to use.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/11/28
+	 */
+	public HostCompiler(JavaCompiler __javac)
+		throws NullPointerException
+	{
+		if (__javac == null)
+			throw new NullPointerException("NARG");
+		
+		// Set
+		this.javac = __javac;
+	}
 }
 
