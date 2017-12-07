b3db2ecf621ee1c14065814e5a7cd38a5eed84f4
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-07 22:36:16 +0000

    Add base package for internal SWM details.

---
+++ runt/apis/meep-swm/net/multiphasicapps/squirreljme/runtime/swm/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the SquirrelJME implementations of the classes needed
+ * by the SWM sub-system.
+ *
+ * @since 2017/12/07
+ */
+
+package net.multiphasicapps.squirreljme.runtime.swm;
+
