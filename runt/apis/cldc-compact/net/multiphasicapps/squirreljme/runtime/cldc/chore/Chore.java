549cc7f9cce64833fd4d782ded9c8026ca91a658
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-08 14:14:50 +0000

    Base work for changearound of the chore system.

---
+++ runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/cldc/chore/Chore.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.runtime.cldc.chore;
+
+/**
+ * This class represents a chore which is used to identify and store
+ * information about tasks running on the system.
+ *
+ * @since 2017/12/08
+ */
+public abstract class Chore
+{
+}
+
---
+++ runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/cldc/chore/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the chore related APIs which are used to manage
+ * tasks either in use by the system or clients.
+ *
+ * @since 2017/12/08
+ */
+
+package net.multiphasicapps.squirreljme.runtime.cldc.chore;
+
