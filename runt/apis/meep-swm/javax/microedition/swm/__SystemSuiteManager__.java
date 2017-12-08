a82a5333a1d1798de53cba031f166c7c3de01f72
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-08 19:58:43 +0000

    Add base for the system suite manager and lazily initialize that.

---
+++ runt/apis/meep-swm/javax/microedition/swm/__SystemSuiteManager__.java
@@ -0,0 +1,100 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package javax.microedition.swm;
+
+import java.util.List;
+
+/**
+ * This class manages the bridge for the suite manager to the native program
+ * manager.
+ *
+ * @since 2017/12/08
+ */
+final class __SystemSuiteManager__
+	implements SuiteManager
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/08
+	 */
+	@Override
+	public void addSuiteListener(SuiteListener __sl)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/08
+	 */
+	@Override
+	public Suite getSuite(String __vendor, String __name)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/08
+	 */
+	@Override
+	public SuiteInstaller getSuiteInstaller(byte[] __b, int __o,
+		int __l, boolean __ignuplock)
+		throws IllegalArgumentException, SecurityException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/08
+	 */
+	@Override
+	public SuiteInstaller getSuiteInstaller(String __url,
+		boolean __ignuplock)
+		throws IllegalArgumentException, SecurityException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/08
+	 */
+	@Override
+	public List<Suite> getSuites(SuiteType __t)
+		throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/08
+	 */
+	@Override
+	public void removeSuite(Suite __s, boolean __ignuplock)
+		throws IllegalArgumentException, SuiteLockedException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/08
+	 */
+	@Override
+	public void removeSuiteListener(SuiteListener __sl)
+	{
+		throw new todo.TODO();
+	}
+}
+
