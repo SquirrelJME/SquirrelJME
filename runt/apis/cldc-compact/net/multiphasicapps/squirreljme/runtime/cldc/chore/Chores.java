737437dde3735a5bc55e4501059667e9093bfab6
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-08 16:34:44 +0000

    Add some base logic for launching tasks.

--- runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/cldc/chore/Chores.java
+++ runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/cldc/chore/Chores.java
@@ -10,6 +10,8 @@
 
 package net.multiphasicapps.squirreljme.runtime.cldc.chore;
 
+import net.multiphasicapps.squirreljme.runtime.cldc.program.Program;
+
 /**
  * This is used to provide native access to system chores.
  *
@@ -49,6 +51,36 @@ public abstract class Chores
 	}
 	
 	/**
+	 * Launches the specified chore.
+	 *
+	 * Note that even if the chore fails to start it will still be created
+	 * except that it will be in the start failed state.
+	 *
+	 * @param __cp The class path consisting of programs to use for execution.
+	 * @param __cl The midlet class to enter.
+	 * @return The chore which represents the given launched program.
+	 * @throws SecurityException If this chore is not permitted to launch the
+	 * specified program.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/12/08
+	 */
+	public final Chore launch(Program[] __cp, String __cl)
+		throws SecurityException, NullPointerException
+	{
+		if (__cp == null || __cl == null)
+			throw new NullPointerException("NARG");
+		
+		// {@squirreljme.error ZZ0g The current chore is not permitted to
+		// launch new chores.}
+		if (0 == (this.currentGroup().basicPermissions() &
+			(ChoreGroup.BASIC_PERMISSION_CLIENT_MANAGE_TASKS |
+			ChoreGroup.BASIC_PERMISSION_CROSSCLIENT_MANAGE_TASKS)))
+			throw new SecurityException("ZZ0g");
+		
+		throw new todo.TODO();
+	}
+	
+	/**
 	 * Returns the list of chores which are currently running.
 	 *
 	 * @param __sys If {@code true} then system chores are included in the
@@ -61,7 +93,7 @@ public abstract class Chores
 	public final Chore[] list(boolean __sys)
 		throws SecurityException
 	{
-		// {@squirreljme.error ZZ0f The current process is not permitted to
+		// {@squirreljme.error ZZ0f The current chore is not permitted to
 		// list the available chores.}
 		if (0 == (this.currentGroup().basicPermissions() &
 			(ChoreGroup.BASIC_PERMISSION_CLIENT_MANAGE_TASKS |
