5eb87c4c17850c6625b2065264cb8c08f0e5201c
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-25 16:21:32 +0000

    Add some documentation on KernelProgram and KernelPrograms; Allow up to 4096 program slots.

--- runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/kernel/KernelProgram.java
+++ runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/kernel/KernelProgram.java
@@ -11,11 +11,28 @@
 package net.multiphasicapps.squirreljme.runtime.kernel;
 
 /**
- * This represents a single program which exists within the kernel.
+ * This represents a single program which exists within the kernel and maps
+ * with suites within the Java ME environment. Each program is identified by
+ * an identifier which represents the program index. The index remains constant
+ * for the same program (unless that program has been changed). The index is
+ * used to refer to the program slot. Since programs in the same slot may be
+ * changed, there is a separate change mask which indicates the change number
+ * of a program.
  *
  * @since 2017/12/11
  */
 public abstract class KernelProgram
 {
+	/** This is the mask used to represent the slot position. */
+	public static final int SLOT_MASK =
+		0b0000_0000__0000_0000___0000_1111__1111_1111;
+	
+	/** This represents the number of times the program has changed. */
+	public static final int CHANGE_MASK =
+		0b1111_1111__1111_1111___1111_0000__0000_0000;
+	
+	/** This represents the shift used to read and write the change mask. */
+	public static final int CHANGE_SHIFT =
+		12;
 }
 
