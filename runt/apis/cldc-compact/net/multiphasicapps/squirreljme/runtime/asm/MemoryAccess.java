a8e9aff1144441acf5f9af8d04202f06f099d547
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-27 23:21:42 +0000

    Add base for assembly operations.

---
+++ runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/asm/MemoryAccess.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.runtime.asm;
+
+/**
+ * This class contains static methods which are replaced by the compiler to
+ * provide native access to memory.
+ *
+ * @since 2017/12/27
+ */
+public final class MemoryAccess
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2017/12/27
+	 */
+	private MemoryAccess()
+	{
+	}
+}
+
