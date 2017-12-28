89fbe62ab03b2c401fa9fe83dc0de61628b052c6
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-28 20:44:01 +0000

    Add base collection for IdentityHashSet and IdentityLinkedHashSet.

---
+++ runt/libs/collections/net/multiphasicapps/collections/__IdentityWrapper__.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.collections;
+
+/**
+ * This wraps an object and allows comparison of the object by value only.
+ *
+ * @param <T> The type of value to wrap.
+ * @since 2017/12/28
+ */
+final class __IdentityWrapper__<T>
+{
+}
+
