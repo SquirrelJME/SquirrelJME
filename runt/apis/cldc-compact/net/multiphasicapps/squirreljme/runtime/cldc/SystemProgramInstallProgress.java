6fcbbf24448951f9122e1d82d2f9440b6ddf2120
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-28 19:04:18 +0000

    Add base class for an installer and installation progress.

---
+++ runt/apis/cldc-compact/net/multiphasicapps/squirreljme/runtime/cldc/SystemProgramInstallProgress.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.runtime.cldc;
+
+/**
+ * This interface is used by the system to notify of progress made installing
+ * an application.
+ *
+ * @since 2017/12/28
+ */
+public interface SystemProgramInstallProgress
+{
+}
+
