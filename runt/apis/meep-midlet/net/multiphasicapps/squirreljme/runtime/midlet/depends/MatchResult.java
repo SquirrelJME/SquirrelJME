a767b87c82202bf0f63d462af552de98eda1a425
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-31 05:10:00 +0000

    Add some missing methods for set/map usage; Implement base of MatchResult.

--- runt/apis/meep-midlet/net/multiphasicapps/squirreljme/runtime/midlet/depends/MatchResult.java
+++ runt/apis/meep-midlet/net/multiphasicapps/squirreljme/runtime/midlet/depends/MatchResult.java
@@ -10,6 +10,11 @@
 
 package net.multiphasicapps.squirreljme.runtime.midlet.depends;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import net.multiphasicapps.collections.EmptySet;
+
 /**
  * This class contains the results of a dependency match.
  *
@@ -17,6 +22,48 @@ package net.multiphasicapps.squirreljme.runtime.midlet.depends;
  */
 public final class MatchResult
 {
+	/** The matched results. */
+	protected final DependencyInfo matched;
+	
+	/** The unmatched results. */
+	protected final DependencyInfo unmatched;
+	
+	/**
+	 * Initializes the match result.
+	 *
+	 * @param __matched The matched dependencies.
+	 * @param __unmatched The unmatched dependencies.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/12/31
+	 */
+	public MatchResult(DependencyInfo __matched, DependencyInfo __unmatched)
+		throws NullPointerException
+	{
+		if (__matched == null || __unmatched == null)
+			throw new NullPointerException("NARG");
+		
+		this.matched = __matched;
+		this.unmatched = __unmatched;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/31
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		if (this == __o)
+			return true;
+		
+		if (!(__o instanceof MatchResult))
+			return false;
+		
+		MatchResult o = (MatchResult)__o;
+		return this.matched.equals(o.matched) &&
+			this.unmatched.equals(o.unmatched);
+	}
+	
 	/**
 	 * Returns {@code true} if there have been matches in the result.
 	 *
@@ -25,7 +72,18 @@ public final class MatchResult
 	 */
 	public final boolean hasMatches()
 	{
-		throw new todo.TODO();
+		return !this.matched.isEmpty();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/31
+	 */
+	@Override
+	public final int hashCode()
+	{
+		return this.matched.hashCode() ^
+			this.unmatched.hashCode();
 	}
 	
 	/**
@@ -37,7 +95,7 @@ public final class MatchResult
 	 */
 	public final DependencyInfo matched()
 	{
-		throw new todo.TODO();
+		return this.matched;
 	}
 	
 	/**
@@ -48,6 +106,16 @@ public final class MatchResult
 	 * @since 2017/11/30
 	 */
 	public final DependencyInfo unmatched()
+	{
+		return this.unmatched;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/31
+	 */
+	@Override
+	public final String toString()
 	{
 		throw new todo.TODO();
 	}
