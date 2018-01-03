313de87347f3e7fd18c495a20caca214299f9dd9
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-03 02:49:42 +0000

    Make a new kernel-program-info which can be used to parse program manifests as needed, it consists of mostly meep-midlet and launcher-support.

---
+++ runt/klib/kernel-program-info/net/multiphasicapps/squirreljme/kernel/programinfo/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the program information classes which can be used to
+ * parse and determine what a program is along with its dependencies.
+ *
+ * @since 2018/01/02
+ */
+
+package net.multiphasicapps.squirreljme.kernel.programinfo;
+
