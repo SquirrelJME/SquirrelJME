2f0d0fad45037447e97cc6433d393ffa311e2e94
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-03 04:08:20 +0000

    Clear out Kernel; Move InstallErrorCodes to new project.

--- runt/klib/kernel/net/multiphasicapps/squirreljme/kernel/Kernel.java
+++ runt/klib/kernel/net/multiphasicapps/squirreljme/kernel/Kernel.java
@@ -10,6 +10,9 @@
 
 package net.multiphasicapps.squirreljme.kernel;
 
+import java.util.Map;
+import net.multiphasicapps.collections.SortedTreeMap;
+
 /**
  * This class represents the micro-kernel which manages the entire SquirrelJME
  * system and all of the needed IPC and running tasks/threads.
@@ -23,128 +26,41 @@ package net.multiphasicapps.squirreljme.kernel;
  */
 public abstract class Kernel
 {
-	/** Program manager. */
-	@Deprecated
-	protected final KernelPrograms kernelprograms;
-	
-	/** Task manager. */
-	@Deprecated
-	protected final KernelTasks kerneltasks;
+	/** Tasks which exist within the kernel. */
+	private Map<Integer, KernelTask> _tasks =
+		new SortedTreeMap<>();
 	
 	/**
 	 * Initializes the base kernel.
 	 *
-	 * @param __kt The output array which is placed the kernel task.
-	 * @param __kif Initialization factory.
-	 * @throws NullPointerException On null arguments.
-	 * @since 2017/12/10
+	 * @since 2018/01/02
 	 */
-	protected Kernel(KernelTask[] __kt, KernelInitializerFactory __kif)
-		throws NullPointerException
+	public Kernel()
 	{
-		if (__kt == null || __kif == null)
-			throw new NullPointerException("NARG");
-		
-		// Initialize the base kernel task
-		KernelTask kerneltask = __kif.initializeKernelTask(this);
-		__kt[0] = kerneltask;
-		
-		// Initialize the kernel
-		this.kerneltasks = __kif.initializeTasks(this, kerneltask);
-		this.kernelprograms = __kif.initializePrograms(this);
 	}
 	
 	/**
-	 * Requests that the system map the specified service class to a default
-	 * provided service name.
+	 * Returns the task associated with the given index.
 	 *
-	 * @param __s The service to map.
-	 * @return The default service to map to or {@code null} if there is no
-	 * mapping for the given service.
-	 * @throws NullPointerException On null arguments.
-	 * @since 2017/12/31
+	 * @param __id The index of the task to get.
+	 * @return The task associated with the given index.
+	 * @throws NoSuchKernelTaskException If no such task exists by that index.
+	 * @since 2018/01/02
 	 */
-	public abstract String mapService(String __sv)
-		throws NullPointerException;
-	
-	/**
-	 * Specifies that the specified thread should become a daemon thread.
-	 *
-	 * @param __t The thread to set as a daemon.
-	 * @throws IllegalThreadStateException If the thread has already been
-	 * started or is already a daemon thread.
-	 * @throws NullPointerException On null arguments.
-	 * @since 2017/12/28
-	 */
-	@Deprecated
-	public abstract void setDaemonThread(Thread __t)
-		throws IllegalThreadStateException, NullPointerException;
-	
-	/**
-	 * Returns the program manager.
-	 *
-	 * @param __by The task requesting the manager.
-	 * @return The program manager.
-	 * @throws NullPointerException On null arguments.
-	 * @throws SecurityException If the task is not permitted access to the
-	 * program manager.
-	 * @since 2017/12/14
-	 */
-	@Deprecated
-	public final KernelPrograms programs(KernelTask __by)
-		throws NullPointerException, SecurityException
+	public final KernelTask taskByIndex(int __id)
+		throws NoSuchKernelTaskException
 	{
-		if (__by == null)
-			throw new NullPointerException("NARG");
-		
-		// {@squirreljme.error ZZ0g The specified task is not permitted to get
-		// the program manager instance. (The task requesting the program
-		// manager)}
-		if (!__by.hasSimplePermissions(__by,
-			KernelSimplePermission.GET_PROGRAMS_INSTANCE))
-			throw new SecurityException(
-				String.format("ZZ0g %s", __by));
-		
-		return this.kernelprograms;
-	}
-	
-	/**
-	 * Returns the task manager.
-	 *
-	 * @param __by The thread requesting the manager.
-	 * @return The task manager.
-	 * @throws NullPointerException On null arguments.
-	 * @throws SecurityException If the task is not permitted to access the
-	 * task manager.
-	 * @since 2017/12/27
-	 */
-	@Deprecated
-	public final KernelTasks tasks(KernelTask __by)
-		throws NullPointerException, SecurityException
-	{
-		if (__by == null)
-			throw new NullPointerException("NARG");
-		
-		// {@squirreljme.error ZZ0j The specified task is not permitted to get
-		// the task manager instance. (The task requesting the task manager)}
-		if (!__by.hasSimplePermissions(__by,
-			KernelSimplePermission.GET_TASKS_INSTANCE))
-			throw new SecurityException(
-				String.format("ZZ0j %s", __by));
-		
-		return this.kerneltasks;
-	}
-	
-	/**
-	 * Returns the kernel program manager with no access check.
-	 *
-	 * @return The kernel program manager.
-	 * @since 2017/12/31
-	 */
-	@Deprecated
-	final KernelPrograms __programs()
-	{
-		return this.kernelprograms;
+		Map<Integer, KernelTask> tasks = this._tasks;
+		synchronized (tasks)
+		{
+			// {@squirreljme.error AP0g The specified task does not exist.
+			// (The task index)}
+			KernelTask rv = tasks.get(__id);
+			if (rv == null)
+				throw new NoSuchKernelTaskException(
+					String.format("AP0g %d", __id));
+			return rv;
+		}
 	}
 }
 
