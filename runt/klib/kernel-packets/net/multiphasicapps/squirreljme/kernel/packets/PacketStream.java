d358d4af4dfd8b66ee2d9a877b026bcc9d02cfb0
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-03 02:53:58 +0000

    Rename packets to kernel-packets and move the package around.

--- runt/klib/kernel-packets/net/multiphasicapps/squirreljme/kernel/packets/PacketStream.java
+++ runt/klib/kernel-packets/net/multiphasicapps/squirreljme/kernel/packets/PacketStream.java
@@ -8,7 +8,7 @@
 // See license.mkd for licensing and copyright information.
 // ---------------------------------------------------------------------------
 
-package net.multiphasicapps.squirreljme.runtime.packets;
+package net.multiphasicapps.squirreljme.kernel.packets;
 
 import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
