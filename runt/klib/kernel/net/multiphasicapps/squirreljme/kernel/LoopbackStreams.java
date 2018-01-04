d892c30d61e1120fb76f87f379555d7753a3e260
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-04 14:37:54 +0000

    Base for a LoopbackStreams.

---
+++ runt/klib/kernel/net/multiphasicapps/squirreljme/kernel/LoopbackStreams.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirreljme.kernel;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * This is a set of streams which loop into each other, this allows for
+ * communication where one source of streams writes into the stream of the
+ * other side.
+ *
+ * @since 2018/01/04
+ */
+public final class LoopbackStreams
+{
+	/**
+	 * This represents the side of the loopback stream.
+	 *
+	 * @since 2018/01/04
+	 */
+	public final class Side
+	{
+		/**
+		 * Initializes the side.
+		 *
+		 * @since 2018/01/04
+		 */
+		private Side()
+		{
+		}
+	}
+}
+
