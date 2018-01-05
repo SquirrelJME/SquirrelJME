dff504963db506de1e66d9937739609cd185b054
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-05 05:20:28 +0000

    Add base methods for encoding Throwables to PacketStream.

--- runt/klib/kernel-packets/net/multiphasicapps/squirreljme/kernel/packets/RemoteThrowable.java
+++ runt/klib/kernel-packets/net/multiphasicapps/squirreljme/kernel/packets/RemoteThrowable.java
@@ -24,5 +24,81 @@ package net.multiphasicapps.squirreljme.kernel.packets;
 public final class RemoteThrowable
 	extends RuntimeException
 {
+	/**
+	 * Initialize the exception with no message or cause.
+	 *
+	 * @since 2018/01/05
+	 */
+	public RemoteThrowable()
+	{
+	}
+	
+	/**
+	 * Initialize the exception with a message and no cause.
+	 *
+	 * @param __m The message.
+	 * @since 2018/01/05
+	 */
+	public RemoteThrowable(String __m)
+	{
+		super(__m);
+	}
+	
+	/**
+	 * Initialize the exception with a message and cause.
+	 *
+	 * @param __m The message.
+	 * @param __c The cause.
+	 * @since 2018/01/05
+	 */
+	public RemoteThrowable(String __m, Throwable __c)
+	{
+		super(__m, __c);
+	}
+	
+	/**
+	 * Initialize the exception with no message and with a cause.
+	 *
+	 * @param __c The cause.
+	 * @since 2018/01/05
+	 */
+	public RemoteThrowable(Throwable __c)
+	{
+		super(__c);
+	}
+	
+	/**
+	 * This decodes a throwable which was stored in a packet.
+	 *
+	 * @param __r The reader to decode from.
+	 * @return The decoded throwable from the remote end.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/05
+	 */
+	public static final RemoteThrowable decode(PacketReader __r)
+		throws NullPointerException
+	{
+		if (__r == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Encodes the given throwable to the packet.
+	 *
+	 * @param __t The throwable to encode.
+	 * @param __w The target writer.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/05
+	 */
+	public static final void encode(Throwable __t, PacketWriter __w)
+		throws NullPointerException
+	{
+		if (__t == null || __w == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
 }
 
