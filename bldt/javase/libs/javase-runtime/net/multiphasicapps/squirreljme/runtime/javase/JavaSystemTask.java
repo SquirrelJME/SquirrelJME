7a6c08ad9d0b50c71354568e4b8496fa8e0847b6
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-06 15:18:29 +0000

    Implement the needed methods to list tasks.

--- bldt/javase/libs/javase-runtime/net/multiphasicapps/squirreljme/runtime/javase/JavaSystemTask.java
+++ bldt/javase/libs/javase-runtime/net/multiphasicapps/squirreljme/runtime/javase/JavaSystemTask.java
@@ -13,6 +13,9 @@ package net.multiphasicapps.squirreljme.runtime.javase;
 import java.lang.ref.Reference;
 import net.multiphasicapps.squirreljme.kernel.Kernel;
 import net.multiphasicapps.squirreljme.kernel.KernelTask;
+import net.multiphasicapps.squirreljme.runtime.cldc.SystemTaskFlag;
+import net.multiphasicapps.squirreljme.runtime.cldc.SystemTaskMetric;
+import net.multiphasicapps.squirreljme.runtime.cldc.SystemTaskStatus;
 
 /**
  * This task represents the kernel itself.
@@ -40,7 +43,8 @@ public class JavaSystemTask
 	@Override
 	public final int flags()
 	{
-		throw new todo.TODO();
+		return SystemTaskFlag.SYSTEM |
+			SystemTaskStatus.RUNNING;
 	}
 	
 	/**
@@ -50,7 +54,24 @@ public class JavaSystemTask
 	@Override
 	public final long metric(int __m)
 	{
-		throw new todo.TODO();
+		switch (__m)
+		{
+			case SystemTaskMetric.PRIORITY:
+				return -100;
+				
+			case SystemTaskMetric.MEMORY_USED:
+				return this.metric(SystemTaskMetric.MEMORY_TOTAL) -
+					this.metric(SystemTaskMetric.MEMORY_FREE);
+				
+			case SystemTaskMetric.MEMORY_FREE:
+				return Runtime.getRuntime().freeMemory();
+				
+			case SystemTaskMetric.MEMORY_TOTAL:
+				return Runtime.getRuntime().totalMemory();
+				
+			default:
+				return Long.MIN_VALUE;
+		}
 	}
 	
 	/**
