447ab6d7b7b81f0250ccec91af8560e40da67051
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-09 15:44:52 +0000

    Use better names for services and such.

--- runt/klib/kernel-service/net/multiphasicapps/squirreljme/kernel/service/ServiceProviderFactory.java
+++ runt/klib/kernel-service/net/multiphasicapps/squirreljme/kernel/service/ServiceProviderFactory.java
@@ -11,21 +11,22 @@
 package net.multiphasicapps.squirreljme.kernel.service;
 
 /**
- * This class is used to initialize service servers which are used as a host
+ * This class is used to initialize service providers which are used as a host
  * interface when interacting with client services.
  *
  * The kernel uses this with the service loader to initialize services.
  *
  * @since 2018/01/03
  */
-public interface ServiceServerFactory
+public interface ServiceProviderFactory
 {
 	/**
-	 * Creates the service server which will be used by clients.
+	 * Creates the service provider which will be used to create servers for
+	 * client tasks.
 	 *
-	 * @return The newly created service server.
+	 * @return The newly created service provider.
 	 * @since 2018/01/03
 	 */
-	public abstract ServiceServer createServer();
+	public abstract ServiceProvider createProvider();
 }
 
