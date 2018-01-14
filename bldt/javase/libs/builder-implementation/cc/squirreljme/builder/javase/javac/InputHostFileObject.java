36dea9d1fab999c33d3a6c0ff7f9ef8096c9524e
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-14 21:12:10 +0000

    Sort imports for bldt/javase/libs/builder-implementation/.

--- bldt/javase/libs/builder-implementation/cc/squirreljme/builder/javase/javac/InputHostFileObject.java
+++ bldt/javase/libs/builder-implementation/cc/squirreljme/builder/javase/javac/InputHostFileObject.java
@@ -10,9 +10,9 @@
 
 package cc.squirreljme.builder.javase.javac;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
