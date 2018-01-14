dac75cb3bf148a4e11a945ef81a064b0ef1ffeca
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-14 21:06:59 +0000

    Correct compilation error.

--- utils-dev/SortImports.java
+++ utils-dev/SortImports.java
@@ -13,6 +13,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
