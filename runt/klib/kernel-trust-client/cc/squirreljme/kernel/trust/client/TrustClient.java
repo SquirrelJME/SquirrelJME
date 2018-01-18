8f23bb6c8b20d0f655e4fcc21c0d80674dc7d7f0
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-18 15:24:07 +0000

    Add base call to get the trust group.

--- runt/klib/kernel-trust-client/cc/squirreljme/kernel/trust/client/TrustClient.java
+++ runt/klib/kernel-trust-client/cc/squirreljme/kernel/trust/client/TrustClient.java
@@ -13,6 +13,7 @@ package cc.squirreljme.kernel.trust.client;
 import cc.squirreljme.kernel.packets.Packet;
 import cc.squirreljme.kernel.service.ClientInstance;
 import cc.squirreljme.kernel.service.ServicePacketStream;
+import cc.squirreljme.runtime.cldc.SystemTrustGroup;
 
 /**
  * This is the client which manages trusts which are stored on the remote end
@@ -52,6 +53,46 @@ public final class TrustClient
 				throw new IllegalArgumentException(
 					String.format("BI01 %s", __p));
 		}
+	}
+	
+	/**
+	 * Returns a trust group for a trusted program which goes by the
+	 * specified name.
+	 *
+	 * @param __name The midlet name.
+	 * @param __vendor The midlet vendor.
+	 * @return The trust group for the midlet.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/18
+	 */
+	public final SystemTrustGroup trustedTrust(String __name,
+		String __vendor)
+		throws NullPointerException
+	{
+		if (__name == null || __vendor == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Returns a trust group for an untrusted program which goes by the
+	 * specified name.
+	 *
+	 * @param __name The midlet name.
+	 * @param __vendor The midlet vendor.
+	 * @return The trust group for the midlet.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/18
+	 */
+	public final SystemTrustGroup untrustedTrust(String __name,
+		String __vendor)
+		throws NullPointerException
+	{
+		if (__name == null || __vendor == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
 	}
 }
 
