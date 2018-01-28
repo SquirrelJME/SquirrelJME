22a91436b20080c700c997504412fc81cd21ce8b
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-28 03:07:16 +0000

    Add base sub-sdk package which will contain the needed stuff for constructing SDKs.

---
+++ bldt/libs/builder-support/cc/squirreljme/builder/support/sdk/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the SDK support code which can be used to build and
+ * construct the SDK as required.
+ *
+ * @since 2018/01/27
+ */
+
+package cc.squirreljme.builder.support.sdk;
+
