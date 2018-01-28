cc1621cf169fc08184fd443645faf2c5fa94ef8c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-01-28 21:25:22 +0000

    Add base for appending classes to a file.

---
+++ utils-dev/AppendClass.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * Reads a class list from a file, then appends a number of classes to that
+ * file.
+ *
+ * @since 2018/01/28
+ */
+public class AppendClass
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2018/01/28
+	 */
+	public static void main(String... __args)
+	{
+		if (__args == null)
+			__args = new String[0];
+		
+		throw new Error("TODO");
+	}
+}
+
