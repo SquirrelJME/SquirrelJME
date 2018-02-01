06580fe3cde5871bf5b8240bc16c73e912afdac2
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-01 01:15:10 +0000

    Register and initialize trusts.

--- runt/klib/kernel-trust-server/cc/squirreljme/kernel/trust/server/TrustProvider.java
+++ runt/klib/kernel-trust-server/cc/squirreljme/kernel/trust/server/TrustProvider.java
@@ -27,6 +27,10 @@ import java.util.LinkedHashMap;
 public abstract class TrustProvider
 	extends ServiceProvider
 {
+	/** Lock to prevent multiple threads from accessing trusts. */
+	protected final Object lock =
+		new Object();
+	
 	/** The trusts which are available. */
 	private final Map<Integer, SystemTrustGroup> _trusts =
 		new LinkedHashMap<>();
@@ -42,6 +46,21 @@ public abstract class TrustProvider
 	}
 	
 	/**
+	 * Creates the specified trust group.
+	 *
+	 * @param __trusted If {@code true} then this group is trusted.
+	 * @param __dx The index that the trust will be created under.
+	 * @param __name The name this trust is for.
+	 * @param __vendor The vendor this trust is for.
+	 * @return The newly created trust.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/31
+	 */
+	protected abstract SystemTrustGroup createTrustGroup(boolean __trusted,
+		int __dx, String __name, String __vendor)
+		throws NullPointerException;
+	
+	/**
 	 * {@inheritDoc}
 	 * @since 2018/01/17
 	 */
@@ -54,6 +73,44 @@ public abstract class TrustProvider
 			throw new NullPointerException("NARG");
 		
 		return new TrustServer(this, __st, __ps);
+	}
+	
+	/**
+	 * Registers a new trust group.
+	 *
+	 * @param __t The trust group to register.
+	 * @throws IllegalStateException If a trust already exists with the given
+	 * index or has a
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/31
+	 */
+	protected final void register(SystemTrustGroup __t)
+		throws IllegalStateException, NullPointerException
+	{
+		if (__t == null)
+			throw new NullPointerException("NARG");
+		
+		Integer idx = __t.index();
+		
+		// Lock
+		Map<Integer, SystemTrustGroup> trusts = this._trusts;
+		synchronized (this.lock)
+		{
+			// {@squirreljme.error BK04 Attempt to register a trust with a
+			// duplicate index. (The index)}
+			if (trusts.containsKey(idx))
+				throw new IllegalStateException(String.format("BK04 %d", idx));
+			
+			// {@squirreljme.error BK05 Cannot add a trust which refers to the
+			// same group. (The first group; The second group}}
+			for (SystemTrustGroup v : trusts.values())
+				if (TrustProvider.isSameReference(v, __t))
+					throw new IllegalStateException(String.format("BK05 %s %s",
+						__t, v));
+			
+			// Is okay, so add it
+			trusts.put(idx, __t);
+		}
 	}
 	
 	/**
@@ -75,7 +132,7 @@ public abstract class TrustProvider
 		
 		// Need to go through every trust to find a match
 		Map<Integer, SystemTrustGroup> trusts = this._trusts;
-		synchronized (trusts)
+		synchronized (this.lock)
 		{
 			// See if a trust already exists
 			int nextid = 1;
@@ -86,19 +143,66 @@ public abstract class TrustProvider
 				if ((dx = rv.index()) >= nextid)
 					nextid = dx + 1;
 				
-				// Ignore trusted ones
-				if (rv.isTrusted())
-					continue;
-				
 				// Matches this trust
-				if (__name.equals(rv.name()) &&
-					__vendor.equals(rv.vendor()))
+				if (TrustProvider.isSameReference(rv, false, __name, __vendor))
 					return rv;
 			}
 			
 			// No trust exists, so the provider must create it and store it
-			throw new todo.TODO();
+			SystemTrustGroup rv = this.createTrustGroup(false, nextid,
+				__name, __vendor);
+			
+			// {@squirreljme.error BK03 No trust created or it had a different
+			// index, name, or vendor.}
+			if (rv == null || rv.index() != nextid ||
+				!__name.equals(rv.name()) || !__vendor.equals(rv.vendor()))
+				throw new RuntimeException("BK03");
+			
+			// Register it
+			this.register(rv);
+			return rv;
 		}
+	}
+	
+	/**
+	 * Checks if the two trust groups refer to the same trust.
+	 *
+	 * @param __a The first trust group.
+	 * @param __b The second trust group.
+	 * @return {@code true} if they are equal.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/31
+	 */
+	public static final boolean isSameReference(SystemTrustGroup __a,
+		SystemTrustGroup __b)
+		throws NullPointerException
+	{
+		if (__a == null || __b == null)
+			throw new NullPointerException("NARG");
+		
+		return TrustProvider.isSameReference(__a, __b.isTrusted(), __b.name(),
+			__b.vendor());
+	}
+	
+	/**
+	 * Checks if the given trust group refers to the given parameters.
+	 *
+	 * @param __a The first trust group.
+	 * @param __btrusted Is the second
+	 * @return {@code true} if they are equal.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/01/31
+	 */
+	public static final boolean isSameReference(SystemTrustGroup __a,
+		boolean __btrusted, String __bname, String __bvendor)
+		throws NullPointerException
+	{
+		if (__a == null || __bname == null || __bvendor == null)
+			throw new NullPointerException("NARG");
+		
+		return __a.isTrusted() == __btrusted &&
+			__bname.equals(__a.name()) &&
+			__bvendor.equals(__a.vendor());
 	}
 }
 
