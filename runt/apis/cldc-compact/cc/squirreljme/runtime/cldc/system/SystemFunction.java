dadb5eacd563e5aa35715e4fbc00b338f7b9af81
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-21 23:13:58 +0000

    Add base for new classes and interfaces used to manage system calls.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/system/SystemFunction.java
@@ -0,0 +1,27 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.system;
+
+/**
+ * This class contains the list of functions which are available to the client
+ * processes when it needs to interact with the kernel or perform an action.
+ *
+ * All system calls should be prefixed by a name then followed by the
+ * arguments the call takes in standard Java descriptors.
+ *
+ * @since 2018/02/21
+ */
+public enum SystemFunction
+{
+	/** End. */
+	;
+}
+
