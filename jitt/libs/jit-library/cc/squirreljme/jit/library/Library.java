1a09a8978e99cb4f6f2b5b397b8e1b15d3770412
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-22 01:10:22 +0000

    Add base project for libraries.

---
+++ jitt/libs/jit-library/cc/squirreljme/jit/library/Library.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jit.library;
+
+/**
+ * This class represents a library within the JIT which is used to provide
+ * access to class files and resources.
+ *
+ * @since 2018/02/21
+ */
+public abstract class Library
+{
+}
+
