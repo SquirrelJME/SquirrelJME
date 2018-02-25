12cd995a9693ec8f93c7d7628a9cb449b4e93d8f
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-25 04:47:19 +0000

    Add base class for SectionWriter; Add DataProperties which is described by Endian and IntegerType (for pointers).

---
+++ jitt/libs/object-file/cc/squirreljme/jit/objectfile/IntegerType.java
@@ -0,0 +1,35 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jit.objectfile;
+
+/**
+ * Represents the type of integer that is used.
+ *
+ * @since 2018/02/24
+ */
+public enum IntegerType
+{
+	/** Byte. */
+	BYTE,
+	
+	/** Short. */
+	SHORT,
+	
+	/** Integer. */
+	INTEGER,
+	
+	/** Long. */
+	LONG,
+	
+	/** End. */
+	;
+}
+
