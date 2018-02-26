13d1a743a34316ef6dbd2c794390d7fd872aab79
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-26 14:13:50 +0000

    Add base for ClassStructure and the classes needed for its representation.

---
+++ jitt/libs/jit-compiler/cc/squirreljme/jit/compiler/ClassStructureSymbolName.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jit.compiler;
+
+/**
+ * This represents the name and reference to the structure within the class
+ * information.
+ *
+ * @since 2018/02/26
+ */
+public final class ClassStructureSymbolName
+{
+}
+
