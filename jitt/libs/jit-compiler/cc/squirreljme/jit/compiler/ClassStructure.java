e6e3577b58a4543e514c122fb53d2481ef7f7119
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-26 14:21:19 +0000

    Add some more class structure information.

--- jitt/libs/jit-compiler/cc/squirreljme/jit/compiler/ClassStructure.java
+++ jitt/libs/jit-compiler/cc/squirreljme/jit/compiler/ClassStructure.java
@@ -21,6 +21,9 @@ public enum ClassStructure
 	/** Name of the class. */
 	NAME,
 	
+	/** The has code for the class name, for quicker finding. */
+	HASHCODE,
+	
 	/** Superclass pointer. */
 	SUPER,
 	
@@ -30,14 +33,38 @@ public enum ClassStructure
 	/** Pointer to implemented interfaces. */
 	INTERFACES,
 	
+	/** Interface method implementations, pointers to tables for methods. */
+	INTERFACES_METHODS_TABLE,
+	
 	/** Class flags. */
 	FLAGS,
 	
 	/** Default constructor pointer (used for {@code newInstance()}). */
 	DEFAULT_CONSTRUCTOR,
 	
+	/** The flags for the default constructor. */
+	DEFAULT_CONSTRUCTOR_FLAGS,
+	
+	/** Pointer to the method {@code static main(String[])}. */
+	MAIN_METHOD,
+	
+	/** Flags the main method uses. */
+	MAIN_METHOD_FLAGS,
+	
 	/** The size of the class in bytes, for allocation. */
 	ALLOCATION_SIZE,
+	
+	/** The offset of the field in global space to the {@link Class} object. */
+	GLOBAL_OBJECT_OFFSET,
+	
+	/** The resource group which contains the resources for the class. */
+	RESOURCE_GROUP,
+	
+	/** Number of elements in the enumeration. */
+	ENUM_COUNT,
+	
+	/** Offsets to fields in the global space representing enum fields. */
+	ENUM_FIELD_OFFSETS,
 	
 	/** End. */
 	;
