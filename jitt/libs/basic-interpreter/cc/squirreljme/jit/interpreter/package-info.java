089fc638dfd757fc31bcc57e6489ef369cf6f1bb
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-27 17:16:51 +0000

    Move interpreter to be a JIT library.

---
+++ jitt/libs/basic-interpreter/cc/squirreljme/jit/interpreter/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the basic interpreter which is used for development
+ * purposes.
+ *
+ * @since 2018/02/27
+ */
+
+package cc.squirreljme.jit.interpreter;
+
