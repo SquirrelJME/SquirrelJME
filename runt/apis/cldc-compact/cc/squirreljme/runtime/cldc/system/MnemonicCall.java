c4d04ba54145a04e4b94ff8098b70570a02e3d4d
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-27 15:37:21 +0000

    Add base for MnemonicCall which will run all system calls with standard and documented arguments.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/system/MnemonicCall.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.system;
+
+/**
+ * This class contains static methods which perform system calls with the
+ * documented and correct set of arguments for system calls.
+ *
+ * @since 2018/02/27
+ */
+public final MnemonicCall
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2018/02/27
+	 */
+	private MnemonicCall()
+	{
+	}
+}
+
