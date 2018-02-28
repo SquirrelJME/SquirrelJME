ff7cf5be8607ef365c84302e585e78dd81270727
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-28 21:39:32 +0000

    Add base for the interpreter process and threads.

---
+++ jitt/libs/basic-interpreter/cc/squirreljme/jit/interpreter/InterpreterThread.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jit.interpreter;
+
+/**
+ * This represents a thread which is interprets and runs Java byte code.
+ *
+ * @since 2018/02/28
+ */
+public final class InterpreterThread
+	implements Runnable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/02/28
+	 */
+	@Override
+	public final void run()
+	{
+		throw new todo.TODO();
+	}
+}
+
