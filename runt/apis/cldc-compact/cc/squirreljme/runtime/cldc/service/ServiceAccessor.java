39e0ee19a2cd1597b0bbba888af410666703c65c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-02 17:39:08 +0000

    Add new classes for system services using the system call interface rather than a stream of datagrams (faster and simpler).

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/service/ServiceAccessor.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.service;
+
+/**
+ * This class provides access to services which are available to the system
+ * for clients to use.
+ *
+ * @since 2018/03/02
+ */
+public final class ServiceAccessor
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2018/03/02
+	 */
+	private ServiceAccessor()
+	{
+	}
+}
+
