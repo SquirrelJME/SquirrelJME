642ba85eb2dd47dfdf0a9356c640bdc69f7cf5ec
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-02 21:21:04 +0000

    Move InvalidTrustException over.

--- runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/trust/InvalidTrustException.java
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/trust/InvalidTrustException.java
@@ -8,7 +8,7 @@
 // See license.mkd for licensing and copyright information.
 // ---------------------------------------------------------------------------
 
-package cc.squirreljme.kernel.trust;
+package cc.squirreljme.runtime.cldc.trust;
 
 /**
  * This is thrown when the trust is not valid.
