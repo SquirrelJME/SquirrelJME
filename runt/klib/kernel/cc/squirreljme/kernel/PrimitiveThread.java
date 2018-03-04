491948f3d7d660f38e54838db48ecb0734ff00ac
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-04 00:55:17 +0000

    Base for primitive kernel and new code.

---
+++ runt/klib/kernel/cc/squirreljme/kernel/PrimitiveThread.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.kernel;
+
+/**
+ * This represents a primitive thread which can run a method within a process.
+ *
+ * @since 2018/03/03
+ */
+public interface PrimitiveThread
+{
+}
+
