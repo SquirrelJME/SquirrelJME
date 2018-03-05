87d5f6b74278933cef244f7415383203a870c41f
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-05 16:54:03 +0000

    Base read of base64 data, currently not correct.

--- runt/libs/io/net/multiphasicapps/io/Base64Decoder.java
+++ runt/libs/io/net/multiphasicapps/io/Base64Decoder.java
@@ -13,6 +13,7 @@ package net.multiphasicapps.io;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Arrays;
 
 /**
  * This decodes the base64 character set, ignoring invalid characters, and
@@ -32,6 +33,18 @@ public final class Base64Decoder
 	
 	/** The alphabet to use for decoding. */
 	private final char[] _alphabet;
+	
+	/** The ASCII map for quick lookup. */
+	private final byte[] _ascii;
+	
+	/** The current fill buffer. */
+	private volatile int _buffer;
+	
+	/** The number of bits which are in the buffer. */
+	private volatile int _bits;
+	
+	/** Has EOF been reached if the pad has been detected? */
+	private volatile boolean _paddedeof;
 	
 	/**
 	 * Initializes the decoder using the default alphabet.
@@ -138,9 +151,22 @@ public final class Base64Decoder
 		if ((n = __chars.length) != 65)
 			throw new IllegalArgumentException(String.format("BD0g %d", n));
 		
+		// Set
 		this.in = __in;
 		this.ignorepadding = __ip;
-		this._alphabet = __chars.clone();
+		this._alphabet = (__chars = __chars.clone());
+		
+		// Build ASCII map for quick in-range character lookup
+		byte[] ascii = new byte[128];
+		for (int i = 0; i < 128; i++)
+			ascii[i] = -1;
+		for (int i = 0; i < 65; i++)
+		{
+			int dx = __chars[i];
+			if (dx < 128)
+				ascii[dx] = (byte)i;
+		}
+		this._ascii = ascii;
 	}
 	
 	/**
@@ -190,7 +216,93 @@ public final class Base64Decoder
 		if (__o < 0 || __l < 0 || (__o + __l) > __b.length)
 			throw new IndexOutOfBoundsException("IOOB");
 		
-		throw new todo.TODO();
+		// Did a previous read cause a padded EOF?
+		boolean paddedeof = this._paddedeof;
+		if (paddedeof)
+			return -1;
+		
+		// Need lookups
+		Reader in = this.in;
+		boolean ignorepadding = this.ignorepadding;
+		char[] alphabet = this._alphabet;
+		byte[] ascii = this._ascii;
+		
+		// This buffer is filled into as needed when input characters are read
+		int buffer = this._buffer,
+			bits = this._bits;
+		
+		// Fill the input in as much as possible
+		int base = __o,
+			eo = __o + __l;
+		boolean goteof = false;
+		while (__o < eo)
+		{
+			// EOF
+			int ch = in.read();
+			if (ch < 0)
+			{
+				goteof = true;
+				break;
+			}
+			
+			// Determine the value of the character
+			int val;
+			if (ch < 128)
+				val = ascii[ch];
+			else
+			{
+				for (val = 0; val < 65; val++)
+					if (ch == alphabet[val])
+						break;
+				if (val == 65)
+					val = -1;
+			}
+			
+			// Value is not valid, ignore it
+			if (val < 0)
+				continue;
+			
+			// Special padding value, indicates EOF
+			if (val == 64)
+			{
+				// Treat as an invalid character
+				if (ignorepadding)
+					continue;
+				
+				// Trigger padded EOF
+				paddedeof = true;
+				
+				// Finish with the bytes in the buffer
+				__b[__o++] = (byte)(buffer & 0xFF);
+			}
+			
+			else
+			{
+				// Shift in six bits
+				buffer <<= 6;
+				buffer |= val;
+				bits += 6;
+			
+				// Read enough bits to output data
+				if (bits >= 8)
+				{
+					__b[__o++] = (byte)(buffer & 0xFF);
+					buffer >>>= 8;
+					bits -= 8;
+				}
+			}
+		}
+		
+		// Store state for next run
+		this._buffer = buffer;
+		this._bits = bits;
+		this._paddedeof = paddedeof;
+		
+		// Return the read count
+		int rv = __o - base;
+		if (goteof && rv == 0)
+			return -1;
+		return rv;
 	}
 }
 
