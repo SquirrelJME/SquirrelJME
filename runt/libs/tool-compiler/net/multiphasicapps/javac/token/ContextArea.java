e251e97c6fc3d230bf621b38918bde937b88d436
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-07 23:38:11 +0000

    Implement ContextToken; Add ContextArea to give another clue also.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/token/ContextArea.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.token;
+
+/**
+ * This represents the area in which a token was read from.
+ *
+ * @since 2018/03/07
+ */
+public enum ContextArea
+{
+	/** End. */
+	;
+}
+
