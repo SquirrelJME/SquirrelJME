e1ea919105138de3cae888948c91f55a825e7a11
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-08 00:13:40 +0000

    Base for context sensitive stack for storing state.

--- runt/libs/tool-compiler/net/multiphasicapps/javac/token/ContextTokenizer.java
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/token/ContextTokenizer.java
@@ -30,11 +30,25 @@ public final class ContextTokenizer
 	protected final BottomTokenizer bottom;
 	
 	/** The queue for the tokenizer. */
-	protected final Deque<ContextToken> _queue =
+	private final Deque<ContextToken> _queue =
+		new ArrayDeque<>();
+	
+	/** The stack for context parsing. */
+	private final Deque<__Context__> _stack =
 		new ArrayDeque<>();
 	
 	/** The last read token. */
 	private volatile ContextToken _lasttoken;
+	
+	/**
+	 * Initializes the context stack.
+	 *
+	 * @since 2018/03/07
+	 */
+	{
+		Deque<__Context__> stack = this._stack;
+		stack.addLast(new __ContextIntro__());
+	}
 	
 	/**
 	 * Initializes the context sensitive tokenizer.
@@ -155,15 +169,106 @@ public final class ContextTokenizer
 	}
 	
 	/**
-	 * Reads 
+	 * Reads input tokens from the bottom tokenizer and generates context
+	 * based tokens for simpler parsing.
 	 *
+	 * @return The next token in the queue.
 	 * @throws IOException On read errors.
 	 * @since 2018/03/07
 	 */
-	private final void __fill()
+	private final ContextToken __fill()
 		throws IOException
 	{
+		// Used to detect EOF if the stack is empty
+		Deque<__Context__> stack = this._stack;
+		__Context__ context = stack.peekLast();
+		if (context == null)
+			return null;
+		
+		// Depends on the area
+		ContextArea area = context.area;
+		switch (area)
+		{
+			case INTRO:
+				__runIntro((__ContextIntro__)context);
+				break;
+			
+				// Not implemented
+			default:
+				throw new RuntimeException(String.format("OOPS %s", area));
+		}
+		
+		// {@squirreljme.error AQ12 Enqueued no input tokens during token
+		// parsing when not in the EOF state.}
+		Deque<ContextToken> queue = this._queue;
+		if (queue.isEmpty() && !stack.isEmpty())
+			throw new TokenizerException(this, "AQ12");
+		return queue.peekFirst();
+	}
+	
+	/**
+	 * Decodes the introductory sequence.
+	 *
+	 * @param __context The input context.
+	 * @throws IOException On read errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/03/07
+	 */
+	private final void __runIntro(__ContextIntro__ __context)
+		throws IOException, NullPointerException
+	{
+		if (__context == null)
+			throw new NullPointerException("NARG");
+		
 		throw new todo.TODO();
+	}
+	
+	/**
+	 * This is the base class for context sensitive parsers for input
+	 * tokens. This interface is just used as a base for state storage.
+	 *
+	 * @since 2018/03/07
+	 */
+	private abstract class __Context__
+	{
+		/** The context area. */
+		public final ContextArea area;
+		
+		/**
+		 * Initializes the base context.
+		 *
+		 * @param __a The area this is in.
+		 * @throws NullPointerException On null arguments.
+		 * @since 2018/03/07
+		 */
+		private __Context__(ContextArea __a)
+			throws NullPointerException
+		{
+			if (__a == null)
+				throw new NullPointerException("NARG");
+			
+			this.area = __a;
+		}
+	}
+	
+	/**
+	 * This is the area for the introduction to the class file and is used
+	 * to store data for the package and imports.
+	 *
+	 * @since 2018/03/07
+	 */
+	private final class __ContextIntro__
+		extends __Context__
+	{
+		/**
+		 * Initializes the context.
+		 *
+		 * @since 2018/03/07
+		 */
+		private __ContextIntro__()
+		{
+			super(ContextArea.INTRO);
+		}
 	}
 }
 
