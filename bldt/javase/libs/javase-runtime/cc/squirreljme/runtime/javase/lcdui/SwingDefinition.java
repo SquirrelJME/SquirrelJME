88e4a02fe79e4dee8e781c9b384072a629a0987c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-15 16:38:49 +0000

    Add base for the LCDUI server and client.

---
+++ bldt/javase/libs/javase-runtime/cc/squirreljme/runtime/javase/lcdui/SwingDefinition.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.javase.lcdui;
+
+import cc.squirreljme.runtime.lcdui.server.LcdDefinition;
+
+/**
+ * This contains the implementation of the LCDUI server which utilizes Swing
+ * to display graphics to the user.
+ *
+ * @since 2018/03/15
+ */
+public class SwingDefinition
+	extends LcdDefinition
+{
+}
+
---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/server/LcdDefinition.java
@@ -0,0 +1,51 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.server;
+
+import cc.squirreljme.runtime.cldc.service.ServiceDefinition;
+import cc.squirreljme.runtime.cldc.service.ServiceServer;
+import cc.squirreljme.runtime.cldc.task.SystemTask;
+import cc.squirreljme.runtime.lcdui.LcdClientManagerProvider;
+
+/**
+ * This class implements the base of the graphical LCDUI display system which
+ * is used by the public facing LCDUI code to enable the use of graphics.
+ *
+ * @since 2018/03/15
+ */
+public abstract class LcdDefinition
+	extends ServiceDefinition
+{
+	/**
+	 * Initializes the base definition.
+	 *
+	 * @since 2018/03/15
+	 */
+	public LcdDefinition()
+	{
+		super(LcdClientManagerProvider.class);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/15
+	 */
+	@Override
+	public final ServiceServer newServer(SystemTask __task)
+		throws NullPointerException
+	{
+		if (__task == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+}
+
