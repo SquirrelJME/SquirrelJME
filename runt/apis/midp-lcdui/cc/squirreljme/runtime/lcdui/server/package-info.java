bfe6c03a93f71e8f820b5ec06d3bfd087de6db3f
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-15 16:06:55 +0000

    Add base for the LCDUI server.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/server/package-info.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the server which is used as a base for the
+ * implemntation of kernel side graphics API. All of the code is considered to
+ * be the client and is routed through the server.
+ *
+ * @since 2018/03/15
+ */
+
+package cc.squirreljme.runtime.lcdui.server;
+
