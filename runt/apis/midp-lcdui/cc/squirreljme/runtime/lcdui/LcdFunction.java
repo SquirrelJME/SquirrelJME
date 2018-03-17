1ef2bbd5dcdcdf7eb8e6c4794592e840c9b2ff7c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-17 01:21:50 +0000

    Add base for LcdFunction.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/LcdFunction.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui;
+
+/**
+ * This represents a function for the LCDUI interface for system calls, all
+ * service calls to the host interface server.
+ *
+ * @since 2018/03/16
+ */
+public enum LcdFunction
+{
+	/** End. */
+	;
+}
+
