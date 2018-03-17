67d56dafb6ef1a8d4998fd488c1ed8946acd4c83
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-17 23:36:17 +0000

    Removal of some code for the event loop refactor.

--- bldt/javase/libs/javase-runtime/cc/squirreljme/runtime/javase/lcdui/SwingDefinition.java
+++ bldt/javase/libs/javase-runtime/cc/squirreljme/runtime/javase/lcdui/SwingDefinition.java
@@ -11,12 +11,9 @@
 package cc.squirreljme.runtime.javase.lcdui;
 
 import cc.squirreljme.runtime.cldc.task.SystemTask;
-import cc.squirreljme.runtime.lcdui.DisplayableType;
 import cc.squirreljme.runtime.lcdui.server.LcdDefinition;
-import cc.squirreljme.runtime.lcdui.server.LcdDisplay;
-import cc.squirreljme.runtime.lcdui.server.LcdDisplayable;
 import cc.squirreljme.runtime.lcdui.server.LcdRequestHandler;
-import cc.squirreljme.runtime.lcdui.server.LcdServer;
+import javax.swing.SwingUtilities;
 
 /**
  * This contains the implementation of the LCDUI server which utilizes Swing
@@ -36,56 +33,6 @@ public class SwingDefinition
 	{
 		// Have swing always invoke the request handler
 		SwingUtilities.invokeLater(this.requestHandler());
-	}
-	
-	
-	
-	
-	/**
-	 * {@inheritDoc}
-	 * @since 2018/03/17
-	 */
-	@Override
-	protected LcdDisplayable internalCreateDisplayable(Object __lock,
-		int __handle, SystemTask __task, DisplayableType __type)
-		throws NullPointerException
-	{
-		if (__lock == null || __task == null || __type == null)
-			throw new NullPointerException("NARG");
-		
-		return new SwingDisplayable(__lock, __handle, __task, __type);
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 * @since 2018/03/17
-	 */
-	@Override
-	protected LcdDisplay[] internalQueryDisplays(LcdDisplay[] __k)
-		throws NullPointerException
-	{
-		if (__k == null)
-			throw new NullPointerException("NARG");
-		
-		// Swing only uses a single display which is shared among all
-		// programs
-		if (__k.length == 0)
-			return new LcdDisplay[]{new SwingDisplay(this.lock, 0)};
-		return __k;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 * @since 2018/03/16
-	 */
-	@Override
-	protected LcdServer newLcdServer(SystemTask __task)
-		throws NullPointerException
-	{
-		if (__task == null)
-			throw new NullPointerException("NARG");
-		
-		return new SwingServer(__task, this);
 	}
 }
 
