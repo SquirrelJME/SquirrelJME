1bdd4407f4f160648fd9c7b0a20094693e60f32c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-18 21:14:39 +0000

    Have a blank drawing canvas; Add base for LcdCallback which contains methods for callback.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/LcdCallback.java
@@ -0,0 +1,27 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui;
+
+/**
+ * This represents the type of callback function is being requested by the
+ * server side.
+ *
+ * @since 2018/03/18
+ */
+public enum LcdCallback
+{
+	/** Paint graphics. */
+	PAINT,
+	
+	/** End. */
+	;
+}
+
