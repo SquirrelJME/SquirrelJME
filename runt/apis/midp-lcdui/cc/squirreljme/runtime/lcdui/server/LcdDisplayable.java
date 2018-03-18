e0df45e56524fd16ab1abe3feee76d07956d7ab6
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-18 14:38:51 +0000

    Initialization of displayables class.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/server/LcdDisplayable.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.server;
+
+/**
+ * This represents a single displayable which may be shown on a Display as
+ * required.
+ *
+ * @since 2018/03/18
+ */
+public abstract class LcdDisplayable
+{
+}
+
---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/server/LcdDisplayables.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.server;
+
+/**
+ * This represents and manages all of the displayables which are available to
+ * the display server.
+ *
+ * @since 2018/03/18
+ */
+public abstract class LcdDisplayables
+{
+}
+
