fb21fed0fc69fce38a6a4b0a60a81adeb261e344
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-19 03:05:54 +0000

    Add base for MegaTileIndex.

---
+++ runt/mids/squirrel-quarrel/net/multiphasicapps/squirrelquarrel/world/MegaTileIndex.java
@@ -0,0 +1,82 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel.world;
+
+/**
+ * This represents an index into a megatile.
+ *
+ * @since 2018/03/18
+ */
+public final class MegaTileIndex
+	implements Comparable<MegaTileIndex>
+{
+	/** The x coordinate. */
+	protected final int x;
+	
+	/** The y coordinate. */
+	protected final int y;
+	
+	/** The ordinal index. */
+	protected final ordinal;
+	
+	/**
+	 * Initializes the mega tile index.
+	 *
+	 * @param __x The x coordinate.
+	 * @param __y The y coordinate.
+	 * @since 2018/03/18
+	 */
+	public MegaTileIndex(int __x, int __y)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/18
+	 */
+	@Override
+	public final boolean compareTo(MegaTileIndex __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/18
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/18
+	 */
+	@Override
+	public final int hashCode()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/18
+	 */
+	@Override
+	public final String toString()
+	{
+		throw new todo.TODO();
+	}
+}
+
