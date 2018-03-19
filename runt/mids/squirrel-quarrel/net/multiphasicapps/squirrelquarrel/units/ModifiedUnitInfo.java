0b19948b1b38a0ca88f7da2bcf377ce1060c518a
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-19 03:02:42 +0000

    Add base for ModifiedUnitInfo.

---
+++ runt/mids/squirrel-quarrel/net/multiphasicapps/squirrelquarrel/units/ModifiedUnitInfo.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel.units;
+
+/**
+ * This class allows for the modification of unit information which is used
+ * to manage such stuff as upgrades which happen over the course of the game.
+ *
+ * @since 2018/03/18
+ */
+public final class ModifiedUnitInfo
+{
+}
+
