3f5fdcf9e2497a29bc63d41150fc35fdeec25755
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-20 02:09:37 +0000

    Add basic headers which would define types and such for common buffer formats.

---
+++ utils-dev/arraygfx/type/byte_i.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// --------------------------------------------------------------------------*/
+
+/**
+ * Byte indexed.
+ *
+ * @since 2018/03/19
+ */
+
+/** Header guard. */
+#ifndef SJME_hGUTILSDEVARRAYE_IHBYTE_IH
+#define SJME_hGUTILSDEVARRAYE_IHBYTE_IH
+
+/** Anti-C++. */
+#ifdef _cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_cXUTILSDEVARRAYE_IHBYTE_IH
+extern "C"
+{
+#endif /** #ifdef SJME_CXX_IS_EXTERNED */
+#endif /** #ifdef __cplusplus */
+
+/****************************************************************************/
+
+/****************************************************************************/
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_cXUTILSDEVARRAYE_IHBYTE_IH
+}
+#undef SJME_cXUTILSDEVARRAYE_IHBYTE_IH
+#undef SJME_CXX_IS_EXTERNED
+#endif /** #ifdef SJME_cXUTILSDEVARRAYE_IHBYTE_IH */
+#endif /** #ifdef __cplusplus */
+
+/** Header guard. */
+#endif /* #ifndef SJME_hGUTILSDEVARRAYE_IHBYTE_IH */
+
---
+++ utils-dev/arraygfx/type/short_i.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// --------------------------------------------------------------------------*/
+
+/**
+ * Short Indexed.
+ *
+ * @since 2018/03/19
+ */
+
+/** Header guard. */
+#ifndef SJME_hGUTILSDEVARRAY_IHSHORT_IH
+#define SJME_hGUTILSDEVARRAY_IHSHORT_IH
+
+/** Anti-C++. */
+#ifdef _cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_cXUTILSDEVARRAY_IHSHORT_IH
+extern "C"
+{
+#endif /** #ifdef SJME_CXX_IS_EXTERNED */
+#endif /** #ifdef __cplusplus */
+
+/****************************************************************************/
+
+/****************************************************************************/
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_cXUTILSDEVARRAY_IHSHORT_IH
+}
+#undef SJME_cXUTILSDEVARRAY_IHSHORT_IH
+#undef SJME_CXX_IS_EXTERNED
+#endif /** #ifdef SJME_cXUTILSDEVARRAY_IHSHORT_IH */
+#endif /** #ifdef __cplusplus */
+
+/** Header guard. */
+#endif /* #ifndef SJME_hGUTILSDEVARRAY_IHSHORT_IH */
+
