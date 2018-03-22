3f99871478c679969f8b263e7254a73562342bf9
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-22 16:03:43 +0000

    Add base for ClassContainerLayout.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/layout/ClassContainerLayout.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.layout;
+
+/**
+ * This contains the layout for the class container which essentially is the
+ * outer portion of the class declaration which includes modifiers (including
+ * annotations), classnames, possible generic type declarations, extends, and
+ * implements.
+ *
+ * @since 2018/03/22
+ */
+public final class ClassContainerLayout
+	implements Layout
+{
+}
+
