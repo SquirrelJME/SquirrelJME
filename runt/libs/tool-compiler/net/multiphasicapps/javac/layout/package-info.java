72209af78b73a067899edcfb7f123cfcaf053378
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-22 15:38:52 +0000

    Add base for layout package.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/layout/package-info.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the classes which are used to describe a depth
+ * based layout of a source code file. Only simple parsing of a class file is
+ * done and much of it would be deferred for when the information is actually
+ * needed by a processor or compiler.
+ *
+ * @since 2018/03/22
+ */
+
+package net.multiphasicapps.javac.layout;
+
