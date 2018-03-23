1d8991162aa6af19c4f08016f72c6be5cecf0d18
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-23 21:14:59 +0000

    Some minor work.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/server/LcdWidgetFactory.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.server;
+
+/**
+ * This class is used for the creation of widgets as needed, this includes
+ * displays, displayables, and items.
+ *
+ * @since 2018/03/23
+ */
+public abstract class LcdWidgetFactory
+{
+}
+
