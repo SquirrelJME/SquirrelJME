15cf597ee5fe983535bf09bc5fc03957f192dd8c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-23 17:53:09 +0000

    Add new project and have a base for the graphics demo.

---
+++ runt/mids/graphics-demo/net/multiphasicapps/gfxdemo/DemoCanvas.java
@@ -0,0 +1,35 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.gfxdemo;
+
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
+/**
+ * This is the demonstration canvas which renders all of the kinds of graphics
+ * to the screen for demonstration and testing purposes.
+ *
+ * @since 2018/03/23
+ */
+public class DemoCanvas
+	extends Canvas
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	protected final void paint(Graphics __a)
+	{
+	}
+}
+
