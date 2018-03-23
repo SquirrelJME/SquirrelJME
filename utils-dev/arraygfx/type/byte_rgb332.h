1c2373080832ff69f4b564b49b66f257fbe7d2d6
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-23 16:44:22 +0000

    Clip code from the type headers; Base methods for the TemplateArrayGraphics.

--- utils-dev/arraygfx/type/byte_rgb332.h
+++ utils-dev/arraygfx/type/byte_rgb332.h
@@ -1,46 +1,4 @@
-/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
-// ---------------------------------------------------------------------------
-// Multi-Phasic Applications: SquirrelJME
-//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
-//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
-// ---------------------------------------------------------------------------
-// SquirrelJME is under the GNU General Public License v3+, or later.
-// See license.mkd for licensing and copyright information.
-// --------------------------------------------------------------------------*/
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
 
-/**
- * Byte RGB332.
- *
- * @since 2018/03/19
- */
-
-/** Header guard. */
-#ifndef SJME_hGUTILSDEVARRAYYTE_RGB332H
-#define SJME_hGUTILSDEVARRAYYTE_RGB332H
-
-/** Anti-C++. */
-#ifdef _cplusplus
-#ifndef SJME_CXX_IS_EXTERNED
-#define SJME_CXX_IS_EXTERNED
-#define SJME_cXUTILSDEVARRAYYTE_RGB332H
-extern "C"
-{
-#endif /** #ifdef SJME_CXX_IS_EXTERNED */
-#endif /** #ifdef __cplusplus */
-
-/****************************************************************************/
-
-/****************************************************************************/
-
-/** Anti-C++. */
-#ifdef __cplusplus
-#ifdef SJME_cXUTILSDEVARRAYYTE_RGB332H
-}
-#undef SJME_cXUTILSDEVARRAYYTE_RGB332H
-#undef SJME_CXX_IS_EXTERNED
-#endif /** #ifdef SJME_cXUTILSDEVARRAYYTE_RGB332H */
-#endif /** #ifdef __cplusplus */
-
-/** Header guard. */
-#endif /* #ifndef SJME_hGUTILSDEVARRAYYTE_RGB332H */
+#define TemplateArrayGraphics ByteRGB332ArrayGraphics
 
