0d331c55c19fe11e7542dd43b6c5af232f618d9f
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-24 17:55:11 +0000

    Add base classes generated from templates for all the graphics types.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/gfx/IntegerRGB888ArrayGraphics.java
@@ -0,0 +1,547 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.gfx;
+
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.Text;
+
+/**
+ * This class is automatically generated to from a template to support
+ * multiple pixel formats which are backed by arrays.
+ *
+ * @since 2018/03/22
+ */
+public final class IntegerRGB888ArrayGraphics
+	extends Graphics
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void clipRect(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void copyArea(int __sx, int __sy, int __w, int __h,
+		int __dx, int __dy, int __anchor)
+	throws IllegalArgumentException, IllegalStateException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawArc(int __x, int __y, int __w, int __h, int __sa,
+		int __aa)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawARGB16(short[] __data, int __off, int __scanlen,
+		int __x, int __y, int __w, int __h)
+	throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawChar(char __s, int __x, int __y, int __anchor)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawChars(char[] __s, int __o, int __l, int __x,
+		int __y, int __anchor)
+	throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawImage(Image __i, int __x, int __y, int __anchor)
+	throws IllegalArgumentException, NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawLine(int __x1, int __y1, int __x2, int __y2)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawRGB(int[] __data, int __off, int __scanlen,
+		int __x, int __y, int __w, int __h, boolean __alpha)
+	throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawRGB16(short[] __data, int __off, int __scanlen,
+		int __x, int __y, int __w, int __h)
+	throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawRect(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawRegion(Image __src, int __xsrc, int __ysrc,
+		int __wsrc, int __hsrc, int __trans, int __xdest, int __ydest,
+		int __anch)
+	throws IllegalArgumentException, NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawRegion(Image __src, int __xsrc, int __ysrc,
+		int __wsrc, int __hsrc, int __trans, int __xdest, int __ydest,
+		int __anch, int __wdest, int __hdest)
+	throws IllegalArgumentException, NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawRoundRect(int __x, int __y, int __w, int __h,
+		int __aw, int __ah)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawString(String __s, int __x, int __y,
+		int __anchor)
+	throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawSubstring(String __s, int __o, int __l, int __x,
+		int __y, int __anchor)
+	throws NullPointerException, StringIndexOutOfBoundsException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void drawText(Text __t, int __x, int __y)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void fillArc(int __x, int __y, int __w, int __h, int __sa,
+		int __aa)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void fillRect(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void fillRoundRect(int __x, int __y, int __w, int __h,
+		int __aw, int __ah)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void fillTriangle(int __x1, int __y1, int __x2, int __y2,
+		int __x3, int __y3)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getAlpha()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getAlphaColor()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getBlendingMode()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getBlueComponent()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getClipHeight()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getClipWidth()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getClipX()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getClipY()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getColor()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getDisplayColor(int __rgb)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final Font getFont()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getGrayScale()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getGreenComponent()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getRedComponent()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getStrokeStyle()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getTranslateX()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final int getTranslateY()
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setAlpha(int __a)
+	throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setAlphaColor(int __argb)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setAlphaColor(int __a, int __r, int __g, int __b)
+	throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setBlendingMode(int __m)
+	throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setClip(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setColor(int __rgb)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setColor(int __r, int __g, int __b)
+	throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setFont(Font __a)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setGrayScale(int __v)
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void setStrokeStyle(int __a)
+	throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/23
+	 */
+	@Override
+	public final void translate(int __x, int __y)
+	{
+		throw new todo.TODO();
+	}
+}
