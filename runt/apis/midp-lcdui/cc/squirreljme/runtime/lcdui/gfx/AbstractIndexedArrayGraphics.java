f3e9ac0949ee66d5498d035f99d55b9af68f82a0
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-25 05:48:17 +0000

    Replace all the format classes with new classes which hopefully will end being far simpler and still hopefully fast enough (but should be good enough) because the template graphics is just far too complex and messy, the loss of maintainability is not worth the speed benefits.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/gfx/AbstractIndexedArrayGraphics.java
@@ -0,0 +1,65 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.gfx;
+
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.Text;
+
+/**
+ * This class acts as the base for graphics drawers which are based on palettes
+ * rather than a RGB color model.
+ *
+ * @since 2018/03/25
+ */
+public abstract class AbstractIndexedArrayGraphics
+	extends AbstractArrayGraphics
+{
+	/** The palette data. */
+	protected final int[] palette;
+	
+	/** The number of colors in the palette. */
+	protected final int numcolors;
+	
+	/**
+	 * Initializes the base graphics.
+	 *
+	 * @param __w The image width.
+	 * @param __h The image height.
+	 * @param __p The image pitch.
+	 * @param __o The buffer offset.
+	 * @param __l The length of the buffer.
+	 * @param __pal The palette.
+	 * @param __ppe Pixels per element.
+	 * @param __numc The number of colors in the palette.
+	 * @throws IllegalArgumentException If the palette is too small.
+	 * @throws NullPointerException If no palette was specified.
+	 * @since 2018/03/25
+	 */
+	public AbstractIndexedArrayGraphics(int __w, int __h, int __p, int __o,
+		int __l, int __ppe, int[] __pal, int __numc)
+		throws IllegalArgumentException, NullPointerException
+	{
+		super(__w, __h, __p, __o, __l, __ppe);
+		
+		if (__pal == null)
+			throw new NullPointerException("NARG");
+		
+		// {@squirreljme.error EB2f The input palette does not have enough
+		// entries to store color information.}
+		if (__pal.length < __numc)
+			throw new IllegalArgumentException("EB2f");
+		
+		throw new todo.TODO();
+	}
+}
+
---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/gfx/AbstractRGBArrayGraphics.java
@@ -0,0 +1,92 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.gfx;
+
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.Text;
+
+/**
+ * This class acts as the base for RGB based graphics.
+ *
+ * @since 2018/03/25
+ */
+public abstract class AbstractRGBArrayGraphics
+	extends AbstractArrayGraphics
+{
+	/** Is there an alpha channel for this. */
+	protected final boolean hasalpha;
+	
+	/** The alpha mask. */
+	protected final int alphamask;
+	
+	/** The alpha shift. */
+	protected final int alphashift;
+	
+	/** The red mask. */
+	protected final int redmask;
+	
+	/** The red shift. */
+	protected final int redshift;
+	
+	/** The green mask. */
+	protected final int greenmask;
+	
+	/** The green shift. */
+	protected final int greenshift;
+	
+	/** The blue mask. */
+	protected final int bluemask;
+	
+	/** The blue shift. */
+	protected final int blueshift;
+	
+	/**
+	 * Initializes the base graphics.
+	 *
+	 * @param __w The image width.
+	 * @param __h The image height.
+	 * @param __p The image pitch.
+	 * @param __o The buffer offset.
+	 * @param __l The length of the buffer.
+	 * @param __ppe The number of pixels per element.
+	 * @param __alpha Is there an alpha specified?
+	 * @param __amask Alpha mask.
+	 * @param __rmask Red mask.
+	 * @param __gmask Green mask.
+	 * @param __bmask Blue mask.
+	 * @throws IllegalArgumentException If the input parameters are not
+	 * correct.
+	 * @throws NullPointerException If no palette was specified.
+	 * @since 2018/03/25
+	 */
+	public AbstractRGBArrayGraphics(int __w, int __h, int __p, int __o,
+		int __l, int __ppe, boolean __alpha, int __amask, int __rmask,
+		int __gmask, int __bmask)
+		throws IllegalArgumentException, NullPointerException
+	{
+		super(__w, __h, __p, __o, __l, __ppe);
+		
+		this.hasalpha = __alpha;
+		
+		// Set shifts and masks
+		this.alphamask = __amask;
+		this.alphashift = Integer.numberOfTrailingZeros(__amask);
+		this.redmask = __rmask;
+		this.redshift = Integer.numberOfTrailingZeros(__rmask);
+		this.greenmask = __gmask;
+		this.greenshift = Integer.numberOfTrailingZeros(__gmask);
+		this.bluemask = __bmask;
+		this.blueshift = Integer.numberOfTrailingZeros(__bmask);
+	}
+}
+
