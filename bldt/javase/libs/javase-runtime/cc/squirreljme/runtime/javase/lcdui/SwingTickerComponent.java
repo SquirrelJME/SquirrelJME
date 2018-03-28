103e44d8c9ccd130a9375f4aab3f29dc7ef6e924
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-03-28 01:17:26 +0000

    If the ticker text is too big to displayed it will bounce left and right to show the message.

--- bldt/javase/libs/javase-runtime/cc/squirreljme/runtime/javase/lcdui/SwingTickerComponent.java
+++ bldt/javase/libs/javase-runtime/cc/squirreljme/runtime/javase/lcdui/SwingTickerComponent.java
@@ -12,8 +12,21 @@ package cc.squirreljme.runtime.javase.lcdui;
 
 import cc.squirreljme.runtime.lcdui.server.LcdTicker;
 import cc.squirreljme.runtime.lcdui.server.LcdTickerListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
+import java.awt.Point;
+import java.awt.Rectangle;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
+import javax.swing.Timer;
 
 /**
  * This is the compoent which is shown to display text.
@@ -21,11 +34,18 @@ import javax.swing.JPanel;
  * @since 2018/03/27
  */
 public class SwingTickerComponent
-	extends JPanel
-	implements LcdTickerListener
+	extends JScrollPane
+	implements ActionListener, ComponentListener, LcdTickerListener
 {
 	/** The label. */
 	protected final JLabel label;
+	
+	/** Timer used to scroll things. */
+	protected final Timer timer;
+	
+	/** Moving to the right? */
+	private boolean _right =
+		true;
 	
 	/**
 	 * Initializes the component.
@@ -34,22 +54,143 @@ public class SwingTickerComponent
 	 */
 	public SwingTickerComponent()
 	{
+		super(JScrollPane.VERTICAL_SCROLLBAR_NEVER,
+			JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);//AS_NEEDED);
+		
 		JLabel label = new JLabel();
 		this.label = label;
 		
 		// Only the label is here
-		this.add(label);
+		this.setViewportView(label);
+		this.addComponentListener(this);
+		
+		// Setup base timer for scrolling
+		Timer timer = new Timer(200, this);
+		this.timer = timer;
+		
+		// Make it so the user cannot scroll the message
+		this.setWheelScrollingEnabled(false);
 	}
 	
 	/**
 	 * {@inheritDoc}
-	 * @since 2018/03/26
+	 * @since 2018/03/27
+	 */
+	@Override
+	public void actionPerformed(ActionEvent __e)
+	{
+		JViewport vp = this.getViewport();
+		JLabel label = this.label;
+		
+		// Moving to the right?
+		boolean right = this._right;
+		
+		// Determine where we are and the end position
+		Point pos = vp.getViewPosition();
+		int vsx = pos.x,
+			vsw = vp.getSize().width,
+			lbw = label.getWidth(),
+			ren = vsx + vsw;
+		
+		// Calculate new position
+		int nxx = vsx + (right ? 10 : -10);
+		
+		// Change position?
+		if (nxx < 0)
+			this._right = true;
+		else if (ren >= lbw)
+			this._right = false;
+		
+		// Set new position
+		vp.setViewPosition(new Point(nxx, pos.y));
+		//vp.revalidate();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/27
+	 */
+	@Override
+	public void componentHidden(ComponentEvent __e)
+	{
+		Timer timer = this.timer;
+		
+		// Do not do scrolling if it is not even seen
+		if (timer.isRunning())
+			this.timer.stop();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/27
+	 */
+	@Override
+	public void componentMoved(ComponentEvent __e)
+	{
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/27
+	 */
+	@Override
+	public void componentResized(ComponentEvent __e)
+	{
+		Timer timer = this.timer;
+		boolean running = timer.isRunning();
+		
+		if (this.__runTimer())
+		{
+			if (!running)
+				this.timer.start();
+		}
+		else
+		{
+			if (running)
+				this.timer.stop();
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/27
+	 */
+	@Override
+	public void componentShown(ComponentEvent __e)
+	{
+		Timer timer = this.timer;
+		
+		if (this.__runTimer())
+			if (!timer.isRunning())
+				this.timer.start();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/27
 	 */
 	@Override
 	public void textChanged(LcdTicker __lcd, String __text)
 		throws NullPointerException
 	{
 		this.label.setText(__text);
+	}
+	
+	/**
+	 * Checks whether the timer should be ran.
+	 *
+	 * @return If the timer should be ran.
+	 * @since 2018/03/27
+	 */
+	private final boolean __runTimer()
+	{
+		JLabel label = this.label;
+		
+		// Do not run when it is invisible
+		if (!this.isVisible() || !label.isVisible())
+			return false;
+		
+		return this.getViewport().getSize().width < label.getWidth();
 	}
 }
 
