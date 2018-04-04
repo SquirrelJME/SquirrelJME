722d1f075bf6ba7f605b4d18f4f41e0102a6e693
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-04 13:50:12 +0000

    Add base ui sub-package.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/ui/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * Server side user interface interfaces which are used to describe UI
+ * elements and such.
+ *
+ * @since 2018/04/04
+ */
+
+package cc.squirreljme.runtime.lcdui.ui;
+
