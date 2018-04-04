3a8bfe290e3585b5b647294b72ccfab0c12125d0
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-04 16:39:48 +0000

    Add more base interfaces and classes.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/ui/UiInputEvents.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.ui;
+
+/**
+ * This represents an interface which may generate input events to be passed
+ * to the client code for handling.
+ *
+ * @since 2018/04/04
+ */
+public interface UiInputEvents
+{
+}
+
---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/ui/UiMenuLayout.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.ui;
+
+/**
+ * This class contains the actual menu layout which is used to construct how
+ * a menu is laid out for when it is displayed.
+ *
+ * @since 2018/04/04
+ */
+public final class UiMenuLayout
+{
+}
+
