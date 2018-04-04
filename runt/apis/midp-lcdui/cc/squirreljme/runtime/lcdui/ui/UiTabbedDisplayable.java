1f0fea79ca7425116ee60f08d5b6b51d76186b70
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-04 15:29:50 +0000

    Add some more base interfaces.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/ui/UiTabbedDisplayable.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.ui;
+
+/**
+ * This represents a displayable which may be placed within a tabbed pane.
+ *
+ * @since 2018/04/04
+ */
+public interface UiTabbedDisplayable
+	extends UiWidget
+{
+}
+
