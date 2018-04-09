5fdaf43aa7f603d234b058d65aec6c2048609b37
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-09 04:16:06 +0000

    Add base for GenericBinaryName.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/layout/GenericBinaryName.java
@@ -0,0 +1,51 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.layout;
+
+/**
+ * This represents a a binary name which additionally has generic parameters
+ * associated with it.
+ *
+ * @since 2018/04/08
+ */
+public final class GenericBinaryName
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/04/08
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/04/08
+	 */
+	@Override
+	public final int hashCode()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/04/08
+	 */
+	@Override
+	public final String toString()
+	{
+		throw new todo.TODO();
+	}
+}
+
