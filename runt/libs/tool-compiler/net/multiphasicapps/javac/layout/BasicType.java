ccc3dbdbc71758af9453649d99715ce10b0b56eb
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-09 23:30:17 +0000

    Add base for BasicType and GenericType.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/layout/BasicType.java
@@ -0,0 +1,50 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.layout;
+
+/**
+ * This represents a basic type without generic type information.
+ *
+ * @since 2018/04/09
+ */
+public final class BasicType
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/04/09
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/04/09
+	 */
+	@Override
+	public final int hashCode()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/04/09
+	 */
+	@Override
+	public final String toString()
+	{
+		throw new todo.TODO();
+	}
+}
+
