f0bcef7d21ac7b8fbd3137ee7b4c5a04d4d560b9
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-11 14:11:54 +0000

    Implement ExpandingPeeker.

--- runt/libs/tool-compiler/net/multiphasicapps/javac/token/ExpandingPeeker.java
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/token/ExpandingPeeker.java
@@ -10,6 +10,9 @@
 
 package net.multiphasicapps.javac.token;
 
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+
 /**
  * This is essentially a fork of an {@link ExpandingSource} which uses the
  * forward peek operation to obtain tokens as needed. This allows this to be
@@ -18,6 +21,72 @@ package net.multiphasicapps.javac.token;
  * @since 2018/04/11
  */
 public final class ExpandingPeeker
+	extends ExpandingSource
 {
+	/** The base source to peek from. */
+	protected final ExpandingSource base;
+	
+	/** The number of tokens returned from the base. */
+	protected final int basecount;
+	
+	/** The number of tokens to forward. */
+	private int _forward;
+	
+	/**
+	 * Initializes the expanding peeker using the given as a base.
+	 *
+	 * @param __base The base source.
+	 * @param __forward The number of base tokens to peek to start with.
+	 * @throws IllegalArgumentException If the forward token count is
+	 * negative.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/04/11
+	 */
+	public ExpandingPeeker(ExpandingSource __base, int __forward)
+		throws IllegalArgumentException, NullPointerException
+	{
+		if (__base == null)
+			throw new NullPointerException("NARG");
+		
+		// {@squirreljme.error AQ2v Cannot forward a negative number of
+		// tokens.}
+		if (__forward < 0)
+			throw new IllegalArgumentException("AQ2v");
+		
+		this.base = __base;
+		this.basecount = __base.count();
+		this._forward = __forward;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/04/11
+	 */
+	@Override
+	public final void close()
+		throws IOException
+	{
+		this.base.close();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @throws ConcurrentModificationException If the base source returned a
+	 * next token when it is not valid to use splits after that has happened.
+	 * @since 2018/04/11
+	 */
+	@Override
+	protected final ExpandedToken readNext()
+		throws ConcurrentModificationException, TokenizerException
+	{
+		// {@squirreljme.error AQ2w The expanding tokenizer split is not valid
+		// because the source
+		ExpandingSource base = this.base;
+		if (this.basecount != base.count())
+			throw new ConcurrentModificationException("AQ2w");
+		
+		// Always peek
+		return base.peek(this._forward++);
+	}
 }
 
