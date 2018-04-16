55b3229f81a7206ff9b6f661baa7a28e98c3c0de
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-16 01:03:42 +0000

    Add base parse methods.

--- runt/libs/tool-compiler/net/multiphasicapps/javac/lexical/NormalInterfaceDeclaration.java
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/lexical/NormalInterfaceDeclaration.java
@@ -10,6 +10,10 @@
 
 package net.multiphasicapps.javac.lexical;
 
+import net.multiphasicapps.javac.token.ExpandedToken;
+import net.multiphasicapps.javac.token.ExpandingSource;
+import net.multiphasicapps.javac.token.TokenType;
+
 /**
  * This represents a declaration of a normal interface.
  *
@@ -45,6 +49,24 @@ public final class NormalInterfaceDeclaration
 	@Override
 	public final String toString()
 	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Parses a normal interface.
+	 *
+	 * @param __t The input token source.
+	 * @return The parsed type.
+	 * @throws LexicalStructureException If the class is not valid.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/04/15
+	 */
+	public static NormalInterfaceDeclaration parse(ExpandingSource __t)
+		throws LexicalStructureException, NullPointerException
+	{
+		if (__t == null)
+			throw new NullPointerException("NARG");
+		
 		throw new todo.TODO();
 	}
 }
