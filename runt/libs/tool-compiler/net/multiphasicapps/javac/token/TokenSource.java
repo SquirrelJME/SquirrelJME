4f1979d1f6535525576202f2523373de4c7875f9
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-18 23:33:26 +0000

    Add interface TokenSource which provides input for tokens.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/token/TokenSource.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.token;
+
+/**
+ * This interface represents a source for tokens.
+ *
+ * @since 2018/04/18
+ */
+public interface TokenSource
+{
+	/**
+	 * Returns the next token from the input source.
+	 *
+	 * @return The next token, if there are no more tokens left then a special
+	 * end of file token should be used instead.
+	 * @throws TokenizerException If the read input token is not valid.
+	 * @since 2018/04/18
+	 */
+	public abstract Token next()
+		throws TokenizerException;
+}
+
