7119d7eaadd8bb964aa1255b85f8dcadd2497cb8
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-22 02:13:19 +0000

    Add base package which contain simplified class structures.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/structure/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the information about classes and members.
+ *
+ * @since 2018/04/21
+ */
+
+package net.multiphasicapps.javac.structure;
+
