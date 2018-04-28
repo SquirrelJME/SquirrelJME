d080b9cd4525242324812a48d80e8c36060049c2
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-28 04:11:58 +0000

    Base for MethodStructure.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/structure/MethodStructure.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.structure;
+
+/**
+ * This represents a method which is a member of a class and may contain Java
+ * code to be compiled.
+ *
+ * @since 2018/04/27
+ */
+public final class MethodStructure
+	implements MemberStructure
+{
+}
+
