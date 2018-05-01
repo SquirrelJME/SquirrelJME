c9dc11ecec583a3eaac82865158d49702afe518d
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-01 00:46:08 +0000

    Initialization of compilation units.

--- runt/libs/tool-compiler/net/multiphasicapps/javac/structure/CompilationUnit.java
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/structure/CompilationUnit.java
@@ -11,6 +11,7 @@
 package net.multiphasicapps.javac.structure;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -27,6 +28,118 @@ import net.multiphasicapps.javac.token.TokenType;
  */
 public final class CompilationUnit
 {
+	/** The modifiers for the package. */
+	protected final Modifiers modifiers;
+	
+	/** The package this compilation unit is in. */
+	protected final QualifiedIdentifier inpackage;
+	
+	/** The imports being performed. */
+	private final ImportStatement[] _imports;
+	
+	/** The classes being declared. */
+	private final ClassStructure[] _classes;
+	
+	/**
+	 * Initializes the compilation unit.
+	 *
+	 * @param __pmod The modifiers to the package.
+	 * @param __pk The owning package.
+	 * @throws NullPointerException On null arguments.
+	 * @throws StructureDefinitionException If the compilation unit is not
+	 * valid.
+	 * @since 2018/04/30
+	 */
+	public CompilationUnit(Modifiers __pmod, QualifiedIdentifier __pk)
+		throws NullPointerException, StructureDefinitionException
+	{
+		if (__pmod == null || __pk == null)
+			throw new NullPointerException("NARG");
+		
+		// {@squirreljme.error AQ4u Only annotation are valid modifiers for
+		// packages.}
+		if (__pmod.isPublic() || __pmod.isProtected() || __pmod.isPrivate() ||
+			__pmod.isStatic() || __pmod.isAbstract() || __pmod.isFinal() ||
+			__pmod.isNative() || __pmod.isSynchronized() ||
+			__pmod.isTransient() || __pmod.isVolatile() ||
+			__pmod.isStrictFloatingPoint())
+			throw new StructureDefinitionException(
+				String.format("AQ4u %s", __pmod));
+		
+		this.modifiers = __pmod;
+		this.inpackage = __pk;
+		this._imports = new ImportStatement[0];
+		this._classes = new ClassStructure[0];
+	}
+	
+	/**
+	 * Initializes the compilation unit.
+	 *
+	 * @param __pk The owning package.
+	 * @param __imports Imports that are used.
+	 * @param __classes Classes which have been declared.
+	 * @throws NullPointerException On null arguments.
+	 * @throws StructureDefinitionException If the compilation unit is not
+	 * valid.
+	 * @since 2018/04/30
+	 */
+	public CompilationUnit(QualifiedIdentifier __pk,
+		ImportStatement[] __imports, ClassStructure[] __classes)
+		throws NullPointerException, StructureDefinitionException
+	{
+		this(__pk, Arrays.<ImportStatement>asList(
+			__imports == null ? new ImportStatement[0] : __imports),
+			Arrays.<ClassStructure>asList(__classes == null ?
+			new ClassStructure[0] : __classes));
+	}
+	
+	/**
+	 * Initializes the compilation unit.
+	 *
+	 * @param __pk The owning package.
+	 * @param __imports Imports that are used.
+	 * @param __classes Classes which have been declared.
+	 * @throws NullPointerException On null arguments.
+	 * @throws StructureDefinitionException If the compilation unit is not
+	 * valid.
+	 * @since 2018/04/30
+	 */
+	public CompilationUnit(QualifiedIdentifier __pk,
+		Iterable<ImportStatement> __imports,
+		Iterable<ClassStructure> __classes)
+		throws NullPointerException, StructureDefinitionException
+	{
+		if (__pk == null || __imports == null || __classes == null)
+			throw new NullPointerException("NARG");
+		
+		// Check imports
+		Set<ImportStatement> imports = new LinkedHashSet<>();
+		for (ImportStatement v : __imports)
+		{
+			if (v == null)
+				throw new NullPointerException("NARG");
+			
+			imports.add(v);
+		}
+		
+		// Check classes
+		Set<ClassStructure> classes = new LinkedHashSet<>();
+		for (ClassStructure v : __classes)
+		{
+			if (v == null)
+				throw new NullPointerException("NARG");
+			
+			classes.add(v);
+		}
+		
+		this.modifiers = new Modifiers();
+		this.inpackage = __pk;
+		this._imports = imports.<ImportStatement>toArray(
+			new ImportStatement[imports.size()]);
+		this._classes = classes.<ClassStructure>toArray(
+			new ClassStructure[classes.size()]);
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 * @since 2018/04/21
@@ -112,7 +225,7 @@ public final class CompilationUnit
 					if (token.type() != TokenType.SYMBOL_SEMICOLON)
 						throw new StructureParseException(token, "AQ3f");
 				
-				throw new todo.TODO();
+				return new CompilationUnit(modifiers, inpackage);
 			}
 		}
 		
@@ -160,7 +273,7 @@ public final class CompilationUnit
 			modifiers = null;
 		}
 		
-		throw new todo.TODO();
+		return new CompilationUnit(inpackage, imports, classes);
 	}
 }
 
