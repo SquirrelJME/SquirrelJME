89b0d77e25860659c29cbd05b6a30ba9789c4970
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-01 23:53:35 +0000

    Read annotations but do not parse them for expressions yet.

--- runt/libs/tool-compiler/net/multiphasicapps/javac/syntax/AnnotationSyntax.java
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/syntax/AnnotationSyntax.java
@@ -10,6 +10,7 @@
 
 package net.multiphasicapps.javac.syntax;
 
+import java.util.Objects;
 import net.multiphasicapps.javac.token.BufferedTokenSource;
 import net.multiphasicapps.javac.token.Token;
 import net.multiphasicapps.javac.token.TokenSource;
@@ -23,6 +24,48 @@ import net.multiphasicapps.javac.token.TokenType;
 public final class AnnotationSyntax
 	implements ModifierSyntax
 {
+	/** The identifier which identifies the annotation. */
+	protected final QualifiedIdentifierSyntax identifier;
+	
+	/** The expressions which make up the annotation. */
+	protected final UnparsedExpressions arguments;
+	
+	/**
+	 * Initializes the annotation which is just a marker.
+	 *
+	 * @param __qi The identifier used to identify the annotation.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/05/01
+	 */
+	public AnnotationSyntax(QualifiedIdentifierSyntax __qi)
+		throws NullPointerException
+	{
+		if (__qi == null)
+			throw new NullPointerException("NARG");
+		
+		this.identifier = __qi;
+		this.arguments = null;
+	}
+	
+	/**
+	 * Initializes the annotation which contains the given unparsed arguments.
+	 *
+	 * @param __qi The identifier used to identify the annotation.
+	 * @param __ue The unparsed expression.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/05/01
+	 */
+	public AnnotationSyntax(QualifiedIdentifierSyntax __qi,
+		UnparsedExpressions __ue)
+		throws NullPointerException
+	{
+		if (__qi == null || __ue == null)
+			throw new NullPointerException("NARG");
+		
+		this.identifier = __qi;
+		this.arguments = __ue;
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 * @since 2018/04/21
@@ -40,7 +83,8 @@ public final class AnnotationSyntax
 	@Override
 	public final int hashCode()
 	{
-		throw new todo.TODO();
+		return this.identifier.hashCode() ^
+			Objects.hashCode(this.arguments);
 	}
 	
 	/**
@@ -68,7 +112,23 @@ public final class AnnotationSyntax
 		if (__in == null)
 			throw new NullPointerException("NARG");
 		
-		throw new todo.TODO();
+		// {@squirreljme.error AQ4v Expected at symbol at start of annotation.}
+		Token token = __in.next();
+		if (token.type() != TokenType.SYMBOL_AT)
+			throw new SyntaxParseException(token, "AQ4v");
+		
+		// Read qualified identifier which identifies the type used for the
+		// annotation
+		QualifiedIdentifierSyntax qi = QualifiedIdentifierSyntax.parse(__in);
+		
+		// If there is no open parenthesis then it is just a marker
+		token = __in.peek();
+		if (token.type() != TokenType.SYMBOL_OPEN_PARENTHESIS)
+			return new AnnotationSyntax(qi);
+		
+		// Just read the parenthesis block
+		return new AnnotationSyntax(qi,
+			UnparsedExpressions.parseArguments(__in));
 	}
 }
 
