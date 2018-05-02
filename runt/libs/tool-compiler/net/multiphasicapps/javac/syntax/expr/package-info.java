eead75fdfa2f8bfe3fd226f57db405ee911b5b3e
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-02 21:48:02 +0000

    Add base for expr sub-package which will contain all the expression representations for cleanliness.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/syntax/expr/package-info.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This class contains the representation and the parsers for expressions
+ * that exist within the language. Expressions for the most part contain
+ * actual code that may be executed.
+ *
+ * @since 2018/05/02
+ */
+
+package net.multiphasicapps.javac.syntax.expr;
+
