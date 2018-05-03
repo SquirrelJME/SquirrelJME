0158527fbd1c09fa99fdc214abb6974163b4b3c3
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-03 19:17:16 +0000

    Add base for structure package which will contain the ability to load inputs and such.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/structure/package-info.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the parser and the structure representation of the
+ * input class path and source code. This parses class files and source files
+ * and enables resolution of the virtual machine state.
+ *
+ * @since 2018/05/03
+ */
+
+package net.multiphasicapps.javac.structure;
+
