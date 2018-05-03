71c73368092d07add072cf0f89cfc233ed4cc093
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-03 19:38:48 +0000

    Base for some structure classes.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/structure/StructureSymbol.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.structure;
+
+/**
+ * This interface represents a symbol which can be used to lookup a class.
+ *
+ * @since 2018/05/03
+ */
+public interface StructureSymbol
+{
+}
+
