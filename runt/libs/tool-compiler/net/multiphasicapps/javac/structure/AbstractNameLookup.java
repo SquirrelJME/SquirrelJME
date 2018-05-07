d5936da0f90e2e747a7587bf513c80e3c9a90327
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-07 19:59:11 +0000

    Add base for AbstractNameLookup.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/structure/AbstractNameLookup.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.structure;
+
+/**
+ * This class manages the lookup for names and may recursively lookup what a
+ * name means depending on the scope.
+ *
+ * @since 2018/05/07
+ */
+public abstract class AbstractNameLookup
+{
+}
+
