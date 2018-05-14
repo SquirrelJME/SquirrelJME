80bf3e1e1e8d8beb0d96e45884b673ce72856c55
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-14 15:41:49 +0000

    Add some base classes in the class file parser code.

---
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/ExecutableCode.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.classfile;
+
+/**
+ * This interface represents anything that is executable and is associated
+ * with method code contained within a class.
+ *
+ * @since 2018/05/14
+ */
+public interface ExecutableCode
+{
+}
+
---
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/FormalParameter.java
@@ -0,0 +1,51 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.classfile;
+
+/**
+ * This represents a single formal parameter which is used as input for a
+ * method call.
+ *
+ * @since 2018/05/14
+ */
+public final class FormalParameter
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/15
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/15
+	 */
+	@Override
+	public final int hashCode()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/15
+	 */
+	@Override
+	public final String toString()
+	{
+		throw new todo.TODO();
+	}
+}
+
