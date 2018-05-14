577f505b1ffb8425979ef44936475969fd1c6a22
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-14 16:03:45 +0000

    Add base for Attribute and AttributeTable (will be a bit cleaner); Add base for a dump-class project which will be similar to `javap -verbose`.

---
+++ bldt/mids/dump-class/net/multiphasicapps/dumpclass/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the class dumper.
+ *
+ * @since 2018/05/14
+ */
+
+package net.multiphasicapps.dumpclass;
+
---
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/AttributeTable.java
@@ -0,0 +1,61 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.classfile;
+
+/**
+ * This represents the attribute table that exists within a class.
+ *
+ * @since 2018/05/14
+ */
+public final class AttributeTable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/15
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/15
+	 */
+	@Override
+	public final int hashCode()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Returns the length of the attribute table.
+	 *
+	 * @return The attribute table length.
+	 * @since 2018/05/14
+	 */
+	public final int length()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/15
+	 */
+	@Override
+	public final String toString()
+	{
+		throw new todo.TODO();
+	}
+}
+
