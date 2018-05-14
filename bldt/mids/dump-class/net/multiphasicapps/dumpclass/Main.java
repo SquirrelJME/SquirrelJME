b2fd1821fbf12c7ed18426d25fc72cc60328bfe1
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-14 16:18:59 +0000

    Correct dumping for JAR/ZIP files; Initial print of class name for dumping.

--- bldt/mids/dump-class/net/multiphasicapps/dumpclass/Main.java
+++ bldt/mids/dump-class/net/multiphasicapps/dumpclass/Main.java
@@ -12,6 +12,14 @@ package net.multiphasicapps.dumpclass;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import net.multiphasicapps.classfile.ClassFile;
+import net.multiphasicapps.classfile.InvalidClassFormatException;
+import net.multiphasicapps.zip.streamreader.ZipStreamEntry;
+import net.multiphasicapps.zip.streamreader.ZipStreamReader;
 
 /**
  * Main entry class for the dumper.
@@ -20,6 +28,25 @@ import java.io.InputStream;
  */
 public class Main
 {
+	/**
+	 * Dumps the class information.
+	 *
+	 * @param __out The output.
+	 * @param __in The input.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/05/14
+	 */
+	public static void dumpClass(PrintStream __out, ClassFile __in)
+		throws NullPointerException
+	{
+		if (__out == null || __in == null)
+			throw new NullPointerException("NARG");
+		
+		__out.printf("*** %s ***%n", __in.thisName());
+		
+		throw new todo.TODO();
+	}
+	
 	/**
 	 * Main entry point.
 	 *
@@ -32,7 +59,51 @@ public class Main
 		if (__args == null)
 			__args = new String[0];
 		
-		throw new todo.TODO();
+		// Dump all of them
+		PrintStream out = System.out;
+		for (String a : __args)
+		{
+			if (a == null)
+				continue;
+			
+			// Parse the class
+			try (InputStream in = Files.newInputStream(Paths.get(a),
+				StandardOpenOption.READ))
+			{
+				// Dump everything in the JAR/ZIP
+				if (a.endsWith(".jar") || a.endsWith(".zip"))
+					try (ZipStreamReader zip = new ZipStreamReader(in))
+					{
+						for (;;)
+							try (ZipStreamEntry ent = zip.nextEntry())
+							{
+								if (ent == null)
+									break;
+								
+								if (!ent.name().endsWith(".class"))
+									continue;
+								
+								Main.dumpClass(out, ClassFile.decode(ent));
+							}
+							catch (IOException|InvalidClassFormatException e)
+							{
+								e.printStackTrace();
+								continue;
+							}
+					}
+				
+				// Treat as class
+				else
+					Main.dumpClass(out, ClassFile.decode(in));
+			}
+			
+			// Note it
+			catch (IOException|InvalidClassFormatException e)
+			{
+				e.printStackTrace();
+				continue;
+			}
+		}
 	}
 }
 
