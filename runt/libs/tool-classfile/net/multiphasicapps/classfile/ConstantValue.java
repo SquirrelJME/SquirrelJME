9315003fc0b6285b736595802a367079b8c5e685
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-17 00:53:38 +0000

    Add base class for ConstantValue and ConstantValueType.

---
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/ConstantValue.java
@@ -0,0 +1,102 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.classfile;
+
+/**
+ * This represents a constant value.
+ *
+ * @since 2018/05/16
+ */
+public final class ConstantValue
+	extends Number
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/16
+	 */
+	@Override
+	public final double doubleValue()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/16
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/16
+	 */
+	@Override
+	public final float floatValue()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/16
+	 */
+	@Override
+	public final int hashCode()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/16
+	 */
+	@Override
+	public final int intValue()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/16
+	 */
+	@Override
+	public final long longValue()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/05/16
+	 */
+	@Override
+	public final String toString()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * The type of value which is contained here.
+	 *
+	 * @return The constant value type.
+	 * @since 2018/05/16
+	 */
+	public final ConstantValueType type()
+	{
+		throw new todo.TODO();
+	}
+}
+
---
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/ConstantValueType.java
@@ -0,0 +1,38 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.classfile;
+
+/**
+ * This indicates the type that a constant value is.
+ *
+ * @since 2018/05/16
+ */
+public enum ConstantValueType
+{
+	/** Integer. */
+	INTEGER,
+	
+	/** Long. */
+	LONG,
+	
+	/** Float. */
+	FLOAT,
+	
+	/** Double. */
+	DOUBLE,
+	
+	/** String. */
+	STRING,
+	
+	/** End. */
+	;
+}
+
