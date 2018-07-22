e0d76fe6acf7a7672f5e82507a0c09de7aafec30
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-07-22 21:51:33 +0000

    Add base classes for fields, instances, and methods.

---
+++ bldt/mids/springcoat-vm/cc/squirreljme/springcoat/vm/SpringInstance.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.springcoat.vm;
+
+/**
+ * This represents an instance of an object which has a class and defined
+ * fields.
+ *
+ * @since 2018/07/22
+ */
+public final class SpringInstance
+{
+}
+
