08353902115e155059bd8ef4840eefba61610b1a
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-07-29 14:38:29 +0000

    Add base packages and move the Main class for SpringCoat down; Add missing Files.isRegularFile(Path).

---
+++ bldt/mids/springcoat-vm/cc/squirreljme/springcoat/Main.java
@@ -0,0 +1,32 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.springcoat;
+
+/**
+ * Main entry class for the SpringCoat virtual machine, this initializes and
+ * runs the virtual machine or runs the compiler.
+ *
+ * @since 2018/07/29
+ */
+public class Main
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Arguments.
+	 * @since 2018/07/29
+	 */
+	public static void main(String... __args)
+	{
+		throw new todo.TODO();
+	}
+}
+
---
+++ bldt/mids/springcoat-vm/cc/squirreljme/springcoat/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This is the main package for the SpringCoat virtual machine and compiler.
+ *
+ * @since 2018/07/29
+ */
+
+package cc.squirreljme.springcoat;
+
