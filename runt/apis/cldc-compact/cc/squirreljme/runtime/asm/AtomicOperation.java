50e5114e4b76ad3f38e08d3bb15070c5620c19a1
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-08 15:03:06 +0000

    Add base class for atomic operations.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/asm/AtomicOperation.java
@@ -0,0 +1,29 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.asm;
+
+/**
+ * This class contains utilities for atomic operations.
+ *
+ * @since 2018/09/08
+ */
+public final class AtomicOperation
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2018/09/08
+	 */
+	private AtomicOperation()
+	{
+	}
+}
+
