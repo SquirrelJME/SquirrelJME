509b543b92327623720f1a3c561be6bccc65bd1c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-13 23:47:09 +0000

    Add base project for the fork which runs on the build system.

---
+++ bldt/mids/springcoat-vm-build/cc/squirreljme/springcoat/build/Main.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.springcoat.build;
+
+/**
+ * Main entry point for the virtual machine which is layered on the build
+ * system.
+ *
+ * @since 2018/09/13
+ */
+public class Main
+{
+}
+
