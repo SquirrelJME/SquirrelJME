96df532fbfbe79f0dab39d9b2a7bb7a02e183430
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-14 00:11:54 +0000

    Base work on splitting the build system from the VM and individualizing it.

--- bldt/mids/springcoat-vm-build/cc/squirreljme/springcoat/build/Main.java
+++ bldt/mids/springcoat-vm-build/cc/squirreljme/springcoat/build/Main.java
@@ -10,6 +10,11 @@
 
 package cc.squirreljme.springcoat.build;
 
+import cc.squirreljme.builder.support.Binary;
+import cc.squirreljme.builder.support.ProjectManager;
+import cc.squirreljme.builder.support.TimeSpaceType;
+import cc.squirreljme.springcoat.vm.SpringClassLoader;
+
 /**
  * Main entry point for the virtual machine which is layered on the build
  * system.
@@ -18,5 +23,63 @@ package cc.squirreljme.springcoat.build;
  */
 public class Main
 {
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2018/09/13
+	 */
+	public static void main(String... __args)
+	{
+		// Copy arguments for processing
+		Queue<String> args = new ArrayDeque<>();
+		if (__args != null)
+			for (String a : __args)
+				if (a != null)
+					args.add(a);
+		
+		// Setup project manager
+		ProjectManager pm = ProjectManager.fromArguments(args);
+		
+		// {@squirreljme.error BA01 No project to launch was specified. The
+		// format is project-name or project-name:entry-point-id.}
+		if (args.isEmpty())
+			throw new IllegalArgumentException("BA01");
+		
+		// Determine the project and launch ID to execute
+		String project = args.remove();
+		int launchid = 0;
+		
+		// Check if a launch ID was specified, separate with colon because
+		// there could be command line arguments to launch with using the
+		// classic entry point. So we cannot really specificy the entry point
+		// using a switch as we would need to specially handle it. Not to
+		// mention the program may require it.
+		int col = project.indexOf(':');
+		if (col >= 0)
+		{
+			launchid = Integer.valueOf(project.substring(col + 1));
+			project = project.substring(0, col);
+		}
+		
+		// Get the project and all of its dependencies built which forms
+		// the class path
+		Binary[] classpath = pm.build(TimeSpaceType.BUILD, project);
+		
+		// Setup wrapped libraries
+		int numlibs = classpath.length;
+		BuildClassLibrary[] libs = new BuildClassLibrary[numlibs];
+		for (int i = 0; i < numlibs; i++)
+			libs[i] = new BuildClassLibrary(classpath[i]);
+		
+		// Initialize the class loader
+		SpringClassLoader classloader = new SpringClassLoader(libs);
+		
+		// Initialize the virtual machine with our launch ID
+		SpringMachine machine = new SpringMachine(classloader, launchid);
+		
+		// Run the VM until it terminates
+		machine.run();
+	}
 }
 
--- runt/libs/springcoat-vm/cc/squirreljme/springcoat/vm/SpringMachine.java
+++ runt/libs/springcoat-vm/cc/squirreljme/springcoat/vm/SpringMachine.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import net.multiphasicapps.classfile.ClassFile;
 import net.multiphasicapps.classfile.ClassName;
 import net.multiphasicapps.classfile.InvalidClassFormatException;
+import net.multiphasicapps.tool.manifest.JavaManifest;
 import net.multiphasicapps.zip.blockreader.ZipBlockReader;
 import net.multiphasicapps.zip.blockreader.ZipBlockEntry;
 import net.multiphasicapps.zip.blockreader.ZipEntryNotFoundException;
@@ -33,9 +34,13 @@ import net.multiphasicapps.zip.blockreader.ZipEntryNotFoundException;
  * @since 2018/07/29
  */
 public final class SpringMachine
+	implements Runnable
 {
 	/** The class loader. */
 	protected final SpringClassLoader classloader;
+	
+	/** The boot index. */
+	protected final int bootdx;
 	
 	/** Threads which are available. */
 	private final List<SpringThread> _threads =
@@ -56,16 +61,19 @@ public final class SpringMachine
 	 * Initializes the virtual machine.
 	 *
 	 * @param __cl The class loader.
+	 * @param __bootdx The entry point which should be booted when the VM
+	 * runs.
 	 * @throws NullPointerException On null arguments.
 	 * @since 2018/09/03
 	 */
-	public SpringMachine(SpringClassLoader __cl)
+	public SpringMachine(SpringClassLoader __cl, int __bootdx)
 		throws NullPointerException
 	{
 		if (__cl == null)
 			throw new NullPointerException("NARG");
 		
 		this.classloader = __cl;
+		this.bootdx = __bootdx;
 	}
 	
 	/**
@@ -151,6 +159,118 @@ public final class SpringMachine
 		{
 			return threads.size();
 		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/09/13
+	 */
+	@Override
+	public final void run()
+	{
+		// Obtain the boot library to read entry points from
+		SpringClassLoader classloader = this.classloader;
+		SpringClassLibrary bootbin = classloader.bootLibrary();
+		
+		// Need to load the manifest where the entry points will be
+		EntryPoints entries;
+		try (InputStream in = bootbin.resourceAsStream("META-INF/MANIFEST.MF"))
+		{
+			// {@squirreljme.error BK0v Entry point JAR has no manifest.}
+			if (in == null)
+				throw new SpringVirtualMachineException("BK0v");
+			
+			entries = new JavaManifest(in);
+		}
+		
+		// {@squirreljme.error BK0u Failed to read the manifest.}
+		catch (IOException e)
+		{
+			throw new SpringVirtualMachineException("BK0u", e);
+		}
+		
+		// The binary to launch is the final binary specified, get those
+		// entry points
+		SpringClassLibrary bootbin = classpath[classpath.length - 1];
+		EntryPoints entries = new EntryPoints(bootbin.manifest());
+		
+		// Print entry points out out for debug
+		todo.DEBUG.note("Entry points:");
+		int n = entries.size();
+		for (int i = 0; i < n; i++)
+			todo.DEBUG.note("    %d: %s", i, entries.get(i));
+		
+		// Use the first program if the ID is not valid
+		int launchid = this.bootdx;
+		if (launchid < 0 || launchid >= n)
+			launchid = 0;
+		
+		// Thread that will be used as the main thread of execution, also used
+		// to initialize classes when they are requested
+		SpringThread mainthread = this.createThread("main");
+		
+		// We will be using the same logic in the thread worker if we need to
+		// initialize any objects or arguments
+		SpringThreadWorker worker = new SpringThreadWorker(this,
+			mainthread);
+		
+		// Load the entry point class
+		EntryPoint entry = entries.get(launchid);
+		SpringClass entrycl = worker.loadClass(new ClassName(
+			entry.entryPoint().replace('.', '/')));
+		
+		// Find the method to be entered in
+		SpringMethod mainmethod;
+		boolean ismidlet;
+		if ((ismidlet = entry.isMidlet()))
+			mainmethod = entrycl.lookupMethod(false,
+				new MethodNameAndType("startApp", "()V"));
+		else
+			mainmethod = entrycl.lookupMethod(true,
+				new MethodNameAndType("main", "(Ljava/lang/String;)V"));
+		
+		// If this is a midlet, we are going to need to initialize a new
+		// instance of our MIDlet and then push that to the current frame's
+		// stack then call the main method on it
+		Object[] entryargs;
+		if (ismidlet)
+		{
+			// Allocate an object for our instance
+			SpringObject midinstance = worker.allocateObject(entrycl);
+			
+			// Initialize the object
+			SpringMethod defcon = entrycl.lookupDefaultConstructor();
+			mainthread.enterFrame(defcon, midinstance);
+			
+			// Since the constructor was entered, run all the code needed to
+			// actually initialize it and such, this method will return once
+			// there are no frames left
+			worker.run();
+			
+			// The arguments to the method we are calling is just the instance
+			// of the midlet we created and initialized
+			entryargs = new Object[]{midinstance};
+		}
+		
+		// Initialize program arguments from a bunch of string arguments that
+		// way they are passed to the main entry point
+		else
+		{
+			if (true)
+				throw new todo.TODO();
+		}
+		
+		// Enter the frame for that method using the arguments we passed (in
+		// a static fashion)
+		mainthread.enterFrame(mainmethod, entryargs);
+		
+		// The main although it executes in this context will always have the
+		// same exact logic as other threads running apart from this main
+		// thread, so no code is needed to be duplicated at all.
+		worker.run();
+		
+		// Wait until all threads have terminated before actually leaving
+		throw new todo.TODO();
 	}
 	
 	/**
