3f81ba60738c607761bbd1e8ebb4cf75507741c2
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-15 22:59:47 +0000

    Base implementations for: Throwable, AbstractCollection, AbstractList, and ArrayList.

--- runt/apis/cldc-compact/java/util/AbstractCollection.java
+++ runt/apis/cldc-compact/java/util/AbstractCollection.java
@@ -10,68 +10,106 @@
 
 package java.util;
 
+/**
+ * This is a basic implementation of a {@link Collection}.
+ *
+ * Sub-classes are only required to implement {@link #iterator()} and
+ * {@link #size()}.
+ *
+ * To support adding elements {@link add(Object)} must be implemented.
+ *
+ * To support removing elements {@link Iterator#remove()} must be implemented.
+ *
+ * @param <E> The element type.
+ * @since 2018/09/15
+ */
 public abstract class AbstractCollection<E>
 	implements Collection<E>
 {
+	/**
+	 * Constructor which requires a sub-class to exist.
+	 *
+	 * @since 2018/09/15
+	 */
 	protected AbstractCollection()
 	{
-		throw new todo.TODO();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/09/15
+	 */
+	@Override
 	public abstract Iterator<E> iterator();
 	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/09/15
+	 */
+	@Override
 	public abstract int size();
 	
+	@Override
 	public boolean add(E __a)
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public boolean addAll(Collection<? extends E> __a)
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public void clear()
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public boolean contains(Object __a)
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public boolean containsAll(Collection<?> __a)
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public boolean isEmpty()
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public boolean remove(Object __a)
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public boolean removeAll(Collection<?> __a)
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public boolean retainAll(Collection<?> __a)
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public Object[] toArray()
 	{
 		throw new todo.TODO();
 	}
 	
+	@Override
 	public <T> T[] toArray(T[] __a)
 	{
 		throw new todo.TODO();
