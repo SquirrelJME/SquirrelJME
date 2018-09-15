4ccf08b2e8fd93a48f85600a2168775f24166bea
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-15 03:24:18 +0000

    Base script.

---
+++ utils-dev/__projclasses.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+#     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3+, or later.
+# See license.mkd for licensing and copyright information.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Creates stuff to lay out into cards accordingly for GitHub.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
