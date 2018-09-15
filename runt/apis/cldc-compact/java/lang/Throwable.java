791992c1a4ae666d5092ed8ae38903dfee0f4c95
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-15 22:37:23 +0000

    Implement the bulk of Throwable.

--- runt/apis/cldc-compact/java/lang/Throwable.java
+++ runt/apis/cldc-compact/java/lang/Throwable.java
@@ -11,82 +11,304 @@
 package java.lang;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * This class is the base class for all types which are thrown, itself being a
+ * checked exception handled by the compiler.
+ *
+ * Each {@link Throwable} contains internal stack trace information which is
+ * initialized when the {@link Throwable} is.
+ *
+ * A {@link Throwable} may have suppressed exceptions, which are exceptions
+ * which may be added to a {@link Throwable} to indicate that there was an
+ * exception that was caused in a try-with-resources when the resource was
+ * attempted to be closed.
+ *
+ * @since 2018/09/15
+ */
 public class Throwable
 {
+	/** The message for this exception. */
+	private final String _message;
+	
+	/** Suppressed exceptions. */
+	private final List<Throwable> _suppressed =
+		new ArrayList<>();
+	
+	/** Was a cause initialized already? */
+	private volatile boolean _initcause;
+	
+	/**
+	 * The cause of this exception, note this is writeable because of
+	 * {@link #initCause(Throwable)}. This is mostly just for older versions
+	 * of the class library which did not have a cause specified in the
+	 * constructor.
+	 */
+	private volatile Throwable _cause;
+	
+	/**
+	 * Initializes a throwable with no cause or message.
+	 *
+	 * @since 2018/09/15
+	 */
 	public Throwable()
 	{
-		throw new todo.TODO();
+		this(false, null, null);
 	}
 	
-	public Throwable(String __a)
+	/**
+	 * Initializes a throwable with the given message and no cause.
+	 *
+	 * @param __m The message.
+	 * @since 2018/09/15
+	 */
+	public Throwable(String __m)
 	{
-		throw new todo.TODO();
+		this(false, __m, null);
 	}
 	
-	public Throwable(String __a, Throwable __b)
+	/**
+	 * Initializes a throwable with the given cause and no message.
+	 *
+	 * @param __t The cause.
+	 * @since 2018/09/15
+	 */
+	public Throwable(Throwable __t)
 	{
-		throw new todo.TODO();
+		this(true, null, __t);
 	}
 	
-	public Throwable(Throwable __a)
+	/**
+	 * Initializes a throwable with the given cause and cause.
+	 *
+	 * @param __m The message.
+	 * @param __t The cause.
+	 * @since 2018/09/15
+	 */
+	public Throwable(String __m, Throwable __t)
 	{
-		throw new todo.TODO();
+		this(true, __m, __t);
 	}
 	
-	public Throwable(String __a, Throwable __b, boolean __c, boolean __d)
+	/**
+	 * Since the cause can only be set once, this constructor needs to keep
+	 * track of whether it was set by a constructor or not.
+	 *
+	 * @param __ic Is the cause initialized?
+	 * @param __m The exception message.
+	 * @param __t The cause.
+	 * @since 2018/09/15
+	 */
+	private Throwable(boolean __ic, String __m, Throwable __t)
 	{
-		throw new todo.TODO();
+		// These are trivially set
+		this._message = __m;
+		this._cause = __t;
+		this._initcause = __ic;
+		
+		// The stack trace is implicitly filled in by this constructor, it
+		// matches the stack trace of the current thread of execution
+		this.fillInStackTrace();
 	}
 	
-	public final void addSuppressed(Throwable __a)
+	/**
+	 * Adds a suppressed throwable which will be thrown alongside this
+	 * throwable. This is mainly used with try-with-resources although a
+	 * programmer may wish to add related throwables that additionally
+	 * happened.
+	 *
+	 * This method is thread safe.
+	 *
+	 * @param __t The throwable to suppress.
+	 * @throws IllegalArgumentException If the passed throwable is this.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/09/15
+	 */
+	public final void addSuppressed(Throwable __t)
+		throws IllegalArgumentException, NullPointerException
 	{
-		throw new todo.TODO();
+		if (__t == null)
+			throw new NullPointerException("NARG");
+		
+		// {@squirreljme.error ZZ0v Cannot add a suppressed exception which
+		// is this exception.}
+		if (__t == this)
+			throw new IllegalArgumentException("ZZ0v");
+		
+		// Lock to prevent multiple threads from adding these and messing up
+		// exceptions
+		List<Throwable> suppressed = this._suppressed;
+		synchronized (suppressed)
+		{
+			suppressed.add(__t);
+		}
 	}
 	
+	/**
+	 * Fills in the stack trace of this throwable for the current thread.
+	 *
+	 * @return {@code this}.
+	 * @since 2018/09/15
+	 */
 	public Throwable fillInStackTrace()
 	{
-		throw new todo.TODO();
+		if (true)
+			throw new todo.TODO();
+		
+		// Returns self
+		return this;
 	}
 	
+	/**
+	 * Returns the throwable which caused this throwable to occur.
+	 *
+	 * @return The throwable which caused this throwable.
+	 * @since 2018/09/15
+	 */
 	public Throwable getCause()
 	{
-		throw new todo.TODO();
+		return this._cause;
 	}
 	
+	/**
+	 * Returns a potentially localized message for this throwable, the default
+	 * implementation just returns {@link #getMessage()}.
+	 *
+	 * @return A localized message.
+	 * @since 2018/09/15
+	 */
 	public String getLocalizedMessage()
 	{
-		throw new todo.TODO();
+		return this.getMessage();
 	}
 	
+	/**
+	 * Returns the message which was set for this throwable, if one was set.
+	 *
+	 * @return The message for this throwable, may be {@code null} if one was
+	 * not set.
+	 * @since 2018/09/15
+	 */
 	public String getMessage()
 	{
-		throw new todo.TODO();
+		return this._message;
 	}
 	
+	/**
+	 * Returns an array of all the throwables which were suppressed.
+	 *
+	 * This method is thread safe.
+	 *
+	 * @return An array of all the suppresed throwables.
+	 * @since 2018/09/15
+	 */
 	public final Throwable[] getSuppressed()
 	{
-		throw new todo.TODO();
+		// Prevent multiple threads from racing on these
+		List<Throwable> suppressed = this._suppressed;
+		synchronized (suppressed)
+		{
+			return suppressed.<Throwable>toArray(
+				new Throwable[suppressed.size()]);
+		}
 	}
 	
-	public Throwable initCause(Throwable __a)
+	/**
+	 * Initializes the cause of the throwable if it has not been set.
+	 *
+	 * Calls to {@link #initCause(Throwable)}, {@link #Throwable(Throwable)},
+	 * or {@link #Throwable(String, Throwable)} will cause the cause to be
+	 * initialized, preventing this from being called or called again.
+	 *
+	 * @param __t The cause of the throwable.
+	 * @return {@code this}.
+	 * @throws IllegalArgumentException If the cause is this throwable.
+	 * @throws IllegalStateException If a cause has already been set.
+	 * @since 2018/09/15
+	 */
+	public Throwable initCause(Throwable __t)
+		throws IllegalArgumentException, IllegalStateException
 	{
-		throw new todo.TODO();
+		// {@squirreljme.error ZZ0t Cannot initialize the cause of the
+		// throwable with itself as the cause.}
+		if (__t == this)
+			throw new IllegalArgumentException("ZZ0t");
+		
+		// {@squirreljme.error ZZ0u The cause of the throwable has already
+		// been initialized.}
+		if (this._initcause)
+			throw new IllegalStateException("ZZ0u");
+		
+		// Set
+		this._initcause = true;
+		this._cause = __t;
+		
+		return this;
 	}
 	
+	/**
+	 * Prints the stack trace to the standard error stream.
+	 *
+	 * @since 2018/09/15
+	 */
 	public void printStackTrace()
 	{
-		throw new todo.TODO();
+		this.__printStackTrace(System.err);
 	}
 	
-	public void printStackTrace(PrintStream __a)
+	/**
+	 * Prints the stack trace to the specified stream.
+	 *
+	 * @param __ps The stream to print to.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/09/15
+	 */
+	public void printStackTrace(PrintStream __ps)
+		throws NullPointerException
 	{
-		throw new todo.TODO();
+		if (__ps == null)
+			throw new NullPointerException("NARG");
+		
+		this.__printStackTrace(__ps);
 	}
 	
+	/**
+	 * Returns a string representation of this throwable.
+	 *
+	 * If there is a localized message, the form is `<class>: <message>`.
+	 * If there is no message, the form is `<class>`. 
+	 *
+	 * @return A string representing this throwable.
+	 * @since 2018/09/15
+	 */
 	@Override
 	public String toString()
 	{
+		String clname = this.getClass().getName(),
+			lm = this.getLocalizedMessage();
+		
+		if (lm == null)
+			return clname;
+		return clname + ": " + lm;
+	}
+	
+	/**
+	 * Prints the stack trace to the specified stream. This is internal so that
+	 * one stack printing does not call the other since it is not specified if
+	 * it actually does it.
+	 *
+	 * @param __ps The stream to print to.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/09/15
+	 */
+	private void __printStackTrace(PrintStream __ps)
+		throws NullPointerException
+	{
+		if (__ps == null)
+			throw new NullPointerException("NARG");
+		
 		throw new todo.TODO();
 	}
 }
