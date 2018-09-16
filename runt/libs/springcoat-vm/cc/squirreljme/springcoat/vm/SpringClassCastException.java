a62505ce1860cfafbc152735d40f4377cf1f030d
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-16 02:23:54 +0000

    Add SpringClassCastException.

---
+++ runt/libs/springcoat-vm/cc/squirreljme/springcoat/vm/SpringClassCastException.java
@@ -0,0 +1,65 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.springcoat.vm;
+
+/**
+ * This is thrown when an attempt is made to cast from one class type to
+ * another.
+ *
+ * @since 2018/09/15
+ */
+public class SpringClassCastException
+	extends SpringException
+{
+	/**
+	 * Initialize the exception with no message or cause.
+	 *
+	 * @since 2018/09/15
+	 */
+	public SpringClassCastException()
+	{
+	}
+	
+	/**
+	 * Initialize the exception with a message and no cause.
+	 *
+	 * @param __m The message.
+	 * @since 2018/09/15
+	 */
+	public SpringClassCastException(String __m)
+	{
+		super(__m);
+	}
+	
+	/**
+	 * Initialize the exception with a message and cause.
+	 *
+	 * @param __m The message.
+	 * @param __c The cause.
+	 * @since 2018/09/15
+	 */
+	public SpringClassCastException(String __m, Throwable __c)
+	{
+		super(__m, __c);
+	}
+	
+	/**
+	 * Initialize the exception with no message and with a cause.
+	 *
+	 * @param __c The cause.
+	 * @since 2018/09/15
+	 */
+	public SpringClassCastException(Throwable __c)
+	{
+		super(__c);
+	}
+}
+
