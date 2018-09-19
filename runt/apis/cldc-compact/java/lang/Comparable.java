15567296ff528cd636fb91f9a616bb3c2d76e8e7
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-19 04:10:52 +0000

    Document Comparable.

--- runt/apis/cldc-compact/java/lang/Comparable.java
+++ runt/apis/cldc-compact/java/lang/Comparable.java
@@ -10,9 +10,38 @@
 
 package java.lang;
 
+/**
+ * This interface is used to set two classes as being comparable to other
+ * classes.
+ *
+ * It is not required to but comparisons performed by this interface should
+ * be comparable to {@link Object#equals(Object)} so that
+ * {@code a.equals(b) == (a.compareTo(b) == 0)}.
+ *
+ * It is recommended to implement this class so that
+ * {@code a.compareTo(b) == -b.compareTo(a)} is true, this makes the
+ * comparisons consistent. However this might not be feasible for speed
+ * purposes so it is highly recommended to at least have comparible negative,
+ * zero, and positive.
+ *
+ * @param <T> The type to compare.
+ * @since 2018/09/19
+ */
 public interface Comparable<T>
 {
-	public abstract int compareTo(T __a);
+	/**
+	 * Compares this instance to the argument passed, 
+	 *
+	 * Note that {@code NullPointerException} should be thrown for {@code __b}
+	 * since {@code __b.compareTo(this)} would thrown an exception.
+	 *
+	 * @param __b The object to compare against.
+	 * @return The result of the comparison, negative values mean
+	 * {@code this < __b}, zero means {@code this == __b}, and positive values
+	 * mean {@code this > __b}.
+	 * @since 2018/09/19
+	 */
+	public abstract int compareTo(T __b);
 }
 
 
