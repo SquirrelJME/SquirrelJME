be11ae9ba8f2806823e6d8760aa2e0fe1caa1f27
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-19 04:13:34 +0000

    Document Runnable.

--- runt/apis/cldc-compact/java/lang/Runnable.java
+++ runt/apis/cldc-compact/java/lang/Runnable.java
@@ -10,8 +10,22 @@
 
 package java.lang;
 
+/**
+ * This interface should be used by any class which is intended and capable
+ * of being run in its own thread (via {@link Thread(Runnable)}.
+ *
+ * When used with {@link Thread}, the method {@link #run()} will be executed
+ * when the thread is started.
+ *
+ * @since 2018/09/19
+ */
 public interface Runnable
 {
+	/**
+	 * Performs any action that is required as needed.
+	 *
+	 * @since 2018/09/19
+	 */
 	public abstract void run();
 }
 
