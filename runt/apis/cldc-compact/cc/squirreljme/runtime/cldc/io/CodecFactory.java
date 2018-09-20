fc8748bf1dc4b88e87def5058049bf8946616e4a
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-20 00:33:43 +0000

    Add a base mapping and normalization of encoding names since there are a ton of aliases; Additionally support ASCII for it is everywhere reasons.

--- runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/io/CodecFactory.java
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/io/CodecFactory.java
@@ -11,6 +11,8 @@
 package cc.squirreljme.runtime.cldc.io;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 
 /**
  * This contains the code used to obtain the default encoding as well as
@@ -73,7 +75,27 @@ public final class CodecFactory
 		if (__enc == null)
 			throw new NullPointerException("NARG");
 		
-		throw new todo.TODO();
+		// Lowercasing makes it easier to match
+		switch ((__enc = CodecFactory.__normalizeEncodingName(__enc)))
+		{
+				// ASCII
+			case "ascii":
+				throw new todo.TODO();
+
+				// ISO-8859-1
+			case "iso-8859-1":
+				throw new todo.TODO();
+				
+				// UTF-8
+			case "utf-8":
+				throw new todo.TODO();
+			
+				// {@squirreljme.error ZZ16 Unknown encoding. (The input
+				// encoding)}
+			default:
+				throw new UnsupportedEncodingException(
+					String.format("ZZ16 %s", __enc));
+		}
 	}
 	
 	/**
@@ -103,6 +125,74 @@ public final class CodecFactory
 		{
 			throw new RuntimeException(String.format("ZZ11 %s", __enc), e);
 		}
+	}
+	
+	/**
+	 * Normalizes the name of the encoding since there are so many aliases that
+	 * way this code can operate very simply.
+	 *
+	 * @param __n The encoding to normalize.
+	 * @return The normalized encoding, if it is not known then the input is
+	 * returned.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/09/19
+	 */
+	private static final String __normalizeEncodingName(String __n)
+		throws NullPointerException
+	{
+		if (__n == null)
+			throw new NullPointerException("NARG");
+		
+		// Store original since it will be lowercase
+		String orign = __n;
+		
+		// Lowercase and map values
+		switch ((__n = __n.toLowerCase()))
+		{
+				// ASCII
+			case "646":
+			case "ansi_x3.4-1968":
+			case "ansi_x3.4-1986":
+			case "ascii":
+			case "ascii7":
+			case "cp367":
+			case "csascii":
+			case "ibm367":
+			case "iso_646.irv:1983":
+			case "iso_646.irv:1991":
+			case "iso646-us":
+			case "iso-ir-6":
+			case "us":
+			case "us-ascii":
+				return "ascii";
+			
+				// ISO-8859-1
+			case "819":
+			case "8859_1":
+			case "cp819":
+			case "csisolatin1":
+			case "ibm819":
+			case "ibm-819":
+			case "iso_8859_1":
+			case "iso_8859-1":
+			case "iso-8859-1":
+			case "iso8859_1":
+			case "iso8859-1":
+			case "iso_8859-1:1987":
+			case "iso-ir-100":
+			case "l1":
+			case "latin1":
+				return "iso-8859-1";
+			
+				// UTF-8
+			case "unicode-1-1-utf-8":
+			case "utf8":
+			case "utf-8":
+				return "utf-8";
+			
+				// Unknown use original
+			default:
+				return orign;
 	}
 }
 
