bff85700d786b20680bc9ba1f5a8ed2406749177
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-21 03:21:53 +0000

    Add base for ISO-8859-1 encoder.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/io/ISO88591Encoder.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.io;
+
+/**
+ * Encodes character data to ISO-8859-1.
+ *
+ * @since 2018/09/20
+ */
+public final class ISO88591Encoder
+	implements Encoder
+{
+}
+
