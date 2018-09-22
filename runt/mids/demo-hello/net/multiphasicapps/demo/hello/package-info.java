7328815dae32aca1478205f2d8be564e31b6c983
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-22 16:52:16 +0000

    Add Hello world.

---
+++ runt/mids/demo-hello/net/multiphasicapps/demo/hello/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the hello program.
+ *
+ * @since 2018/09/22
+ */
+
+package net.multiphasicapps.demo.hello;
+
