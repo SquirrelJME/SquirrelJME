7c3960a83dd90309b7eed6cbf3bd8a4031b22524
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-28 05:53:09 +0000

    Base conversion class.

---
+++ runt/apis/cldc-compact/java/util/__PrintFConversion__.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+/**
+ * This represents a conversion that can be used for formatted printing.
+ *
+ * @since 2018/09/28
+ */
+enum __PrintFConversion__
+{
+	/** End. */
+	;
+}
+
