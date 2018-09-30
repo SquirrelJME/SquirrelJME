3a29b9a2dc1ab68b8f4b44434a01e31a6ef885bc
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-30 23:11:22 +0000

    Calculations for 0-2048 to size mapping.

--- utils-dev/test/SizeMap.java
+++ utils-dev/test/SizeMap.java
@@ -25,11 +25,65 @@ public class SizeMap
 			//val = Math.pow(2, Math.pow(2, Math.log(ii * 44.0)));
 			//val = Math.pow(2, ii);
 			
-			val = Math.min(536870911, 1 << ((i >>> 6) - 1));
+			val =
+				// Base minimum size of all allocated objects
+				8 +
+				
+				// Index counter multiplied to a multiple of four
+				(i * 4) + 
+				
+				// Steady rise on the small scale
+				1024 * (Math.max(0, i - 512)) +
+				
+				// Big rise on the large scale
+				8192 * (Math.max(0, i - 1024)) +
+				
+				// Very big rise on the large scale
+				65536 * (Math.max(0, i - 1536))
+				
+				/*Math.min(134217727, 1 << ((i >>> 8) - 1)) +*/
+				/*Math.min(268435455, 1 << ((i >>> 7) - 1)) +*/
+				
+				// This one alone seems to work well
+				/*Math.max(0, Math.min(536870911, 1 << ((i >>> 6) - 1)))*/
+				
+				;
+			
+			// Base size, all objects and allocations are at least this size
+			val = 8;
+			
+			// Very small objects, the usual
+			if (i < 1024)
+				val += i * 4;
+			
+			// Smaller gains
+			else if (i < 1536)
+				val += 4096 + ((i - 1023) * 1024);
+			
+			// Larger gains
+			else if (i < 1792)
+				val += 528392 + ((i - 1535) * 4096);
+			
+			// Larger gains
+			else if (i < 2000)
+				val += 1576976 + ((i - 1791) * 214795);
+			
+			// The last amounts maximize usage
+			else if (i < 2048)
+				val += 2428944 + ((i - 1999) * 44040192);
 			
 			long x = (long)val;
-			System.out.printf("%4d: %10d%n", i,
-				Math.max(0, Math.min(x, Integer.MAX_VALUE)));
+			
+			long clipped = Math.max(0, Math.min(x, Integer.MAX_VALUE));
+			
+			System.out.printf(
+				"%4d: %10d (%7dKiB, %4dMiB, %9d int[], %9d long[])%n",
+				i,
+				clipped,
+				clipped / 1024,
+				clipped / 1048576,
+				(clipped - 12) / 4,
+				(clipped - 12) / 8);
 		}
 	}
 }
