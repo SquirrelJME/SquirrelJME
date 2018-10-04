7f312f1a03118e3b048c89b108c0729729ac2b28
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-04 02:31:02 +0000

    Add base TAC package.

---
+++ runt/libs/tac/net/multiphasicapps/tac/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package the the test framework.
+ *
+ * @since 2018/10/03
+ */
+
+package net.multiphasicapps.tac;
+
