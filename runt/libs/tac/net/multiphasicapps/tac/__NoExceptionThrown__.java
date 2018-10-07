fa577000c3e6992fedf658051901172f477ff5de
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-07 02:47:18 +0000

    Handle exceptions and differentiate between thrown excpetions and not.

---
+++ runt/libs/tac/net/multiphasicapps/tac/__NoExceptionThrown__.java
@@ -0,0 +1,50 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tac;
+
+/**
+ * Indicates that no exception was thrown.
+ *
+ * @since 2018/10/06
+ */
+final class __NoExceptionThrown__
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/10/06
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		return __o == this || (__o instanceof __NoExceptionThrown__);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/10/06
+	 */
+	@Override
+	public final int hashCode()
+	{
+		return 0;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/10/06
+	 */
+	@Override
+	public final String toString()
+	{
+		return "NoExceptionThrown";
+	}
+}
+
