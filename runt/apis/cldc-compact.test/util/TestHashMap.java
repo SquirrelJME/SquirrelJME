3bb21ebe22abd30e3a317090afdebc372a82cea5
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-07 16:14:12 +0000

    Add test for maps.

--- runt/apis/cldc-compact.test/util/TestHashMap.java
+++ runt/apis/cldc-compact.test/util/TestHashMap.java
@@ -31,7 +31,81 @@ public class TestHashMap
 		// Initialize a map to work with
 		HashMap<Integer, String> map = new HashMap<>();
 		
-		throw new todo.TODO();
+		// Initial size and such
+		this.secondary("initempty", map.isEmpty());
+		this.secondary("initsize", map.size());
+		
+		// Some initial values to use
+		Integer ka = Integer.valueOf(1989),
+			kb = Integer.valueOf(2018);
+		String va = "Squirrels",
+			vb = "Cute";
+		
+		// Store into the map
+		map.put(ka, va);
+		
+		// Some checks for that initial value
+		this.secondary("firstcontained", map.containsKey(ka));
+		this.secondary("firstvalue", map.get(ka));
+		
+		// Has the same equality, despite a different object?
+		this.secondary("firstcontainednew",
+			map.containsKey(new Integer(1989)));
+		this.secondary("firstvaluenew",
+			map.get(new Integer(1989)));
+		this.secondary("firstsize", map.size());
+		this.secondary("firsthassecond", map.containsKey(kb));
+		
+		// Add second value
+		map.put(kb, vb);
+		
+		// Check for second value
+		this.secondary("secondcontained", map.containsKey(kb));
+		this.secondary("secondvalue", map.get(kb));
+		this.secondary("secondsize", map.size());
+		
+		// Replace first value
+		map.put(ka, vb);
+		
+		// Checks for replaced value
+		this.secondary("replacevalue", map.get(ka));
+		this.secondary("replacesecondvalue", map.get(kb));
+		this.secondary("replacesize", map.size());
+		
+		// Add in a bunch of values
+		for (int i = 0; i < 128; i++)
+			map.put(i, "" + i);
+		
+		// Check the size
+		this.secondary("dumpsize", map.size());
+		
+		// Remove second value
+		this.secondary("removesecond", map.remove(kb));
+		this.secondary("removesize", map.size());
+		
+		// Add new value
+		map.put(-1, "Adorable");
+		
+		// Check that
+		this.secondary("hasanother", map.get(-1));
+		this.secondary("hasanothersize", map.size());
+		
+		// Empty the map
+		map.clear();
+		
+		// Check that
+		this.secondary("clearsize", map.size());
+		
+		// Store into the map again
+		map.put(ka, va);
+		
+		// Reused values
+		this.secondary("reusedcontainednew",
+			map.containsKey(new Integer(1989)));
+		this.secondary("reusedvaluenew",
+			map.get(new Integer(1989)));
+		this.secondary("reusedsize", map.size());
+		this.secondary("reusedhassecond", map.containsKey(kb));
 	}
 }
 
