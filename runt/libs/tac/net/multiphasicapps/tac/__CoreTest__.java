7f61b19a11d39f369d12fd7155aba72d55319e1d
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-07 00:05:51 +0000

    Implement more of the test framework such as results and input.

--- runt/libs/tac/net/multiphasicapps/tac/__CoreTest__.java
+++ runt/libs/tac/net/multiphasicapps/tac/__CoreTest__.java
@@ -10,7 +10,15 @@
 
 package net.multiphasicapps.tac;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.microedition.midlet.MIDlet;
+import net.multiphasicapps.tool.manifest.JavaManifest;
+import net.multiphasicapps.tool.manifest.JavaManifestAttributes;
 
 /**
  * This is the core test framework which handles reading test information and
@@ -21,7 +29,6 @@ import javax.microedition.midlet.MIDlet;
  */
 abstract class __CoreTest__
 	extends MIDlet
-	implements Runnable
 {
 	/**
 	 * Runs the given test with the given arguments and resulting in the
@@ -34,13 +41,89 @@ abstract class __CoreTest__
 	abstract Object __runTest(Object... __args);
 	
 	/**
-	 * {@inheritDoc}
+	 * Runs the specified test using the given main arguments, if any.
+	 *
+	 * @param __mainargs The main arguments to the test which allow parameters
+	 * to be used accordingly.
 	 * @since 2018/10/06
 	 */
-	@Override
-	public final void run()
+	public final void runTest(String... __mainargs)
 	{
-		throw new todo.TODO();
+		if (__mainargs == null)
+			__mainargs = new String[0];
+		
+		// Used to refer to resources for parameters and default results
+		Class<?> self = this.getClass();
+		
+		// Get the basename of the class, used to refer to resources
+		String classname = self.getName(),
+			basename = classname;
+		int ld = basename.lastIndexOf('.');
+		if (ld >= 0)
+			basename = basename.substring(ld + 1);
+		
+		// Read the inputs for the test
+		Object[] args;
+		try (InputStream in = self.getResourceAsStream(basename + ".in"))
+		{
+			args = this.__parseInput(classname, __mainargs, in);
+		}
+		catch (IOException e)
+		{
+			// {@squirreljme.error BU02 Could not read the argument input.}
+			throw new InvalidTestException("BU02", e);
+		}
+		
+		// Run the test, catch any exception to report it
+		Object rv;
+		try
+		{
+			rv = this.__runTest(args);
+		}
+		catch (Throwable t)
+		{
+			// The test parameter is not valid, so whoops!
+			if (t instanceof InvalidTestException)
+			{
+				// {@squirreljme.error BU04 The test failed to run properly.
+				// (The given test)}
+				System.err.printf("BU04 %s%n", classname);
+				t.printStackTrace(System.err);
+				return;
+			}
+			
+			// Make the result just the exception, to simplify things
+			rv = t;
+		}
+		
+		// Get string result representation
+		String rvstr = __CoreTest__.__convertToString(rv),
+			expected = null;
+		
+		// If an output file was specified, read the result from it and compare
+		// it to see if it was expected
+		try (InputStream out = self.getResourceAsStream(basename + ".out"))
+		{
+			if (out != null)
+				try (BufferedReader br = new BufferedReader(
+					new InputStreamReader(out, "utf-8")))
+				{
+					expected = br.readLine();
+				}
+		}
+		catch (IOException e)
+		{
+			expected = null;
+		}
+		
+		// Print the result of the test, in a manifest compatible format
+		System.out.printf("%s: %s%n", classname, rvstr);
+		
+		// {@squirreljme.error BU05 An expected result was specified but it
+		// did not match. (The class; The expected value; The result)}
+		if (expected != null)
+			if (!expected.equals(rvstr))
+				System.err.printf("BU05 %s %s", classname, expected, rvstr);
 	}
 	
 	/**
@@ -61,8 +144,135 @@ abstract class __CoreTest__
 	 */
 	protected final void startApp()
 	{
-		// Just forward to run
-		this.run();
+		// Just forward to run, no main arguments are used at all
+		this.runTest((String[])null);
+	}
+	
+	/**
+	 * Parses the input file for arguments.
+	 *
+	 * @param __sysprefix System property prefix.
+	 * @param __mainargs Main program arguments.
+	 * @param __in Input manifest data which may be null.
+	 * @return The input arguments.
+	 * @throws IOException On read errors.
+	 * @since 2018/10/06
+	 */
+	private Object[] __parseInput(String __sysprefix, String[] __mainargs,
+		InputStream __in)
+		throws IOException
+	{
+		if (__mainargs == null)
+			__mainargs = new String[0];
+		
+		List<Object> rv = new ArrayList<>();
+		
+		// Parse manifest
+		JavaManifest man = (__in == null ? new JavaManifest() :
+			new JavaManifest(__in));
+		JavaManifestAttributes attr = man.getMainAttributes();
+		
+		// Read argument values in this order, to allow new ones to be
+		// specified accordingly: main arguments, system properties, the
+		// default input manifest
+		for (int i = 1; i >= 1; i++)
+		{
+			String parse;
+			
+			// Main arguments first
+			if (__mainargs != null && (i - 1) < __mainargs.length)
+				parse = __mainargs[i - 1];
+			
+			// Then system properties
+			else
+			{
+				String maybe = System.getProperty(__sysprefix + "." + i);
+				if (maybe != null)
+					parse = maybe;
+				
+				// Otherwise just read a value from the manifest
+				else
+					parse = attr.getValue("argument-" + i);
+			}
+			
+			// Nothing to parse
+			if (parse == null)
+				break;
+			
+			// Parse the value
+			rv.add(__CoreTest__.__convertToObject(parse));
+		}
+		
+		return rv.<Object>toArray(new Object[rv.size()]);
+	}
+	
+	/**
+	 * Converts the given string to an object.
+	 *
+	 * @param __s The object to convert.
+	 * @return The converted object.
+	 * @throws InvalidTestParameterException If the input could not be
+	 * converted.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/10/06
+	 */
+	private static Object __convertToObject(String __s)
+		throws InvalidTestParameterException, NullPointerException
+	{
+		if (__s == null)
+			throw new NullPointerException("NARG");
+		
+		// Basic conversions
+		switch (__s)
+		{
+			case "null":
+				return null;
+			
+			case "NoResult":
+				return new NoResult();
+			
+			case "true":
+				return Boolean.TRUE;
+			
+			case "false":
+				return Boolean.FALSE;
+			
+			default:
+				break;
+		}
+		
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Converts the specified object to a string.
+	 *
+	 * @param __o The object to convert.
+	 * @return The resulting string.
+	 * @throws InvalidTestParameterException If the object cannot be
+	 * converted.
+	 * @since 2018/10/06
+	 */
+	private static String __convertToString(Object __o)
+		throws InvalidTestParameterException
+	{
+		// Null
+		if (__o == null)
+			return "null";
+		
+		// No result generated
+		else if (__o instanceof NoResult)
+			return "NoResult";
+		
+		// Boolean values
+		else if (__o instanceof Boolean)
+			return __o.toString();
+		
+		// {@squirreljme.error BU03 Could not convert the object to a string
+		// representation. (The class; The toString() of that object)}
+		else
+			throw new InvalidTestParameterException(
+				String.format("BU03 %s %s", __o.getClass(), __o));
 	}
 }
 
