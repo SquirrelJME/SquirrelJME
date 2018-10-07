b6cfa4555bb77ada7f6f1ef1e864a2a20db93c7b
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-07 15:00:16 +0000

    Base for cldc-compact tests.

---
+++ runt/apis/cldc-compact.test/util/TestHashMap.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package util;
+
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * Tests that hashmap works properly.
+ *
+ * @since 2018/10/07
+ */
+public class TestHashMap
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/10/07
+	 */
+	@Override
+	public void test()
+	{
+		throw new todo.TODO();
+	}
+}
+
