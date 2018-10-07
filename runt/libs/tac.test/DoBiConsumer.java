0f5a9f3b2fa0429ad8447a967c9eb73c60d48ab8
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-07 01:47:10 +0000

    Add consumer and biconsumer test.

---
+++ runt/libs/tac.test/DoBiConsumer.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+import net.multiphasicapps.tac.TestBiConsumer;
+
+/**
+ * Tests a basic bi consumer.
+ *
+ * @since 2018/10/06
+ */
+public class DoBiConsumer
+	extends TestBiConsumer<String, Integer>
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/10/06
+	 */
+	@Override
+	public void test(String __s, Integer __i)
+	{
+		System.err.printf("Consumer said: `%s` and %d!%n", __s, __i);
+	}
+}
+
