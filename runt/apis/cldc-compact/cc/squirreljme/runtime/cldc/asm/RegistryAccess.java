11b76512680096eb96e9cb3a51b1b2bc871f5126
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-14 17:14:51 +0000

    Add blank asm call classes for system database (PalmOS) and registry (Windows) access in the future.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/asm/RegistryAccess.java
@@ -0,0 +1,38 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.asm;
+
+/**
+ * This is used to access the system registry which is compatible in a fashion
+ * that is compatible with an extrapolation of Windows registries.
+ *
+ * @since 2018/10/14
+ */
+public final class RegistryAccess
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2018/10/14
+	 */
+	private RegistryAccess()
+	{
+	}
+	
+	/**
+	 * Returns {@code true} if a registry is present.
+	 *
+	 * @return If a registry is present.
+	 * @since 2018/10/14
+	 */
+	public static final native boolean present();
+}
+
