7162b458c274d44b916070c556219629dd4ac4f1
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-18 01:23:26 +0000

    Add base for tac-runner, which will run all the tests available.

---
+++ runt/mids/tac-runner/net/multiphasicapps/tac/runner/Database.java
@@ -0,0 +1,44 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tac.runner;
+
+import java.util.Iterator;
+
+/**
+ * This class loads and stores the database for the test system.
+ *
+ * @since 2018/10/17
+ */
+public final class Database
+	implements Iterable<SingleUnit>
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/10/17
+	 */
+	@Override
+	public final Iterator<SingleUnit> iterator()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Builds the test database.
+	 *
+	 * @return The built test database.
+	 * @since 2018/10/17
+	 */
+	public static final Database build()
+	{
+		throw new todo.TODO();
+	}
+}
+
---
+++ runt/mids/tac-runner/net/multiphasicapps/tac/runner/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the TAC runner which is used to search all of the
+ * available suites on the system and be able to execute them as such.
+ *
+ * @since 2018/10/17
+ */
+
+package net.multiphasicapps.tac.runner;
+
