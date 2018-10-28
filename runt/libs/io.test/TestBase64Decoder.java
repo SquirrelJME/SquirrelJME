75da9f88973de81b21f2da05fb4f0c66553a5055
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-28 22:39:50 +0000

    Correct launcher manifest; Correct the io.test.

---
+++ runt/libs/io.test/TestBase64Decoder.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.io;
+
+import net.multiphasicapps.tac.TestFunction;
+
+/**
+ * This tests the base 64 decoder.
+ *
+ * @since 2018/03/06
+ */
+public class TestBase64Decoder
+	extends TestFunction<String, String>
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/03/06	
+	 */
+	@Override
+	public String test(String __i)
+	{
+		throw new todo.TODO();
+	}
+}
+
