199464e1ed65bb9cbab9f41e6e8b3be46f961581
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-29 13:13:09 +0000

    Do not TODO in AbstractSeqentialList(); Implement loading of the database.

--- runt/mids/tac-runner/net/multiphasicapps/tac/runner/Database.java
+++ runt/mids/tac-runner/net/multiphasicapps/tac/runner/Database.java
@@ -10,8 +10,11 @@
 
 package net.multiphasicapps.tac.runner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import javax.microedition.swm.ManagerFactory;
 import javax.microedition.swm.Suite;
@@ -26,6 +29,22 @@ import javax.microedition.swm.SuiteType;
 public final class Database
 	implements Iterable<SingleUnit>
 {
+	/**
+	 * Initializes the database of units.
+	 *
+	 * @param __it The units to use.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/10/29
+	 */
+	public Database(Iterable<SingleUnit> __it)
+		throws NullPointerException
+	{
+		if (__it == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 * @since 2018/10/17
@@ -44,16 +63,26 @@ public final class Database
 	 */
 	public static final Database build()
 	{
-		Map<String, SingleUnit> units = new HashMap<>();
+		List<SingleUnit> units = new LinkedList<>();
 		
 		// Need this to go through all the available suites for testing
 		SuiteManager sm = ManagerFactory.getSuiteManager();
 		for (Suite s : sm.getSuites(SuiteType.APPLICATION))
 		{
-			throw new todo.TODO();
+			// This is not a test program for SquirrelJME, ignore
+			if (!Boolean.valueOf(s.getAttributeValue("x-squirreljme-tests")))
+				continue;
+			
+			// Load single units for tests
+			for (Iterator<String> it = s.getMIDlets(); it.hasNext();)
+				units.add(new SingleUnit(s, it.next()));
 		}
 		
-		throw new todo.TODO();
+		// Debug
+		todo.DEBUG.note("Loaded units: %s", units);
+		
+		// Build the final database
+		return new Database(units);
 	}
 }
 
