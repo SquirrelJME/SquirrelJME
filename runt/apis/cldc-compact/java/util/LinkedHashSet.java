b300e302d127fba1a8218bffd1afd071f3ba4526
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-01 13:06:21 +0000

    Refactor the bucket map; Base for HashSet and LinkedHashSet; Part of LinkedHashMap.

--- runt/apis/cldc-compact/java/util/LinkedHashSet.java
+++ runt/apis/cldc-compact/java/util/LinkedHashSet.java
@@ -10,28 +10,69 @@
 
 package java.util;
 
+/**
+ * This is a set which is backed by a hash table except that the iterator
+ * order is in the order of which elements were added first.
+ *
+ * Otherwise this class is exactly the same as {@link HashSet}.
+ *
+ * @param <E> The element type to store.
+ * @see HashSet
+ * @since 2018/11/01
+ */
 public class LinkedHashSet<E>
 	extends HashSet<E>
 	implements Set<E>, Cloneable
 {
+	/**
+	 * Initializes the set with the given capacity and load factor.
+	 *
+	 * @param __cap The capacity used.
+	 * @param __load The load factor used.
+	 * @throws IllegalArgumentException If the capacity is negative or the
+	 * load factor is not positive.
+	 * @since 2018/11/01
+	 */
 	public LinkedHashSet(int __initcap, float __load)
 	{
 		super(__initcap, __load);
 	}
 	
+	/**
+	 * Initializes the set with the given capacity and the default load factor.
+	 *
+	 * @param __cap The capacity used.
+	 * @throws IllegalArgumentException If the capacity is negative.
+	 * @since 2018/11/01
+	 */
 	public LinkedHashSet(int __initcap)
 	{
 		super(__initcap);
 	}
 	
+	/**
+	 * Initializes the set with the default capacity and load factor.
+	 *
+	 * @since 2018/11/01
+	 */
 	public LinkedHashSet()
 	{
 		super();
 	}
 	
-	public LinkedHashSet(Collection<? extends E> __c)
+	/**
+	 * Initializes a set which is a copy of the other map.
+	 *
+	 * The default load factor is used and the capacity is set to the
+	 * capacity of the input set.
+	 *
+	 * @param __s The set to copy from.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/10/07
+	 */
+	public LinkedHashSet(Collection<? extends E> __s)
 	{
-		super(__c);
+		super(__s);
 	}
 }
 
