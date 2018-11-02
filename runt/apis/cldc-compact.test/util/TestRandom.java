741e847867483386a73828d7b77953a93c510ddf
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-02 13:16:52 +0000

    Add base tests for Random (int) and Random (float).

---
+++ runt/apis/cldc-compact.test/util/TestRandom.java
@@ -0,0 +1,34 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package util;
+
+import net.multiphasicapps.tac.TestRunnable;
+import java.util.Random;
+
+/**
+ * Tests the random number generator, integer values only.
+ *
+ * @since 2018/11/02
+ */
+public class TestRandom
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/02
+	 */
+	@Override
+	public void test()
+	{
+		throw new todo.TODO();
+	}
+}
+
