ad33b2e758ace02140ebc18ce0941017d220a4c0
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-03 15:58:40 +0000

    Implement Double.doubleToRawLongBits().

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/asm/PrimitiveAccess.java
@@ -0,0 +1,38 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.asm;
+
+/**
+ * Access to primitive.
+ *
+ * @since 2018/11/03
+ */
+public final class PrimitiveAccess
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2018/11/03
+	 */
+	private PrimitiveAccess()
+	{
+	}
+	
+	/**
+	 * Double to raw long bits.
+	 *
+	 * @param __d The input double.
+	 * @return The raw long bits.
+	 * @since 2018/11/03
+	 */
+	public static final native long doubleToRawLongBits(double __d);
+}
+
