b8157f32da708ca2aa79712c39708573c49f5d5b
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-04 12:20:54 +0000

    Check multianew bounds; Add skip parameter, it is needed.

--- runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/lang/MultiANewArray.java
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/lang/MultiANewArray.java
@@ -10,6 +10,8 @@
 
 package cc.squirreljme.runtime.cldc.lang;
 
+import cc.squirreljme.runtime.cldc.asm.ObjectAccess;
+
 /**
  * This method contains the code which is able to initialize multidimensional
  * arrays.
@@ -31,6 +33,8 @@ public final class MultiANewArray
 	 * Allocates a new multi-dimensional array.
 	 *
 	 * @param __type The type with minimum dimension sizes specified.
+	 * @param __skip The initial number of dimensions to skip in the initial
+	 * array.
 	 * @param __dims The dimensions and the number of them to use.
 	 * @return The allocated multi-dimensional array.
 	 * @throws NegativeArraySizeException If an allocated array would be
@@ -38,14 +42,28 @@ public final class MultiANewArray
 	 * @throws NullPointerException On null arguments.
 	 * @since 2018/11/03
 	 */
-	public static final Object multiANewArray(Class<?> __type, int... __dims)
+	public static final Object multiANewArray(Class<?> __type, int __skip,
+		int... __dims)
 		throws NegativeArraySizeException, NullPointerException
 	{
 		if (__type == null || __dims == null)
 			throw new NullPointerException("NARG");
 		
+		// Count the number of dimensions represented in the type
+		String typename = __type.getName();
+		int typedims = 0;
+		while (typename.charAt(typedims) == '[')
+			typedims++;
+		
+		// {@squirreljme.error ZZ30 Negative number of dimensions available
+		// or input type is not correct for the array type.}
+		int dims = __dims.length - __skip;
+		if (__skip < 0 || dims <= 0 || typedims < dims)
+			throw new IllegalArgumentException("ZZ30");
+		
 		// Debug
-		todo.DEBUG.note("For %s, %d dims", __type, __dims.length);
+		todo.DEBUG.note("For %s (%d dims), do %d dims", __type, typedims,
+			dims);
 		
 		throw new todo.TODO();
 	}
