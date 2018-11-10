69cfc737c3b0f32faab41272f126f288727b7406
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-10 11:39:09 +0000

    Base profiler snapshot class.

---
+++ runt/libs/tool-profiler/net/multiphasicapps/profiler/ProfilerSnapshot.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.profiler;
+
+/**
+ * This represents the main profiler snapshot which contains all of the data
+ * within what is to be profiled, it is mutable and accordingly allows for
+ * export to NPS formats.
+ *
+ * @since 2018/11/10
+ */
+public final class ProfilerSnapshot
+{
+}
+
---
+++ runt/libs/tool-profiler/net/multiphasicapps/profiler/package-info.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This package contains the profiler or rather the code which supports the
+ * generation of profiling snapshots which can be used to measure performance
+ * and such.
+ *
+ * @since 2018/11/10
+ */
+
+package net.multiphasicapps.profiler;
+
