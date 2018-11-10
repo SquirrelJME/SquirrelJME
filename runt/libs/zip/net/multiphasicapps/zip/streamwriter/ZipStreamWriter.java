72afdf2de27a53013da7e4edb1033e14b4f2c6de
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-10 19:58:00 +0000

    ZipStreamWriter is no longer thread safe.

--- runt/libs/zip/net/multiphasicapps/zip/streamwriter/ZipStreamWriter.java
+++ runt/libs/zip/net/multiphasicapps/zip/streamwriter/ZipStreamWriter.java
@@ -29,6 +29,8 @@ import net.multiphasicapps.zip.ZipCRCConstants;
  * When the stream is closed, the central directory of the ZIP file will be
  * written to the end of the file.
  *
+ * This class is not thread safe.
+ *
  * @since 2016/07/09
  */
 public class ZipStreamWriter
@@ -57,10 +59,6 @@ public class ZipStreamWriter
 	/** General purpose flags for entries (use descriptor; UTF-8 names). */
 	private static final int _GENERAL_PURPOSE_FLAGS =
 		(1 << 3) | (1 << 11);
-	
-	/** Lock for safety. */
-	protected final Object lock =
-		new Object();
 	
 	/** The output stream to write to. */
 	protected final ExtendedDataOutputStream output;
@@ -116,112 +114,104 @@ public class ZipStreamWriter
 		if (this._closed)
 			return;
 		
-		// Lock
-		synchronized (this.lock)
+		// {@squirreljme.error BF15 Cannot close the ZIP writer because
+		// an entry is still being written.}
+		if (this._inner != null || this._outer != null)
+			throw new IOException("BF15");
+		
+		// Mark closed to prevent failing closes from writing multiple
+		// times
+		this._closed = true;
+		
+		// Get output and the TOC entries
+		ExtendedDataOutputStream output = this.output;
+		LinkedList<__TOCEntry__> toc = this._toc;
+		int numtoc = toc.size();
+		
+		// The position where the central directory starts
+		long cdstart = output.size();
+		
+		// Write all entries
+		int bestversion = this._bestversion;
+		for (__TOCEntry__ entry : toc)
 		{
-			// Do nothing if already closed
-			if (this._closed)
-				return;
+			// The entry position
+			long epos = output.size();
 			
-			// {@squirreljme.error BF15 Cannot close the ZIP writer because
-			// an entry is still being written.}
-			if (this._inner != null || this._outer != null)
-				throw new IOException("BF15");
+			if (epos > _MAX_FILE_SIZE)
+				throw new IOException();
 			
-			// Mark closed to prevent failing closes from writing multiple
-			// times
-			this._closed = true;
+			// Write directory header
+			output.writeInt(_CENTRAL_DIRECTORY_MAGIC_NUMBER);
 			
-			// Get output and the TOC entries
-			ExtendedDataOutputStream output = this.output;
-			LinkedList<__TOCEntry__> toc = this._toc;
-			int numtoc = toc.size();
+			// The created by version (use the highest version)
+			output.writeShort(bestversion);
 			
-			// The position where the central directory starts
-			long cdstart = output.size();
+			// Version needed to extract
+			ZipCompressionType ecomp = entry._compression;
+			output.writeShort(ecomp.extractVersion());
 			
-			// Write all entries
-			int bestversion = this._bestversion;
-			for (__TOCEntry__ entry : toc)
-			{
-				// The entry position
-				long epos = output.size();
-				
-				if (epos > _MAX_FILE_SIZE)
-					throw new IOException();
-				
-				// Write directory header
-				output.writeInt(_CENTRAL_DIRECTORY_MAGIC_NUMBER);
-				
-				// The created by version (use the highest version)
-				output.writeShort(bestversion);
-				
-				// Version needed to extract
-				ZipCompressionType ecomp = entry._compression;
-				output.writeShort(ecomp.extractVersion());
-				
-				// General purpose flags
-				output.writeShort(_GENERAL_PURPOSE_FLAGS);
-				
-				// Compression method
-				output.writeShort(ecomp.method());
-				
-				// Date/time not used
-				output.writeShort(0);
-				output.writeShort(0);
-				
-				// CRC and sizes
-				output.writeInt(entry._crc);
-				output.writeInt((int)entry._compressed);
-				output.writeInt((int)entry._uncompressed);
-				
-				// Write name length
-				byte[] efn = entry._name;
-				output.writeShort(efn.length);
-				
-				// No extra data
-				output.writeShort(0);
-				
-				// No comment
-				output.writeShort(0);
-				
-				// Always the first disk
-				output.writeShort(0);
-				
-				// No iternal or external attributes
-				output.writeShort(0);
-				output.writeInt(0);
-				
-				// Relative offset to local header
-				output.writeInt((int)entry._localposition);
-				
-				// Write file name
-				output.write(efn);
-			}
+			// General purpose flags
+			output.writeShort(_GENERAL_PURPOSE_FLAGS);
 			
-			// The position where it ends
-			long cdend = output.size();
+			// Compression method
+			output.writeShort(ecomp.method());
 			
-			// Write magic number
-			output.writeInt(_END_DIRECTORY_MAGIC_NUMBER);
-			
-			// Only a single disk is written
+			// Date/time not used
 			output.writeShort(0);
 			output.writeShort(0);
 			
-			// Number of entries on this disk and in all of them
-			output.writeShort(numtoc);
-			output.writeShort(numtoc);
+			// CRC and sizes
+			output.writeInt(entry._crc);
+			output.writeInt((int)entry._compressed);
+			output.writeInt((int)entry._uncompressed);
 			
-			// The size of the central directory
-			output.writeInt((int)(cdend - cdstart));
+			// Write name length
+			byte[] efn = entry._name;
+			output.writeShort(efn.length);
 			
-			// Offset to the central directory
-			output.writeInt((int)cdstart);
+			// No extra data
+			output.writeShort(0);
 			
 			// No comment
 			output.writeShort(0);
+			
+			// Always the first disk
+			output.writeShort(0);
+			
+			// No iternal or external attributes
+			output.writeShort(0);
+			output.writeInt(0);
+			
+			// Relative offset to local header
+			output.writeInt((int)entry._localposition);
+			
+			// Write file name
+			output.write(efn);
 		}
+		
+		// The position where it ends
+		long cdend = output.size();
+		
+		// Write magic number
+		output.writeInt(_END_DIRECTORY_MAGIC_NUMBER);
+		
+		// Only a single disk is written
+		output.writeShort(0);
+		output.writeShort(0);
+		
+		// Number of entries on this disk and in all of them
+		output.writeShort(numtoc);
+		output.writeShort(numtoc);
+		
+		// The size of the central directory
+		output.writeInt((int)(cdend - cdstart));
+		
+		// Offset to the central directory
+		output.writeInt((int)cdstart);
+		
+		// No comment
+		output.writeShort(0);
 	}
 	
 	/**
@@ -276,82 +266,80 @@ public class ZipStreamWriter
 		
 		// Lock
 		LinkedList<__TOCEntry__> toc = this._toc;
-		synchronized (this.lock)
-		{
-			// {@squirreljme.error BF16 Cannot write new entry because the ZIP
-			// has been closed.}
-			if (this._closed)
-				throw new IOException("BF16");
-			
-			// {@squirreljme.error BF17 Cannot create a new entry for output
-			// because the previous entry has not be closed.}
-			if (this._inner != null || this._outer != null)
-				throw new IOException("BF17");
-			
-			// {@squirreljme.error BF18 A ZIP file cannot have more than
-			// 65536 entries.}
-			if (toc.size() >= 65535)
-				throw new IOException("BF18");
-			
-			// {@squirreljme.error BF19 The length of the input file exceeds
-			// 65535 UTF-8 characters. (The filename length)}
-			byte[] utfname = __name.getBytes("utf-8");
-			int fnn;
-			if ((fnn = utfname.length) > 65535)
-				throw new IOException(String.format("BF19 %d", fnn));
-			
-			// Setup contents
-			__TOCEntry__ last = new __TOCEntry__(this.output.size(), utfname,
-				__comp);
-			toc.addLast(last);
-			
-			// Write ZIP header data
-			ExtendedDataOutputStream output = this.output;
-			output.writeInt(_LOCAL_FILE_MAGIC_NUMBER);
-			
-			// Extract version
-			output.writeShort(__comp.extractVersion());
-			
-			// General purpose flag
-			output.writeShort(_GENERAL_PURPOSE_FLAGS);
-			
-			// Method
-			output.writeShort(__comp.method());
-			
-			// Modification date/time
-			output.writeShort(0);
-			output.writeShort(0);
-			
-			// CRC-32 and compress/uncompressed size are unknown
-			output.writeInt(0);
-			output.writeInt(0);
-			output.writeInt(0);
-			
-			// Write file name bytes
-			output.writeShort(fnn);
-			
-			// No extra field
-			output.writeShort(0);
-			
-			// Write file name
-			output.write(utfname);
-			
-			// Setup inner stream (for compressed size)
-			__InnerOutputStream__ inner = new __InnerOutputStream__();
-			
-			// Wrap inner with the compression algorithm
-			OutputStream wrapped = __comp.outputStream(inner);
-			
-			// Wrap that with the outer stream (uncompressed size)
-			__OuterOutputStream__ outer = new __OuterOutputStream__(wrapped);
-			
-			// Set
-			this._inner = inner;
-			this._outer = outer;
-			
-			// Return the outer stream
-			return outer;
-		}
+		
+		// {@squirreljme.error BF16 Cannot write new entry because the ZIP
+		// has been closed.}
+		if (this._closed)
+			throw new IOException("BF16");
+		
+		// {@squirreljme.error BF17 Cannot create a new entry for output
+		// because the previous entry has not be closed.}
+		if (this._inner != null || this._outer != null)
+			throw new IOException("BF17");
+		
+		// {@squirreljme.error BF18 A ZIP file cannot have more than
+		// 65536 entries.}
+		if (toc.size() >= 65535)
+			throw new IOException("BF18");
+		
+		// {@squirreljme.error BF19 The length of the input file exceeds
+		// 65535 UTF-8 characters. (The filename length)}
+		byte[] utfname = __name.getBytes("utf-8");
+		int fnn;
+		if ((fnn = utfname.length) > 65535)
+			throw new IOException(String.format("BF19 %d", fnn));
+		
+		// Setup contents
+		__TOCEntry__ last = new __TOCEntry__(this.output.size(), utfname,
+			__comp);
+		toc.addLast(last);
+		
+		// Write ZIP header data
+		ExtendedDataOutputStream output = this.output;
+		output.writeInt(_LOCAL_FILE_MAGIC_NUMBER);
+		
+		// Extract version
+		output.writeShort(__comp.extractVersion());
+		
+		// General purpose flag
+		output.writeShort(_GENERAL_PURPOSE_FLAGS);
+		
+		// Method
+		output.writeShort(__comp.method());
+		
+		// Modification date/time
+		output.writeShort(0);
+		output.writeShort(0);
+		
+		// CRC-32 and compress/uncompressed size are unknown
+		output.writeInt(0);
+		output.writeInt(0);
+		output.writeInt(0);
+		
+		// Write file name bytes
+		output.writeShort(fnn);
+		
+		// No extra field
+		output.writeShort(0);
+		
+		// Write file name
+		output.write(utfname);
+		
+		// Setup inner stream (for compressed size)
+		__InnerOutputStream__ inner = new __InnerOutputStream__();
+		
+		// Wrap inner with the compression algorithm
+		OutputStream wrapped = __comp.outputStream(inner);
+		
+		// Wrap that with the outer stream (uncompressed size)
+		__OuterOutputStream__ outer = new __OuterOutputStream__(wrapped);
+		
+		// Set
+		this._inner = inner;
+		this._outer = outer;
+		
+		// Return the outer stream
+		return outer;
 	}
 	
 	/**
@@ -365,57 +353,55 @@ public class ZipStreamWriter
 	{
 		// Lock
 		LinkedList<__TOCEntry__> toc = this._toc;
-		synchronized (this.lock)
-		{
-			__InnerOutputStream__ inner = this._inner;
-			__OuterOutputStream__ outer = this._outer;
-			
-			// {@squirreljme.error BF1a Cannot close entry because a current
-			// one is not being used.}
-			if (inner == null || outer == null)
-				throw new IOException("BF1a");
-			
-			// Flush both sides
-			inner.flush();
-			outer.flush();
-			
-			// Need to fill the size information and CRC for later
-			__TOCEntry__ last = toc.getLast();
-			
-			// Get sizes
-			long uncomp = outer._size;
-			long comp = inner._size;
-			
-			// {@squirreljme.error BF1b Either one or both of the compressed
-			// or uncompressed file sizes exceeds 4GiB. (The uncompressed size;
-			// The compressed size)}
-			if (uncomp >= _MAX_FILE_SIZE || comp >= _MAX_FILE_SIZE)
-				throw new IOException(String.format("BF1b %d %d", uncomp,
-					comp));
-			
-			// Store sizes
-			last._uncompressed = uncomp;
-			last._compressed = comp;
-			
-			// Determine CRC
-			int crc = outer.crccalc.checksum();
-			last._crc = crc;
-			
-			// The magic number of the data descriptor is not needed, however
-			// it helps prevent some abiguity when the input data stream is
-			// not compressed and contains a ZIP file.
-			ExtendedDataOutputStream output = this.output;
-			output.writeInt(_DATA_DESCRIPTOR_MAGIC_NUMBER);
-			
-			// Write CRC and sizes
-			output.writeInt((int)crc);
-			output.writeInt((int)comp);
-			output.writeInt((int)uncomp);
-			
-			// Clear streams to allow for next entry
-			this._inner = null;
-			this._outer = null;
-		}
+		
+		__InnerOutputStream__ inner = this._inner;
+		__OuterOutputStream__ outer = this._outer;
+		
+		// {@squirreljme.error BF1a Cannot close entry because a current
+		// one is not being used.}
+		if (inner == null || outer == null)
+			throw new IOException("BF1a");
+		
+		// Flush both sides
+		inner.flush();
+		outer.flush();
+		
+		// Need to fill the size information and CRC for later
+		__TOCEntry__ last = toc.getLast();
+		
+		// Get sizes
+		long uncomp = outer._size;
+		long comp = inner._size;
+		
+		// {@squirreljme.error BF1b Either one or both of the compressed
+		// or uncompressed file sizes exceeds 4GiB. (The uncompressed size;
+		// The compressed size)}
+		if (uncomp >= _MAX_FILE_SIZE || comp >= _MAX_FILE_SIZE)
+			throw new IOException(String.format("BF1b %d %d", uncomp,
+				comp));
+		
+		// Store sizes
+		last._uncompressed = uncomp;
+		last._compressed = comp;
+		
+		// Determine CRC
+		int crc = outer.crccalc.checksum();
+		last._crc = crc;
+		
+		// The magic number of the data descriptor is not needed, however
+		// it helps prevent some abiguity when the input data stream is
+		// not compressed and contains a ZIP file.
+		ExtendedDataOutputStream output = this.output;
+		output.writeInt(_DATA_DESCRIPTOR_MAGIC_NUMBER);
+		
+		// Write CRC and sizes
+		output.writeInt((int)crc);
+		output.writeInt((int)comp);
+		output.writeInt((int)uncomp);
+		
+		// Clear streams to allow for next entry
+		this._inner = null;
+		this._outer = null;
 	}
 	
 	/**
@@ -469,17 +455,13 @@ public class ZipStreamWriter
 		public void flush()
 			throws IOException
 		{
-			// Lock
-			synchronized (ZipStreamWriter.this.lock)
-			{
-				// Ignore if finished since the streams should be disconnected
-				// at this time
-				if (this.finished)
-					return;
-				
-				// Forward flush
-				this.wrapped.flush();
-			}
+			// Ignore if finished since the streams should be disconnected
+			// at this time
+			if (this.finished)
+				return;
+			
+			// Forward flush
+			this.wrapped.flush();
 		}
 		
 		/**
@@ -490,24 +472,20 @@ public class ZipStreamWriter
 		public void write(int __b)
 			throws IOException
 		{
-			// Lock
-			synchronized (ZipStreamWriter.this.lock)
-			{
-				// {@squirreljme.error BF1c Cannot write a single byte because
-				// the stream is closed.}
-				if (this.finished)
-					throw new IOException("BF1c");
-				
-				// {@squirreljme.error BF1d Cannot write a single byte because
-				// the ZIP entry would exceed 4GiB.}
-				int oldsize = this._size, newsize = oldsize + 1;
-				if (newsize < 0 || newsize < oldsize)
-					throw new IOException("BF1d");
-				
-				// Write data and increase size
-				this.wrapped.write(__b);
-				this._size = newsize;
-			}
+			// {@squirreljme.error BF1c Cannot write a single byte because
+			// the stream is closed.}
+			if (this.finished)
+				throw new IOException("BF1c");
+			
+			// {@squirreljme.error BF1d Cannot write a single byte because
+			// the ZIP entry would exceed 4GiB.}
+			int oldsize = this._size, newsize = oldsize + 1;
+			if (newsize < 0 || newsize < oldsize)
+				throw new IOException("BF1d");
+			
+			// Write data and increase size
+			this.wrapped.write(__b);
+			this._size = newsize;
 		}
 		
 		/**
@@ -525,24 +503,20 @@ public class ZipStreamWriter
 			if (__o < 0 || __l < 0 || (__o + __l) > n)
 				throw new IndexOutOfBoundsException("IOOB");
 			
-			// Lock
-			synchronized (ZipStreamWriter.this.lock)
-			{
-				// {@squirreljme.error BF1e Cannot write multiple bytes because
-				// the stream is closed.}
-				if (this.finished)
-					throw new IOException("BF1e");
-				
-				// {@squirreljme.error BF1f Cannot write multiple bytes because
-				// the ZIP entry would exceed 4GiB.}
-				int oldsize = this._size, newsize = oldsize + __l;
-				if (newsize < 0 || newsize < oldsize)
-					throw new IOException("BF1f");
-				
-				// Write data and increase size
-				this.wrapped.write(__b, __o, __l);
-				this._size = newsize;
-			}
+			// {@squirreljme.error BF1e Cannot write multiple bytes because
+			// the stream is closed.}
+			if (this.finished)
+				throw new IOException("BF1e");
+			
+			// {@squirreljme.error BF1f Cannot write multiple bytes because
+			// the ZIP entry would exceed 4GiB.}
+			int oldsize = this._size, newsize = oldsize + __l;
+			if (newsize < 0 || newsize < oldsize)
+				throw new IOException("BF1f");
+			
+			// Write data and increase size
+			this.wrapped.write(__b, __o, __l);
+			this._size = newsize;
 		}
 	}
 	
@@ -572,17 +546,13 @@ public class ZipStreamWriter
 		public final void close()
 			throws IOException
 		{
-			// Lock
-			synchronized (ZipStreamWriter.this.lock)
-			{
-				// Ignore if already finished
-				if (this.finished)
-					return;
-				
-				// Close and finish
-				this.finished = true;
-				ZipStreamWriter.this.__closeEntry();
-			}
+			// Ignore if already finished
+			if (this.finished)
+				return;
+			
+			// Close and finish
+			this.finished = true;
+			ZipStreamWriter.this.__closeEntry();
 		}
 	}
 	
@@ -620,17 +590,13 @@ public class ZipStreamWriter
 		public final void close()
 			throws IOException
 		{
-			// Lock
-			synchronized (ZipStreamWriter.this.lock)
-			{
-				// Ignore if already finished
-				if (this.finished)
-					return;
-				
-				// Close the wrapped stream
-				this.finished = true;
-				this.wrapped.close();
-			}
+			// Ignore if already finished
+			if (this.finished)
+				return;
+			
+			// Close the wrapped stream
+			this.finished = true;
+			this.wrapped.close();
 		}
 		
 		/**
