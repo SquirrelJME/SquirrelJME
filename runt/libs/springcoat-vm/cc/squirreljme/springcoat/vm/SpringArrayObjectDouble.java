0fdd337188e3948c3690589683a25034d6c57116
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-14 12:55:35 +0000

    Add distinction for double arrays.

---
+++ runt/libs/springcoat-vm/cc/squirreljme/springcoat/vm/SpringArrayObjectDouble.java
@@ -0,0 +1,113 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.springcoat.vm;
+
+/**
+ * Array backed by a double array.
+ *
+ * @since 2018/11/14
+ */
+public final class SpringArrayObjectDouble
+	extends SpringArrayObject
+{
+	/** Elements in the array. */
+	private final double[] _elements;
+	
+	/**
+	 * Initializes the array.
+	 *
+	 * @param __self The self type.
+	 * @param __cl The component type.
+	 * @param __l The array length.
+	 * @throws NullPointerException On null arguments.
+	 * @throws SpringNegativeArraySizeException If the array size is negative.
+	 * @since 2018/11/14
+	 */
+	public SpringArrayObjectDouble(SpringClass __self, SpringClass __cl,
+		int __l)
+		throws NullPointerException
+	{
+		super(__self, __cl, __l);
+		
+		// Initialize elements
+		this._elements = new double[__l];
+	}
+	
+	/**
+	 * Sets the index to the specified value.
+	 *
+	 * @param <C> The type of value to get.
+	 * @param __cl The type of value to get.
+	 * @param __dx The index to set.
+	 * @return The contained value.
+	 * @throws NullPointerException On null arguments.
+	 * @throws SpringArrayStoreException If the array cannot store the given
+	 * type.
+	 * @throws SpringArrayIndexOutOfBoundsException If the index is not within
+	 * bounds.
+	 * @since 2018/11/14
+	 */
+	@SuppressWarnings({"unchecked"})
+	public final <C> C get(Class<C> __cl, int __dx)
+		throws NullPointerException, SpringArrayIndexOutOfBoundsException
+	{
+		// Read value
+		try
+		{
+			return (C)Double.valueOf(this._elements[__dx]);
+		}
+		
+		// {@squirreljme.error BK30 Out of bounds access to array. (The index;
+		// The length of the array)}
+		catch (IndexOutOfBoundsException e)
+		{
+			throw new SpringArrayIndexOutOfBoundsException(
+				String.format("BK30 %d %d", __dx, length), e);
+		}
+	}
+	
+	/**
+	 * Sets the index to the specified value.
+	 *
+	 * @param __dx The index to set.
+	 * @param __v The value to set.
+	 * @throws SpringArrayStoreException If the array cannot store the given
+	 * type.
+	 * @throws SpringArrayIndexOutOfBoundsException If the index is not within
+	 * bounds.
+	 * @since 2018/11/14
+	 */
+	public final void set(int __dx, Object __v)
+		throws SpringArrayStoreException, SpringArrayIndexOutOfBoundsException
+	{
+		// Try setting
+		try
+		{
+			this._elements[__dx] = ((Double)__v).doubleValue();
+		}
+		
+		// {@squirreljme.error BK31 Could not set the index in the double
+		// array.}
+		catch (ClassCastException e)
+		{
+			throw new SpringArrayStoreException("BK31", e);
+		}
+		
+		// {@squirreljme.error BK32 Out of bounds access to array. (The index;
+		// The length of the array)}
+		catch (IndexOutOfBoundsException e)
+		{
+			throw new SpringArrayIndexOutOfBoundsException(
+				String.format("BK32 %d %d", __dx, this.length), e);
+		}
+	}
+}
+
