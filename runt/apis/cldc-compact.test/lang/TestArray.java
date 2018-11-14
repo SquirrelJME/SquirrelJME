031d216c26f753150906f8aab3825c7e4b43da98
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-14 12:58:29 +0000

    Add base class for testing arrays.

---
+++ runt/apis/cldc-compact.test/lang/TestArray.java
@@ -0,0 +1,34 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package lang;
+
+import java.util.Random;
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * This tests that arrays work properly.
+ *
+ * @since 2018/11/14
+ */
+public class TestArray
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/14
+	 */
+	@Override
+	public void test()
+	{
+		throw new todo.TODO();
+	}
+}
+
