058ad4e114a04c78cabe236fbfc9ff8ffab8a856
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-14 13:16:36 +0000

    Add array test.

--- runt/apis/cldc-compact.test/lang/TestArray.java
+++ runt/apis/cldc-compact.test/lang/TestArray.java
@@ -21,6 +21,10 @@ import net.multiphasicapps.tac.TestRunnable;
 public class TestArray
 	extends TestRunnable
 {
+	/** Storage count. */
+	public static final int COUNT =
+		8;
+	
 	/**
 	 * {@inheritDoc}
 	 * @since 2018/11/14
@@ -28,7 +32,66 @@ public class TestArray
 	@Override
 	public void test()
 	{
-		throw new todo.TODO();
+		Random rand = new Random(0x4C6F7665596F7521L);
+		
+		// Store values
+		boolean az[] = new boolean[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			az[i] = rand.nextBoolean();
+		
+		byte ab[] = new byte[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			ab[i] = (byte)rand.nextInt();
+		
+		short as[] = new short[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			as[i] = (short)rand.nextInt();
+		
+		char ac[] = new char[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			ac[i] = (char)rand.nextInt();
+		
+		int ai[] = new int[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			ai[i] = rand.nextInt();
+		
+		long al[] = new long[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			al[i] = rand.nextLong();
+		
+		float af[] = new float[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			af[i] = rand.nextFloat();
+		
+		double ad[] = new double[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			ad[i] = rand.nextDouble();
+		
+		// Read all values
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("boolean-" + i, az[i]);
+		
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("byte-" + i, ab[i]);
+		
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("short-" + i, as[i]);
+		
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("char-" + i, ac[i]);
+		
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("int-" + i, ai[i]);
+		
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("long-" + i, al[i]);
+		
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("float-" + i, Float.floatToRawIntBits(af[i]));
+		
+		for (int i = 0; i < COUNT; i++)
+			this.secondary("double-" + i, Double.doubleToRawLongBits(ad[i]));
+		
 	}
 }
 
