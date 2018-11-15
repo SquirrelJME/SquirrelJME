653f704d663c3a93b8c56881ef9e715adf2862c0
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-15 13:12:59 +0000

    Add base for shaded main.

---
+++ runt/libs/springcoat-vm/cc/squirreljme/springcoat/vm/ShadedMain.java
@@ -0,0 +1,51 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.springcoat.vm;
+
+/**
+ * This class is used to setup and initialize the shaded entry point for
+ * execution.
+ *
+ * @since 2018/11/15
+ */
+public class ShadedMain
+{
+	/**
+	 * Main entry point which runs off the given class.
+	 *
+	 * @param __acl The class to use resources from.
+	 * @param __pfx The prefix for library lookup.
+	 * @param __args Arguments to the VM.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/11/15
+	 */
+	public static final void main(Class<?> __acl, String __pfx,
+		String... __args)
+		throws NullPointerException
+	{
+		if (__acl == null || __pfx == null || __args == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2018/11/15
+	 */
+	public static final void main(String... __args)
+	{
+		throw new todo.TODO();
+	}
+}
+
