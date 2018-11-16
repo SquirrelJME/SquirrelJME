e60bee3e0d461b83f34d126740e7fcf9f3c83076
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-16 21:03:22 +0000

    Add base for shaded entry points.

---
+++ runt/libs/springcoat-vm/cc/squirreljme/springcoat/vm/ShadedEntryMain.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.springcoat.vm;
+
+/**
+ * This is the main entry point, generally for Java SE.
+ *
+ * @since 2018/11/16
+ */
+public class ShadedEntryMain
+{
+	/**
+	 * Main entry class.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2018/11/16
+	 */
+	public static void main(String... __args)
+	{
+		throw new todo.TODO();
+	}
+}
+
