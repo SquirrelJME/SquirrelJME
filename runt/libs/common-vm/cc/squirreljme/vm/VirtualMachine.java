e1e6ba748614f1e3e091cb7248bacd199ebeb387
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-17 15:02:34 +0000

    Most of the base factory initialization is done.

---
+++ runt/libs/common-vm/cc/squirreljme/vm/VirtualMachine.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.vm;
+
+/**
+ * This interface represents the virtual machine which may be executed
+ * accordingly.
+ *
+ * @since 2018/11/17
+ */
+public interface VirtualMachine
+{
+	/**
+	 * Runs the virtual machine.
+	 *
+	 * @return The exit code of the virtual machine.
+	 * @throws VMException If the VM threw an exception.
+	 * @since 2018/11/17
+	 */
+	public abstract int run()
+		throws VMException;
+}
+
--- runt/libs/springcoat-vm/cc/squirreljme/vm/springcoat/SpringCoatFactory.java
+++ runt/libs/springcoat-vm/cc/squirreljme/vm/springcoat/SpringCoatFactory.java
@@ -10,7 +10,14 @@
 
 package cc.squirreljme.vm.springcoat;
 
+import cc.squirreljme.vm.VirtualMachine;
+import cc.squirreljme.vm.VMClassLibrary;
+import cc.squirreljme.vm.VMException;
 import cc.squirreljme.vm.VMFactory;
+import cc.squirreljme.vm.VMSuiteManager;
+import java.util.HashMap;
+import java.util.Map;
+import net.multiphasicapps.profiler.ProfilerSnapshot;
 
 /**
  * Factory which creates instances of the SpringCoat virtual machine.
@@ -28,6 +35,20 @@ public class SpringCoatFactory
 	public SpringCoatFactory()
 	{
 		super("springcoat");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/17
+	 */
+	@Override
+	protected VirtualMachine createVM(ProfilerSnapshot __ps,
+		VMSuiteManager __sm, VMClassLibrary[] __cp, String __maincl,
+		boolean __ismid, int __gd, Map<String, String> __sprops,
+		String[] __args)
+		throws IllegalArgumentException, NullPointerException, VMException
+	{
+		throw new todo.TODO();
 	}
 }
 
