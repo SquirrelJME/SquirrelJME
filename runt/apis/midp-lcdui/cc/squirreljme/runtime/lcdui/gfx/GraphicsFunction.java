1e6739a285bd71a59e115f0d7a7c8b802f388f0c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-20 02:08:55 +0000

    Allow SpringCoat to access the backing arrays directly; Add graphics function serializer to forward and such.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/gfx/GraphicsFunction.java
@@ -0,0 +1,44 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.gfx;
+
+/**
+ * This represents a graphical function.
+ *
+ * @since 2018/11/19
+ */
+public enum GraphicsFunction
+{
+	/** End. */
+	;
+	
+	/**
+	 * Returns the graphics function for the given ID.
+	 *
+	 * @param __id The ID to translate.
+	 * @return The function for the ID.
+	 * @throws IllegalArgumentException If the ID is not valid.
+	 * @since 2018/11/19
+	 */
+	public static GraphicsFunction of(int __id)
+		throws IllegalArgumentException
+	{
+		// Depends
+		switch (__id)
+		{
+				// {@squirreljme.error EB2d Invalid graphics function.
+				// (The function ID)}
+			default:
+				throw new IllegalArgumentException("EB2d " + __id);
+		}
+	}
+}
+
---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/gfx/SerializedGraphics.java
@@ -0,0 +1,586 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.gfx;
+
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.Text;
+
+/**
+ * This class takes any input graphics operations that were performed for it,
+ * serializing anything that was sent to it. The serialized graphics can be
+ * forwarded somewhere for example for later deserialization.
+ *
+ * @since 2018/11/19
+ */
+public abstract class SerializedGraphics
+	extends Graphics
+{
+	/**
+	 * This method is called for any operation which serializes to graphics.
+	 *
+	 * @param __func The graphics operation to perform.
+	 * @param __args The input arguments to the function.
+	 * @return Any result from the operation.
+	 * @since 2018/11/19
+	 */
+	public abstract Object serialize(GraphicsFunction __func,
+		Object... __args);
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void clipRect(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void copyArea(int __sx, int __sy, int __w, int __h,
+		int __dx, int __dy, int __anchor)
+		throws IllegalArgumentException, IllegalStateException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawArc(int __x, int __y, int __w, int __h, int __sa,
+		int __aa)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawARGB16(short[] __data, int __off, int __scanlen,
+		int __x, int __y, int __w, int __h)
+		throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawChar(char __s, int __x, int __y, int __anchor)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawChars(char[] __s, int __o, int __l, int __x,
+		int __y, int __anchor)
+		throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawImage(Image __i, int __x, int __y, int __anchor)
+		throws IllegalArgumentException, NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawLine(int __x1, int __y1, int __x2, int __y2)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawRGB(int[] __data, int __off, int __scanlen,
+		int __x, int __y, int __w, int __h, boolean __alpha)
+		throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawRGB16(short[] __data, int __off, int __scanlen,
+		int __x, int __y, int __w, int __h)
+		throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawRect(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawRegion(Image __src, int __xsrc, int __ysrc,
+		int __wsrc, int __hsrc, int __trans, int __xdest, int __ydest,
+		int __anch)
+		throws IllegalArgumentException, NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawRegion(Image __src, int __xsrc, int __ysrc,
+		int __wsrc, int __hsrc, int __trans, int __xdest, int __ydest,
+		int __anch, int __wdest, int __hdest)
+		throws IllegalArgumentException, NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawRoundRect(int __x, int __y, int __w, int __h,
+		int __aw, int __ah)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawString(String __s, int __x, int __y,
+		int __anchor)
+		throws NullPointerException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawSubstring(String __s, int __o, int __l, int __x,
+		int __y, int __anchor)
+		throws NullPointerException, StringIndexOutOfBoundsException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void drawText(Text __t, int __x, int __y)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void fillArc(int __x, int __y, int __w, int __h, int __sa,
+		int __aa)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void fillRect(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void fillRoundRect(int __x, int __y, int __w, int __h,
+		int __aw, int __ah)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void fillTriangle(int __x1, int __y1, int __x2, int __y2,
+		int __x3, int __y3)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getAlpha()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getAlphaColor()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getBlendingMode()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getBlueComponent()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getClipHeight()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getClipWidth()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getClipX()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getClipY()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getColor()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getDisplayColor(int __rgb)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public Font getFont()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getGrayScale()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getGreenComponent()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getRedComponent()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getStrokeStyle()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getTranslateX()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public int getTranslateY()
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setAlpha(int __a)
+		throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setAlphaColor(int __argb)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setAlphaColor(int __a, int __r, int __g, int __b)
+		throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setBlendingMode(int __m)
+		throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setClip(int __x, int __y, int __w, int __h)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setColor(int __rgb)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setColor(int __r, int __g, int __b)
+		throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setFont(Font __a)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setGrayScale(int __v)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void setStrokeStyle(int __a)
+		throws IllegalArgumentException
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/19
+	 */
+	@Override
+	public void translate(int __x, int __y)
+	{
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Deserializes the input operation arguments and performs the call on
+	 * the destination graphics.
+	 *
+	 * @param __g The destination graphics object.
+	 * @param __func The graphics function to call.
+	 * @param __args Arguments to the function.
+	 * @return The result of the call.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/11/19
+	 */
+	public static Object deserialize(Graphics __g, GraphicsFunction __func,
+		Object... __args)
+		throws IllegalArgumentException, NullPointerException
+	{
+		if (__g == null || __func == null)
+			throw new NullPointerException("NARG");
+		
+		// Depends on the function
+		switch (__func)
+		{
+			default:
+				throw new RuntimeException("OOPS " + __func);
+		}
+	}
+}
+
