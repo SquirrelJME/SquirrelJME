033bdea95722cc7903d5e55955a120ebfaf45e6e
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-25 15:33:09 +0000

    Base class which will contain the PCF stuff in the future.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/font/PCFFont.java
@@ -0,0 +1,29 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.font;
+
+/**
+ * This class is capable of reading PCF formatted fonts and reading all of
+ * the information from it.
+ *
+ * Information on the font is at these locations:
+ * * <https://fontforge.github.io/en-US/documentation/reference/pcf-format/>
+ * * <https://web.archive.org/web/20020215194039if_/
+ *   http://myhome.hananet.net:80/~bumchul/xfont/pcf.txt>
+ * * <https://web.archive.org/web/20090205034052/
+ *   http://www.tsg.ne.jp:80/GANA/S/pcf2bdf/pcf.pdf>
+ *
+ * @since 2018/11/25
+ */
+public class PCFFont
+{
+}
+
