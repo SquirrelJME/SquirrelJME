f22d0ebb2d76f9ffc4f6d0ed34bdfa379786affc
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-25 18:46:14 +0000

    Add base for test.

---
+++ runt/apis/midp-lcdui.test/TestFont.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * Tests that the fonts load.
+ *
+ * @since 2018/11/25
+ */
+public class TestFont
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/25
+	 */
+	@Override
+	public void test()
+	{
+		throw new todo.TODO();
+	}
+}
+
