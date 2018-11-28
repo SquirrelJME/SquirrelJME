9249c80632dd5f24b4943598b7aa0a5487717ef9
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-28 04:34:53 +0000

    Add base for main entry for the converter and the stuff to actually convert.

---
+++ bldt/mids/pcf-to-sqf/net/multiphasicapps/pcftosqf/Main.java
@@ -0,0 +1,66 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.pcftosqf;
+
+import cc.squirreljme.runtime.lcdui.font.SQFFont;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * This is the main entry point for the font conversion.
+ *
+ * @since 2018/11/27
+ */
+public class Main
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @throws Throwable On any exception.
+	 * @since 2018/11/27
+	 */
+	public static final void main(String... __args)
+		throws Throwable
+	{
+		byte[] sqf;
+		try (InputStream in = Files.newInputStream(Paths.get(__args[0])))
+		{
+			// Read PCF Font
+			PCFFont pcf = PCFFont.read(in);
+			
+			// Convert to SQF
+			try (ByteArrayOutputStream baos = new ByteArrayOutputStream())
+			{
+				// Convert to SQF
+				new SQFConverter(pcf).convertTo(baos);
+				
+				// Use those bytes
+				sqf = baos.toByteArray();
+			}
+			
+			// Check that the font is valid
+			try (ByteArrayInputStream bais = new ByteArrayInputStream(sqf))
+			{
+				SQFFont.read(bais);
+			}
+		}
+		
+		// Write output SQF
+		System.out.write(sqf);
+	}
+}
+
---
+++ bldt/mids/pcf-to-sqf/net/multiphasicapps/pcftosqf/SQFConverter.java
@@ -0,0 +1,59 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.pcftosqf;
+
+import java.io.OutputStream;
+import java.io.IOException;
+
+/**
+ * Converts to SQF format from another font.
+ *
+ * @since 2018/11/27
+ */
+public class SQFConverter
+{
+	/** The input PCF font. */
+	protected final PCFFont pcf;
+	
+	/**
+	 * Initializes the converter
+	 *
+	 * @param __pcf The PCF font to convert.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/11/27
+	 */
+	public SQFConverter(PCFFont __pcf)
+		throws NullPointerException
+	{
+		if (__pcf == null)
+			throw new NullPointerException("NARG");
+		
+		this.pcf = __pcf;
+	}
+	
+	/**
+	 * Converts the font to SQF.
+	 *
+	 * @param __os The output stream.
+	 * @throws IOException On write errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/11/27
+	 */
+	public final void convertTo(OutputStream __os)
+		throws IOException, NullPointerException
+	{
+		if (__os == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+}
+
