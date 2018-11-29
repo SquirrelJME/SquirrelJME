9ce25fab8eade47d6c733ff4166676587d5f572f
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-29 15:06:24 +0000

    Add EmptyStackException.

---
+++ runt/apis/cldc-compact/java/util/EmptyStackException.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+/**
+ * This is thrown when the stack is empty.
+ *
+ * @since 2018/11/29
+ */
+public class EmptyStackException
+	extends RuntimeException
+{
+}
+
