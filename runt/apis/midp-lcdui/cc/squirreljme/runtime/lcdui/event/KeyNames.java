6c71dae9d6cc4b6012eb52e793d9800fc268577d
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-02 07:21:17 +0000

    Simplify the keys.

--- runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/event/KeyNames.java
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/event/KeyNames.java
@@ -42,62 +42,51 @@ public final class KeyNames
 		switch (__c)
 		{
 				// More friendly names
-			case Canvas.KEY_BACKSPACE:	return "Backspace";
-			case Canvas.KEY_DELETE:		return "Delete";
-			case Canvas.KEY_DOWN:		return "Down";
-			case Canvas.KEY_ENTER:		return "Enter";
-			case Canvas.KEY_ESCAPE:		return "Escape";
-			case Canvas.KEY_LEFT:		return "Left";
-			case Canvas.KEY_RIGHT:		return "Right";
-			case Canvas.KEY_SELECT:		return "Select";
-			case Canvas.KEY_SPACE:		return "Space";
-			case Canvas.KEY_TAB:		return "Tab";
-			case Canvas.KEY_UP:			return "Up";
-			
-				// Function keys
-			case NonStandardKey.F1:		return "F1";
-			case NonStandardKey.F2:		return "F2";
-			case NonStandardKey.F3:		return "F3";
-			case NonStandardKey.F4:		return "F4";
-			case NonStandardKey.F5:		return "F5";
-			case NonStandardKey.F6:		return "F6";
-			case NonStandardKey.F7:		return "F7";
-			case NonStandardKey.F8:		return "F8";
-			case NonStandardKey.F9:		return "F9";
-			case NonStandardKey.F10:	return "F10";
-			case NonStandardKey.F11:	return "F11";
-			case NonStandardKey.F12:	return "F12";
-			case NonStandardKey.F13:	return "F13";
-			case NonStandardKey.F14:	return "F14";
-			case NonStandardKey.F15:	return "F15";
-			case NonStandardKey.F16:	return "F16";
-			case NonStandardKey.F17:	return "F17";
-			case NonStandardKey.F18:	return "F18";
-			case NonStandardKey.F19:	return "F19";
-			case NonStandardKey.F20:	return "F20";
-			case NonStandardKey.F21:	return "F21";
-			case NonStandardKey.F22:	return "F22";
-			case NonStandardKey.F23:	return "F23";
-			case NonStandardKey.F24:	return "F24";
+			case Canvas.KEY_BACKSPACE:		return "Backspace";
+			case Canvas.KEY_DELETE:			return "Delete";
+			case Canvas.KEY_DOWN:			return "Down";
+			case Canvas.KEY_ENTER:			return "Enter";
+			case Canvas.KEY_ESCAPE:			return "Escape";
+			case Canvas.KEY_LEFT:			return "Left";
+			case Canvas.KEY_RIGHT:			return "Right";
+			case Canvas.KEY_SELECT:			return "Select";
+			case Canvas.KEY_SPACE:			return "Space";
+			case Canvas.KEY_TAB:			return "Tab";
+			case Canvas.KEY_UP:				return "Up";
 			
 				// Other keys
-			case NonStandardKey.LOGO:	return "LOGO";
+			case NonStandardKey.ALT:		return "Alt";
+			case NonStandardKey.CAPSLOCK:	return "CapsLock";
+			case NonStandardKey.CONTEXT_MENU:	return "ContextMenu";
+			case NonStandardKey.CONTROL:	return "Control";
+			case NonStandardKey.END:		return "End";
+			case NonStandardKey.HOME:		return "Home";
+			case NonStandardKey.INSERT:		return "Insert";
+			case NonStandardKey.LOGO:		return "Logo";
+			case NonStandardKey.META:		return "Meta";
+			case NonStandardKey.NUMLOCK:	return "NumLock";
+			case NonStandardKey.PAGE_UP:	return "PageUp";
+			case NonStandardKey.PAGE_DOWN:	return "PageDown";
+			case NonStandardKey.PAUSE:		return "Pause";
+			case NonStandardKey.PRINTSCREEN:return "PrintScreen";
+			case NonStandardKey.SCROLLLOCK:	return "ScrollLock";
+			case NonStandardKey.SHIFT:		return "Shift";
 			
 				// Non-standard game keys (used by the VM perhaps)
-			case NonStandardKey.GAME_UP:	return "VirtualGameUp";
-			case NonStandardKey.GAME_DOWN:	return "VirtualGameDown";
-			case NonStandardKey.GAME_LEFT:	return "VirtualGameLeft";
-			case NonStandardKey.GAME_RIGHT:	return "VirtualGameRight";
-			case NonStandardKey.GAME_FIRE:	return "VirtualGameFire";
-			case NonStandardKey.GAME_A:		return "VirtualGameA";
-			case NonStandardKey.GAME_B:		return "VirtualGameB";
-			case NonStandardKey.GAME_C:		return "VirtualGameC";
-			case NonStandardKey.GAME_D:		return "VirtualGameD";
+			case NonStandardKey.VGAME_UP:	return "VirtualGameUp";
+			case NonStandardKey.VGAME_DOWN:	return "VirtualGameDown";
+			case NonStandardKey.VGAME_LEFT:	return "VirtualGameLeft";
+			case NonStandardKey.VGAME_RIGHT:return "VirtualGameRight";
+			case NonStandardKey.VGAME_FIRE:	return "VirtualGameFire";
+			case NonStandardKey.VGAME_A:	return "VirtualGameA";
+			case NonStandardKey.VGAME_B:	return "VirtualGameB";
+			case NonStandardKey.VGAME_C:	return "VirtualGameC";
+			case NonStandardKey.VGAME_D:	return "VirtualGameD";
 			
-				// Unknown or default
+				// Default character or unknown
 			default:
-				// Default ASCII character, use the glyph name
-				if (__c >= 0x21 && __c <= 0x7E)
+				// Anything space and above just becomes the unicode character 
+				if (__c >= ' ')
 					return Character.valueOf((char)__c).toString();
 				
 				// {@squirreljme.error EB06 Cannot get the name for the given
--- runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/event/NonStandardKey.java
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/event/NonStandardKey.java
@@ -11,486 +11,114 @@
 package cc.squirreljme.runtime.lcdui.event;
 
 /**
- * This interface contains identifiers for non-standard keys.
+ * This byteerface contains identifiers for non-standard keys.
  *
  * @since 2017/02/12
  */
 public interface NonStandardKey
 {
 	/** Unknown. */
-	public static final int UNKNOWN =
-		-(0xFF + 0);
-	
-	/** F1. */
-	public static final int F1 =
-		-(0xFF + 24);
-	
-	/** F2. */
-	public static final int F2 =
-		-(0xFF + 23);
-	
-	/** F3. */
-	public static final int F3 =
-		-(0xFF + 22);
-	
-	/** F4. */
-	public static final int F4 =
-		-(0xFF + 21);
-	
-	/** F5. */
-	public static final int F5 =
-		-(0xFF + 20);
-	
-	/** F6. */
-	public static final int F6 =
-		-(0xFF + 19);
-	
-	/** F7. */
-	public static final int F7 =
-		-(0xFF + 18);
-	
-	/** F8. */
-	public static final int F8 =
-		-(0xFF + 17);
-	
-	/** F9. */
-	public static final int F9 =
-		-(0xFF + 16);
-	
-	/** F10. */
-	public static final int F10 =
-		-(0xFF + 15);
-		
-	/** F11. */
-	public static final int F11 =
-		-(0xFF + 14);
-	
-	/** F12. */
-	public static final int F12 =
-		-(0xFF + 13);
-	
-	/** F13. */
-	public static final int F13 =
-		-(0xFF + 12);
-	
-	/** F14. */
-	public static final int F14 =
-		-(0xFF + 11);
-	
-	/** F15. */
-	public static final int F15 =
-		-(0xFF + 10);
-	
-	/** F16. */
-	public static final int F16 =
-		-(0xFF + 9);
-	
-	/** F17. */
-	public static final int F17 =
-		-(0xFF + 8);
-	
-	/** F18. */
-	public static final int F18 =
-		-(0xFF + 7);
-	
-	/** F19. */
-	public static final int F19 =
-		-(0xFF + 6);
-	
-	/** F20. */
-	public static final int F20 =
-		-(0xFF + 5);
-		
-	/** F21. */
-	public static final int F21 =
-		-(0xFF + 4);
-	
-	/** F22. */
-	public static final int F22 =
-		-(0xFF + 3);
-	
-	/** F23. */
-	public static final int F23 =
-		-(0xFF + 2);
-	
-	/** F24. */
-	public static final int F24 =
-		-(0xFF + 1);
-	
-	/** Logo Key. */
-	public static final int LOGO =
-		-(0xFF + 25);
+	public static final byte UNKNOWN =
+		-8;
 	
 	/** Game Up. */
-	public static final int GAME_UP =
-		-(0xFF + 26);
+	public static final byte VGAME_UP =
+		-9;
 	
 	/** Game Down. */
-	public static final int GAME_DOWN =
-		-(0xFF + 27);
+	public static final byte VGAME_DOWN =
+		-10;
 	
 	/** Game Left. */
-	public static final int GAME_LEFT =
-		-(0xFF + 28);
+	public static final byte VGAME_LEFT =
+		-11;
 	
 	/** Game Right. */
-	public static final int GAME_RIGHT =
-		-(0xFF + 29);
+	public static final byte VGAME_RIGHT =
+		-12;
 	
 	/** Game fire. */
-	public static final int GAME_FIRE =
-		-(0xFF + 30);
+	public static final byte VGAME_FIRE =
+		-13;
 	
 	/** Game A. */
-	public static final int GAME_A =
-		-(0xFF + 31);
+	public static final byte VGAME_A =
+		-14;
 	
 	/** Game B. */
-	public static final int GAME_B =
-		-(0xFF + 32);
+	public static final byte VGAME_B =
+		-15;
 	
 	/** Game C. */
-	public static final int GAME_C =
-		-(0xFF + 33);
+	public static final byte VGAME_C =
+		-16;
 	
 	/** Game D. */
-	public static final int GAME_D =
-		-(0xFF + 34);
+	public static final byte VGAME_D =
+		-17;
 	
-	/** Key. */
-	public static final int ACCEPT =
-		-(0xFF + 40);
-
-	/** Key. */
-	public static final int AGAIN =
-		-(0xFF + 41);
-
-	/** Key. */
-	public static final int ALL_CANDIDATES =
-		-(0xFF + 42);
-
-	/** Key. */
-	public static final int ALPHANUMERIC =
-		-(0xFF + 43);
-
-	/** Key. */
-	public static final int ALT =
-		-(0xFF + 44);
-
-	/** Key. */
-	public static final int ALT_GRAPH =
-		-(0xFF + 45);
-
-	/** Key. */
-	public static final int BEGIN =
-		-(0xFF + 47);
+	/** Shift. */
+	public static final byte SHIFT =
+		-18;
 	
-	/** Key. */
-	public static final int CANCEL =
-		-(0xFF + 50);
-
-	/** Key. */
-	public static final int CAPS_LOCK =
-		-(0xFF + 51);
-
-	/** Key. */
-	public static final int CLEAR =
-		-(0xFF + 52);
-
-	/** Key. */
-	public static final int CODE_INPUT =
-		-(0xFF + 54);
-
-	/** Key. */
-	public static final int COMPOSE =
-		-(0xFF + 55);
-
-	/** Key. */
-	public static final int CONTEXT_MENU =
-		-(0xFF + 56);
-
-	/** Key. */
-	public static final int CONTROL =
-		-(0xFF + 57);
-
-	/** Key. */
-	public static final int CONVERT =
-		-(0xFF + 58);
-
-	/** Key. */
-	public static final int COPY =
-		-(0xFF + 59);
-
-	/** Key. */
-	public static final int CUT =
-		-(0xFF + 60);
-
-	/** Key. */
-	public static final int DEAD_ABOVEDOT =
-		-(0xFF + 61);
-
-	/** Key. */
-	public static final int DEAD_ABOVERING =
-		-(0xFF + 62);
-
-	/** Key. */
-	public static final int DEAD_ACUTE =
-		-(0xFF + 63);
-
-	/** Key. */
-	public static final int DEAD_BREVE =
-		-(0xFF + 64);
-
-	/** Key. */
-	public static final int DEAD_CARON =
-		-(0xFF + 65);
-
-	/** Key. */
-	public static final int DEAD_CEDILLA =
-		-(0xFF + 66);
-
-	/** Key. */
-	public static final int DEAD_CIRCUMFLEX =
-		-(0xFF + 67);
-
-	/** Key. */
-	public static final int DEAD_DIAERESIS =
-		-(0xFF + 68);
-
-	/** Key. */
-	public static final int DEAD_DOUBLEACUTE =
-		-(0xFF + 69);
-
-	/** Key. */
-	public static final int DEAD_GRAVE =
-		-(0xFF + 70);
-
-	/** Key. */
-	public static final int DEAD_IOTA =
-		-(0xFF + 71);
-
-	/** Key. */
-	public static final int DEAD_MACRON =
-		-(0xFF + 72);
-
-	/** Key. */
-	public static final int DEAD_OGONEK =
-		-(0xFF + 73);
-
-	/** Key. */
-	public static final int DEAD_SEMIVOICED_SOUND =
-		-(0xFF + 74);
-
-	/** Key. */
-	public static final int DEAD_TILDE =
-		-(0xFF + 75);
-
-	/** Key. */
-	public static final int DEAD_VOICED_SOUND =
-		-(0xFF + 76);
-
-	/** Key. */
-	public static final int END =
-		-(0xFF + 79);
-
-	/** Key. */
-	public static final int FINAL =
-		-(0xFF + 82);
-
-	/** Key. */
-	public static final int FIND =
-		-(0xFF + 83);
-
-	/** Key. */
-	public static final int FULL_WIDTH =
-		-(0xFF + 84);
-
-	/** Key. */
-	public static final int HALF_WIDTH =
-		-(0xFF + 85);
-
-	/** Key. */
-	public static final int HELP =
-		-(0xFF + 86);
-
-	/** Key. */
-	public static final int HIRAGANA =
-		-(0xFF + 87);
-
-	/** Key. */
-	public static final int HOME =
-		-(0xFF + 88);
-
-	/** Key. */
-	public static final int INPUT_METHOD_ON_OFF =
-		-(0xFF + 90);
-
-	/** Key. */
-	public static final int INSERT =
-		-(0xFF + 91);
-
-	/** Key. */
-	public static final int INVERTED_EXCLAMATION_MARK =
-		-(0xFF + 92);
-
-	/** Key. */
-	public static final int JAPANESE_HIRAGANA =
-		-(0xFF + 93);
-
-	/** Key. */
-	public static final int JAPANESE_KATAKANA =
-		-(0xFF + 94);
-
-	/** Key. */
-	public static final int JAPANESE_ROMAN =
-		-(0xFF + 95);
-
-	/** Key. */
-	public static final int KANA =
-		-(0xFF + 96);
-
-	/** Key. */
-	public static final int KANA_LOCK =
-		-(0xFF + 97);
-
-	/** Key. */
-	public static final int KANJI =
-		-(0xFF + 98);
-
-	/** Key. */
-	public static final int KATAKANA =
-		-(0xFF + 99);
-
-	/** Key. */
-	public static final int KP_DOWN =
-		-(0xFF + 90);
-
-	/** Key. */
-	public static final int KP_LEFT =
-		-(0xFF + 101);
-
-	/** Key. */
-	public static final int KP_RIGHT =
-		-(0xFF + 102);
-
-	/** Key. */
-	public static final int KP_UP =
-		-(0xFF + 103);
-
-	/** Key. */
-	public static final int META =
-		-(0xFF + 105);
-
-	/** Key. */
-	public static final int MODECHANGE =
-		-(0xFF + 106);
-
-	/** Key. */
-	public static final int MULTIPLY =
-		-(0xFF + 107);
-
-	/** Key. */
-	public static final int NONCONVERT =
-		-(0xFF + 108);
-
-	/** Key. */
-	public static final int NUM_LOCK =
-		-(0xFF + 109);
-
-	/** Key. */
-	public static final int PAGE_DOWN =
-		-(0xFF + 110);
-
-	/** Key. */
-	public static final int PAGE_UP =
-		-(0xFF + 111);
-
-	/** Key. */
-	public static final int PASTE =
-		-(0xFF + 112);
-
-	/** Key. */
-	public static final int PAUSE =
-		-(0xFF + 113);
-
-	/** Key. */
-	public static final int PREVIOUS_CANDIDATE =
-		-(0xFF + 114);
-
-	/** Key. */
-	public static final int PRINTSCREEN =
-		-(0xFF + 115);
-
-	/** Key. */
-	public static final int PROPS =
-		-(0xFF + 116);
-
-	/** Key. */
-	public static final int RIGHT_PARENTHESIS =
-		-(0xFF + 118);
-
-	/** Key. */
-	public static final int ROMAN_CHARACTERS =
-		-(0xFF + 119);
-
-	/** Key. */
-	public static final int SCROLL_LOCK =
-		-(0xFF + 120);
-
-	/** Key. */
-	public static final int SEPARATOR =
-		-(0xFF + 121);
-
-	/** Key. */
-	public static final int SHIFT =
-		-(0xFF + 122);
-
-	/** Key. */
-	public static final int STOP =
-		-(0xFF + 123);
-
-	/** Key. */
-	public static final int UNDO =
-		-(0xFF + 124);
+	/** Control. */
+	public static final byte CONTROL =
+		-19;
 	
-	/** Number pad 0. */
-	public static final int NUMPAD0 =
-		-(0xFF + 139);
+	/** Alt. */
+	public static final byte ALT =
+		-20;
 	
-	/** Number pad 1. */
-	public static final int NUMPAD1 =
-		-(0xFF + 138);
+	/** Logo. */
+	public static final byte LOGO =
+		-21;
 	
-	/** Number pad 2. */
-	public static final int NUMPAD2 =
-		-(0xFF + 137);
+	/** Caps lock. */
+	public static final byte CAPSLOCK =
+		-22;
 	
-	/** Number pad 3. */
-	public static final int NUMPAD3 =
-		-(0xFF + 136);
+	/** Context menu. */
+	public static final byte CONTEXT_MENU =
+		-23;
 	
-	/** Number pad 4. */
-	public static final int NUMPAD4 =
-		-(0xFF + 135);
+	/** Home. */
+	public static final byte HOME =
+		-24;
 	
-	/** Number pad 5. */
-	public static final int NUMPAD5 =
-		-(0xFF + 134);
+	/** End. */
+	public static final byte END =
+		-25;
 	
-	/** Number pad 6. */
-	public static final int NUMPAD6 =
-		-(0xFF + 133);
+	/** Page Up. */
+	public static final byte PAGE_UP =
+		-26;
 	
-	/** Number pad 7. */
-	public static final int NUMPAD7 =
-		-(0xFF + 132);
+	/** Page Down. */
+	public static final byte PAGE_DOWN =
+		-27;
 	
-	/** Number pad 8. */
-	public static final int NUMPAD8 =
-		-(0xFF + 131);
+	/** Meta. */
+	public static final byte META =
+		-28;
 	
-	/** Number pad 9. */
-	public static final int NUMPAD9 =
-		-(0xFF + 130);
+	/** Numlock. */
+	public static final byte NUMLOCK =
+		-29;
+	
+	/** Pause. */
+	public static final byte PAUSE =
+		-30;
+	
+	/** Print Screen. */
+	public static final byte PRINTSCREEN =
+		-31;
+	
+	/** Scroll lock. */
+	public static final byte SCROLLLOCK =
+		-32;
+	
+	/** Insert. */
+	public static final byte INSERT =
+		-33;
 }
 
