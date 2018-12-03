5b176a6660d5a1bf564efbc0bc36c04f61407854
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-03 02:21:20 +0000

    Minor work.

---
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/ui/UIDisplayState.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.lcdui.ui;
+
+import javax.microedition.lcdui.Display;
+
+/**
+ * This class contains the state that is contained within a {@link Display}.
+ *
+ * @since 2018/12/02
+ */
+public final class UIDisplayState
+{
+}
+
