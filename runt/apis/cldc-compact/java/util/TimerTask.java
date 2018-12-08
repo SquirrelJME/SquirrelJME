ffebae0ac191452c5434c83cb66c6487bf2399a5
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-08 18:25:54 +0000

    Add base Timer and TimerTask.

---
+++ runt/apis/cldc-compact/java/util/TimerTask.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+public abstract class TimerTask
+	implements Runnable
+{
+	protected TimerTask()
+	{
+		throw new todo.TODO();
+	}
+	
+	public boolean cancel()
+	{
+		throw new todo.TODO();
+	}
+	
+	public long scheduledExcutionTime()
+	{
+		throw new todo.TODO();
+	}
+}
+
