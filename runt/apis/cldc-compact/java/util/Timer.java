616ba06e6fe581bb32561158d5ea4b1a834b51ce
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-08 18:25:54 +0000

    Add base Timer and TimerTask.

---
+++ runt/apis/cldc-compact/java/util/Timer.java
@@ -0,0 +1,71 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+public class Timer
+{
+	public Timer()
+	{
+		throw new todo.TODO();
+	}
+	
+	public Timer(String __s)
+		throws NullPointerException
+	{
+		if (__s == null)
+			throw new NullPointerException("NARG");
+		
+		throw new todo.TODO();
+	}
+	
+	public void cancel()
+	{
+		throw new todo.TODO();
+	}
+	
+	public void purge()
+	{
+		throw new todo.TODO();
+	}
+	
+	public void schedule(TimerTask __task, Date __time)
+	{
+		throw new todo.TODO();
+	}
+	
+	public void schedule(TimerTask __task, Date __time, long __period)
+	{
+		throw new todo.TODO();
+	}
+	
+	public void schedule(TimerTask __task, long __delay)
+	{
+		throw new todo.TODO();
+	}
+	
+	public void schedule(TimerTask __task, long __delay, long __period)
+	{
+		throw new todo.TODO();
+	}
+	
+	public void scheduleAtFixedRate(TimerTask __task, Data __first,
+		long __period)
+	{
+		throw new todo.TODO();
+	}
+	
+	public void scheduleAtFixedRate(TimerTask __task, long __delay,
+		long __period)
+	{
+		throw new todo.TODO();
+	}
+}
+
