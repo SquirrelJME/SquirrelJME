ebba90372449419f18fbcdabed9a5a74b1dc96b2
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-09 15:45:18 +0000

    Make the draw in TextEditor final.

--- runt/apis/midp-lcdui/javax/microedition/lcdui/TextEditor.java
+++ runt/apis/midp-lcdui/javax/microedition/lcdui/TextEditor.java
@@ -181,7 +181,7 @@ public class TextEditor
 	 * @since 2018/12/08
 	 */
 	@Override
-	void __draw(UIStack __parent, UIStack __self, Graphics __g)
+	final void __draw(UIStack __parent, UIStack __self, Graphics __g)
 	{
 		__g.drawString(this.getClass().getName().toString(),
 			__g.getClipX(), __g.getClipY(), 0);
