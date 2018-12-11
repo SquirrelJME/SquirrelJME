6242d818da42967883464008e76dd888ee254292
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-11 05:19:51 +0000

    Split classes off.

---
+++ runt/kmid/launcher/cc/squirreljme/runtime/launcher/ui/__ActiveTask__.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.launcher.ui;
+
+import javax.microedition.swm.Task;
+
+/**
+ * The currently active task.
+ *
+ * @since 2018/12/10
+ */
+final class __ActiveTask__
+{
+	/** The current task. */
+	volatile Task _task;
+}
+
