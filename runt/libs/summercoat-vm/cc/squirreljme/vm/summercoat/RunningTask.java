fb4ecfd9fe1ad184824d473f3e92649e318973bf
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-02 01:50:37 +0000

    Add base for creating new tasks and the running task itself.

---
+++ runt/libs/summercoat-vm/cc/squirreljme/vm/summercoat/RunningTask.java
@@ -0,0 +1,34 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.vm.summercoat;
+
+import cc.squirreljme.vm.VirtualMachine;
+
+/**
+ * This represents a task which is running within the virtual machine.
+ *
+ * @since 2019/01/01
+ */
+public final class RunningTask
+	implements VirtualMachine
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/01/01
+	 */
+	@Override
+	public final int runVm()
+		throws VMException
+	{
+		throw new todo.TODO();
+	}
+}
+
