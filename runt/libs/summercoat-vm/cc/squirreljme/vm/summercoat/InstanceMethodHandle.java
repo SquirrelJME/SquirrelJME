05eaefdd5bd89b0ea94c34f32512b972fb3c8645
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-11 02:21:25 +0000

    Add MethodHandle which is a better and much simpler means to refering to methods for entry and it can have multiple lookup types as well.

---
+++ runt/libs/summercoat-vm/cc/squirreljme/vm/summercoat/InstanceMethodHandle.java
@@ -0,0 +1,25 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.vm.summercoat;
+
+/**
+ * This is a method handle which when called, it will look up the appropriate
+ * method to execute for the given object.
+ *
+ * Lookup will naturally fail if the method is not valid.
+ *
+ * @since 2019/01/10
+ */
+public final class InstanceMethodHandle
+	implements MethodHandle
+{
+}
+
---
+++ runt/libs/summercoat-vm/cc/squirreljme/vm/summercoat/StaticMethodHandle.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.vm.summercoat;
+
+/**
+ * This is a method handle which is bound to a single method and one where the
+ * execution uses the exactly specified method, no lookups are performed.
+ *
+ * @since 2019/01/10
+ */
+public final class StaticMethodHandle
+	implements MethodHandle
+{
+}
+
