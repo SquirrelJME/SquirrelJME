73b1fcfba79b825b63b1188a38a1e8eac429cab8
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-12 19:13:46 +0000

    Add base for vtable entry type.

---
+++ runt/libs/tool-scrf/net/multiphasicapps/scrf/VTableEntryType.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.scrf;
+
+/**
+ * This represents the type of entry used in the VTable.
+ *
+ * @since 2019/01/12
+ */
+public interface VTableEntryType
+{
+}
+
