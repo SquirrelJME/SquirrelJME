395f4e27e7ca26554cba89ba6be2886cef460c4d
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-12 15:13:16 +0000

    Add base for VTableBuilder.

---
+++ runt/libs/tool-classfile-scrf-compiler/net/multiphasicapps/scrf/compiler/VTableBuilder.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.scrf.compiler;
+
+/**
+ * This is the builder for vtables which are used to contain some run-time
+ * determined information about a class, it effectively allows the VM to
+ * access other classes and information without needing to build a combined
+ * structure, just interlinked.
+ *
+ * @since 2019/01/12
+ */
+public final class VTableBuilder
+{
+}
+
