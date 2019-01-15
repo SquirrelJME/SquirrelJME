2391881ef907c791bf13c6f2c426580fc71e1811
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-15 02:22:13 +0000

    Add base for RatufaCoat header.

---
+++ ratufacoat/ratufac.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// --------------------------------------------------------------------------*/
+
+/**
+ * RatufaCoat main library header.
+ *
+ * @since 2019/01/14
+ */
+
+/** Header guard. */
+#ifndef SJME_hGRATUFACOATRATACHRATUFACH
+#define SJME_hGRATUFACOATRATACHRATUFACH
+
+/** Anti-C++. */
+#ifdef _cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_cXRATUFACOATRATACHRATUFACH
+extern "C"
+{
+#endif /** #ifdef SJME_CXX_IS_EXTERNED */
+#endif /** #ifdef __cplusplus */
+
+/****************************************************************************/
+
+/****************************************************************************/
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_cXRATUFACOATRATACHRATUFACH
+}
+#undef SJME_cXRATUFACOATRATACHRATUFACH
+#undef SJME_CXX_IS_EXTERNED
+#endif /** #ifdef SJME_cXRATUFACOATRATACHRATUFACH */
+#endif /** #ifdef __cplusplus */
+
+/** Header guard. */
+#endif /* #ifndef SJME_hGRATUFACOATRATACHRATUFACH */
+
