15665b32a21ddc9ba0c1f41f337ac11df3043e72
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-17 23:33:45 +0000

    Add some base classes.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/classtree/package-info.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This contains the class tree which is used to structure classes in a package
+ * to class hierarchy.
+ *
+ * @since 2019/01/17
+ */
+
+package net.multiphasicapps.javac.classtree;
+
