3a3ba5755f390df6aae72534554f1c8de379a884
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-18 00:22:01 +0000

    Load classes and sources into units.

---
+++ runt/libs/tool-compiler/net/multiphasicapps/javac/classtree/BinaryUnit.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.javac.classtree;
+
+import net.multiphasicapps.javac.CompilerInput;
+
+/**
+ * This represents a unit which is a binary class file.
+ *
+ * @since 2019/01/17
+ */
+public final class BinaryUnit
+	extends Unit
+{
+	/** The input unit. */
+	protected final CompilerInput input;
+	
+	/**
+	 * Initializes the unit.
+	 *
+	 * @param __i The input unit.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2019/01/17
+	 */
+	public BinaryUnit(CompilerInput __i)
+		throws NullPointerException
+	{
+		if (__i == null)
+			throw new NullPointerException("NARG");
+		
+		this.input = __i;
+	}
+}
+
