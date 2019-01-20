64507fd88ef7b65ef0b9837ee7fe15b7c0548da5
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-20 20:09:10 +0000

    Add the missing Vector class.

---
+++ runt/apis/cldc-compact/java/util/Vector.java
@@ -0,0 +1,372 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+public class Vector<E>
+	extends AbstractList<E>
+	implements RandomAccess, Cloneable
+{
+	protected int capacityIncrement;
+	
+	protected int elementCount;
+	
+	protected Object[] elementData;
+	
+	public Vector(int __a, int __b)
+	{
+		super();
+		throw new todo.TODO();
+	}
+	
+	public Vector(int __a)
+	{
+		super();
+		throw new todo.TODO();
+	}
+	
+	public Vector()
+	{
+		super();
+		throw new todo.TODO();
+	}
+	
+	public Vector(Collection<? extends E> __a)
+	{
+		super();
+		throw new todo.TODO();
+	}
+	
+	@Override
+	public boolean add(E __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public void add(int __a, E __b)
+	{
+		throw new todo.TODO();
+	}
+	
+	@Override
+	public boolean addAll(Collection<? extends E> __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public boolean addAll(int __a, Collection<? extends E> __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void addElement(E __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public int capacity()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public void clear()
+	{
+		throw new todo.TODO();
+	}
+	
+	@Override
+	public Object clone()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public boolean contains(Object __a)
+	{
+		throw new todo.TODO();
+	}
+	
+	@Override
+	public boolean containsAll(Collection<?> __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void copyInto(Object[] __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public E elementAt(int __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public Enumeration<E> elements()
+	{
+		throw new todo.TODO();
+	}
+	
+	public void ensureCapacity(int __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public boolean equals(Object __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public E firstElement()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public E get(int __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public int indexOf(Object __a)
+	{
+		throw new todo.TODO();
+	}
+	
+	public int indexOf(Object __a, int __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void insertElementAt(E __a, int __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public boolean isEmpty()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public Iterator<E> iterator()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public E lastElement()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public int lastIndexOf(Object __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public int lastIndexOf(Object __a, int __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public boolean remove(Object __a)
+	{
+		throw new todo.TODO();
+	}
+	
+	@Override
+	public E remove(int __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public boolean removeAll(Collection<?> __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void removeAllElements()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public boolean removeElement(Object __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void removeElementAt(int __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	protected void removeRange(int __a, int __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public boolean retainAll(Collection<?> __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public E set(int __a, E __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void setElementAt(E __a, int __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void setSize(int __a)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public int size()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	@Override
+	public List<E> subList(int __a, int __b)
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+	
+	public void trimToSize()
+	{
+		synchronized (this)
+		{
+			throw new todo.TODO();
+		}
+	}
+}
+
