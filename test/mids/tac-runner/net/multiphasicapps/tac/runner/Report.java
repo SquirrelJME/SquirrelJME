916f946ae6c658f21f3d2eabced2d4ad03de4075
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-01-24 00:30:02 +0000

    Generation of JUnit test report; Make propertie to get in the hello demo final.

--- test/mids/tac-runner/net/multiphasicapps/tac/runner/Report.java
+++ test/mids/tac-runner/net/multiphasicapps/tac/runner/Report.java
@@ -22,6 +22,38 @@ import net.multiphasicapps.collections.SortedTreeMap;
  */
 public final class Report
 {
+	/** Nanoseconds per second. */
+	public static final double NANOS_PER_SECOND =
+		1_000_000_000D;
+		
+	/** Properties to print. */
+	private static final String[] _PROPERTIES =
+		new String[]
+		{
+			"java.version",
+			"java.vendor",
+			"java.vendor.email",
+			"java.vendor.url",
+			"java.vm.name",
+			"java.vm.version",
+			"cc.squirreljme.apilevel",
+			"java.vm.vendor",
+			"java.vm.vendor.email",
+			"java.vm.vendor.url",
+			"java.runtime.name",
+			"java.runtime.version",
+			"os.name",
+			"os.arch",
+			"os.version",
+			"microedition.locale",
+			"microedition.profiles",
+			"cc.squirreljme.vm.execpath",
+			"cc.squirreljme.vm.freemem",
+			"cc.squirreljme.vm.totalmem",
+			"cc.squirreljme.vm.maxmem",
+			"cc.squirreljme.debug",
+		};
+	
 	/** Report mapping. */
 	private final Map<String, ReportItem> _items =
 		new SortedTreeMap<>();
@@ -101,6 +133,10 @@ public final class Report
 	/**
 	 * Generates a JUnit test report.
 	 *
+	 * JUnit test reports are XML based, which is quite complex however
+	 * the test names and such are simple so we just need to output them
+	 * correctly.
+	 *
 	 * @param __out The stream to write to.
 	 * @throws NullPointerException On null arguments.
 	 * @since 2019/01/23
@@ -111,10 +147,93 @@ public final class Report
 		if (__out == null)
 			throw new NullPointerException("NARG");
 		
+		Map<String, ReportItem> items = this._items;
+		int numtests = this._numtests,
+			numpass = this._numpass,
+			numfail = this._numfail;
+		long totalns = this._totalns;
+		
+		// Lock
 		synchronized (this)
 		{
-			throw new todo.TODO();
+			// XML header
+			__out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			
+			// Testsuites
+			__out.printf("<testsuites failures=\"%d\" " +
+				"tests=\"%d\" time=\"%s\">%n",
+				numfail, numpass,
+				Report.doubleToString(totalns / NANOS_PER_SECOND));
+			
+			// There is just a single test suite
+			__out.printf("<testsuite name=\"SquirrelJME\" " +
+				"tests=\"%d\" failures=\"%d\" id=\"0\" time=\"%s\">%n",
+				numtests, numfail,
+				Report.doubleToString(totalns / NANOS_PER_SECOND));
+			
+			// Dump environment showing the details of the VM, this is taken
+			// from the hello demo. Just used to identify the VM and such.
+			__out.println("<properties>");
+			for (String p : _PROPERTIES)
+				try
+				{
+					String v = System.getProperty(p);
+					if (v != null)
+						__out.printf("<property name=\"%s\" value=\"%s\" />%n",
+							p, v.replace('"', '\''));
+				}
+				catch (SecurityException e)
+				{
+				}
+			__out.println("</properties>");
+			
+			// Print every item
+			for (ReportItem i : items.values())
+			{
+				__out.printf("<testcase name=\"%s\" " +
+					"classname=\"%s\" status=\"%s\" time=\"%s\">%n",
+					i.name, i.name, (i.passed ? "pass" : "fail"),
+					Report.doubleToString(i.duration / NANOS_PER_SECOND));
+				
+				// Failure gets a note attached
+				if (!i.passed)
+					__out.println("<failure message=\"Failed.\" />");
+
+				// End
+				__out.println("</testcase>");
+			}
+			
+			// End everything
+			__out.println("</testsuite>");
+			__out.println("</testsuites>");
 		}
+	}
+	
+	/**
+	 * Converts a double to a string. This is needed because at the time of
+	 * this writing doubles are not supported in {@link java.util.Formatter}.
+	 *
+	 * @param __d The double to convert.
+	 * @return The resulting string.
+	 * @since 2019/01/23
+	 */
+	public static final String doubleToString(double __d)
+	{
+		// Add whole number portion (just cast to number value)
+		StringBuilder sb = new StringBuilder();
+		sb.append((long)__d);
+		
+		// Add decimal point
+		sb.append(".");
+		
+		// Add some fraction parts
+		long frac = ((long)(__d * 1000.0D)) % 1000L;
+		if (frac < 0)
+			frac = -frac;
+		sb.append(String.format("%03d", frac));
+		
+		// Build it
+		return sb.toString();
 	}
 }
 
