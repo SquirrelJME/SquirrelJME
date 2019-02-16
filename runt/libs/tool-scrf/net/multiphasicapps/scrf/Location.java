c8afc0e2263423587308ca1457758fea7d1eb12e
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-02-16 19:18:42 +0000

    Add RegisterLocation which is to refer to a register.

---
+++ runt/libs/tool-scrf/net/multiphasicapps/scrf/Location.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.scrf;
+
+/**
+ * This represents a location that points to some data either in a register,
+ * some memory, or another kind of port.
+ *
+ * @since 2019/02/16
+ */
+public interface Location
+{
+}
+
