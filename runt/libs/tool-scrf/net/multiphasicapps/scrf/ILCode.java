afa4547171016817bdeaf245c463472b2500f74c
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-02-17 00:23:17 +0000

    Add base for a processor of methods.

---
+++ runt/libs/tool-scrf/net/multiphasicapps/scrf/ILCode.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.scrf;
+
+/**
+ * This class stores the intermediate SummerCoat language.
+ *
+ * @since 2019/02/16
+ */
+public final class ILCode
+{
+}
+
