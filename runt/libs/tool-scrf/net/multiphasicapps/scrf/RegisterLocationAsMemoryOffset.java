4537ae25d886ad650577085b965eeaa6b788db41
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-02-28 23:34:26 +0000

    Minor work on new classes.

---
+++ runt/libs/tool-scrf/net/multiphasicapps/scrf/RegisterLocationAsMemoryOffset.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.scrf;
+
+/**
+ * This treats a register location as a memory offset.
+ *
+ * @since 2019/02/28
+ */
+public final class RegisterLocationAsMemoryOffset
+	implements MemoryOffset
+{
+}
+
