256aa03fcfad910ef56f64d93af76b470dfd9535
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-03-10 21:23:06 +0000

    Add test to additionally test loading of minimized classes.

---
+++ runt/libs/tool-classfile.test/TestMinimizedLoad.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import net.multiphasicapps.classfile.ClassFile;
+import net.multiphasicapps.classfile.mini.Minimizer;
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * Tests that minimizing is performed properly and loading minimized classes
+ * works as well.
+ *
+ * @since 2019/03/10
+ */
+public class TestMinimizedLoad
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/03/10
+	 */
+	@Override
+	public void test()
+		throws Throwable
+	{
+		for (String x : new String[]{"ByteDeque.class",
+			"InflaterInputStream.class"})
+			try (InputStream in = TestClassLoad.class.getResourceAsStream(x))
+			{
+				Minimizer.minimize(ClassFile.decode(in));
+			}
+	}
+}
+
