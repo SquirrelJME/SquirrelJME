63ccf54fec9c3c180c8f423303385a27aaf17927
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-03-10 21:18:16 +0000

    Add test in tool-classfile which makes sure classes can be read properly.

---
+++ runt/libs/tool-classfile.test/TestClassLoad.java
@@ -0,0 +1,39 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+import java.io.InputStream;
+import net.multiphasicapps.classfile.ClassFile;
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * Tests that classes load properly.
+ *
+ * @since 2019/03/10
+ */
+public class TestClassLoad
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/03/10
+	 */
+	@Override
+	public void test()
+		throws Throwable
+	{
+		for (String x : new String[]{"ByteDeque.class",
+			"InflaterInputStream.class"})
+			try (InputStream in = TestClassLoad.class.getResourceAsStream(x))
+			{
+				ClassFile.decode(in);
+			}
+	}
+}
+
