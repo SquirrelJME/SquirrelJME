55fe4ca3a94dddb92068a8a24189a43a6e3d24e8
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-03-13 18:24:58 +0000

    Base loop for processing methods.

---
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/mini/__TempMethods__.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.classfile.mini;
+
+/**
+ * Contains temporary method information.
+ *
+ * @since 2019/03/13
+ */
+final class __TempMethods__
+{
+}
+
