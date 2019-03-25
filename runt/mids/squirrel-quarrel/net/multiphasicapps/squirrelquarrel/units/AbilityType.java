22c319e21f086c918400f55d3dec658ed9a15bd4
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-03-25 02:17:55 +0000

    Add some base unit information for the first building.

---
+++ runt/mids/squirrel-quarrel/net/multiphasicapps/squirrelquarrel/units/AbilityType.java
@@ -0,0 +1,26 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.squirrelquarrel.units;
+
+/**
+ * The type of abilities available.
+ *
+ * @since 2019/03/24
+ */
+public enum AbilityType
+{
+	/** Regenerates HP. */
+	REGENERATES_HP,
+	
+	/** End. */
+	;
+}
+
