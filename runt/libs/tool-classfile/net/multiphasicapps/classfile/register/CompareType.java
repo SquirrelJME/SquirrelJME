323e3416f2c98dd0d6f8abf2004c9dc885d2ec55
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-03-26 18:27:54 +0000

    Add int comparison against zero.

---
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/register/CompareType.java
@@ -0,0 +1,41 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.classfile.register;
+
+/**
+ * This represents the type of comparison to perform.
+ *
+ * @since 2019/03/26
+ */
+public enum CompareType
+{
+	/** Equals. */
+	EQUALS,
+	
+	/** Not equals. */
+	NOT_EQUALS,
+	
+	/** Less than. */
+	LESS_THAN,
+	
+	/** Less or equals. */
+	LESS_THAN_OR_EQUALS,
+	
+	/** Greater than. */
+	GREATER_THAN,
+	
+	/** Greater or equals. */
+	GREATER_THAN_OR_EQUALS,
+	
+	/** End. */
+	;
+}
+
