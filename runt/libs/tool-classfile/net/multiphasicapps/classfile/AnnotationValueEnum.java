7d1386838101681382af5f1d1e1cdb55e04db357
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-01 12:33:12 +0000

    Parse of enumerated values.

--- runt/libs/tool-classfile/net/multiphasicapps/classfile/AnnotationValueEnum.java
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/AnnotationValueEnum.java
@@ -18,6 +18,30 @@ package net.multiphasicapps.classfile;
 public final class AnnotationValueEnum
 	implements AnnotationValue
 {
+	/** The type name. */
+	protected final FieldDescriptor type;
+	
+	/** The enum name. */
+	protected final FieldName name;
+	
+	/**
+	 * Initializes the annotation enumeration value.
+	 *
+	 * @param __type The type used.
+	 * @param __name The name of the enumeration key.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2019/04/01
+	 */
+	public AnnotationValueEnum(FieldDescriptor __type, FieldName __name)
+		throws NullPointerException
+	{
+		if (__type == null || __name == null)
+			throw new NullPointerException("NARG");
+		
+		this.type = __type;
+		this.name = __name;
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 * @since 2018/06/16
