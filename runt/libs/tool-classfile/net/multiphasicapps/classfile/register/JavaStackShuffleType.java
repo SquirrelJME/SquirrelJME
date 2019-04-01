7b54874f17a2d3ab3d5ad3425a473cc05c108894
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-01 18:40:59 +0000

    Add shuffle ordering as strings (keeps it simpler and easier to read).

--- runt/libs/tool-classfile/net/multiphasicapps/classfile/register/JavaStackShuffleType.java
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/register/JavaStackShuffleType.java
@@ -19,33 +19,59 @@ package net.multiphasicapps.classfile.register;
 public enum JavaStackShuffleType
 {
 	/** dup. */
-	DUP,
+	DUP("a:aa"),
 	
 	/** dup_x1. */
-	DUP_X1,
+	DUP_X1("ba:aba"),
 	
 	/** dup_x2. */
-	DUP_X2,
+	DUP_X2("cba:acba"),
 	
 	/** dup2. */
-	DUP2,
+	DUP2("ba:baba",
+		"A:AA"),
 	
 	/** dup2_x1. */
-	DUP2_X1,
+	DUP2_X1("cba:bacba",
+		"bA:AbA"),
 	
 	/** dup2_x2. */
-	DUP2_X2,
+	DUP2_X2("dcba:badcba",
+		"cbA:Acba",
+		"Cba:baCba",
+		"BA:ABA"),
 	
 	/** pop. */
-	POP,
+	POP("a:"),
 	
 	/** pop2. */
-	POP2,
+	POP2("ba:",
+		"A:"),
 	
 	/** swap. */
-	SWAP,
+	SWAP("ba:ab"),
 	
 	/** End. */
 	;
+	
+	/** Forms of this operation. */
+	private final String[] _forms;
+	
+	/**
+	 * Initialize the shuffle form information.
+	 *
+	 * The forms consist of characters for the various items on the stack.
+	 * A lowercase letter represents a narrow type while a capital letter
+	 * represents a wide type. Input and output is separated by a colon. The
+	 * operation is just that whatever is pushed to the stack has the same
+	 * value as the items removed from the stack.
+	 *
+	 * @param __fs The forms.
+	 * @since 2019/04/01
+	 */
+	private JavaStackShuffleType(String... __fs)
+	{
+		this._forms = __fs;
+	}
 }
 
