c2384b676f0754c011dbefe91e352160cc769cf1
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-03 12:32:06 +0000

    Copy the initializing stuff from the experimental translator.

--- runt/libs/tool-classfile/net/multiphasicapps/classfile/register/QuickTranslator.java
+++ runt/libs/tool-classfile/net/multiphasicapps/classfile/register/QuickTranslator.java
@@ -9,7 +9,34 @@
 
 package net.multiphasicapps.classfile.register;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import net.multiphasicapps.classfile.ByteCode;
+import net.multiphasicapps.classfile.ClassName;
+import net.multiphasicapps.classfile.ConstantValue;
+import net.multiphasicapps.classfile.ConstantValueInteger;
+import net.multiphasicapps.classfile.ExceptionHandler;
+import net.multiphasicapps.classfile.ExceptionHandlerTable;
+import net.multiphasicapps.classfile.FieldDescriptor;
+import net.multiphasicapps.classfile.FieldReference;
+import net.multiphasicapps.classfile.Instruction;
+import net.multiphasicapps.classfile.InstructionIndex;
+import net.multiphasicapps.classfile.InstructionJumpTarget;
+import net.multiphasicapps.classfile.InstructionJumpTargets;
+import net.multiphasicapps.classfile.JavaType;
+import net.multiphasicapps.classfile.MethodDescriptor;
+import net.multiphasicapps.classfile.MethodHandle;
+import net.multiphasicapps.classfile.MethodName;
+import net.multiphasicapps.classfile.MethodReference;
+import net.multiphasicapps.classfile.PrimitiveType;
+import net.multiphasicapps.classfile.StackMapTable;
+import net.multiphasicapps.classfile.StackMapTableState;
 
 /**
  * This is a translator which is designed to run as quick as possible while
@@ -27,6 +54,19 @@ public class QuickTranslator
 	protected final RegisterCodeBuilder codebuilder =
 		new RegisterCodeBuilder();
 	
+	/** Exception tracker. */
+	protected final __ExceptionTracker__ exceptiontracker;
+	
+	/** Default field access type, to determine how fields are accessed. */
+	protected final FieldAccessTime defaultfieldaccesstime;
+	
+	/** The stacks which have been recorded. */
+	private final Map<Integer, JavaStackState> _stacks =
+		new LinkedHashMap<>();
+	
+	/** The current state of the stack. */
+	private JavaStackState _stack;
+	
 	/**
 	 * Converts the input byte code to a register based code.
 	 *
@@ -41,6 +81,16 @@ public class QuickTranslator
 			throw new NullPointerException("NARG");
 		
 		this.bytecode = __bc;
+		this.exceptiontracker = new __ExceptionTracker__(__bc);
+		this.defaultfieldaccesstime = ((__bc.isInstanceInitializer() ||
+			__bc.isStaticInitializer()) ? FieldAccessTime.INITIALIZER :
+			FieldAccessTime.NORMAL);
+			
+		// Load initial Java stack state from the initial stack map
+		JavaStackState s;
+		this._stack = (s = JavaStackState.of(__bc.stackMapTable().get(0),
+			__bc.writtenLocals()));
+		this._stacks.put(0, s);
 	}
 	
 	/**
