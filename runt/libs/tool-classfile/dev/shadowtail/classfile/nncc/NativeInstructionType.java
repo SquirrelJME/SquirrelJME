f7e57cb0705138a29c6ffe71ca076941344f1534
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-07 00:25:56 +0000

    Base for native instruction type.

---
+++ runt/libs/tool-classfile/dev/shadowtail/classfile/nncc/NativeInstructionType.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package dev.shadowtail.classfile.nncc;
+
+/**
+ * This represents a type of native instruction.
+ *
+ * @since 2019/04/06
+ */
+public interface NativeInstructionType
+{
+}
+
