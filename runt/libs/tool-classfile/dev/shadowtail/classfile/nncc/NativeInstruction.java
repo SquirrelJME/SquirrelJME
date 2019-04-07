e2b7c27ca9ad50efd807544821e91ffd5268e28c
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-07 12:45:43 +0000

    More fixups.

--- runt/libs/tool-classfile/dev/shadowtail/classfile/nncc/NativeInstruction.java
+++ runt/libs/tool-classfile/dev/shadowtail/classfile/nncc/NativeInstruction.java
@@ -8,7 +8,7 @@
 // See license.mkd for licensing and copyright information.
 // ---------------------------------------------------------------------------
 
-package net.multiphasicapps.classfile.register;
+package dev.shadowtail.classfile.nncc;
 
 import java.util.Arrays;
 import java.util.Collection;
--- runt/libs/tool-classfile/dev/shadowtail/classfile/xlate/DataType.java
+++ runt/libs/tool-classfile/dev/shadowtail/classfile/xlate/DataType.java
@@ -49,272 +49,6 @@ public enum DataType
 	;
 	
 	/**
-	 * Return the operation to use for array operations.
-	 *
-	 * @param __s Store into array?
-	 * @return The operation to use for arrays.
-	 * @since 2019/04/02
-	 */
-	public final int arrayOperation(boolean __s)
-	{
-		if (__s)
-			switch (this)
-			{
-				case POINTER:
-					return RegisterOperationType.ARRAY_STORE_OBJECT;
-				
-				case BYTE:
-					return RegisterOperationType.ARRAY_STORE_X8;
-					
-				case SHORT:
-					return RegisterOperationType.ARRAY_STORE_X16;
-					
-				case INTEGER:
-				case FLOAT:
-					return RegisterOperationType.ARRAY_STORE_X32;
-				
-				case LONG:
-				case DOUBLE:
-					return RegisterOperationType.ARRAY_STORE_X64;
-			}
-		else
-			switch (this)
-			{
-				case POINTER:
-					return RegisterOperationType.ARRAY_LOAD_OBJECT;
-				
-				case BYTE:
-					return RegisterOperationType.ARRAY_LOAD_X8;
-					
-				case SHORT:
-					return RegisterOperationType.ARRAY_LOAD_X16;
-					
-				case INTEGER:
-				case FLOAT:
-					return RegisterOperationType.ARRAY_LOAD_X32;
-				
-				case LONG:
-				case DOUBLE:
-					return RegisterOperationType.ARRAY_LOAD_X64;
-			}
-		
-		throw new todo.OOPS();
-	}
-	
-	/**
-	 * Returns the operation used for copies.
-	 *
-	 * @param __nc Is counting needed?
-	 * @return The operation to use.
-	 * @since 2019/03/27
-	 */
-	public final int copyOperation(boolean __nc)
-	{
-		switch (this)
-		{
-			case POINTER:
-				if (__nc)
-					return RegisterOperationType.X32_COPY;
-				return RegisterOperationType.OBJECT_COPY;
-			
-			case BYTE:
-			case SHORT:
-			case INTEGER:
-			case FLOAT:
-				return RegisterOperationType.X32_COPY;
-			
-			case LONG:
-			case DOUBLE:
-				return RegisterOperationType.X64_COPY;
-		}
-		
-		// Should not be reached
-		throw new todo.OOPS();
-	}
-	
-	/**
-	 * Returns the operation that should be used to operate on the field.
-	 *
-	 * @param __static Is the field static?
-	 * @param __write Is the field written to?
-	 * @return The operation to use when accessing a field of this type.
-	 * @since 2019/03/27
-	 */
-	public final int fieldAccessOperation(boolean __static, boolean __write)
-	{
-		if (__static)
-			if (__write)
-				switch (this)
-				{
-					case POINTER:
-						return RegisterOperationType.SFIELD_STORE_OBJECT;
-					case BYTE:
-						return RegisterOperationType.SFIELD_STORE_X8;
-					case SHORT:
-						return RegisterOperationType.SFIELD_STORE_X16;
-					case INTEGER:
-					case FLOAT:
-						return RegisterOperationType.SFIELD_STORE_X32;
-					case LONG:
-					case DOUBLE:
-						return RegisterOperationType.SFIELD_STORE_X64;
-				}
-			else
-				switch (this)
-				{
-					case POINTER:
-						return RegisterOperationType.SFIELD_LOAD_OBJECT;
-					case BYTE:
-						return RegisterOperationType.SFIELD_LOAD_X8;
-					case SHORT:
-						return RegisterOperationType.SFIELD_LOAD_X16;
-					case INTEGER:
-					case FLOAT:
-						return RegisterOperationType.SFIELD_LOAD_X32;
-					case LONG:
-					case DOUBLE:
-						return RegisterOperationType.SFIELD_LOAD_X64;
-				}
-		else
-			if (__write)
-				switch (this)
-				{
-					case POINTER:
-						return RegisterOperationType.IFIELD_STORE_OBJECT;
-					case BYTE:
-						return RegisterOperationType.IFIELD_STORE_X8;
-					case SHORT:
-						return RegisterOperationType.IFIELD_STORE_X16;
-					case INTEGER:
-					case FLOAT:
-						return RegisterOperationType.IFIELD_STORE_X32;
-					case LONG:
-					case DOUBLE:
-						return RegisterOperationType.IFIELD_STORE_X64;
-				}
-			else
-				switch (this)
-				{
-					case POINTER:
-						return RegisterOperationType.IFIELD_LOAD_OBJECT;
-					case BYTE:
-						return RegisterOperationType.IFIELD_LOAD_X8;
-					case SHORT:
-						return RegisterOperationType.IFIELD_LOAD_X16;
-					case INTEGER:
-					case FLOAT:
-						return RegisterOperationType.IFIELD_LOAD_X32;
-					case LONG:
-					case DOUBLE:
-						return RegisterOperationType.IFIELD_LOAD_X64;
-				}
-		
-		// Should not be reached
-		throw new todo.OOPS();
-	}
-	
-	/**
-	 * Returns the operation used for copies.
-	 *
-	 * @param __nc Is counting needed?
-	 * @param __store Is this a store?
-	 * @return The operation to use.
-	 * @since 2019/03/27
-	 */
-	public final int fieldRegisterOperation(boolean __nc, boolean __store)
-	{
-		if (__store)
-			switch (this)
-			{
-				case POINTER:
-					if (__nc)
-						return RegisterOperationType.OBJECT_FIELD_STORE;
-					return RegisterOperationType.X32_FIELD_STORE;
-				
-				case BYTE:
-				case SHORT:
-				case INTEGER:
-				case FLOAT:
-					return RegisterOperationType.X32_FIELD_STORE;
-				
-				case LONG:
-				case DOUBLE:
-					return RegisterOperationType.X64_FIELD_STORE;
-			}
-		else
-			switch (this)
-			{
-				case POINTER:
-					if (__nc)
-						return RegisterOperationType.OBJECT_FIELD_LOAD;
-					return RegisterOperationType.X32_FIELD_LOAD;
-				
-				case BYTE:
-				case SHORT:
-				case INTEGER:
-				case FLOAT:
-					return RegisterOperationType.X32_FIELD_LOAD;
-				
-				case LONG:
-				case DOUBLE:
-					return RegisterOperationType.X64_FIELD_LOAD;
-			}
-		
-		// Should not be reached
-		throw new todo.OOPS();
-	}
-	
-	/**
-	 * The operation to use when loading the return value.
-	 *
-	 * @return The operation used.
-	 * @since 2019/04/06
-	 */
-	public final int returnValueLoadOperation()
-	{
-		switch (this)
-		{
-			case POINTER:
-			case BYTE:
-			case SHORT:
-			case INTEGER:
-			case FLOAT:
-				return RegisterOperationType.GET_X32_RETURN_VALUE;
-				
-			case LONG:
-			case DOUBLE:
-				return RegisterOperationType.GET_X64_RETURN_VALUE;
-		}
-		
-		throw new todo.OOPS();
-	}
-	
-	/**
-	 * The operation to use when loading the return value.
-	 *
-	 * @return The operation used.
-	 * @since 2019/04/06
-	 */
-	public final int returnValueStoreOperation()
-	{
-		switch (this)
-		{
-			case POINTER:
-			case BYTE:
-			case SHORT:
-			case INTEGER:
-			case FLOAT:
-				return RegisterOperationType.SET_X32_RETURN_VALUE;
-				
-			case LONG:
-			case DOUBLE:
-				return RegisterOperationType.SET_X64_RETURN_VALUE;
-		}
-		
-		throw new todo.OOPS();
-	}
-	
-	/**
 	 * Returns this as a basic Java type.
 	 *
 	 * @return The Java type of this.
@@ -324,7 +58,7 @@ public enum DataType
 	{
 		switch (this)
 		{
-			case POINTER:	return JavaType.OBJECT;
+			case OBJECT:	return JavaType.OBJECT;
 			case BYTE:
 			case SHORT:
 			case INTEGER:	return JavaType.INTEGER;
@@ -346,13 +80,13 @@ public enum DataType
 	public static final DataType of(PrimitiveType __t)
 	{
 		if (__t == null)
-			return POINTER;
+			return OBJECT;
 		
 		switch (__t)
 		{
 			case BOOLEAN:
 			case BYTE:		return BYTE;
-			case CHARACTER:
+			case CHARACTER:	return CHARACTER;
 			case SHORT:		return SHORT;
 			case INTEGER:	return INTEGER;
 			case LONG:		return LONG;
@@ -360,7 +94,7 @@ public enum DataType
 			case DOUBLE:	return DOUBLE;
 			
 			default:
-				throw new todo.OOPS(__t.toString());
+				throw new todo.OOPS(__t.name());
 		}
 	}
 	
