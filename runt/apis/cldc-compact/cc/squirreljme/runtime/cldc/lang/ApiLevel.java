1d0b0fc85fc226f549fe72387d9f5828c84dd213
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-14 11:19:52 +0000

    Push release back to May 5.

--- runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/lang/ApiLevel.java
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/lang/ApiLevel.java
@@ -41,8 +41,8 @@ public final class ApiLevel
 	public static final int LEVEL_SQUIRRELJME_0_3_0_DEV =
 		3_0_19001;
 	
-	/** SquirrelJME 0.4.0 (April 21, 2019). */
-	public static final int LEVEL_SQUIRRELJME_0_4_0_20190421 =
+	/** SquirrelJME 0.4.0 (May 5, 2019). */
+	public static final int LEVEL_SQUIRRELJME_0_4_0_20190505 =
 		4_0_19111;
 	
 	/**
