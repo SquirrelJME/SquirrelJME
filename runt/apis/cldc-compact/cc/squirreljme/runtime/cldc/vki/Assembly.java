c2094fbb2e7c720edb493fcb72149a0f7d39ff15
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-20 23:29:15 +0000

    Add base classes for assembly operations and the bootstrap.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/vki/Assembly.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.vki;
+
+/**
+ * This class is used special by the compiler to transform all the various
+ * operations into regular instructions rather than method calls.
+ *
+ * The compiler will take all of the method arguments and instead use their
+ * inputs and outputs from the values of registers instead. However due to
+ * this, this means that these instructions are purely primitive in that
+ * they must not depend on any aspect of the virtual machine.
+ *
+ * @since 2019/04/20
+ */
+public final class Assembly
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2019/04/20
+	 */
+	private Assembly()
+	{
+	}
+	
+	/**
+	 * Exits the host hypervisor with a fatal error. Execution does not
+	 * continue after this point.
+	 *
+	 * @since 2019/04/20
+	 */
+	public static native void fatalExit();
+}
+
---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/vki/Bootstrap.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.vki;
+
+/**
+ * This class is responsible for bootstrapping the SquirrelJME environment
+ * and initializing everything properly.
+ *
+ * @since 2019/04/20
+ */
+public final class Bootstrap
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2019/04/20
+	 */
+	private Bootstrap()
+	{
+	}
+	
+	/**
+	 * This is the booting point for the SquirrelJME kernel, it is
+	 *
+	 * @param __int The pointer which contains the ROM address.
+	 * @param __mbp The memory base pointer.
+	 * @param __msz The amount of memory which is available.
+	 * @since 2019/04/20
+	 */
+	private static final void __boot(int __rom, int __mbp, int __msz)
+	{
+		Assembly.fatalExit();
+	}
+}
+
