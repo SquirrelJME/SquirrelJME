e03bd99af94fe307ff3452bfd61876b2658f93c1
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-21 12:19:30 +0000

    Base memory mapping of suites.

--- runt/libs/summercoat-vm/cc/squirreljme/vm/summercoat/SuitesMemory.java
+++ runt/libs/summercoat-vm/cc/squirreljme/vm/summercoat/SuitesMemory.java
@@ -22,23 +22,68 @@ import cc.squirreljme.vm.VMSuiteManager;
 public final class SuitesMemory
 	implements ReadableMemory
 {
+	/** The suite chunk size. */
+	public static final int SUITE_CHUNK_SIZE =
+		4194304;
+	
+	/** The address where suite chunks start. */
+	public static final int SUITE_CHUNK_START =
+		0x8000;
+	
 	/** The suite manage to base from. */
 	protected final VMSuiteManager suites;
+	
+	/** Offset. */
+	protected final int offset;
+	
+	/** The size of this memory region. */
+	protected final int size;
+	
+	/** The individual regions of suite memory. */
+	private final SuiteMemory[] _suitemem;
 	
 	/**
 	 * Initializes the suites memory.
 	 *
+	 * @param __off The offset of suite memory.
 	 * @param __sm The suite manager.
 	 * @throws NullPointerException On null arguments.
 	 * @since 2019/04/21
 	 */
-	public SuitesMemory(VMSuiteManager __sm)
+	public SuitesMemory(int __off, VMSuiteManager __sm)
 		throws NullPointerException
 	{
 		if (__sm == null)
 			throw new NullPointerException("NARG");
 		
+		// Set suites
 		this.suites = __sm;
+		
+		// All the libraries which are available for usage
+		String[] libnames = __sm.listLibraryNames();
+		
+		// Setup suite memory area
+		int n = libnames.length;
+		SuiteMemory[] suitemem = new SuiteMemory[n];
+		
+		// Setup memory regions for the various suites
+		int off = SUITE_CHUNK_START;
+		for (int i = 0; i < n; i++, off += SUITE_CHUNK_SIZE)
+		{
+			// Set
+			suitemem[i] = new SuiteMemory(off, __sm, libnames[i]);
+			
+			// Debug
+			todo.DEBUG.note("MMap Suite %s -> %08x", libnames[i],
+				__off + off);
+		}
+		
+		// Store all the various suite memories
+		this._suitemem = suitemem;
+		
+		// Store final memory parameters
+		this.offset = __off;
+		this.size = off;
 	}
 	
 	/**
@@ -48,7 +93,7 @@ public final class SuitesMemory
 	@Override
 	public int memRegionOffset()
 	{
-		return 0x80000000;
+		return this.offset;
 	}
 	
 	/**
