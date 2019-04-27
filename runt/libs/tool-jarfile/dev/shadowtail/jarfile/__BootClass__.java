c970418c042a77a94c1daef0b97a4c40d8816b65
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-27 22:27:53 +0000

    Base recursive init of super and interfaces classes.

--- runt/libs/tool-jarfile/dev/shadowtail/jarfile/__BootClass__.java
+++ runt/libs/tool-jarfile/dev/shadowtail/jarfile/__BootClass__.java
@@ -9,6 +9,8 @@
 
 package dev.shadowtail.jarfile;
 
+import dev.shadowtail.classfile.mini.MinimizedClassFile;
+
 /**
  * This class contains boot class information.
  *
@@ -16,5 +18,37 @@ package dev.shadowtail.jarfile;
  */
 final class __BootClass__
 {
+	/** The minimized class. */
+	public final MinimizedClassFile minicf;
+	
+	/** Minimized class offset. */
+	public final int jaroffset;
+	
+	/** The super class of this boot class. */
+	public __BootClass__ superclass;
+	
+	/** Available interfaces. */
+	public __BootClass__[] interfaces;
+	
+	/** The location of the pool pointer. */
+	public int poolptr;
+	
+	/**
+	 * Initializes the boot class.
+	 *
+	 * @param __mcf Mini class file.
+	 * @param __off Offset to the mini-class in the Jar data.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2019/04/27
+	 */
+	__BootClass__(MinimizedClassFile __mcf, int __off)
+		throws NullPointerException
+	{
+		if (__mcf == null)
+			throw new NullPointerException("NARG");
+		
+		this.minicf = __mcf;
+		this.jaroffset = __off;
+	}
 }
 
