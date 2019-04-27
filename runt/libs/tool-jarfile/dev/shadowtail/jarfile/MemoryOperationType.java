18e11e6940301b55632507f7b8a789b62ca5a7d8
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-27 22:52:59 +0000

    Base work for string interning.

---
+++ runt/libs/tool-jarfile/dev/shadowtail/jarfile/MemoryOperationType.java
@@ -0,0 +1,28 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package dev.shadowtail.jarfile;
+
+/**
+ * This represents the type of memory operation to perform.
+ *
+ * @since 2019/04/27
+ */
+public enum MemoryOperationType
+{
+	/** Offset by RAM. */
+	OFFSET_RAM,
+	
+	/** Offset by boot JAR. */
+	OFFSET_JAR,
+	
+	/** End. */
+	;
+}
+
