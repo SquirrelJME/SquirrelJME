be732aa2c60fe09433bb9c4cb46fe5d93e522b9c
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-27 21:40:25 +0000

    Base for JAR minimizer along with basic JAR writing.

--- runt/libs/tool-jarfile/dev/shadowtail/jarfile/JarMinimizer.java
+++ runt/libs/tool-jarfile/dev/shadowtail/jarfile/JarMinimizer.java
@@ -11,12 +11,15 @@ package dev.shadowtail.jarfile;
 
 import cc.squirreljme.vm.VMClassLibrary;
 import dev.shadowtail.classfile.mini.MinimizedClassFile;
+import dev.shadowtail.classfile.mini.Minimizer;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import net.multiphasicapps.classfile.ClassFile;
 import net.multiphasicapps.classfile.ClassName;
 
 /**
@@ -71,9 +74,6 @@ public final class JarMinimizer
 		if (__dos == null)
 			throw new NullPointerException("NARG");
 		
-		// Jar Magic Number
-		__dos.writeInt(MinimizedJarHeader.MAGIC_NUMBER);
-		
 		// This is processed for all entries
 		VMClassLibrary input = this.input;
 		
@@ -85,36 +85,133 @@ public final class JarMinimizer
 		String[] rcnames = input.listResources();
 		int numrc = rcnames.length;
 		
+		// Table of contents
+		ByteArrayOutputStream tbaos = new ByteArrayOutputStream(2048);
+		DataOutputStream tdos = new DataOutputStream(tbaos);
+		
+		// JAR content data
+		ByteArrayOutputStream jbaos = new ByteArrayOutputStream(1048576);
+		DataOutputStream jdos = new DataOutputStream(jbaos);
+		
+		// Relative offset from header and table of contents
+		int reloff = MinimizedJarHeader.HEADER_SIZE_WITH_MAGIC +
+			(4 * 12);
+		
 		// Go through and minimize/concat all resources
 		for (int i = 0; i < numrc; i++)
 		{
 			String rc = rcnames[i];
 			
-			// Minimizing class
-			if (rc.endsWith(".class"))
+			// The resulting byte array containing data
+			byte[] bytes;
+			
+			// Open resource
+			try (InputStream in = input.resourceAsStream(rc))
 			{
-				// Straight through processing
-				if (minicl == null)
+				// Minimizing class
+				if (rc.endsWith(".class"))
 				{
-					throw new todo.TODO();
+					// Minimize file data
+					bytes = Minimizer.minimize(ClassFile.decode(in));
+					
+					// If boot processing is going to be done, we need to
+					// know about this class file
+					if (minicl != null)
+					{
+						// Decode it
+						MinimizedClassFile cf =
+							MinimizedClassFile.decode(bytes);
+						
+						// And cache it for later
+						minicl.put(cf.thisName(), cf);
+					}
 				}
 				
-				// Need to keep the class file loaded so that boot information
-				// can be made from it
+				// Plain resource, sent straight through
 				else
 				{
-					throw new todo.TODO();
+					// Copy bytes
+					byte[] buf = new byte[512];
+					try (ByteArrayOutputStream xbaos =
+						new ByteArrayOutputStream(4096))
+					{
+						for (;;)
+						{
+							int ll = in.read(buf);
+							
+							if (ll < 0)
+								break;
+							
+							xbaos.write(buf, 0, ll);
+						}
+						
+						// Use this
+						bytes = xbaos.toByteArray();
+					}
 				}
 			}
 			
-			// Plain resource, sent straight through
-			else
-			{
-				throw new todo.TODO();
-			}
+			// Round data stream to 2 bytes (so string length is aligned)
+			while ((jdos.size() & 1) != 0)
+				jdos.write(0);
+			
+			// Record offset to resource name
+			tdos.writeInt(reloff + jdos.size());
+			jdos.writeUTF(rc);
+			
+			// Round data stream to 4 bytes
+			while ((jdos.size() & 1) != 0)
+				jdos.write(0);
+			
+			// Write offset to data stream and size
+			tdos.writeInt(reloff + jdos.size());
+			tdos.writeInt(bytes.length);
+			
+			// Then write the actual data stream
+			jdos.write(bytes);
 		}
 		
-		throw new todo.TODO();
+		// Write header
+		__dos.writeInt(MinimizedJarHeader.MAGIC_NUMBER);
+		
+		// Number of resources
+		__dos.writeInt(numrc);
+		
+		// Offset to table of contents
+		__dos.writeInt(MinimizedJarHeader.HEADER_SIZE_WITH_MAGIC);
+		
+		// Byte arrays for boot RAM and init RAM
+		ByteArrayOutputStream bbaos = null,
+			ibaos = null;
+		
+		// Building pre-boot state
+		if (minicl != null)
+		{
+			// Allocate byte arrays for boot and init RAM
+			bbaos = new ByteArrayOutputStream(1048576);
+			ibaos = new ByteArrayOutputStream(4096);
+			
+			throw new todo.TODO();
+		}
+		
+		// No boot data
+		else
+		{
+			__dos.writeInt(0);
+			__dos.writeInt(0);
+			__dos.writeInt(0);
+			__dos.writeInt(0);
+		}
+		
+		// Write table of contents and JAR data
+		__dos.write(tbaos.toByteArray());
+		__dos.write(jbaos.toByteArray());
+		
+		// Write boot RAM?
+		if (bbaos != null)
+			__dos.write(bbaos.toByteArray());
+		if (ibaos != null)
+			__dos.write(ibaos.toByteArray());
 	}
 	
 	/**
--- runt/libs/tool-jarfile/dev/shadowtail/jarfile/MinimizedJarHeader.java
+++ runt/libs/tool-jarfile/dev/shadowtail/jarfile/MinimizedJarHeader.java
@@ -12,6 +12,7 @@ package dev.shadowtail.jarfile;
 import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.IOException;
+import net.multiphasicapps.classfile.InvalidClassFormatException;
 
 /**
  * This represents the header for a minimized Jar file.
@@ -23,5 +24,94 @@ public final class MinimizedJarHeader
 	/** Magic number for the JAR. */
 	public static final int MAGIC_NUMBER =
 		0x00456570;
+	
+	/** Magic number for the end of file. */
+	public static final int END_MAGIC_NUMBER =
+		0x51756161;
+	
+	/** The size of the header without the magic number. */
+	public static final int HEADER_SIZE_WITHOUT_MAGIC =
+		0;
+	
+	/** The size of the header with the magic number. */
+	public static final int HEADER_SIZE_WITH_MAGIC =
+		HEADER_SIZE_WITHOUT_MAGIC + 4;
+	
+	/** Number of resources. */
+	public final int numrc;
+	
+	/** Table of contents offset. */
+	public final int tocoffset;
+	
+	/** Boot RAM offset. */
+	public final int bootramoff;
+	
+	/** Boot RAM size. */
+	public final int bootramsize;
+	
+	/** Initialize RAM offset. */
+	public final int initramoff;
+	
+	/** Initialize RAM size. */
+	public final int initramsize;
+	
+	/**
+	 * Initializes the Jar header.
+	 *
+	 * @param __fs Fields.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2019/04/27
+	 */
+	public MinimizedJarHeader(int... __fs)
+		throws NullPointerException
+	{
+		if (__fs == null)
+			throw new NullPointerException("NARG");
+		
+		int at = 0;
+		
+		// Table of contents
+		this.numrc = __fs[at++];
+		this.tocoffset = __fs[at++];
+		
+		// Boot RAM
+		this.bootramoff = __fs[at++];
+		this.bootramsize = __fs[at++];
+		this.initramoff = __fs[at++];
+		this.initramsize = __fs[at++];
+	}
+	
+	/**
+	 * Decodes the JAR header.
+	 *
+	 * @param __in The input stream.
+	 * @return The resulting header.
+	 * @throws IOException On read errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2019/04/27
+	 */
+	public static final MinimizedJarHeader decode(InputStream __in)
+		throws IOException, NullPointerException
+	{
+		if (__in == null)
+			throw new NullPointerException("NARG");
+		
+		// Need to read fields
+		DataInputStream din = new DataInputStream(__in);
+		
+		// {@squirreljme.error BC01 Invalid minimized Jar magic number.}
+		if (MAGIC_NUMBER != din.readInt())
+			throw new InvalidClassFormatException("BC01");
+		
+		// Build
+		return new MinimizedJarHeader(
+			/* numrc */ din.readInt(),
+			/* tocoffset */ din.readInt(),
+			
+			/* bootramoff */ din.readInt(),
+			/* bootramlen */ din.readInt(),
+			/* initramoff */ din.readInt(),
+			/* initramlen */ din.readInt());
+	}
 }
 
