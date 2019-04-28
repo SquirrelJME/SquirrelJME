a6c13cfacb6b3f5d15114e4aaab1496c3e5c7d2b
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-04-28 18:29:19 +0000

    Add more assembly operations for invocations with and without return values.

--- runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/vki/Assembly.java
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/vki/Assembly.java
@@ -46,6 +46,241 @@ public final class Assembly
 	public static native void entryMarker();
 	
 	/**
+	 * Invoke method at pointer.
+	 *
+	 * @param __addr The address to invoke.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a, int __b);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a, int __b, int __c);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a, int __b, int __c,
+		int __d);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a, int __b, int __c,
+		int __d, int __e);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @param __f Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a, int __b, int __c,
+		int __d, int __e, int __f);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @param __f Argument.
+	 * @param __g Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a, int __b, int __c,
+		int __d, int __e, int __f, int __g);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @param __f Argument.
+	 * @param __g Argument.
+	 * @param __h Argument.
+	 * @since 2019/04/28
+	 */
+	public static native void invoke(int __addr, int __a, int __b, int __c,
+		int __d, int __e, int __f, int __g, int __h);
+	
+	/**
+	 * Invoke method at pointer.
+	 *
+	 * @param __addr The address to invoke.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a, int __b);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a, int __b, int __c);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a, int __b, int __c,
+		int __d);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a, int __b, int __c,
+		int __d, int __e);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @param __f Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a, int __b, int __c,
+		int __d, int __e, int __f);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @param __f Argument.
+	 * @param __g Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a, int __b, int __c,
+		int __d, int __e, int __f, int __g);
+	
+	/**
+	 * Invoke method at pointer, with arguments.
+	 *
+	 * @param __addr The address to invoke.
+	 * @param __a Argument.
+	 * @param __b Argument.
+	 * @param __c Argument.
+	 * @param __d Argument.
+	 * @param __e Argument.
+	 * @param __f Argument.
+	 * @param __g Argument.
+	 * @param __h Argument.
+	 * @return The result of the invocation.
+	 * @since 2019/04/28
+	 */
+	public static native int invokeV(int __addr, int __a, int __b, int __c,
+		int __d, int __e, int __f, int __g, int __h);
+	
+	/**
 	 * Reads byte from address.
 	 *
 	 * @param __p The pointer.
@@ -142,12 +377,62 @@ public final class Assembly
 	public static native void returnFrame();
 	
 	/**
+	 * Returns from the current frame, returning the given value.
+	 *
+	 * @param __v The value to return.
+	 * @since 2019/04/28
+	 */
+	public static native void returnFrame(int __v);
+	
+	/**
+	 * Returns from the current frame, returning both values.
+	 *
+	 * @param __h The high value.
+	 * @param __l The low value.
+	 * @since 2019/04/28
+	 */
+	public static native void returnFrame(int __h, int __l);
+	
+	/**
 	 * Gets the value of the class table.
 	 *
 	 * @return The value of the class table register.
 	 * @since 2019/04/27
 	 */
 	public static native int specialGetClassTableRegister();
+	
+	/**
+	 * Returns the exception register.
+	 *
+	 * @return The exception register.
+	 * @since 2019/04/28
+	 */
+	public static native int specialGetExceptionRegister();
+	
+	/**
+	 * Returns the value of the return register, for long return values this
+	 * is the first high register.
+	 *
+	 * @return The value of the return register.
+	 * @since 2019/04/28
+	 */
+	public static native int specialGetReturnRegister();
+	
+	/**
+	 * Returns the value of the return register, the first high value.
+	 *
+	 * @return The value of the return register, the first high value.
+	 * @since 2019/04/28
+	 */
+	public static native int specialGetReturnHighRegister();
+	
+	/**
+	 * Returns the value of the return register, the second low value.
+	 *
+	 * @return The value of the return register, the second low value.
+	 * @since 2019/04/28
+	 */
+	public static native int specialGetReturnLowRegister();
 	
 	/**
 	 * Reads the value of the static field register.
@@ -172,6 +457,14 @@ public final class Assembly
 	 * @since 2019/04/27
 	 */
 	public static native void specialSetClassTableRegister(int __v);
+	
+	/**
+	 * Sets the value of the exception register.
+	 *
+	 * @param __v The value to use.
+	 * @since 2019/04/28
+	 */
+	public static native void specialSetExceptionRegister(int __v);
 	
 	/**
 	 * Sets the value of the static field register.
