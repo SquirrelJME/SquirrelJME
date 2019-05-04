d1b17b4e298ddf45385e675cb652254185f2fc97
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-04 14:09:00 +0000

    Use another class for the bootstrap.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/vki/__Bootstrap__.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.vki;
+
+/**
+ * This is the bootstrap initializer for SquirrelJME.
+ *
+ * @since 2019/05/04
+ */
+final class __Bootstrap__
+{
+	/**
+	 * Entry point for the bootstrap.
+	 *
+	 * @since 2019/05/04
+	 */
+	static final void __start()
+	{
+		Assembly.breakpoint();
+		throw new todo.TODO();
+	}
+}
+
