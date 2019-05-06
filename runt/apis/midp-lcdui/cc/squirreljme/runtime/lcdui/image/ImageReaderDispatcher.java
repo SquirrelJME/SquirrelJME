0f1ca4262a1e4d4923846f339acb87710e71ae21
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-06 02:13:46 +0000

    Rework the image readers a bit to not be super messy and also support longer magic numbers.

--- runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/image/ImageReaderDispatcher.java
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/image/ImageReaderDispatcher.java
@@ -38,40 +38,51 @@ public class ImageReaderDispatcher
 	 * @return The parsed image data.
 	 * @throws IOException If it could not be parsed.
 	 */
-	public static Image parse(DataInputStream __is)
+	public static Image parse(InputStream __is)
 		throws IOException, NullPointerException
 	{
 		// Check
 		if (__is == null)
 			throw new NullPointerException("NARG");
 		
-		// Read first byte
-		int head = __is.readUnsignedByte();
+		// Mark header
+		__is.mark(4);
+		
+		// Read in the header magic number
+		int first = (__is.read() & 0xFF);
+		int magic = (first << 24) |
+			((__is.read() & 0xFF) << 16) |
+			((__is.read() & 0xFF) << 8) |
+			(__is.read() & 0xFF);
+		
+		// Reset for future read
+		__is.reset();
 		
 		// GIF?
-		if (head == 0x47)
+		if (magic == 0x47494638)
 			throw new todo.TODO();
 		
 		// PNG?
-		else if (head == 0x89)
-			return new PNGReader(__is).__parse();
+		else if (magic == 0x89504E47)
+			return new PNGReader(__is).parse();
 		
 		// JPEG?
-		else if (head == 0xFF)
+		else if ((magic & 0xFFFFFF00) == 0xFFD8FF00)
 			throw new todo.TODO();
 		
 		// SVG?
-		else if (head == '<')
+		else if (first == '<')
 			throw new todo.TODO();
 		
 		// XPM?
-		else if (head == '/')
-			return new XPMReader().readImage(__is);
+		else if (first == '/')
+			return new XPMReader(__is).parse();
 		
 		// {@squirreljme.error EB0s Could not detect the image format used
-		// specified by the starting byte. (The starting byte)}
+		// specified by the starting byte. (The magic number; The first byte)}
 		else
-			throw new IOException(String.format("EB0s %d", head));
+			throw new IOException(String.format("EB0s %08x %02x", magic,
+				first));
 	}
 }
 
