2878d0bfa02da093483edb9a9ea51d37a9fa9e5f
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-08 17:54:01 +0000

    Add base for tests; Add empty java.util.Stack.

---
+++ runt/apis/cldc-compact/java/util/Stack.java
@@ -0,0 +1,49 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.util;
+
+/**
+ * This is a last-in-first-out, this uses {@link Vector} as a base and provides
+ * operations for pushing and popping.
+ *
+ * It is recommended to use {@link LinkedList} or {@link ArrayDeque} as they
+ * are newer classes.
+ *
+ * @since 2019/05/08
+ */
+public class Stack<E>
+	extends Vector<E>
+{
+	public boolean empty()
+	{
+		throw new todo.TODO();
+	}
+	
+	public E peek()
+	{
+		throw new todo.TODO();
+	}
+	
+	public E pop()
+	{
+		throw new todo.TODO();
+	}
+	
+	public E push(E __item)
+	{
+		throw new todo.TODO();
+	}
+	
+	public int search(Object __o)
+	{
+		throw new todo.TODO();
+	}
+}
+
