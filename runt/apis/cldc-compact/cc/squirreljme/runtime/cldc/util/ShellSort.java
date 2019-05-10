f9467a8bacdb41fc50f12bcb8a8344d79febf477
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-10 15:49:09 +0000

    Implement ShellSort.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/util/ShellSort.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.util;
+
+/**
+ * Shell sorting algorithm.
+ *
+ * @since 2019/05/10
+ */
+public class ShellSort
+{
+	/** Gaps used in shell sort. */
+	static final int[] _GAPS =
+		new int[]{701, 301, 132, 57, 23, 10, 4, 1};
+}
+
