fe1af9b493c53124b054d3d7da7078cab98c0d74
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-14 23:38:11 +0000

    Add base for FlowerCoat VM.

---
+++ runt/libs/flowercoat-vm/cc/squirreljme/vm/flowercoat/package-info.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This is the FlowerCoat Virtual Machine which is a more optimized and
+ * refactored SpringCoat.
+ *
+ * @since 2019/05/14
+ */
+
+package cc.squirreljme.vm.flowercoat;
+
