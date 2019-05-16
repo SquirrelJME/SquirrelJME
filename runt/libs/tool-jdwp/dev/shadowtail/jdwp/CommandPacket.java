6b54a8d66b1a64df7885ccd0b93338790b8662c3
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-16 12:14:12 +0000

    Add base for JDWP project.

---
+++ runt/libs/tool-jdwp/dev/shadowtail/jdwp/CommandPacket.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package dev.shadowtail.jdwp;
+
+/**
+ * This represents a command packet which tells the other end that something
+ * has happened or something should be done.
+ *
+ * @since 2019/05/16
+ */
+public final class CommandPacket
+	extends Packet
+{
+}
+
