a570fbe327a0a4b03c08a604d19a3e4c0b4dadcc
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-17 15:46:54 +0000

    Draw command bar if there are commands.

--- runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/phoneui/ActiveDisplay.java
+++ runt/apis/midp-lcdui/cc/squirreljme/runtime/lcdui/phoneui/ActiveDisplay.java
@@ -147,7 +147,7 @@ public final class ActiveDisplay
 			drawtitlebar = true;
 			
 			// The command bar is only drawn if we have actual commands
-			drawcommandbar = false;//(numcommands > 0);
+			drawcommandbar = (numcommands > 0);
 			
 			// Draw area is shortened in height
 			uw = dw;
