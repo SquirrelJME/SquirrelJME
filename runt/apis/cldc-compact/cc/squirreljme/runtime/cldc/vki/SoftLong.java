8e9904cefdd6eedf936700c49030efe8c7ba32aa
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-24 18:40:44 +0000

    Add base classes for software math.

---
+++ runt/apis/cldc-compact/cc/squirreljme/runtime/cldc/vki/SoftLong.java
@@ -0,0 +1,28 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.cldc.vki;
+
+/**
+ * Software math operations on 64-bit integer types.
+ *
+ * @since 2019/05/24
+ */
+public final class SoftLong
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2019/05/24
+	 */
+	private SoftLong()
+	{
+	}
+}
+
