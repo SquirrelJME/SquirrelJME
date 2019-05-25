c3188574bff6c2887d00406ee0d767ae00b45499
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-25 23:52:36 +0000

    Add basic supervisor package with all the classes which are needed by the compiler (so it does not complain about missing things).

---
+++ runt/klib/supervisor/java/lang/StringBuilder.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.lang;
+
+/**
+ * This class is used to build instances of string.
+ *
+ * @since 2019/05/25
+ */
+public final class StringBuilder
+{
+	/**
+	 * Appends a string into this one.
+	 *
+	 * @param __v The value to append.
+	 * @return {@code this}.
+	 * @since 2019/05/25
+	 */
+	public final StringBuilder append(String __v)
+	{
+		throw new todo.TODO();
+	}
+}
+
