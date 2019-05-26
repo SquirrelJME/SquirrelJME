73b4d268923a0f43454ecfa43a996b3288794cea
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-26 19:04:50 +0000

    Add String methods; Implement jvmNew(); Implement basic jvmIsInstance() for the quickest case.

--- runt/klib/supervisor/java/lang/String.java
+++ runt/klib/supervisor/java/lang/String.java
@@ -9,6 +9,8 @@
 
 package java.lang;
 
+import cc.squirreljme.jvm.Assembly;
+
 /**
  * This represents a string.
  *
@@ -16,5 +18,42 @@ package java.lang;
  */
 public final class String
 {
+	/** The backing array. */
+	transient final char[] _chars;
+	
+	/**
+	 * Initializes an empty string.
+	 *
+	 * @since 2019/05/26
+	 */
+	public String()
+	{
+		this._chars = new char[0];
+	}
+	
+	/**
+	 * Initializes string decoded from the given UTF-8 byte.
+	 *
+	 * @param __b The UTF-8 bytes to decode.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2019/05/26
+	 */
+	public String(byte[] __b)
+	{
+		Assembly.breakpoint();
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Returns a string which is a unique internal representation of a string.
+	 *
+	 * @return The unique interned string.
+	 * @since 2019/05/26
+	 */
+	public final String intern()
+	{
+		Assembly.breakpoint();
+		throw new todo.TODO();
+	}
 }
 
