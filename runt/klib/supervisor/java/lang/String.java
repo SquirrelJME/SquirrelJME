0c85106783bc5c6080f49df2a8139ce9010cdefd
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-05-27 12:04:09 +0000

    Implement String(byte[]).

--- runt/klib/supervisor/java/lang/String.java
+++ runt/klib/supervisor/java/lang/String.java
@@ -39,9 +39,57 @@ public final class String
 	 * @since 2019/05/26
 	 */
 	public String(byte[] __b)
+		throws NullPointerException
 	{
-		Assembly.breakpoint();
-		throw new todo.TODO();
+		if (__b == null)
+			throw new NullPointerException("NARG");
+		
+		// Create temporary output which has the input characters and such so
+		// this will be the maximum used
+		int bn = __b.length;
+		char[] temp = new char[bn];
+		
+		// Translate UTF-8 sequences
+		int nc = 0;
+		for (int i = 0; i < bn;)
+		{
+			// Get character
+			int c = __b[i++] & 0xFF;
+			
+			// Single byte
+			if ((c & 0b1000_0000) == 0)
+				temp[nc++] = (char)c;
+			
+			// Double byte
+			else if ((c & 0b1110_0000) == 0b1100_0000)
+			{
+				c = ((c & 0b0001_1111) << 6);
+				c |= (__b[i++] & 0b111111);
+				temp[nc++] = (char)c;
+			}
+			
+			// Triple byte
+			else if ((c & 0b1111_0000) == 0b1110_0000)
+			{
+				c = ((c & 0b0000_1111) << 12);
+				c |= ((__b[i++] & 0b111111) << 6);
+				c |= (__b[i++] & 0b111111);
+				temp[nc++] = (char)c;
+			}
+		}
+		
+		// Use direct array if the same length
+		if (nc == bn)
+			this._chars = temp;
+		
+		// Too short, copy only used chars
+		else
+		{
+			char[] chars = new char[nc];
+			for (int i = 0; i < nc; i++)
+				chars[i] = temp[i];
+			this._chars = chars;
+		}
 	}
 	
 	/**
