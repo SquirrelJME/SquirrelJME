789d70961d72515f1d05eb7a87a304adcc50b1a0
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-01 12:44:22 +0000

    Move CPU state to state structure; Remove C/H modelines since it interferes with Kate and forces a style be used

--- ratufacoat/rc_cpu.c
+++ ratufacoat/rc_cpu.c
@@ -1,5 +1,4 @@
-/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
-// ---------------------------------------------------------------------------
+/* ---------------------------------------------------------------------------
 // Multi-Phasic Applications: SquirrelJME
 //     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
 // ---------------------------------------------------------------------------
@@ -126,10 +125,10 @@ void ratufacoat_cpuexec(ratufacoat_cpu_t* cpu)
 		return;
 	
 	// Get CPU registers
-	r = cpu->r;
+	r = cpu->state.r;
 	
 	// CPU runs within an infinite loop!
-	for (pc = (void*)((uintptr_t)cpu->pc);; pc = nextpc)
+	for (pc = (void*)((uintptr_t)cpu->state.pc);; pc = nextpc)
 	{	
 		// Read the operation to be performed
 		op = *((uint8_t*)pc);
@@ -153,9 +152,12 @@ void ratufacoat_cpuexec(ratufacoat_cpu_t* cpu)
 						(uintptr_t)r[RATUFACOAT_POOL_REGISTER]);
 					
 					// Load pool values
-					cpu->debuginclass = (char*)((uintptr_t)pool[cldx] + 2);
-					cpu->debuginname = (char*)((uintptr_t)pool[mndx] + 2);
-					cpu->debugintype = (char*)((uintptr_t)pool[mtdx] + 2);
+					cpu->state.debuginclass =
+						(char*)((uintptr_t)pool[cldx] + 2);
+					cpu->state.debuginname =
+						(char*)((uintptr_t)pool[mndx] + 2);
+					cpu->state.debugintype =
+						(char*)((uintptr_t)pool[mtdx] + 2);
 				}
 				break;
 			
@@ -163,9 +165,9 @@ void ratufacoat_cpuexec(ratufacoat_cpu_t* cpu)
 			case RATUFACOAT_OP_DEBUG_POINT:
 				{
 					// Load location information
-					cpu->debugline = ratuacoat_decodevuint(&nextpc);
-					cpu->debugjop = ratuacoat_decodevuint(&nextpc);
-					cpu->debugjpc = ratuacoat_decodevuint(&nextpc);
+					cpu->state.debugline = ratuacoat_decodevuint(&nextpc);
+					cpu->state.debugjop = ratuacoat_decodevuint(&nextpc);
+					cpu->state.debugjpc = ratuacoat_decodevuint(&nextpc);
 				}
 				break;
 				
@@ -197,8 +199,9 @@ void ratufacoat_cpuexec(ratufacoat_cpu_t* cpu)
 				ratufacoat_log("Invalid operation (%d/0x%02X) @ %p!",
 					(int)op, (int)op, pc);
 				ratufacoat_log("In %s::%s:%s (L%d / J%d@%d)",
-					cpu->debuginclass, cpu->debuginname, cpu->debugintype,
-					cpu->debugline, cpu->debugjop, cpu->debugjpc);
+					cpu->state.debuginclass, cpu->state.debuginname,
+					cpu->state.debugintype, cpu->state.debugline,
+					cpu->state.debugjop, cpu->state.debugjpc);
 				return;
 		}
 	}
