7c104e261e40ea1903b45316e0085f0767226c42
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-02 19:35:41 +0000

    Add new base for header.

---
+++ ratufacoat/sqljmerc.h
@@ -0,0 +1,44 @@
+/* ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// --------------------------------------------------------------------------*/
+
+/**
+ * Main SquirrelJME RatufaCoat Header.
+ *
+ * @since 2019/06/02
+ */
+
+/** Header guard. */
+#ifndef SJME_hGRATUFACOATSQLCHSQLJMERCH
+#define SJME_hGRATUFACOATSQLCHSQLJMERCH
+
+/** Anti-C++. */
+#ifdef _cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_cXRATUFACOATSQLCHSQLJMERCH
+extern "C"
+{
+#endif /** #ifdef SJME_CXX_IS_EXTERNED */
+#endif /** #ifdef __cplusplus */
+
+/****************************************************************************/
+
+/****************************************************************************/
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_cXRATUFACOATSQLCHSQLJMERCH
+}
+#undef SJME_cXRATUFACOATSQLCHSQLJMERCH
+#undef SJME_CXX_IS_EXTERNED
+#endif /** #ifdef SJME_cXRATUFACOATSQLCHSQLJMERCH */
+#endif /** #ifdef __cplusplus */
+
+/** Header guard. */
+#endif /* #ifndef SJME_hGRATUFACOATSQLCHSQLJMERCH */
+
