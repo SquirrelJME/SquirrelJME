4c86214d8abe5f42f1008b00b2c75e680041b999
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-09 02:36:36 +0000

    Add defines for operations.

--- ratufacoat/sjmerc.c
+++ ratufacoat/sjmerc.c
@@ -59,6 +59,156 @@
 /** The register of the first argument. */
 #define SJME_ARGBASE_REGISTER SJME_JINT_C(8)
 
+/** Encoding mask. */
+#define SJME_ENC_MASK UINT8_C(0xF0)
+
+/** Math, R=RR, Integer. */
+#define SJME_ENC_MATH_REG_INT UINT8_C(0x00)
+
+/** Int comparison, then maybe jump. */
+#define SJME_ENC_IF_ICMP UINT8_C(0x10)
+
+/** Math type mask. */
+#define SJME_ENC_MATH_MASK UINT8_C(0x0F)
+
+/** Memory access, offset is in register. */
+#define SJME_ENC_MEMORY_OFF_REG UINT8_C(0x20)
+
+/** Memory access to big endian Java format, offset is in register. */
+#define SJME_ENC_MEMORY_OFF_REG_JAVA UINT8_C(0x30)
+
+/** Math, R=RC, Integer. */
+#define SJME_ENC_MATH_CONST_INT UINT8_C(0x80)
+
+/** Memory access, offset is a constant. */
+#define SJME_ENC_MEMORY_OFF_ICONST UINT8_C(0xA0)
+
+/** Memory access to big endian Java format, offset is a constant. */
+#define SJME_ENC_MEMORY_OFF_ICONST_JAVA UINT8_C(0xB0)
+
+/** Special. */
+#define SJME_ENC_SPECIAL_A UINT8_C(0xE0)
+
+/** Special.*/
+#define SJME_ENC_SPECIAL_B UINT8_C(0xF0)
+
+/** If equal to constant. */
+#define SJME_OP_IFEQ_CONST UINT8_C(0xE6)
+
+/** Debug entry to method. */
+#define SJME_OP_DEBUG_ENTRY UINT8_C(0xE8)
+
+/** Debug exit from method. */
+#define SJME_OP_DEBUG_EXIT UINT8_C(0xE9)
+
+/** Debug single point in method. */
+#define SJME_OP_DEBUG_POINT UINT8_C(0xEA)
+
+/** Return. */
+#define SJME_OP_RETURN UINT8_C(0xF3)
+
+/** Invoke. */
+#define SJME_OP_INVOKE UINT8_C(0xF7)
+
+/** Copy value in register. */
+#define SJME_OP_COPY UINT8_C(0xF8)
+
+/** Atomically decrements a memory address and gets the value. */
+#define SJME_OP_ATOMIC_INT_DECREMENT_AND_GET UINT8_C(0xF9)
+
+/** Atomically increments a memory address. */
+#define SJME_OP_ATOMIC_INT_INCREMENT UINT8_C(0xFA)
+
+/** System call. */
+#define SJME_OP_SYSTEM_CALL UINT8_C(0xFB)
+
+/** Load from pool, note that at code gen time this is aliased. */
+#define SJME_OP_LOAD_POOL UINT8_C(0xFD)
+
+/** Load from integer array. */
+#define SJME_OP_LOAD_FROM_INTARRAY UINT8_C(0xFE)
+
+/** Compare and exchange. */
+#define SJME_OP_BREAKPOINT UINT8_C(0xFF)
+
+/** Add. */
+#define SJME_MATH_ADD UINT8_C(0)
+
+/** Subtract. */
+#define SJME_MATH_SUB UINT8_C(1)
+
+/** Multiply. */
+#define SJME_MATH_MUL UINT8_C(2)
+
+/** Divide. */
+#define SJME_MATH_DIV UINT8_C(3)
+
+/** Remainder. */
+#define SJME_MATH_REM UINT8_C(4)
+
+/** Negate. */
+#define SJME_MATH_NEG UINT8_C(5)
+
+/** Shift left. */
+#define SJME_MATH_SHL UINT8_C(6)
+
+/** Shift right. */
+#define SJME_MATH_SHR UINT8_C(7)
+
+/** Unsigned shift right. */
+#define SJME_MATH_USHR UINT8_C(8)
+
+/** And. */
+#define SJME_MATH_AND UINT8_C(9)
+
+/** Or. */
+#define SJME_MATH_OR UINT8_C(10)
+
+/** Xor. */
+#define SJME_MATH_XOR UINT8_C(11)
+
+/** Compare (Less). */
+#define SJME_MATH_CMPL UINT8_C(12)
+
+/** Compare (Greater). */
+#define SJME_MATH_CMPG UINT8_C(13)
+
+/** Sign 8-bit. */
+#define SJME_MATH_SIGNX8 UINT8_C(14)
+
+/** Sign 16-bit. */
+#define SJME_MATH_SIGNX16 UINT8_C(15)
+
+/** Mask for read/write in memory. */
+#define SJME_MEM_LOAD_MASK UINT8_C(0x08)
+
+/** Mask for data types in memory. */
+#define SJME_MEM_DATATYPE_MASK UINT8_C(0x07)
+
+/** Object. */
+#define SJME_DATATYPE_OBJECT UINT8_C(0)
+
+/** Byte. */
+#define SJME_DATATYPE_BYTE UINT8_C(1)
+
+/** Short. */
+#define SJME_DATATYPE_SHORT UINT8_C(2)
+
+/** Character. */
+#define SJME_DATATYPE_CHARACTER UINT8_C(3)
+
+/** Integer. */
+#define SJME_DATATYPE_INTEGER UINT8_C(4)
+
+/** Float. */
+#define SJME_DATATYPE_FLOAT UINT8_C(5)
+
+/** Long. */
+#define SJME_DATATYPE_LONG UINT8_C(6)
+
+/** Double. */
+#define SJME_DATATYPE_DOUBLE UINT8_C(7)
+
 /** Maximum number of threads. */
 #define SJME_THREAD_MAX SJME_JINT_C(32)
 
