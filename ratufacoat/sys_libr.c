14854d5ed7f0d876b32830c0dad3b513f0de9bda
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-10 16:08:56 +0000

    Add libretro files and such.

---
+++ ratufacoat/sys_libr.c
@@ -0,0 +1,16 @@
+/* ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// --------------------------------------------------------------------------*/
+
+/**
+ * LibRetro Support.
+ *
+ * @since 2019/06/10
+ */
+
+#include "sjmerc.h"
+
