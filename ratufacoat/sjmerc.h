448444cc31d4381586f4d7b89e8cd5e595496776
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-11 11:16:35 +0000

    Add types for old MSVC.

--- ratufacoat/sjmerc.h
+++ ratufacoat/sjmerc.h
@@ -68,6 +68,24 @@
 	(defined(_MSC_VER) && _MSC_VER >= 1600)
 	#include <stdint.h>
 
+/** Old Microsoft. */
+#elif defined(_MSC_VER) && _MSC_VER < 1600
+	typedef signed __int8 int8_t;
+	typedef signed __int16 int16_t;
+	typedef signed __int32 int32_t;
+	
+	typedef unsigned __int8 uint8_t;
+	typedef unsigned __int16 uint16_t;
+	typedef unsigned __int32 uint32_t;
+	
+	#define INT8_C(x) x
+	#define INT16_C(x) x
+	#define INT32_C(x) x
+	
+	#define UINT8_C(x) x##U
+	#define UINT16_C(x) x##U
+	#define UINT32_C(x) x##U
+
 /** Guessed otherwise. */
 #else
 	#if defined(SCHAR_MAX) && SCHAR_MAX == 127
