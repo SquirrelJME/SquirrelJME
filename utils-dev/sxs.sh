b58a04c8d518d84761f5b016afa59a959bdbb59d
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-12 18:54:28 +0000

    Add side by side debug to compare byte code and native instructions.

---
+++ utils-dev/sxs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3+, or later.
+# See license.mkd for licensing and copyright information.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: This quickly and easily launches the side by side debug.
+
+# Force C locale
+export LC_ALL=C
+
+# Directory of this script
+__exedir="$(dirname -- "$0")"
+
+# Go to temp dir
+__tempdir="/tmp/sx"
+if [ ! -d "$__tempdir" ]
+then
+	mkdir -p "$__tempdir"
+fi
+cd "$__tempdir"
+
+# Execute handler
+"$__exedir/hostedlaunch.sh" sxs "$@"
