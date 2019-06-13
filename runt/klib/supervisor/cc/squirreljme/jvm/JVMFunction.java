f7a5828ef59f82cb142d721c53310f6c72e28c56
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-13 21:46:59 +0000

    Do not count up replaced fields, instead count down.

--- runt/klib/supervisor/cc/squirreljme/jvm/JVMFunction.java
+++ runt/klib/supervisor/cc/squirreljme/jvm/JVMFunction.java
@@ -142,6 +142,9 @@ public final class JVMFunction
 				for (int i = 0, px = 0; i < rnobj; i++, px += 4)
 					Assembly.refUncount(Assembly.memReadInt(rbase, px));
 			}
+			
+			Assembly.breakpoint();
+			throw new todo.TODO();
 		}
 		
 		// Free this memory
