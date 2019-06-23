3fb668efa0d39dfc73902aad42845e24846385b8
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-23 16:23:07 +0000

    Add method to allocator for a task in a given content.

--- runt/klib/supervisor/cc/squirreljme/jvm/ClientTask.java
+++ runt/klib/supervisor/cc/squirreljme/jvm/ClientTask.java
@@ -23,6 +23,9 @@ public final class ClientTask
 	/** The logical task ID. */
 	public final int lid;
 	
+	/** Allocation tag bit. */
+	public final int tagbits;
+	
 	/** The classpath. */
 	public final BootLibrary[] classpath;
 	
@@ -44,6 +47,25 @@ public final class ClientTask
 		this.pid = __pid;
 		this.lid = __lid;
 		this.classpath = __cp;
+		
+		// The tag bits are just the PID shifted up a bit
+		this.tagbits = __pid << 4;
+	}
+	
+	/**
+	 * Allocates memory for this task context.
+	 *
+	 * @param __tag The tag used.
+	 * @param __sz The number of bytes to allocate.
+	 * @return The allocated bytes.
+	 * @since 2019/06/23
+	 */
+	public final int allocate(int __tag, int __sz)
+	{
+		// Just perform the allocation with our PID as part of the tag and
+		// whatever was passed, masked correctly
+		return Allocator.allocate(
+			this.tagbits | (__tag & Allocator.CHUNK_BITS_VALUE_MASK), __sz);
 	}
 }
 
