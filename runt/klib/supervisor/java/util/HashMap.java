1d9df3ef3c1df4f723757f78bb2b687ccf255e94
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-24 12:38:40 +0000

    Base work on HashMap get.

--- runt/klib/supervisor/java/util/HashMap.java
+++ runt/klib/supervisor/java/util/HashMap.java
@@ -18,6 +18,18 @@ import cc.squirreljme.jvm.Assembly;
  */
 public class HashMap<K, V>
 {
+	/** The number of buckets to use. */
+	private static final int _BUCKET_COUNT =
+		16;
+	
+	/** Mask for buckets. */
+	private static final int _BUCKET_MASK =
+		0xF;
+	
+	/** Buckets available. */
+	private final __Bucket__[] _buckets =
+		new __Bucket__[_BUCKET_COUNT];
+	
 	/**
 	 * Checks if the map contains the given key.
 	 *
@@ -38,10 +50,24 @@ public class HashMap<K, V>
 	 * @return The value.
 	 * @since 2019/06/24
 	 */
+	@SuppressWarnings({"unchecked"})
 	public V get(K __k)
 	{
-		Assembly.breakpoint();
-		throw new todo.TODO();
+		// Calculate hash code
+		int hash = __k.hashCode();
+		
+		// Find existing bucket
+		__Bucket__ bucket = this._buckets[hash & _BUCKET_MASK];
+		if (bucket == null)
+			return null;
+		
+		// Go through items
+		for (__Item__ i : bucket._items)
+			if (i._hash == hash && HashMap.__equals(__k, i._key))
+				return (V)i._value;
+		
+		// Not found
+		return null;
 	}
 	
 	/**
@@ -56,6 +82,65 @@ public class HashMap<K, V>
 	{
 		Assembly.breakpoint();
 		throw new todo.TODO();
+	}
+	
+	/**
+	 * Returns if two objects are equal to each other.
+	 *
+	 * @param __a The first object.
+	 * @param __b The second object.
+	 * @return If they are equal or not.
+	 * @since 2019/06/24
+	 */
+	private static final boolean __equals(Object __a, Object __b)
+	{
+		// Either side is null
+		if (__a == null || __b == null)
+			return (__a == __b);
+		
+		// Use equality check
+		return __a.equals(__b);
+	}
+	
+	/**
+	 * This represents a single bucket within the map.
+	 *
+	 * @since 2019/06/24
+	 */
+	private static final class __Bucket__
+	{
+		/** Items within the bucket. */
+		__Item__[] _items =
+			new __Item__[_BUCKET_COUNT];
+	}
+	
+	/**
+	 * A single item within a bucket.
+	 *
+	 * @since 2019/06/24
+	 */
+	private static final class __Item__
+	{
+		/** The hashcode. */
+		final int _hash;
+		
+		/** The key. */
+		final Object _key;
+		
+		/** The value. */
+		Object _value;
+		
+		/**
+		 * Initializes the item.
+		 *
+		 * @param __h The hash.
+		 * @param __k The key.
+		 */
+		__Item__(int __h, Object __k)
+		{
+			this._hash = __h;
+			this._key = __k;
+		}
 	}
 }
 
