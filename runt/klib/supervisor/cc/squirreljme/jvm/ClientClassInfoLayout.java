3f80cbd58141442d7ae7e4ff9db3d3df0239f884
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-06-30 00:32:23 +0000

    Perform base scan for class information.

---
+++ runt/klib/supervisor/cc/squirreljme/jvm/ClientClassInfoLayout.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jvm;
+
+/**
+ * This class contains the layout for the client's {@link ClassInfo} object
+ * such as where all the fields are positioned.
+ *
+ * @since 2019/06/29
+ */
+public final class ClientClassInfoLayout
+{
+}
+
