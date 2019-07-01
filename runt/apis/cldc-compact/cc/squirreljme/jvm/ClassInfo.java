47c94526d3f8ccccb5eba96ea28792ae3b376217
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-07-01 11:53:05 +0000

    Synchronized ClassInfo.

--- runt/apis/cldc-compact/cc/squirreljme/jvm/ClassInfo.java
+++ runt/apis/cldc-compact/cc/squirreljme/jvm/ClassInfo.java
@@ -21,17 +21,29 @@ public final class ClassInfo
 	public static final int MAGIC_NUMBER =
 		0x4C6F7665;
 	
+	/** Self pointer. */
+	public final int selfptr;
+	
 	/** Magic number used to detect corruption. */
 	public final int magic;
 	
+	/** Class information flags. */
+	public final int flags;
+	
 	/** The pointer to the minimized class file. */
 	public final int miniptr;
+	
+	/** The pointer to the class name. */
+	public final int namep;
 	
 	/** The allocation size of this class. */
 	public final int size;
 	
 	/** The base offset for fields in this class. */
 	public final int base;
+	
+	/** The number of objects in the instance fields, for GC. */
+	public final int numobjects;
 	
 	/** The dimensions this class uses, if it is an array. */
 	public final int dimensions;
@@ -42,45 +54,64 @@ public final class ClassInfo
 	/** The super class data. */
 	public final ClassInfo superclass;
 	
+	/** Interfaces. */
+	public final ClassInfo[] interfaceclasses;
+	
+	/** The component class. */
+	public final ClassInfo componentclass;
+	
 	/** Pointer to the class object. */
-	public volatile Class<?> classobjptr;
+	public final Class<?> classobjptr;
 	
 	/** Virtual invoke VTable. */
 	public final int[] vtablevirtual;
 	
-	/** Special invoke VTable. */
-	public final int[] vtablespecial;
+	/** Virtual invoke VTable pool entries. */
+	public final int[] vtablepool;
 	
 	/**
 	 * Class information constructor.
 	 *
+	 * @param __sp Self pointer.
+	 * @param __fl Class information flags.
 	 * @param __minip Pointer to the hardware class data in ROM.
+	 * @param __namep The name pointer.
 	 * @param __sz The size of this class.
 	 * @param __bz The base offset for fields.
+	 * @param __no The number of objects in the field instance.
 	 * @param __dim Dimensions.
 	 * @param __csz Cell size.
 	 * @param __scl The super class data.
+	 * @param __icl Interface classes.
+	 * @param __ccl Component class.
 	 * @param __cop Pointer to the class object.
 	 * @param __vtv Virtual invoke VTable address.
-	 * @param __vts Special invoke VTable address.
+	 * @param __vtp Virtual invoke VTable pool addresses.
 	 * @since 2019/04/26
 	 */
-	public ClassInfo(int __minip, int __sz, int __bz, int __dim, int __csz,
-		ClassInfo __scl, Class<?> __cop, int[] __vtv, int[] __vts)
+	public ClassInfo(int __sp, int __fl, int __minip, int __namep, int __sz,
+		int __bz, int __no, int __dim, int __csz, ClassInfo __scl,
+		ClassInfo[] __icl, ClassInfo __ccl, Class<?> __cop, int[] __vtv,
+		int[] __vtp)
 	{
 		// Always implicitly set magic
 		this.magic = MAGIC_NUMBER;
 		
 		// Set
+		this.selfptr = __sp;
+		this.flags = __fl;
 		this.miniptr = __minip;
+		this.namep = __namep;
 		this.size = __sz;
 		this.base = __bz;
+		this.numobjects = __no;
 		this.dimensions = __dim;
 		this.cellsize = __csz;
 		this.superclass = __scl;
+		this.interfaceclasses = (__icl == null ? new ClassInfo[0] : __icl);
+		this.componentclass = __ccl;
 		this.classobjptr = __cop;
-		this.vtablevirtual = __vtv;
-		this.vtablespecial = __vts;
+		this.vtablevirtual = (__vtv == null ? new int[0] : __vtv);
+		this.vtablepool = (__vtp == null ? new int[0] : __vtp);
 	}
 }
-
