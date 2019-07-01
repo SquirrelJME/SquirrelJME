b3212fbeea1a3cae8a5ec8340fe3b2dd00cec82c
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-07-01 16:09:52 +0000

    Add base for new Squirrel Quarrel MIDlet and such.

---
+++ runt/mids/squirrel-quarrel/dev/shadowtail/squirrelquarrel/MainMidlet.java
@@ -0,0 +1,44 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package dev.shadowtail.squirrelquarrel;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+/**
+ * Main entry point for the game.
+ *
+ * @since 2019/07/01
+ */
+public class MainMidlet
+	extends MIDlet
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/07/01
+	 */
+	@Override
+	protected void destroyApp(boolean __uc)
+		throws MIDletStateChangeException
+	{
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/07/01
+	 */
+	@Override
+	protected void startApp()
+		throws MIDletStateChangeException
+	{
+		throw new todo.TODO();
+	}
+}
+
