27d40c007ad4e617f4ec3b2ad5c3170292d017b2
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-07-13 00:52:31 +0000

    Add base build support for PalmOS.

---
+++ ratufacoat/sys_palm.c
@@ -0,0 +1,30 @@
+/* ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// --------------------------------------------------------------------------*/
+
+/**
+ * RatufaCoat port to PalmOS!
+ *
+ * @since 2019/07/12
+ */
+
+#include <SystemMgr.h>
+
+#include "sjmerc.h"
+
+/**
+ * Main PalmOS entry point.
+ *
+ * @param cmd Command used.
+ * @param cmdpbp Command parameter block.
+ * @param launchflags Flags used to launch the application.
+ * @since 2019/07/12
+ */
+UInt32 PilotMain(UInt16 cmd, void* cmdpbp, UInt16 launchflags)
+{
+	return 0;
+}
