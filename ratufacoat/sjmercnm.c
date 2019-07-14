808ba4a1709bb169e9cdc72a5b63b8b351dfeea2
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-07-14 12:19:12 +0000

    Use PalmOS glue when allocating to allocate more than 64K.

--- ratufacoat/sjmercnm.c
+++ ratufacoat/sjmercnm.c
@@ -12,6 +12,12 @@
  * @since 2019/06/25
  */
 
+/* Palm OS Functions. */
+#if defined(__palmos__)
+	#include <MemoryMgr.h>
+	#include <MemGlue.h>
+#endif
+
 #include "sjmerc.h"
 
 /** Allocates the given number of bytes. */
@@ -30,12 +36,22 @@ void* sjme_malloc(sjme_jint size)
 	if (sizeof(sjme_jint) > sizeof(size_t) && size > (sjme_jint)SIZE_MAX)
 		return NULL;
 	
+#if defined(__palmos__)
+	/* Palm OS, use glue to allow greater than 64K. */
+	rv = MemGluePtrNew(size);
+#else
 	/* Use standard C function otherwise. */
 	rv = calloc(1, size);
+#endif
 
 	/* Did not allocate? */
 	if (rv == NULL)
 		return NULL;
+		
+#if defined(__palmos__)
+	/* Clear memory on Palm OS. */
+	MemSet(rv, size, 0);
+#endif
 	
 	/* Store the size into this memory block for later free. */
 	*((sjme_jint*)rv) = size;
@@ -56,6 +72,11 @@ void sjme_free(void* p)
 	/* Base pointer which is size shifted. */
 	basep = SJME_POINTER_OFFSET_LONG(p, -4);
 	
+#if defined(__palmos__)
+	/* Use Palm OS free. */
+	MemPtrFree(basep);
+#else
 	/* Use Standard C free. */
 	free(basep);
+#endif
 }
