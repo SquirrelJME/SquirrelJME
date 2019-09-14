1464ee07e64630986cefb789e4faae4ddcbd14fe
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-09-14 20:13:59 +0000

    Add blank bootstrap state.

---
+++ runt/libs/tool-jarfile/dev/shadowtail/jarfile/BootstrapState.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package dev.shadowtail.jarfile;
+
+/**
+ * This contains the state of the entire bootstrap, its memory and all of the
+ * classes which may be loaded accordingly.
+ *
+ * @since 2019/09/14
+ */
+public final class BootstrapState
+{
+}
+
