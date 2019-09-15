49f18410b199546e95b03b942bd7c67e5b2fbfc6
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-09-15 00:15:28 +0000

    Move code to the LCI and stub it.

--- runt/libs/tool-jarfile/dev/shadowtail/jarfile/LoadedClassInfo.java
+++ runt/libs/tool-jarfile/dev/shadowtail/jarfile/LoadedClassInfo.java
@@ -541,6 +541,311 @@ public final class LoadedClassInfo
 	}
 	
 	/**
+	 * Returns the method base for the class.
+	 *
+	 * @return The method base.
+	 * @since 2019/05/26
+	 */
+	public final int methodBase()
+	{
+		throw new todo.TODO();
+		/*
+		// Get class method might be in
+		Map<ClassName, LoadedClassInfo> boots = this._boots;
+		LoadedClassInfo bi = boots.get(__cl);
+		
+		// If this has no super class, then the base is zero
+		ClassName supername = bi._class.superName();
+		if (supername == null)
+			return 0;
+		
+		// Otherwise it is the number of available methods in the super class
+		return this.__classMethodSize(supername);
+		*/
+	}
+	
+	/**
+	 * Returns the address of the given method.
+	 *
+	 * @param __mn The method name.
+	 * @param __mt The method type, if {@code null} then the type is
+	 * disregarded.
+	 * @return The address of the given method.
+	 * @since 2019/04/30
+	 */
+	public final int methodCodeAddress(MethodName __mn, MethodDescriptor __mt)
+		throws NullPointerException
+	{
+		throw new todo.TODO();
+		/*
+		if (__cl == null || __mn == null)
+			throw new NullPointerException("NARG");
+		
+		// Get class method might be in
+		LoadedClassInfo bi = this._boots.get(__cl);
+		MinimizedClassFile mcf = bi._class;
+		
+		// Lookup static first
+		MinimizedMethod mm = mcf.method(true, __mn, __mt);
+		if (mm != null)
+			return bi._classoffset + mcf.header.smoff + mm.codeoffset;
+		
+		// Otherwise fallback to instance methods
+		// {@squirreljme.error BC07 Could not locate the given method.
+		// (The class; The name; The type)}
+		mm = mcf.method(false, __mn, __mt);
+		if (mm == null)
+			throw new InvalidClassFormatException(
+				String.format("BC07 %s %s %s", __cl, __mn, __mt));
+		return bi._classoffset + mcf.header.imoff + mm.codeoffset;
+		*/
+	}
+	
+	/**
+	 * Returns the method index for the given method.
+	 *
+	 * @param __mn The name of the method.
+	 * @param __mt The descriptor of the method.
+	 * @return The index of the given method.
+	 * @since 2019/05/26
+	 */
+	public final int methodIndex(MethodName __mn, MethodDescriptor __mt)
+		throws NullPointerException
+	{
+		throw new todo.TODO();
+		/*
+		if (__cl == null || __mn == null || __mt == null)
+			throw new NullPointerException("NARG");
+		
+		// Primitives and array types are not real, so just have everything
+		// about them point to object!
+		if (__cl.isPrimitive() || __cl.isArray())
+			return this.__classMethodIndex(new ClassName("java/lang/Object"),
+				__mn, __mt);
+		
+		// Seeker class
+		ClassName seeker = __cl;
+		
+		// Recursively scan for the method in all classes
+		Map<ClassName, LoadedClassInfo> boots = this._boots;
+		while (seeker != null)
+		{
+			// Get boot information
+			LoadedClassInfo bi = boots.get(seeker);
+			MinimizedClassFile mcf = bi._class;
+			
+			// Is class found in this method?
+			MinimizedMethod mm = mcf.method(false, __mn, __mt);
+			if (mm != null)
+				return this.__classMethodBase(seeker) + mm.index;
+			
+			// Go above
+			seeker = mcf.superName();
+		}
+		
+		// {@squirreljme.error BC08 Could not locate the method. (The class;
+		// Method name; Method type)}
+		throw new InvalidClassFormatException(
+			String.format("BC08 %s %s %s", __cl, __mn, __mt));
+		*/
+	}
+	
+	/**
+	 * Returns the number of methods to use in the method table.
+	 *
+	 * @return The method size.
+	 * @since 2019/05/26
+	 */
+	public final int methodSize()
+	{
+		throw new todo.TODO();
+		/*
+		// Get class method might be in
+		Map<ClassName, LoadedClassInfo> boots = this._boots;
+		LoadedClassInfo bi = boots.get(__cl);
+		
+		// If there is no super class it is just the count
+		ClassName supername = bi._class.superName();
+		if (supername == null)
+			return bi._class.header.imcount;
+		
+		// Otherwise include the super class count as well
+		return this.__classMethodSize(supername) + bi._class.header.imcount;
+		*/
+	}
+	
+	/**
+	 * Returns a pointer to the class's constant pool.
+	 *
+	 * @return The pointer to the class constant pool.
+	 * @since 2019/09/14
+	 */
+	public final int poolPointer()
+	{
+		throw new todo.TODO();
+		
+		/*
+		if (__init == null || __cl == null)
+			throw new NullPointerException("NARG");
+		
+		// Treat as invalid for these
+		if (__cl.isPrimitive() || __cl.isArray())
+			return -1;
+			
+		// {@squirreljme.error BC09 No such class exists. (The class)}
+		LoadedClassInfo bi = this._boots.get(__cl);
+		if (bi == null)
+			throw new InvalidClassFormatException("BC09 " + __cl);
+		
+		// Only calculate it once
+		int rv = bi._pooloffset;
+		if (rv != 0)
+			return rv;
+		
+		// Get constant pool
+		MinimizedClassFile mcl = bi._class;
+		DualClassRuntimePool pool = mcl.pool;
+		
+		throw new todo.TODO();
+		/ *
+		
+		// The JAR offset for the actual pool area
+		int jarpooloff = bi._classoffset + mcl.header.pooloff;
+		
+		// Allocate and store space needed for the active pool contents
+		int n = pool.size();
+		bi._pooloffset = (rv = __init.allocate(n * 4));
+		
+		// Debug
+		if (_ENABLE_DEBUG)
+			todo.DEBUG.note("Building %s at %d (virt %d)",
+				__cl, rv, rv + 1048576);
+		
+		// Process the constant pool
+		for (int i = 1; i < n; i++)
+		{
+			// Get pool type and value
+			MinimizedPoolEntryType pt = pool.type(i);
+			Object pv = pool.get(i);
+			int[] pp = pool.parts(i);
+			
+			// The pointer to this entry
+			int ep = rv + (4 * i);
+			
+			// Depends on the part
+			switch (pt)
+			{
+					// These have no effect on runtime
+				case NULL:
+				case METHOD_DESCRIPTOR:
+				case LONG:
+				case DOUBLE:
+					break;
+					
+				case METHOD_INDEX:
+					{
+						MethodIndex mi = (MethodIndex)pv;
+						__init.memWriteInt(
+							ep, this.__classMethodIndex(mi.inclass,
+								mi.name, mi.type));
+					}
+					break;
+					
+					// Write the pointer to the UTF data
+				case STRING:
+					{
+						__init.memWriteInt(Modifier.JAR_OFFSET,
+							ep, jarpooloff + pool.offset(i) + 4);
+					}
+					break;
+					
+					// Integer constant
+				case INTEGER:
+					__init.memWriteInt(ep, ((Number)pv).intValue());
+					break;
+				
+					// Float constant
+				case FLOAT:
+					__init.memWriteInt(ep,
+						Float.floatToRawIntBits(((Number)pv).floatValue()));
+					break;
+					
+					// Write pointer to the string UTF data
+				case USED_STRING:
+					{
+						// Get the string index
+						int sdx = pp[0];
+						
+						// Write offset of that string
+						__init.memWriteInt(Modifier.JAR_OFFSET,
+							ep, jarpooloff + pool.offset(sdx) + 4);
+					}
+					break;
+					
+					// Field being accessed
+				case ACCESSED_FIELD:
+					{
+						// Static field offsets are always known
+						AccessedField af = (AccessedField)pv;
+						LoadedClassInfo afci = bootstrap.findClass(
+							af.field.className());
+						
+						if (af.type().isStatic())
+							__init.memWriteInt(ep,
+								afci.fieldStaticOffset(
+									af.field.memberName(),
+									af.field.memberType()));
+						
+						// Instance fields are not yet known
+						else
+							__init.memWriteInt(ep,
+								afci.fieldInstanceOffset(
+									af.field.memberName(),
+									af.field.memberType()));
+					}
+					break;
+				
+					// Class ID
+				case CLASS_NAME:
+					__init.memWriteInt(Modifier.RAM_OFFSET,
+						ep, this.__classId(__init, (ClassName)pv));
+					break;
+					
+					// List of class IDs
+				case CLASS_NAMES:
+					__init.memWriteInt(Modifier.RAM_OFFSET,
+						ep, this.__classIds(__init, (ClassNames)pv));
+					break;
+					
+					// Class constant pool
+				case CLASS_POOL:
+					__init.memWriteInt(Modifier.RAM_OFFSET,
+						ep, this.__initPool(__init, ((ClassPool)pv).name));
+					break;
+					
+					// A method to be invoked, these are always direct pointer
+					// references to methods
+				case INVOKED_METHOD:
+					{
+						InvokedMethod im = (InvokedMethod)pv;
+						__init.memWriteInt(Modifier.JAR_OFFSET,
+							ep, this.__classMethodCodeAddress(im.handle.
+								outerClass(), im.handle.name(),
+								im.handle.descriptor()));
+					}
+					break;
+				
+				default:
+					throw new todo.OOPS(pt.name());
+			}
+		}
+		
+		// Return the pointer where the pool was allocated
+		return rv;
+		*/
+	}
+	
+	/**
 	 * Returns the ROM offset of this class.
 	 *
 	 * @return The ROM offset of the class.
@@ -549,6 +854,178 @@ public final class LoadedClassInfo
 	public final int romOffset()
 	{
 		return this._classoffset;
+	}
+	
+	/**
+	 * Returns the pointers to the class method execution pointers along with
+	 * the required pool pointers.
+	 *
+	 * @return The method table pointer and the pool pointer table.
+	 * @since 2019/09/14
+	 */
+	public final int[] vTables()
+	{
+		throw new todo.TODO();
+		/*
+		
+		if (__init == null || __cl == null)
+			throw new NullPointerException("NARG");
+		
+		// Primitive types and array types do not exist so they just use the
+		// same vtable as Object
+		if (__cl.isPrimitive() || __cl.isArray())
+			return this.__classVTable(__init,
+				new ClassName("java/lang/Object"));
+		
+		// We need boot information to get class information!
+		Map<ClassName, LoadedClassInfo> boots = this._boots;
+		
+		// Did we already make the VTable for this? This will happen in the
+		// event arrays or primitives are virtualized
+		LoadedClassInfo selfbi = boots.get(__cl);
+		int rv = selfbi._vtable;
+		if (rv >= 0)
+			return new int[]{rv, selfbi._vtablepool};
+		
+		// Build array of all the classes that are used in the method and
+		// super class chain
+		List<LoadedClassInfo> classes = new ArrayList<>();
+		for (ClassName at = __cl, su = null; at != null; at = su)
+		{
+			// Load class info for this
+			LoadedClassInfo bi = boots.get(at);
+			
+			// Add this class to the start of the chain (so super classes
+			// go in at the start)
+			classes.add(0, bi);
+			
+			// Set super class for next run
+			su = bi._class.superName();
+		}
+		
+		// Abstract methods which are not bound to anything will instead
+		// be bound to this method which indicates failure.
+		int jpvc = this.__classMethodCodeAddress(
+			"cc/squirreljme/jvm/JVMFunction", "jvmPureVirtualCall", "()V"),
+			jpvp = this.__initPool(__init, "cc/squirreljme/jvm/JVMFunction");
+		
+		// Initialize method table with initial values
+		int numv = this.__classMethodSize(__cl);
+		List<Integer> entries = new ArrayList<>(numv);
+		for (int i = 0; i < numv; i++)
+			entries.add(jpvc);
+		
+		// Also there need to be pointers to the constant pool as well
+		List<Integer> pools = new ArrayList<>(numv);
+		for (int i = 0; i < numv; i++)
+			pools.add(jpvp);
+		
+		// Go through every class and fill the table information
+		// The class index here will be used as a stopping point since methods
+		// at higher points will stop here
+		for (int ci = 0, cn = classes.size(); ci < cn; ci++)
+		{
+			// Get the class to scan through
+			LoadedClassInfo mbi = classes.get(ci);
+			MinimizedClassFile mcf = mbi._class;
+			
+			// Get the VTable base for this class
+			ClassName mcfname = mcf.thisName();
+			int vb = this.__classMethodBase(mcfname);
+			
+			// Process each interface method in this class
+			for (MinimizedMethod mm : mcf.methods(false))
+			{
+				// Determine the actual index of this entry
+				int vat = vb + mm.index;
+				
+				// Private visibility modifies how lookup is done
+				boolean ispriv = mm.flags().isPrivate(),
+					ispkpriv = mm.flags().isPackagePrivate();
+				
+				// Start at the end of the class scan to find the highest
+				// replacing class member
+				// Note that for any methods which are private the lookup
+				// only starts at the base class because those are not visible
+				// to any other method
+				MethodNameAndType mnat = mm.nameAndType();
+				for (int pi = (ispriv ? ci : cn - 1); pi >= ci; pi--)
+				{
+					// Get the class information for this one
+					LoadedClassInfo pbi = classes.get(pi);
+					MinimizedClassFile pcf = pbi._class;
+					ClassName pcfname = pcf.thisName();
+					
+					// If our method is package private and the other class
+					// is not in the same package then we cannot link to that
+					// method because it does not inherit.
+					if (ispkpriv && !mcfname.isInSamePackage(pcfname))
+						continue;
+					
+					// If the class has no such method then stop
+					MinimizedMethod pm = pcf.method(false, mnat);
+					if (pm == null)
+						continue;
+					
+					// Debug
+					if (_ENABLE_DEBUG)
+						todo.DEBUG.note("Link: (%s) %s -> %s : %s",
+							__cl, mcfname, pcfname, mnat);
+						
+					// Use this method
+					entries.set(vat, pbi._classoffset + pcf.header.imoff +
+						pm.codeoffset);
+					pools.set(vat, this.__initPool(__init, pcfname));
+					break;
+				}
+			}
+		}
+		
+		// Allocate array
+		rv = __init.allocate(Constants.ARRAY_BASE_SIZE + (4 * numv));
+		selfbi._vtable = rv;
+		
+		// Write array details
+		__init.memWriteInt(Modifier.RAM_OFFSET,
+			rv + Constants.OBJECT_CLASS_OFFSET,
+			this.__classId(__init, new ClassName("[I")));
+		__init.memWriteInt(
+			rv + Constants.OBJECT_COUNT_OFFSET,
+			999999);
+		__init.memWriteInt(
+			rv + Constants.ARRAY_LENGTH_OFFSET,
+			numv);
+		
+		// Write in entries
+		int wp = rv + Constants.ARRAY_BASE_SIZE;
+		for (int i = 0; i < numv; i++, wp += 4)
+			__init.memWriteInt(Modifier.JAR_OFFSET,
+				wp, entries.get(i));
+		
+		// Also write in the pool values
+		int pv = __init.allocate(Constants.ARRAY_BASE_SIZE + (4 * numv));
+		selfbi._vtablepool = pv;
+		
+		// Write array details
+		__init.memWriteInt(Modifier.RAM_OFFSET,
+			pv + Constants.OBJECT_CLASS_OFFSET,
+			this.__classId(__init, new ClassName("[I")));
+		__init.memWriteInt(
+			pv + Constants.OBJECT_COUNT_OFFSET,
+			999999);
+		__init.memWriteInt(
+			pv + Constants.ARRAY_LENGTH_OFFSET,
+			numv);
+		
+		// Write in pools
+		wp = pv + Constants.ARRAY_BASE_SIZE;
+		for (int i = 0; i < numv; i++, wp += 4)
+			__init.memWriteInt(Modifier.RAM_OFFSET,
+				wp, pools.get(i));
+		
+		// Return it
+		return new int[]{rv, pv};
+		*/
 	}
 	
 	/**
