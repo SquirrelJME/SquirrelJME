50a942571f70fa4f5e62c8eed1ec880defa401d8
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-09-29 18:35:01 +0000

    Minor work on the SimJVM.

--- utils-dev/simjvm.c
+++ utils-dev/simjvm.c
@@ -14,8 +14,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define _DEBUG
+
+/**
+ * Arguments which modify the starting state of the JVM.
+ *
+ * @since 2019/09/29
+ */
+typedef struct simjvm_arguments
+{
+} simjvm_arguments;
+
+/**
+ * Processes the command line arguments used to start the JVM.
+ *
+ * @param out Output arguments.
+ * @param argc Argument count.
+ * @param argv Argument strings.
+ * @since 2019/09/29
+ */
+int simjvm_initialize_arguments(simjvm_arguments* out, int argc, char** argv)
+{
+	int i, filemode;
+	char* arg;
+	char* next;
+	
+	/* The pointer must be valid! */
+	if (out == NULL || argv == NULL)
+		return 1;
+	
+	/* Starts not in file mode. */
+	filemode = 0;
+	
+	/* Go through all arguments, first skipped because executable. */
+	for (i = 1; i < argc; i++)
+	{
+		/* Get this argument and the next. */
+		arg = argv[i];
+		next = (i + 1 < argc ? argv[i + 1] : NULL);
+		
+		/* Print it for debug. */
+		fprintf(stderr, "DEBUG: %s\n", arg);
+		
+		fprintf(stderr, "TODO!\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	return 1;
+}
 
 /**
  * Main entry point.
@@ -26,7 +74,7 @@
  */
 int main(int argc, char** argv)
 {
-	int i;
+	simjvm_arguments jvmargs;
 	
 	/* Show welcome message. */
 	fprintf(stderr, "SquirrelJME Simulated JVM 0.1\n");
@@ -34,6 +82,7 @@ int main(int argc, char** argv)
 	fprintf(stderr, "https://squirreljme.cc/\n");
 	fprintf(stderr, "\n");
 	
+	/* Show a warning message why this JVM is being used instead. */
 	fprintf(stderr, "This virtual machine is being used to bootstrap the ");
 	fprintf(stderr, "build of SquirrelJME because a Java run-time has not ");
 	fprintf(stderr, "detected on the host machine or one which was not ");
@@ -41,11 +90,13 @@ int main(int argc, char** argv)
 	fprintf(stderr, "is to allow for building SquirrelJME on any system.\n");
 	fprintf(stderr, "\n");
 	
-	/* Print arguments. */
-#if defined(_DEBUG)
-	for (i = 0; i < argc; i++)
-		fprintf(stderr, "Arg %d: %s\n", i, argv[i]);
-#endif
+	/* Process arguments. */
+	memset(&jvmargs, 0, sizeof(jvmargs));
+	if (simjvm_initialize_arguments(&jvmargs, argc, argv) != 0)
+	{
+		fprintf(stderr, "Could not parse arguments!\n");
+		return EXIT_FAILURE;
+	}
 	
 	fprintf(stderr, "TODO!\n");
 	return EXIT_FAILURE;
