3c2dc5b388e2ef34e0b1fd09db2301eea18e62a0
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-10-13 19:39:45 +0000

    Add base for pool parsers and such.

---
+++ runt/klib/supervisor/cc/squirreljme/jvm/lib/AliasedPoolParser.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jvm.lib;
+
+/**
+ * This class is used to provide access to the aliased pool where entries are.
+ *
+ * @since 2019/10/13
+ */
+public final class AliasedPoolParser
+	extends AbstractPoolParser
+{
+}
+
