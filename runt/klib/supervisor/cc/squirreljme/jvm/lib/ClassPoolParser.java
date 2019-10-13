7ec55c664fa97fb71b46b527bbfb3f40650d6a94
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-10-13 18:16:56 +0000

    Add class pool constants.

--- runt/klib/supervisor/cc/squirreljme/jvm/lib/ClassPoolParser.java
+++ runt/klib/supervisor/cc/squirreljme/jvm/lib/ClassPoolParser.java
@@ -9,6 +9,8 @@
 
 package cc.squirreljme.jvm.lib;
 
+import cc.squirreljme.jvm.Assembly;
+
 /**
  * This class is used to parse individual pool treads.
  *
@@ -20,6 +22,10 @@ public final class ClassPoolParser
 	/** The address where the constant pool is. */
 	public final int romaddress;
 	
+	/** The size of this pool. */
+	private int _size =
+		-1;
+	
 	/**
 	 * Initializes the constant pool parser.
 	 *
@@ -29,6 +35,23 @@ public final class ClassPoolParser
 	public ClassPoolParser(int __ra)
 	{
 		this.romaddress = __ra;
+	}
+	
+	/**
+	 * Returns the size of the constant pool.
+	 *
+	 * @return The pool size.
+	 * @since 2019/10/13
+	 */
+	public final int size()
+	{
+		// If the size is negative, it has never been read before
+		int rv = this._size;
+		if (rv < 0)
+			this._size = (rv = Assembly.memReadInt(this.romaddress,
+				ClassPoolConstants.OFFSET_OF_INT_ENTRY_OFFSET));
+		
+		return rv;
 	}
 }
 
