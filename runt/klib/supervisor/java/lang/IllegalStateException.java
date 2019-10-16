fb3bf038f4a985992a81159296fa0f3acf8e980a
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-10-16 23:14:55 +0000

    Implement illegal state exception.

---
+++ runt/klib/supervisor/java/lang/IllegalStateException.java
@@ -0,0 +1,64 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package java.lang;
+
+/**
+ * This is thrown when a certain state is invalid.
+ *
+ * @since 2018/12/04
+ */
+public class IllegalStateException
+	extends RuntimeException
+{
+	/**
+	 * Initializes the exception with no message or cause.
+	 *
+	 * @since 2018/12/04
+	 */
+	public IllegalStateException()
+	{
+	}
+	
+	/**
+	 * Initializes the exception with the given message and no cause.
+	 *
+	 * @param __m The message.
+	 * @since 2018/12/04
+	 */
+	public IllegalStateException(String __m)
+	{
+		super(__m);
+	}
+	
+	/**
+	 * Initializes the exception with the given message and cause.
+	 *
+	 * @param __m The message.
+	 * @param __t The cause.
+	 * @since 2018/12/04
+	 */
+	public IllegalStateException(String __m, Throwable __t)
+	{
+		super(__m, __t);
+	}
+	
+	/**
+	 * Initializes the exception with the given cause and no message.
+	 *
+	 * @param __t The cause.
+	 * @since 2018/12/04
+	 */
+	public IllegalStateException(Throwable __t)
+	{
+		super(__t);
+	}
+}
+
