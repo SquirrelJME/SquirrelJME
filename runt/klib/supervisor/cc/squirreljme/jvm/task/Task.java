2c4ab7e4261cc0d7d485cdfe232107ac998c32ad
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-10-27 16:28:50 +0000

    Remove deprecated code.

--- runt/klib/supervisor/cc/squirreljme/jvm/task/Task.java
+++ runt/klib/supervisor/cc/squirreljme/jvm/task/Task.java
@@ -39,17 +39,9 @@ public final class Task
 	/** The classpath. */
 	public final ClassPath classpath;
 	
-	/** Classes which have been loaded. */
-	@Deprecated
-	public final HashMap<String, ClientClassInfo> classinfos =
-		new HashMap<>();
-	
 	/** Classes which have been read and initialized. */
 	private final HashMap<String, TaskClass> _classes =
 		new HashMap<>();
-	
-	/** The accessor for client class information. */
-	private volatile MiniClassAccessor _classinfoaccessor;
 	
 	/**
 	 * Initializes the client task.
@@ -72,37 +64,6 @@ public final class Task
 		
 		// Initialize allocator for memory grabbing
 		this.allocator = new TaskAllocator(__pid);
-	}
-	
-	/**
-	 * Returns the mini-class accessor for the {@link ClassInfo} class.
-	 *
-	 * @return The ClassInfo mini-class accessor.
-	 * @since 2019/07/11
-	 */
-	@Deprecated
-	public final MiniClassAccessor classInfoAccessor()
-	{
-		// If it has already been used, only get it once!
-		MiniClassAccessor rv = this._classinfoaccessor;
-		if (rv != null)
-			return rv;
-		
-		// Debug
-		todo.DEBUG.note("Searching for ClassInfo...");
-		
-		// Locate class resource
-		int dx = this.classpath.resourceClassFind(
-			"cc/squirreljme/jvm/ClassInfo");
-		if (dx < 0)
-			return null;
-		
-		// Cache it
-		this._classinfoaccessor = (rv = new MiniClassAccessor(
-			this.classpath.resourceData(dx)));
-		
-		// Use it
-		return rv;
 	}
 	
 	/**
@@ -163,73 +124,6 @@ public final class Task
 		
 		// Perform post initialization as needed
 		return rv.initializeClassInfo(this);
-	}
-	
-	/**
-	 * Loads the in-memory class information for the given class.
-	 *
-	 * @param __cl The class to load.
-	 * @return The loaded class information or {@code null} if it is not found.
-	 * @throws ClientLinkageError If the client class could not be initialized.
-	 * @throws NullPointerException On null arguments.
-	 * @since 2019/06/23
-	 */
-	@Deprecated
-	public final ClientClassInfo loadClassInfo(String __cl)
-		throws ClientLinkageError, NullPointerException
-	{
-		if (__cl == null)
-			throw new NullPointerException("NARG");
-		
-		// See if it was loaded already
-		HashMap<String, ClientClassInfo> classinfos = this.classinfos;
-		ClientClassInfo rv = classinfos.get(__cl);
-		if (classinfos.containsKey(__cl))
-			return (rv != null ? rv : null);
-		
-		// Debug
-		todo.DEBUG.note("Finding class %s...", __cl);
-		
-		// Attempt to locate the class, if not found remember this and stop
-		int dx = this.classpath.resourceClassFind(__cl);
-		if (dx < 0)
-		{
-			classinfos.put(__cl, null);
-			return null;
-		}
-		
-		// Debug
-		todo.DEBUG.note("Initializing class info %s...", __cl);
-		
-		// Get the layout for the class information (where fields go)
-		MiniClassAccessor ccia = this.classInfoAccessor();
-		
-		// {@squirreljme.error SV04 Could not allocate class information.}
-		int cip = this.allocator.allocate(Allocator.CHUNK_BIT_IS_OBJECT,
-			Constants.OBJECT_BASE_SIZE + ccia.baseInstanceSize());
-		if (cip == 0)
-			throw new ClientLinkageError("SV04");
-		
-		// Class initialization involves many recursive calls into the class
-		// loading being called, so as such store the class info pointer and
-		// such before any processing is done so it is done first
-		classinfos.put(__cl,
-			(rv = new ClientClassInfo(cip, this.classpath.resourceData(dx))));
-		
-		// Write the class type for the class information
-		Assembly.memWriteInt(cip, Constants.OBJECT_CLASS_OFFSET,
-			this.loadClassInfo("cc/squirreljme/jvm/ClassInfo").
-			classinfopointer);
-		
-		// "Open" the class so that its accessor can be used
-		try (ClientClassInfo xp = rv.open())
-		{
-			// Load the mini-class accessor
-			MiniClassAccessor clma = xp.accessor();
-			
-			Assembly.breakpoint();
-			throw new todo.TODO();
-		}
 	}
 	
 	/**
