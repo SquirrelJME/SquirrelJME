8e9b2adba030839579032b57d85ecb6fdda1d8d9
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-11-04 12:19:30 +0000

    Add classname utility.

---
+++ runt/klib/supervisor/cc/squirreljme/jvm/lib/ClassNameUtils.java
@@ -0,0 +1,28 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jvm.lib;
+
+/**
+ * Utilities for working with class names.
+ *
+ * @since 2019/11/04
+ */
+public final class ClassNameUtils
+{
+	/**
+	 * Not used.
+	 *
+	 * @since 2019/11/04
+	 */
+	private ClassNameUtils()
+	{
+	}
+}
+
