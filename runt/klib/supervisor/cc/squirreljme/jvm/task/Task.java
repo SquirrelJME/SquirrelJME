fc0efa48002ff11e95246aee7337d799a534574e
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-11-17 15:33:06 +0000

    More base for aliased pool; Add ability to get pool from a ClassLibrary; add get of ClassFileParser from ClassPath; Fail if a constant pool is purely virtual.

--- runt/klib/supervisor/cc/squirreljme/jvm/task/Task.java
+++ runt/klib/supervisor/cc/squirreljme/jvm/task/Task.java
@@ -89,7 +89,7 @@ public final class Task
 			this._classinforc = (cidx = classpath.resourceClassFind(
 				"cc/squirreljme/jvm/ClassInfo"));
 		
-		return new ClassFileParser(classpath.resourceData(cidx));
+		return classpath.classParser(cidx);
 	}
 	
 	/**
