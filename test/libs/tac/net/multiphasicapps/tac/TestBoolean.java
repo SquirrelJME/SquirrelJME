ba58b446fc6c4722cbd402b70a95637d811560f1
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-12-25 13:47:42 +0000

    Correct Object.clone() returning this and not the new array (add test)

---
+++ test/libs/tac/net/multiphasicapps/tac/TestBoolean.java
@@ -0,0 +1,46 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tac;
+
+/**
+ * This is a test which returns a boolean.
+ *
+ * @since 2019/12/25
+ */
+public abstract class TestBoolean
+	extends __CoreTest__
+{
+	/**
+	 * Runs the specified test.
+	 *
+	 * @return The result.
+	 * @throws Throwable On any thrown exception.
+	 * @since 2019/12/25
+	 */
+	public abstract boolean test()
+		throws Throwable;
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/12/25
+	 */
+	@Override
+	final Object __runTest(Object... __args)
+		throws Throwable
+	{
+		// {@squirreljme.error BU0f Test does not take any parameters.}
+		if (__args.length != 0)
+			throw new InvalidTestParameterException("BU0e");
+		
+		// Run the test
+		return Boolean.valueOf(this.test());
+	}
+}
+
