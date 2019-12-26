d723d7da4eff09284600e62ea85aa3fdcc3f4f79
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-12-26 00:00:46 +0000

    Implement part of Date; Forward calls for unsigned Long string printing.

--- runt/apis/cldc-compact/java/util/Date.java
+++ runt/apis/cldc-compact/java/util/Date.java
@@ -10,56 +10,126 @@
 
 package java.util;
 
+/**
+ * This represents a simplified time and date.
+ *
+ * The epoch is the same as {@link System#currentTimeMillis()} and is from
+ * January 1, 1970 at 00:00 GMT.
+ *
+ * @since 2019/12/25
+ */
 public class Date
 	implements Cloneable, Comparable<Date>
 {
+	/** The current Java time. */
+	private transient long _javatime;
+	
+	/**
+	 * Initializes this date at the current system time.
+	 *
+	 * @since 2019/12/25
+	 */
 	public Date()
 	{
-		super();
-		throw new todo.TODO();
+		this(System.currentTimeMillis());
 	}
 	
-	public Date(long __a)
+	/**
+	 * Initializes the date to the given time.
+	 *
+	 * @param __time The time to set.
+	 * @since 2019/12/25
+	 */
+	public Date(long __time)
 	{
-		super();
-		throw new todo.TODO();
+		this._javatime = __time;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/12/25
+	 */
 	@Override
 	public Object clone()
 	{
-		throw new todo.TODO();
+		return new Date(((Date)this)._javatime);
 	}
 	
-	public int compareTo(Date __a)
-	{
-		throw new todo.TODO();
-	}
-	
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/12/25
+	 */
 	@Override
-	public boolean equals(Object __a)
+	public int compareTo(Date __b)
+		throws NullPointerException
 	{
-		throw new todo.TODO();
+		if (__b == null)
+			throw new NullPointerException("NARG");
+		
+		long a = this._javatime,
+			b = __b._javatime;
+		
+		return (a < b ? -1 : (a > b ? 1 : 0));
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/12/25
+	 */
+	@Override
+	public boolean equals(Object __o)
+	{
+		if (this == __o)
+			return true;
+		
+		if (!(this instanceof __o))
+			return false;
+		
+		return this._javatime == ((Date)__o)._javatime;
+	}
+	
+	/**
+	 * Returns the number of milliseconds since the epoch.
+	 *
+	 * @return The number of milliseconds since the epoch.
+	 * @since 2019/12/25
+	 */
 	public long getTime()
 	{
-		throw new todo.TODO();
+		return this._javatime;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/12/25
+	 */
+	@Override
 	public int hashCode()
 	{
-		throw new todo.TODO();
+		long javatime = this._javatime;
+		return (int)(javatime ^ (javatime >>> 32));
 	}
 	
-	public void setTime(long __a)
+	/**
+	 * Sets the current time.
+	 *
+	 * @param __v The time to set.
+	 * @since 2019/12/25
+	 */
+	public void setTime(long __v)
 	{
-		throw new todo.TODO();
+		this._javatime = __v;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * @since 2019/12/25
+	 */
 	@Override
 	public String toString()
 	{
+		// "dow mon dd hh:mm:ss zzz yyyy"
+		
 		throw new todo.TODO();
 	}
 }
