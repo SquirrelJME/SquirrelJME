e187c8aace38af4b1257ed4c33e4a77b63e31d21
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-02-15 19:42:57 +0000

    Add base for SquirrelJME plugin to do SquirrelJME things.

---
+++ buildSrc/src/main/java/cc/squirreljme/plugin/SquirrelJMEPlugin.java
@@ -0,0 +1,18 @@
+package cc.squirreljme.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class SquirrelJMEPlugin
+	implements Plugin<Project>
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/02/15
+	 */
+	@Override
+	public void apply(Project __project)
+	{
+		throw new Error("TODO");
+	}
+}
