6fc6ceed8cb37103d88f41376ad1e0ef8f00b293
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-02-20 01:03:26 +0000

    Add base for emulator runner.

---
+++ emulators/emulator-runner/src/main/java/cc/squirreljme/emulator/runner/Main.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.emulator.runner;
+
+/**
+ * Main entry point for the emulator runner.
+ *
+ * @since 2020/02/19
+ */
+public class Main
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2020/02/19
+	 */
+	public static void main(String... __args)
+	{
+		throw new todo.TODO();
+	}
+}
+
