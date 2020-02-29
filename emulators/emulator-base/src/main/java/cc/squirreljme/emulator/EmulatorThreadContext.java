0873492b4f040554c0559d3e68885f22df218f68
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-02-29 17:30:09 +0000

    Add IntelliJ file templates; Add missing headers to the new emulator classes.

--- emulators/emulator-base/src/main/java/cc/squirreljme/emulator/EmulatorThreadContext.java
+++ emulators/emulator-base/src/main/java/cc/squirreljme/emulator/EmulatorThreadContext.java
@@ -1,5 +1,14 @@
 package cc.squirreljme.emulator;
 
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
 import cc.squirreljme.jvm.SystemCallIndex;
 
 /**
