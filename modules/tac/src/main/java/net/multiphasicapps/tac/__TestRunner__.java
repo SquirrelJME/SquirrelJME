c90e6650e7af93c8acfa1364563465101dee8c0f
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-03-06 13:52:38 +0000

    Add the actual files for tests.

---
+++ modules/tac/src/main/java/net/multiphasicapps/tac/__TestRunner__.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tac;
+
+/**
+ * This is the main program which is responsible for starting and executing
+ * tests that are available for running.
+ *
+ * @since 2020/03/06
+ */
+class __TestRunner__
+{
+	/**
+	 * Main entry point for test running.
+	 *
+	 * @param __args Program arguments.
+	 * @since 2020/03/06
+	 */
+	public static void main(String... __args)
+	{
+		throw new todo.TODO();
+	}
+}
