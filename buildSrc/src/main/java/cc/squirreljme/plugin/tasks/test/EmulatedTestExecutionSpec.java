578e57e13fb6620d3738cc9b486ca0ef8f2ba74f
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-03-07 12:44:01 +0000

    Basis for running tests and such.

--- buildSrc/src/main/java/cc/squirreljme/plugin/tasks/test/EmulatedTestExecutionSpec.java
+++ buildSrc/src/main/java/cc/squirreljme/plugin/tasks/test/EmulatedTestExecutionSpec.java
@@ -10,6 +10,7 @@
 package cc.squirreljme.plugin.tasks.test;
 
 import org.gradle.api.internal.tasks.testing.TestExecutionSpec;
+import org.gradle.jvm.tasks.Jar;
 
 /**
  * This contains the test execution specification which configures how tests
@@ -23,14 +24,24 @@ public class EmulatedTestExecutionSpec
 	/** The emulator to use. */
 	protected final String emulator;
 	
+	/** The JAR to test. */
+	protected final Jar jar;
+	
 	/**
 	 * Initializes the emulation specification.
 	 *
 	 * @param __emu The emulator to run, defaults to SpringCoat if not set.
+	 * @param __jar The JAR to test.
+	 * @throws NullPointerException On null arguments.
 	 * @since 2020/03/06
 	 */
-	public EmulatedTestExecutionSpec(String __emu)
+	public EmulatedTestExecutionSpec(String __emu, Jar __jar)
+		throws NullPointerException
 	{
+		if (__jar == null)
+			throw new NullPointerException("No JAR specified.");
+		
 		this.emulator = (__emu == null ? "springcoat" : "summercoat");
+		this.jar = __jar;
 	}
 }
