afa4a489c5be242230caf8181efbf3fb20a45395
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-03-21 23:20:44 +0000

    Add package-info for exceptions.

---
+++ emulators/springcoat-vm/src/main/java/cc/squirreljme/vm/springcoat/exceptions/package-info.java
@@ -0,0 +1,15 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This contains the exceptions for SpringCoat.
+ *
+ * @since 2020/03/21
+ */
+package cc.squirreljme.vm.springcoat.exceptions;
