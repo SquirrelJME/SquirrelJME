d4458292d67e131877d6cc1dd92867e85fa2e5c2
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-04-19 16:07:11 +0000

    Add Docker image build.

---
+++ modules/launcher/src/main/java/cc/squirreljme/runtime/launcher/Main.java
@@ -0,0 +1,42 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.launcher;
+
+import cc.squirreljme.runtime.cldc.debug.Debugging;
+import javax.microedition.midlet.MIDlet;
+
+/**
+ * Main entry point for the launcher which decides the path of LUI or LCDUI.
+ *
+ * @since 2020/04/19
+ */
+public class Main
+	extends MIDlet
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/04/19
+	 */
+	@Override
+	protected void destroyApp(boolean __uc)
+	{
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/04/19
+	 */
+	@Override
+	protected void startApp()
+	{
+		throw Debugging.todo();
+	}
+}
---
+++ modules/launcher/src/main/java/cc/squirreljme/runtime/launcher/package-info.java
@@ -0,0 +1,16 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This is the root package for the launcher interface.
+ *
+ * @since 2020/04/19
+ */
+
+package cc.squirreljme.runtime.launcher;
