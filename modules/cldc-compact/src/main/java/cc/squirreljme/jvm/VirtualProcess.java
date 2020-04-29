816dfef08cbb3f4acf91d6ddbe2910f42e6280f6
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-04-29 00:08:08 +0000

    Add base for spawning, starting, and waiting for a virtual process to exit.

--- modules/cldc-compact/src/main/java/cc/squirreljme/jvm/VirtualProcess.java
+++ modules/cldc-compact/src/main/java/cc/squirreljme/jvm/VirtualProcess.java
@@ -9,6 +9,8 @@
 
 package cc.squirreljme.jvm;
 
+import cc.squirreljme.runtime.cldc.debug.Debugging;
+
 /**
  * This is a virtualized process within the virtual machine which contains
  * the task context information along with hardware threads that a process
@@ -30,5 +32,46 @@ public final class VirtualProcess
 	private VirtualProcess(int __tid)
 	{
 		this.taskId = __tid;
+	}
+	
+	/**
+	 * Starts this process.
+	 *
+	 * @since 2020/04/28
+	 */
+	public final void start()
+	{
+		Assembly.breakpoint();
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * Waits for this process to terminate.
+	 *
+	 * @return The exit code of this process.
+	 * @throws InterruptedException If this process was interrupted.
+	 * @since 2020/04/28
+	 */
+	public final int waitForExit()
+		throws InterruptedException
+	{
+		Assembly.breakpoint();
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * Spawns a process.
+	 *
+	 * @return The resulting process that was spawned.
+	 * @since 2020/04/28
+	 */
+	public static VirtualProcess spawn()
+	{
+		// Grab a "unique" value from the atomic ticker, there are 4 billion
+		// values here so hopefully this does not cause too much of an issue
+		int taskId = Assembly.atomicTicker();
+		
+		Assembly.breakpoint();
+		throw Debugging.todo();
 	}
 }
