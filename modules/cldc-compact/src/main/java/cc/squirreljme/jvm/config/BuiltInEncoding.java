0a7e5cbdc832a9fc2c94dcfba15f095fc71a5b7c
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-05-25 20:52:49 +0000

    Bring in debugging and configuration updates.

---
+++ modules/cldc-compact/src/main/java/cc/squirreljme/jvm/config/BuiltInEncoding.java
@@ -0,0 +1,46 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jvm.config;
+
+/**
+ * Encoding IDs which are built-in to SquirrelJME.
+ *
+ * @since 2020/04/07
+ */
+public interface BuiltInEncoding
+{
+	/** Unspecified, use defined property or assume UTF-8. */
+	byte UNSPECIFIED =
+		0;
+	
+	/** UTF-8. */
+	byte UTF8 =
+		1;
+	
+	/** ASCII. */
+	byte ASCII =
+		2;
+	
+	/** IBM037 (EBCDIC). */
+	byte IBM037 =
+		3;
+	
+	/** ISO-8859-1. */
+	byte ISO_8859_1 =
+		4;
+	
+	/** ISO-8859-15. */
+	byte ISO_8859_15 =
+		5;
+	
+	/** The number of built-in encodings. */
+	byte NUM_BUILTIN_ENCODINGS =
+		6;
+}
