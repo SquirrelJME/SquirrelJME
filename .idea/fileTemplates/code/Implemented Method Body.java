3b96973ac4afc7d7dbcdcdc27a6c36fbed4c349a
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-05-27 23:21:15 +0000

    Pick some IntelliJ updates.

---
+++ .idea/fileTemplates/code/Implemented Method Body.java
@@ -0,0 +1,1 @@
+throw cc.squirreljme.runtime.cldc.debug.Debugging.todo();
--- .idea/fileTemplates/internal/package-info.java
+++ .idea/fileTemplates/internal/package-info.java
@@ -1,3 +1,8 @@
 #parse("File Header.java")
 
+/**
+ * Not Described.
+ *
+ * @since ${YEAR}/${MONTH}/${DAY}
+ */
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
