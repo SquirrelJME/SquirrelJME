051339171656fc761ad58c6559ebb3f85cdfb4ce
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-05-31 00:39:03 +0000

    Add missing files.

---
+++ emulators/springcoat-vm/src/main/java/cc/squirreljme/vm/springcoat/AbstractGhostObject.java
@@ -0,0 +1,55 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.vm.springcoat;
+
+import cc.squirreljme.runtime.cldc.debug.Debugging;
+
+/**
+ * This is the base for ghost objects which do not have a defined class type
+ * or anything associated with a class object.
+ *
+ * @since 2020/05/30
+ */
+public abstract class AbstractGhostObject
+	implements SpringObject
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/05/30
+	 */
+	@Override
+	public final SpringMonitor monitor()
+	{
+		throw new SpringVirtualMachineException(
+			"Ghost objects cannot have monitors: " + this.toString());
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/05/30
+	 */
+	@Override
+	public final SpringPointerArea pointerArea()
+	{
+		throw new SpringVirtualMachineException(
+			"Ghost objects cannot have pointers: " + this.toString());
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/05/30
+	 */
+	@Override
+	public final SpringClass type()
+	{
+		throw new SpringVirtualMachineException(
+			"Ghost objects cannot have types: " + this.toString());
+	}
+}
---
+++ emulators/springcoat-vm/src/main/java/cc/squirreljme/vm/springcoat/RefLinkBracketObject.java
@@ -0,0 +1,23 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.vm.springcoat;
+
+import cc.squirreljme.jvm.mle.brackets.RefLinkBracket;
+
+/**
+ * This contains the storage for reference links, these chain to each other.
+ *
+ * @see RefLinkBracket
+ * @since 2020/05/30
+ */
+public final class RefLinkBracketObject
+	extends AbstractGhostObject
+{
+}
