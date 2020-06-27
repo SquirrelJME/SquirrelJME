ac92cbb2e4c900270c4a66244b95ecbd604a8e39
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-06-27 19:40:04 +0000

    Remove the bulk of the old utils-dev scripts.

--- utils-dev/arraygfx/type/byte_rgb332.h
+++
@@ -1,5 +0,0 @@
-// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
-
-#define TemplateArrayGraphics ByteRGB332ArrayGraphics
-#define TYPE byte
-
--- utils-dev/arraygfx/type/short_i16.h
+++
@@ -1,7 +0,0 @@
-// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
-
-#define TemplateArrayGraphics ShortIndexed16ArrayGraphics
-#define TYPE short
-#define HAS_PALETTE
-#define PALETTE_SIZE 65536
-
