daa75864617d914ec85283ec992f13d3ab0ff827
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-06-30 22:14:59 +0000

    Removal of ObjectAccess.

--- modules/cldc-compact/src/main/java/java/lang/Character.java
+++ modules/cldc-compact/src/main/java/java/lang/Character.java
@@ -12,7 +12,6 @@ package java.lang;
 
 import cc.squirreljme.jvm.mle.TypeShelf;
 import cc.squirreljme.runtime.cldc.annotation.ImplementationNote;
-import cc.squirreljme.runtime.cldc.asm.ObjectAccess;
 import cc.squirreljme.runtime.cldc.i18n.DefaultLocale;
 import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
