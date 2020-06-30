4e6f2d4a43c17491826a4da0cec21fde53c3c61a
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-06-30 22:38:44 +0000

    Add missing package-info for LCDUI.

---
+++ modules/midp-lcdui/src/main/java/javax/microedition/lcdui/package-info.java
@@ -0,0 +1,16 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * This packages contains the implementation of MIDP 3.
+ *
+ * @since 2020/06/30
+ */
+
+package javax.microedition.lcdui;
