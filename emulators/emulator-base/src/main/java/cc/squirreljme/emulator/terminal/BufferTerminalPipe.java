9b230a0f1ac73f0390669d2b44d165b8f67fdc82
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-07-06 14:22:42 +0000

    Add base for output pipes that will be used in the system.

---
+++ emulators/emulator-base/src/main/java/cc/squirreljme/emulator/terminal/BufferTerminalPipe.java
@@ -0,0 +1,119 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.emulator.terminal;
+
+import cc.squirreljme.jvm.mle.exceptions.MLECallError;
+import java.io.IOException;
+import net.multiphasicapps.io.ByteDeque;
+
+/**
+ * This is a terminal pipe which manages its own buffer for output, it is
+ * used for the output of sub-tasks to be allowed to be read by another task.
+ *
+ * @since 2020/07/06
+ */
+public final class BufferTerminalPipe
+	implements TerminalPipe
+{
+	/** The backing double-ended byte queue for buffer data. */
+	protected final ByteDeque deque =
+		new ByteDeque();
+	
+	/** Has this been closed? */
+	private boolean _closed;
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void close()
+	{
+		synchronized (this)
+		{
+			// Mark as closed
+			this._closed = true;
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void flush()
+		throws IOException
+	{
+		synchronized (this)
+		{
+			if (this._closed)
+				throw new IOException("Cannot flush closed buffer pipe.");
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final int read(byte[] __b, int __o, int __l)
+		throws MLECallError
+	{
+		if (__b == null || __o < 0 || __l < 0 || (__o + __l) > __b.length)
+			throw new MLECallError("Null or out of bounds argument.");
+		
+		synchronized (this)
+		{
+			int rv = this.deque.getFirst(__b, __o, __l);
+			
+			// Check if a full end of file has been reached
+			if (rv == 0 && this._closed && this.deque.isEmpty())
+				return -1;
+			return rv;
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void write(int __b)
+		throws IOException
+	{
+		synchronized (this)
+		{
+			if (this._closed)
+				throw new IOException("Cannot write to closed buffer pipe.");
+			
+			this.deque.addLast((byte)__b);
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void write(byte[] __b, int __o, int __l)
+		throws IOException, MLECallError
+	{
+		if (__b == null || __o < 0 || __l < 0 || (__o + __l) > __b.length)
+			throw new MLECallError("Null or out of bounds argument.");
+		
+		synchronized (this)
+		{
+			if (this._closed)
+				throw new IOException("Cannot write to closed buffer pipe.");
+				
+			this.deque.addLast(__b, __o, __l);
+		}
+	}
+}
---
+++ emulators/emulator-base/src/main/java/cc/squirreljme/emulator/terminal/DiscardTerminalPipe.java
@@ -0,0 +1,97 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.emulator.terminal;
+
+import cc.squirreljme.jvm.mle.exceptions.MLECallError;
+import java.io.IOException;
+
+/**
+ * This is a terminal pipe which just discards output.
+ *
+ * @since 2020/07/06
+ */
+public final class DiscardTerminalPipe
+	implements TerminalPipe
+{
+	/** Is this closed? */
+	private boolean _closed;
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void close()
+	{
+		synchronized (this)
+		{
+			this._closed = true;
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void flush()
+		throws IOException
+	{
+		synchronized (this)
+		{
+			if (this._closed)
+				throw new IOException("Discard pipe closed.");
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final int read(byte[] __b, int __o, int __l)
+		throws MLECallError
+	{
+		throw new MLECallError("Cannot read a discard pipe.");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void write(int __b)
+		throws IOException
+	{
+		synchronized (this)
+		{
+			if (this._closed)
+				throw new IOException("Discard pipe closed.");
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/07/06
+	 */
+	@Override
+	public final void write(byte[] __b, int __o, int __l)
+		throws IOException, MLECallError
+	{
+		if (__b == null || __o < 0 || __l < 0 || (__o + __l) > __b.length)
+			throw new MLECallError("Null or out of bounds argument.");
+		
+		synchronized (this)
+		{
+			if (this._closed)
+				throw new IOException("Discard pipe closed.");
+		}
+	}
+}
