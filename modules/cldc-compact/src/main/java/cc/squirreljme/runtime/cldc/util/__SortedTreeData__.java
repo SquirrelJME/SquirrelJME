c4efbed3ca17770ae4d68b5a3f238de4af0fa2ec
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-07-12 16:25:41 +0000

    Move Sorted maps to CLDC.

--- modules/cldc-compact/src/main/java/cc/squirreljme/runtime/cldc/util/__SortedTreeData__.java
+++ modules/cldc-compact/src/main/java/cc/squirreljme/runtime/cldc/util/__SortedTreeData__.java
@@ -8,7 +8,7 @@
 // See license.mkd for licensing and copyright information.
 // ---------------------------------------------------------------------------
 
-package net.multiphasicapps.collections;
+package cc.squirreljme.runtime.cldc.util;
 
 import java.util.Comparator;
 import java.util.Map;
