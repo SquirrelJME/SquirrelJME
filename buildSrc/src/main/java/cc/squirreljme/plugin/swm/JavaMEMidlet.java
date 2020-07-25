2890971d7f8746c5a70745d1d1e011558c9e3c19
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-07-25 13:51:23 +0000

    Allow alternative MIDlets to be selected by using `squirreljme.midlet`.

--- buildSrc/src/main/java/cc/squirreljme/plugin/swm/JavaMEMidlet.java
+++ buildSrc/src/main/java/cc/squirreljme/plugin/swm/JavaMEMidlet.java
@@ -9,6 +9,8 @@
 
 package cc.squirreljme.plugin.swm;
 
+import java.util.List;
+
 /**
  * Defines a MIDlet that can be launched.
  *
@@ -16,6 +18,10 @@ package cc.squirreljme.plugin.swm;
  */
 public final class JavaMEMidlet
 {
+	/** Which MIDlet is to be run? */
+	public static final String MIDLET_PROPERTY =
+		"squirreljme.midlet";
+	
 	/** The title. */
 	public final String title;
 	
@@ -29,7 +35,7 @@ public final class JavaMEMidlet
 	 * Initializes the MIDlet information.
 	 *
 	 * @param __title The title of the MIDlet.
-	 * @param __icon The icon of the MIDlet.
+	 * @param __icon The icon of the MIDlet, may be {@code null}.
 	 * @param __main The main class.
 	 * @throws NullPointerException If no title or main class was specified.
 	 * @since 2020/02/15
@@ -85,6 +91,66 @@ public final class JavaMEMidlet
 	{
 		return String.format("%s, %s, %s",
 			this.title, this.icon, this.mainClass);
+	}
+	
+	/**
+	 * Search through the list of MIDlets and also 
+	 * 
+	 * @param __mids The MIDlets to search within.
+	 * @return The MIDlet to be used, may be {@code null}.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2020/07/25
+	 */
+	public static JavaMEMidlet find(List<JavaMEMidlet> __mids)
+		throws NullPointerException
+	{
+		if (__mids == null)
+			throw new NullPointerException("NARG");
+		
+		// Decode the values it could possibly be
+		String string = System.getProperty(JavaMEMidlet.MIDLET_PROPERTY);
+		Integer number = null;
+		if (string != null)
+			try
+			{
+				number = Integer.parseInt(string, 10);
+			}
+			catch (NumberFormatException ignored)
+			{
+			}
+		
+		// If this is a number...
+		if (number != null)
+		{
+			// Force the selection of no MIDlet (run main class)
+			// Even so if there are no MIDlets available
+			if (number == -1 || __mids.isEmpty())
+				return null;
+			
+			// If out of range only use the first MIDlet
+			if (number < 0 || number >= __mids.size())
+				return __mids.get(0);
+			
+			// Return the exact MIDlet
+			return __mids.get(number);
+		}
+		
+		// String based search
+		if (string != null)
+		{
+			// Search for a matching case-insensitive title
+			for (JavaMEMidlet midlet : __mids)
+				if (string.equalsIgnoreCase(midlet.title))
+					return midlet;
+			
+			// Otherwise construct a new MIDlet entry point
+			return new JavaMEMidlet(string, null, string);
+		}
+		
+		// Use the first MIDlet if available, or otherwise stop
+		if (!__mids.isEmpty())
+			return __mids.get(0);
+		return null;
 	}
 }
 
