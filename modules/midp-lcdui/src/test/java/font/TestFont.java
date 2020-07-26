63cf82e29bf103763f656234981ebc818d3a3269
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-07-26 12:53:23 +0000

    Move files around.

--- modules/midp-lcdui/src/test/java/font/TestFont.java
+++ modules/midp-lcdui/src/test/java/font/TestFont.java
@@ -1,4 +1,4 @@
-// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+package font;// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
 // ---------------------------------------------------------------------------
 // Multi-Phasic Applications: SquirrelJME
 //     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
