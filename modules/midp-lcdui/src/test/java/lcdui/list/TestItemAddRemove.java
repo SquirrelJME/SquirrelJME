7a01586a4bc7442c5b6ec262b5789f684096c2f8
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-11-07 00:03:52 +0000

    Add tests; Describe CMake Project.

---
+++ modules/midp-lcdui/src/test/java/lcdui/list/TestItemAddRemove.java
@@ -0,0 +1,34 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package lcdui.list;
+
+import cc.squirreljme.runtime.cldc.debug.Debugging;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.List;
+
+/**
+ * Tests adding and removing items.
+ *
+ * @since 2020/11/03
+ */
+public class TestItemAddRemove
+	extends BaseList
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2020/11/03
+	 */
+	@Override
+	protected void test(Display __display, List __list, int __type,
+		String __typeName)
+	{
+		throw Debugging.todo();
+	}
+}
