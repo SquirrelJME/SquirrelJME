0b9892735fd99ce5ea86809051c7bd2cd62a382b
Stephanie Gawroriski <xer@multiphasicapps.net>
2020-11-13 22:43:16 +0000

    Add additional list tests.

--- modules/midp-lcdui/src/test/java/lcdui/list/TestSelectionCommands.java
+++ modules/midp-lcdui/src/test/java/lcdui/list/TestSelectionCommands.java
@@ -9,7 +9,10 @@
 
 package lcdui.list;
 
-import cc.squirreljme.runtime.cldc.debug.Debugging;
+import cc.squirreljme.jvm.mle.constants.UIWidgetProperty;
+import cc.squirreljme.runtime.lcdui.mle.StaticDisplayState;
+import cc.squirreljme.runtime.lcdui.mle.UIBackend;
+import cc.squirreljme.runtime.lcdui.mle.UIBackendFactory;
 import javax.microedition.lcdui.Choice;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.List;
@@ -31,15 +34,40 @@ public class TestSelectionCommands
 	 * {@inheritDoc}
 	 * @since 2020/11/03
 	 */
+	@SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
 	@Override
 	protected void test(Display __display, List __list, int __type,
 		String __typeName)
-		throws Throwable
 	{
 		// This is only valid on implicit tests
 		if (__type != Choice.IMPLICIT)
-			throw new UntestableException("Implicit only."); 
+			throw new UntestableException("Implicit only.");
 		
-		throw Debugging.todo();
+		// Listener used to keep track of state
+		SelectionListener listener = new SelectionListener();
+		__list.setCommandListener(listener);
+		
+		// Add items to the list
+		for (int i = 0; i < TestSelectionCommands.NUM_ITEMS; i++)
+			__list.append(Character.toString((char)('a' + i)), null);
+		
+		// Set items as selected, which should trigger selection
+		UIBackend backend = UIBackendFactory.getInstance();
+		for (int i = 0; i < TestSelectionCommands.NUM_ITEMS; i++)
+		{
+			// Send event and wait for it to be flushed out
+			backend.widgetProperty(StaticDisplayState.locate(__list),
+				UIWidgetProperty.INT_LIST_ITEM_SELECTED, i, 1);
+			backend.flushEvents();
+			
+			// Make sure it was selected
+			synchronized (listener)
+			{
+				this.secondary("selected-" + i,
+					listener.selectedItems.contains(i)); 
+				this.secondary("last-" + i,
+					listener.lastSelected == i);
+			}
+		}
 	}
 }
