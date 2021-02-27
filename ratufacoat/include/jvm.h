a4c2e3535bfdb482cc31913f1b0b5e3c96986eaf
Stephanie Gawroriski <xer@multiphasicapps.net>
2021-02-27 23:26:00 +0000

    Add additional headers for audio and video.

---
+++ ratufacoat/include/jvm.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * JVM Definitions.
+ * 
+ * @since 2021/02/27
+ */
+
+#ifndef SQUIRRELJME_JVM_H
+#define SQUIRRELJME_JVM_H
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_CXX_SQUIRRELJME_JVM_H
+extern "C"
+{
+#endif /** #ifdef SJME_CXX_IS_EXTERNED */
+#endif /** #ifdef __cplusplus */
+
+/****************************************************************************/
+
+/****************************************************************************/
+
+/** Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_CXX_SQUIRRELJME_JVM_H
+}
+#undef SJME_CXX_SQUIRRELJME_JVM_H
+#undef SJME_CXX_IS_EXTERNED
+#endif /** #ifdef SJME_CXX_SQUIRRELJME_JVM_H */
+#endif /** #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_JVM_H */
