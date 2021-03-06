e0b3df4084a4c70ddd9893fd536b0358b55d65de
Stephanie Gawroriski <xer@multiphasicapps.net>
2021-03-06 14:59:59 +0000

    Add a bunch of source files for what will be needed in the future.

---
+++ ratufacoat/include/gpio.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * GPIO Support.
+ * 
+ * @since 2021/03/06
+ */
+
+#ifndef SQUIRRELJME_GPIO_H
+#define SQUIRRELJME_GPIO_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_CXX_SQUIRRELJME_GPIO_H
+extern "C"
+{
+#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_CXX_SQUIRRELJME_GPIO_H
+}
+#undef SJME_CXX_SQUIRRELJME_GPIO_H
+#undef SJME_CXX_IS_EXTERNED
+#endif /* #ifdef SJME_CXX_SQUIRRELJME_GPIO_H */
+#endif /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_GPIO_H */
---
+++ ratufacoat/include/ipnet.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * IP Networking.
+ * 
+ * @since 2021/03/06
+ */
+
+#ifndef SQUIRRELJME_IPNET_H
+#define SQUIRRELJME_IPNET_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_CXX_SQUIRRELJME_IPNET_H
+extern "C"
+{
+#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_CXX_SQUIRRELJME_IPNET_H
+}
+#undef SJME_CXX_SQUIRRELJME_IPNET_H
+#undef SJME_CXX_IS_EXTERNED
+#endif /* #ifdef SJME_CXX_SQUIRRELJME_IPNET_H */
+#endif /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_IPNET_H */
