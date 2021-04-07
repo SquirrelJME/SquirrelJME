148e1dc8c849c1fcc5660922f18c4bb9a9adfa3a
Stephanie Gawroriski <xer@multiphasicapps.net>
2021-04-07 23:36:37 +0000

    Base SoftFloat work.

--- modules/cldc-compact/src/main/java/cc/squirreljme/jvm/SoftFloat.java
+++ modules/cldc-compact/src/main/java/cc/squirreljme/jvm/SoftFloat.java
@@ -14,8 +14,24 @@ package cc.squirreljme.jvm;
  *
  * @since 2019/05/24
  */
-public class SoftFloat
+public final class SoftFloat
 {
+	/** The sign mask. */
+	public static final int SIGN_MASK =
+		0b1000_0000_0000_0000__0000_0000_0000_0000;
+	
+	/** Exponent Mask. */
+	public static final int EXPONENT_MASK =
+		0b0111_1111_1000_0000__0000_0000_0000_0000;
+	
+	/** Fraction Mask. */
+	public static final int FRACTION_MASK =
+		0b0000_0000_0111_1111__1111_1111_1111_1111;
+	
+	/** The mask for NaN values. */
+	public static final int NAN_MASK =
+		0b0111_1111_1000_0000__0000_0000_0000_0000;
+	
 	/**
 	 * Not used.
 	 *
@@ -40,31 +56,37 @@ public class SoftFloat
 	}
 	
 	/**
-	 * Compares two values.
+	 * Compares two values, NaN returns {@code -1}.
 	 *
 	 * @param __a A.
 	 * @param __b B.
 	 * @return The result.
 	 * @since 2019/05/24
 	 */
+	@SuppressWarnings("SpellCheckingInspection")
 	public static int cmpl(int __a, int __b)
 	{
-		Assembly.breakpoint();
-		throw new todo.TODO();
+		if (SoftFloat.isNaN(__a) || SoftFloat.isNaN(__b))
+			return -1;
+		
+		return SoftFloat.__cmp(__a, __b);
 	}
 	
 	/**
-	 * Compares two values.
+	 * Compares two values, NaN returns {@code 1}.
 	 *
 	 * @param __a A.
 	 * @param __b B.
 	 * @return The result.
 	 * @since 2019/05/24
 	 */
+	@SuppressWarnings("SpellCheckingInspection")
 	public static int cmpg(int __a, int __b)
 	{
-		Assembly.breakpoint();
-		throw new todo.TODO();
+		if (SoftFloat.isNaN(__a) || SoftFloat.isNaN(__b))
+			return 1;
+		
+		return SoftFloat.__cmp(__a, __b);
 	}
 	
 	/**
@@ -79,6 +101,18 @@ public class SoftFloat
 	{
 		Assembly.breakpoint();
 		throw new todo.TODO();
+	}
+	
+	/**
+	 * Is this Not a Number?
+	 * 
+	 * @param __a The value to check.
+	 * @return If this is not a number.
+	 * @since 2021/04/07
+	 */
+	public static boolean isNaN(int __a)
+	{
+		return SoftFloat.NAN_MASK == (__a & SoftFloat.NAN_MASK);
 	}
 	
 	/**
@@ -183,6 +217,20 @@ public class SoftFloat
 	 * @since 2019/05/24
 	 */
 	public static long toLong(int __a)
+	{
+		Assembly.breakpoint();
+		throw new todo.TODO();
+	}
+	
+	/**
+	 * Compares two values.
+	 *
+	 * @param __a A.
+	 * @param __b B.
+	 * @return The result.
+	 * @since 2021/04/07
+	 */
+	private static int __cmp(int __a, int __b)
 	{
 		Assembly.breakpoint();
 		throw new todo.TODO();
