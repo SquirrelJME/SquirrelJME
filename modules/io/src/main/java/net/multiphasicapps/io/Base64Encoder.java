abbe53a738359ee5549fe117e92eafd7077ca6a9
Stephanie Gawroriski <xer@multiphasicapps.net>
2021-05-23 23:49:51 +0000

    Base64 encoding attempt; Add test for encoding and decoding.

--- modules/io/src/main/java/net/multiphasicapps/io/Base64Encoder.java
+++ modules/io/src/main/java/net/multiphasicapps/io/Base64Encoder.java
@@ -9,7 +9,6 @@
 
 package net.multiphasicapps.io;
 
-import cc.squirreljme.runtime.cldc.debug.Debugging;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
@@ -22,11 +21,32 @@ import java.io.Reader;
 public final class Base64Encoder
 	extends Reader
 {
+	/** Character mask. */
+	private static final int _CHAR_MASK =
+		0b111_111;
+	
+	/** Character shift. */
+	private static final int _BIT_COUNT =
+		6;
+	
 	/** The stream to read from. */
 	protected final InputStream in;
 	
 	/** The alphabet. */
 	private final char[] _alphabet;
+	
+	/** The current bit stream that is left over. */
+	int _bitStream;
+	
+	/** The number of bits in the stream. */
+	byte _count;
+	
+	/** Was EOF hit? */
+	boolean _hitEof;
+	
+	/** Padding that is left to output, when EOF. */
+	byte _paddingLeft =
+		-1;
 	
 	/**
 	 * Initializes the base64 encoder, using the basic alphabet.
@@ -82,7 +102,80 @@ public final class Base64Encoder
 			throw new NullPointerException("NARG");
 		if (__o < 0 || __l < 0 || (__o + __l) > __c.length)
 			throw new IndexOutOfBoundsException("IOOB");
+			
+		InputStream in = this.in;
+		char[] alphabet = this._alphabet;
 		
-		throw Debugging.todo();
+		int bitStream = this._bitStream;
+		byte count = this._count;
+		boolean hitEof = this._hitEof;
+		int paddingLeft = this._paddingLeft;
+		
+		// We want to restore the data fields back when we are done reading
+		try
+		{
+			int converted = 0;
+			while (converted < __l)
+			{
+				// Is there enough bits to eat?
+				if (count >= Base64Encoder._BIT_COUNT || hitEof)
+				{
+					// Is there padding left to put in
+					if (paddingLeft > 0)
+					{
+						__c[__o + (converted++)] = '=';
+						
+						// We took this padding
+						paddingLeft--;
+						continue;
+					}
+					
+					// No data left to encode? Stop now
+					if (hitEof && count == 0)
+						return (converted > 0 ? converted : -1);
+					
+					// Determine the character
+					__c[__o + (converted++)] =
+						alphabet[bitStream & Base64Encoder._CHAR_MASK];
+					
+					// Determine the amount of padding to add
+					if (hitEof && paddingLeft < 0)
+						paddingLeft = (count > 8 ? 2 : (count > 0 ? 1 : 0));
+					
+					// Eat up the bit stream
+					bitStream >>>= Base64Encoder._BIT_COUNT;
+					count -= Base64Encoder._BIT_COUNT;
+					
+					// No point reading if we already know to stop
+					if (hitEof)
+						continue;
+				}
+				
+				// Read in more data
+				int read = in.read();
+				
+				// If EOF hit, then we will pad
+				if (read < 0)
+					hitEof = true;
+				
+				// Add on top of the stream
+				else
+				{
+					bitStream |= (read & 0xFF) << count;
+					count += 8;
+				}
+			}
+			
+			return (hitEof && converted == 0 ? -1 : converted);
+		}
+		
+		// Always store the fields back when done
+		finally
+		{
+			this._bitStream = bitStream;
+			this._count = count;
+			this._hitEof = hitEof;
+			this._paddingLeft = (byte)paddingLeft;
+		}
 	}
 }
