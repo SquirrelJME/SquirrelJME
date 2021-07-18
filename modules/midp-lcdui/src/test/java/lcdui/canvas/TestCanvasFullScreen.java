eb231029ae8d10a969ea5740092a21366f319a5d
Stephanie Gawroriski <xer@multiphasicapps.net>
2021-07-18 23:11:13 +0000

    Add mechanism to skip from timeout tests.

--- modules/midp-lcdui/src/test/java/lcdui/canvas/TestCanvasFullScreen.java
+++ modules/midp-lcdui/src/test/java/lcdui/canvas/TestCanvasFullScreen.java
@@ -9,11 +9,9 @@
 
 package lcdui.canvas;
 
-import cc.squirreljme.runtime.cldc.debug.Debugging;
 import javax.microedition.lcdui.Canvas;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.Display;
-import net.multiphasicapps.tac.UntestableException;
 
 /**
  * Tests that a canvas is full-screen via
@@ -30,16 +28,6 @@ public class TestCanvasFullScreen
 	 */
 	@Override
 	public void test(Display __display, CanvasPlatform __platform)
-	{
-		// This test can fail multiple times due to tight timings
-		for (int i = 0; i < 3; i++)
-			if (this.runSequence(__display, __platform))
-				return;
-		
-		throw new UntestableException("Ignoring full-screen test.");
-	}
-	
-	public boolean runSequence(Display __display, CanvasPlatform __platform)
 	{
 		// Add a button, it should go away
 		__platform.addCommand(
@@ -60,31 +48,16 @@ public class TestCanvasFullScreen
 		__platform.repaint();
 		__platform.serviceRepaints();
 		
-		// Wait a bit to allow this to stay on
-		try
-		{
-			Thread.sleep(1000);
-		}
-		catch (InterruptedException ignored)
-		{
-		}
-		
 		// Query the sizes again for checking
 		int newWidth = __platform.getWidth();
 		int newHeight = __platform.getHeight();
 		
 		// Full-screen should cause the canvas to grow in size
-		boolean didWidth = newWidth >= width;
-		boolean didHeight = newHeight >= height;
-		this.secondary("width", didWidth);
-		this.secondary("height", didHeight);
+		this.secondary("width", newWidth >= width);
+		this.secondary("height", newHeight >= height);
 		
 		// The canvas should be smaller or at the maximum display resolution
-		boolean widthSmaller = newWidth <= __display.getWidth();
-		boolean heightSmaller = newHeight <= __display.getHeight();
-		this.secondary("wdisp", widthSmaller);
-		this.secondary("hdisp", heightSmaller);
-		
-		return didWidth && didHeight && widthSmaller && heightSmaller;
+		this.secondary("wdisp", newWidth <= __display.getWidth());
+		this.secondary("hdisp", newHeight <= __display.getHeight());
 	}
 }
