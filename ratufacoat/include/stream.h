d13ed042eb7ae8d60d01151e5c2fb54bbda4f234
Stephanie Gawroriski <xer@multiphasicapps.net>
2021-11-12 01:46:31 +0000

    Add blank stream for the use of streaming data.

---
+++ ratufacoat/include/stream.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Streaming of a linear piece of data.
+ * 
+ * @since 2021/11/11
+ */
+
+#ifndef SQUIRRELJME_STREAM_H
+#define SQUIRRELJME_STREAM_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_CXX_SQUIRRELJME_STREAM_H
+extern "C"
+{
+#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_CXX_SQUIRRELJME_STREAM_H
+}
+#undef SJME_CXX_SQUIRRELJME_STREAM_H
+#undef SJME_CXX_IS_EXTERNED
+#endif /* #ifdef SJME_CXX_SQUIRRELJME_STREAM_H */
+#endif /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_STREAM_H */
