8a847b3a7bb3a1e1f45cff67cc85ecfd1c951c91
Stephanie Gawroriski <xer@multiphasicapps.net>
2022-03-19 13:31:09 +0000

    Add base blanks for pipe functions.

--- ratufacoat/src/engine/pipe.c
+++ ratufacoat/src/engine/pipe.c
@@ -7,35 +7,162 @@
 // See license.mkd for licensing and copyright information.
 // -------------------------------------------------------------------------*/
 
+#include "debug.h"
 #include "engine/pipe.h"
+
+struct sjme_pipeInstance
+{
+	/** The type of pipe this is. */
+	sjme_taskPipeRedirectType type;
+	
+	/** The functions used to handle this pipe. */
+	const sjme_pipeFunction* functions;
+};
+
+/* -------------------------------- DISCARD ------------------------------- */
+
+static sjme_jboolean sjme_discardPipeClose(sjme_pipeInstance* pipe,
+	sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+static sjme_jboolean sjme_discardPipeFlush(sjme_pipeInstance* pipe,
+	sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+static sjme_jboolean sjme_discardPipeNewInstance(sjme_pipeInstance** outPipe,
+	sjme_jint fd, sjme_jboolean isInput, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+	
+static sjme_jboolean sjme_discardPipeRead(sjme_pipeInstance* pipe,
+	sjme_taskPipeDirection direction, sjme_jbyte* buf,
+	sjme_jint off, sjme_jint len, sjme_jint* result, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+	
+static sjme_jboolean sjme_discardPipeWrite(sjme_pipeInstance* pipe,
+	sjme_taskPipeDirection direction, sjme_jbyte* buf,
+	sjme_jint off, sjme_jint len, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+/* --------------------------------- BUFFER ------------------------------- */
+
+static sjme_jboolean sjme_bufferPipeClose(sjme_pipeInstance* pipe,
+	sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+static sjme_jboolean sjme_bufferPipeFlush(sjme_pipeInstance* pipe,
+	sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+static sjme_jboolean sjme_bufferPipeNewInstance(sjme_pipeInstance** outPipe,
+	sjme_jint fd, sjme_jboolean isInput, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+	
+static sjme_jboolean sjme_bufferPipeRead(sjme_pipeInstance* pipe,
+	sjme_taskPipeDirection direction, sjme_jbyte* buf,
+	sjme_jint off, sjme_jint len, sjme_jint* result, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+	
+static sjme_jboolean sjme_bufferPipeWrite(sjme_pipeInstance* pipe,
+	sjme_taskPipeDirection direction, sjme_jbyte* buf,
+	sjme_jint off, sjme_jint len, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+/* -------------------------------- TERMINAL ------------------------------ */
+
+static sjme_jboolean sjme_terminalPipeClose(sjme_pipeInstance* pipe,
+	sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+static sjme_jboolean sjme_terminalPipeFlush(sjme_pipeInstance* pipe,
+	sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+
+static sjme_jboolean sjme_terminalPipeNewInstance(sjme_pipeInstance** outPipe,
+	sjme_jint fd, sjme_jboolean isInput, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+	
+static sjme_jboolean sjme_terminalPipeRead(sjme_pipeInstance* pipe,
+	sjme_taskPipeDirection direction, sjme_jbyte* buf,
+	sjme_jint off, sjme_jint len, sjme_jint* result, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
+	
+static sjme_jboolean sjme_terminalPipeWrite(sjme_pipeInstance* pipe,
+	sjme_taskPipeDirection direction, sjme_jbyte* buf,
+	sjme_jint off, sjme_jint len, sjme_error* error)
+{
+	sjme_todo("Implement this?");
+	return sjme_false;
+}
 
 const sjme_pipeFunction sjme_pipeFunctions
 	[NUM_SJME_TASK_PIPE_REDIRECTS] =
 {
 	/** @c SJME_TASK_PIPE_REDIRECT_DISCARD . */
 	{
-		.close = NULL,
-		.flush = NULL,
-		.newInstance = NULL,
-		.read = NULL,
-		.write = NULL,
+		.close = sjme_discardPipeClose,
+		.flush = sjme_discardPipeFlush,
+		.newInstance = sjme_discardPipeNewInstance,
+		.read = sjme_discardPipeRead,
+		.write = sjme_discardPipeWrite,
 	},
 	
 	/** @c SJME_TASK_PIPE_REDIRECT_BUFFER . */
 	{
-		.close = NULL,
-		.flush = NULL,
-		.newInstance = NULL,
-		.read = NULL,
-		.write = NULL,
+		.close = sjme_bufferPipeClose,
+		.flush = sjme_bufferPipeFlush,
+		.newInstance = sjme_bufferPipeNewInstance,
+		.read = sjme_bufferPipeRead,
+		.write = sjme_bufferPipeWrite,
 	},
 	
 	/** @c SJME_TASK_PIPE_REDIRECT_TERMINAL . */
 	{
-		.close = NULL,
-		.flush = NULL,
-		.newInstance = NULL,
-		.read = NULL,
-		.write = NULL,
+		.close = sjme_terminalPipeClose,
+		.flush = sjme_terminalPipeFlush,
+		.newInstance = sjme_terminalPipeNewInstance,
+		.read = sjme_terminalPipeRead,
+		.write = sjme_terminalPipeWrite,
 	},
 };
