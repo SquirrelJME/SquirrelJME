244743630a75cca5497ac0d89cbeea6e6b70d62f
Stephanie Gawroriski <xer@multiphasicapps.net>
2022-06-26 15:54:48 +0000

    Add test for creating an array of negative size.

---
+++ modules/cldc-compact/src/test/java/lang/TestArrayNegativeSize.java
@@ -0,0 +1,86 @@
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package lang;
+
+import net.multiphasicapps.tac.TestConsumer;
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * Tests that arrays of negative sizes cannot be made.
+ *
+ * @since 2022/06/26
+ */
+public class TestArrayNegativeSize
+	extends TestConsumer<String>
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2022/06/26
+	 */
+	@Override
+	public void test(String __what)
+	{
+		Object array;
+		switch (__what)
+		{
+			case "BOOLEAN":
+				array = new boolean[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "BYTE":
+				array = new byte[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "SHORT":
+				array = new short[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "CHAR":
+				array = new char[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "INT":
+				array = new int[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "LONG":
+				array = new long[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "FLOAT":
+				array = new float[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "DOUBLE":
+				array = new double[TestArrayNegativeSize.__size()];
+				break;
+				
+			case "OBJECT":
+				array = new Object[TestArrayNegativeSize.__size()];
+				break;
+			
+			default:
+				array = new Object[0];
+				break;
+		}
+		
+		this.secondary("array", array.getClass().toString());
+	}
+    
+    /**
+     * Returns a negative size.
+     * 
+     * @return A negative value.
+     * @since 2022/06/26
+     */
+	private static int __size()
+	{
+		return System.identityHashCode(new Object()) | Integer.MIN_VALUE;
+	}
+}
