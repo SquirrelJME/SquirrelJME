51d4685c9f0e2ccb277629c9aa759e2db9f57cdc
Stephanie Gawroriski <xer@multiphasicapps.net>
2022-06-29 22:37:03 +0000

    Do out of bounds check in additional location.

--- modules/ntt-docomo-doja-api/src/main/java/cc/squirreljme/runtime/nttdocomo/io/__ScratchPadStore__.java
+++ modules/ntt-docomo-doja-api/src/main/java/cc/squirreljme/runtime/nttdocomo/io/__ScratchPadStore__.java
@@ -120,7 +120,7 @@ final class __ScratchPadStore__
 		throws IndexOutOfBoundsException
 	{
 		byte[] data = this._data;
-		if (__pos < 0 || __length < 0 || (__pos + __length) > data.length)
+		if (__pos < 0 || __length < 0 || (__pos + __length) < 0 || (__pos + __length) > data.length)
 			throw new IndexOutOfBoundsException("IOOB");
 		
 		return new ByteArrayInputStream(data, __pos, __length);
@@ -142,7 +142,7 @@ final class __ScratchPadStore__
 		throws IndexOutOfBoundsException, IOException
 	{
 		byte[] data = this._data;
-		if (__pos < 0 || __len < 0 || (__pos + __len) > data.length)
+		if (__pos < 0 || __len < 0 || (__pos + __len) < 0 || (__pos + __len) > data.length)
 			throw new IndexOutOfBoundsException("IOOB");
 		
 		return new __ScratchPadOutputTransaction__(this, __pos, __len);
