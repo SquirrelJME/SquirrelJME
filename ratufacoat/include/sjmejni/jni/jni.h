bcc7645dc245de3123e875a02658c7bcc1dae62d
Stephanie Gawroriski <xer@multiphasicapps.net>
2022-12-12 01:26:45 +0000

    Declaration of JNI structures and types, incomplete.

--- ratufacoat/include/sjmejni/jni/jni.h
+++ ratufacoat/include/sjmejni/jni/jni.h
@@ -29,6 +29,27 @@ extern "C" {
 
 /*--------------------------------------------------------------------------*/
 
+/** JNI Version 1.1. */
+#define JNI_VERSION_1_1 SJME_INTERFACE_VERSION_1_1
+
+/** JNI Version 1.2. */
+#define JNI_VERSION_1_2 SJME_INTERFACE_VERSION_1_2
+
+/** JNI Version 1.4. */
+#define JNI_VERSION_1_4 SJME_INTERFACE_VERSION_1_4
+
+/** JNI Version 1.6. */
+#define JNI_VERSION_1_6 SJME_INTERFACE_VERSION_1_6
+
+/** No error. */
+#define JNI_OK SJME_INTERFACE_ERROR_NONE
+
+/** The virtual machine is detached. */
+#define JNI_EDETACHED SJME_INTERFACE_ERROR_DETACHED
+
+/** The virtual machine version is incorrect. */
+#define JNI_EVERSION SJME_INTERFACE_ERROR_INVALID_VERSION
+
 /** Boolean. */
 typedef sjme_jboolean jboolean;
 
@@ -61,6 +82,75 @@ typedef jint jsize;
 
 /** True. */
 #define JNI_TRUE SJME_TRUE
+
+/** Value type. */
+typedef sjme_jvalue jvalue;
+
+/** Object type. */
+typedef sjme_jobject jobject;
+
+/** Class type. */
+typedef sjme_jobject jclass;
+
+/** String type. */
+typedef sjme_jstring jstring;
+
+/** Throwable. */
+typedef sjme_jthrowable jthrowable;
+
+/** Array type. */
+typedef sjme_jarray jarray;
+
+/** Boolean array type. */
+typedef sjme_jbooleanArray jbooleanArray;
+
+/** Byte array type. */
+typedef sjme_jbyteArray jbyteArray;
+
+/** Character array type. */
+typedef sjme_jcharArray jcharArray;
+
+/** Short array type. */
+typedef sjme_jshortArray jshortArray;
+
+/** Integer array type. */
+typedef sjme_jintArray jintArray;
+
+/** Long array type. */
+typedef sjme_jlongArray jlongArray;
+
+/** Float array type. */
+typedef sjme_jfloatArray jfloatArray;
+
+/** Double array type. */
+typedef sjme_jdoubleArray jdoubleArray;
+
+/** Object array type. */
+typedef sjme_jobjectArray jobjectArray;
+
+/** Field Identifier. */
+typedef sjme_fieldId fieldID;
+
+/** Method Identifier. */
+typedef sjme_methodId methodID;
+
+/** Virtual machine initialization arguments. */
+#define JavaVMInitArgs sjme_vmInitArgs
+
+/** Virtual machine options. */
+#define JavaVMOption sjme_vmInitOption
+
+/** Interface functions for the virtual machine. */
+#define JNINativeInterface sjme_vmFunctions
+
+/** Virtual machine interface. */
+typedef struct JNINativeInterface* JNIEnv;
+
+/** Invocation interface. */
+#define JNIInvokeInterface sjme_vmState
+
+/** Virtual machine state. */
+typedef struct JNIInvokeInterface* JavaVM;
 
 /*--------------------------------------------------------------------------*/
 
--- ratufacoat/include/sjmejni/sjmejni.h
+++ ratufacoat/include/sjmejni/sjmejni.h
@@ -82,18 +82,457 @@ extern "C" {
 
 	/** Double. */
 	typedef double sjme_jdouble;
+
+	/** Signed 8-bit constant. */
+	#define INT8_C(x) x
+
+	/** Signed 16-bit constant. */
+	#define INT16_C(x) x
+
+	/** Signed 32-bit constant. */
+	#define INT32_C(x) x
+
+	/** Unsigned 8-bit constant. */
+	#define UINT8_C(x) x##U
+
+	/** Unsigned 16-bit constant. */
+	#define UINT16_C(x) x##U
+
+	/** Unsigned 32-bit constant. */
+	#define UINT32_C(x) x##U
 #else
 	#error No standard types are known.
 #endif
+
+/** Interface version 1.1. */
+#define SJME_INTERFACE_VERSION_1_1 INT32_C(0x00010001)
+
+/** Interface version 1.2. */
+#define SJME_INTERFACE_VERSION_1_2 INT32_C(0x00010002)
+
+/** Interface version 1.4. */
+#define SJME_INTERFACE_VERSION_1_4 INT32_C(0x00010004)
+
+/** Interface version 1.6. */
+#define SJME_INTERFACE_VERSION_1_6 INT32_C(0x00010006)
+
+/** No interface error. */
+#define SJME_INTERFACE_ERROR_NONE INT32_C(0)
+
+/** The virtual machine is detached. */
+#define SJME_INTERFACE_ERROR_DETACHED INT32_C(-2)
+
+/** The virtual machine version is incorrect. */
+#define SJME_INTERFACE_ERROR_INVALID_VERSION INT32_C(-3)
 
 /** Size type. */
 typedef sjme_jint sjme_jsize;
 
 /** False. */
-#define SJME_FALSE 0
+#define SJME_FALSE INT8_C(0)
 
 /** True. */
-#define SJME_TRUE 1
+#define SJME_TRUE INT8_C(1)
+
+/** Object type. */
+typedef struct sjme_jobject* sjme_jobject;
+
+/** Class type. */
+typedef struct sjme_jobject* sjme_jclass;
+
+/** String type. */
+typedef struct sjme_jstring* sjme_jstring;
+
+/** Throwable. */
+typedef struct sjme_jthrowable* sjme_jthrowable;
+
+/** Array type. */
+typedef struct sjme_jarray* sjme_jarray;
+
+/** Boolean array type. */
+typedef struct sjme_jbooleanArray* sjme_jbooleanArray;
+
+/** Byte array type. */
+typedef struct sjme_jbyteArray* sjme_jbyteArray;
+
+/** Character array type. */
+typedef struct sjme_jcharArray* sjme_jcharArray;
+
+/** Short array type. */
+typedef struct sjme_jshortArray* sjme_jshortArray;
+
+/** Integer array type. */
+typedef struct sjme_jintArray* sjme_jintArray;
+
+/** Long array type. */
+typedef struct sjme_jlongArray* sjme_jlongArray;
+
+/** Float array type. */
+typedef struct sjme_jfloatArray* sjme_jfloatArray;
+
+/** Double array type. */
+typedef struct sjme_jdoubleArray* sjme_jdoubleArray;
+
+/** Object array type. */
+typedef struct sjme_jobjectArray* sjme_jobjectArray;
+
+/**
+ * Value type.
+ *
+ * @since 2022/12/11
+ */
+typedef union sjme_jvalue
+{
+	sjme_jboolean z;
+	sjme_jbyte b;
+	sjme_jchar c;
+	sjme_jshort s;
+	sjme_jint i;
+	sjme_jlong j;
+	sjme_jfloat f;
+	sjme_jdouble d;
+	sjme_jobject l;
+} sjme_jvalue;
+
+/** Field identifier. */
+typedef struct sjme_fieldId* sjme_fieldId;
+
+/** Method identifier. */
+typedef struct sjme_methodId* sjme_methodId;
+
+/**
+ * Virtual machine initialization options.
+ *
+ * @since 2022/12/11
+ */
+typedef struct sjme_vmInitOption
+{
+	/** The option specified on the launch of the virtual machine. */
+	char* optionString;
+
+	/** Extra argument information to the option. */
+	void* extraInfo;
+} sjme_vmInitOption;
+
+/**
+ * Initialization arguments for the virtual machine.
+ *
+ * @since 2022/12/11
+ */
+typedef struct sjme_vmInitArgs
+{
+	/** The requesting virtual machine version. */
+	sjme_jint version;
+
+	/** The number of specified options. */
+	sjme_jint nOptions;
+
+	/** Virtual machine options. */
+	sjme_vmInitOption* options;
+
+	/** Ignore unrecognized options? */
+	sjme_jboolean ignoreUnrecognized;
+} sjme_vmInitArgs;
+
+/** Virtual machine functions. */
+typedef struct sjme_vmFunctions* sjme_vmFunctions;
+
+/** Virtual machine state. */
+typedef struct sjme_vmState* sjme_vmState;
+
+/** Declare function pointer. */
+#define SJME_FUNC_PTR__(x) (*x)
+
+/** Function: Destroy Java VM. */
+#define SJME_FUNCTION_DESTROY_JAVA_VM(funcName) \
+	sjme_jint funcName(sjme_vmState* vm)
+
+/**
+ * Virtual machine state.
+ *
+ * @since 2022/12/11
+ */
+struct sjme_vmState
+{
+	/** Reserved, do not use. */
+	void* reserved0;
+
+	/** Reserved, do not use. */
+	void* reserved1;
+
+	/** Reserved, do not use. */
+	void* reserved2;
+
+	/**
+	 * Destroys the virtual machine.
+	 *
+	 * @param vm The virtual machine to destroy.
+	 * @return
+	 * @since 2022/12/11
+	 */
+	SJME_FUNCTION_DESTROY_JAVA_VM(SJME_FUNC_PTR__(DestroyJavaVM))
+#if 0
+	AttachCurrentThread,
+	DetachCurrentThread,
+	GetEnv,
+	AttachCurrentThreadAsDaemon
+#endif
+};
+
+/**
+ * Virtual machine functions.
+ *
+ * @since 2022/12/11
+ */
+struct sjme_vmFunctions
+{
+	/** Reserved, do not use. */
+	void* reserved0;
+
+	/** Reserved, do not use. */
+	void* reserved1;
+
+	/** Reserved, do not use. */
+	void* reserved2;
+
+	/** Reserved, do not use. */
+	void* reserved3;
+
+#if 0
+	GetVersion,
+	DefineClass,
+	FindClass,
+	FromReflectedMethod,
+	FromReflectedField,
+	ToReflectedMethod,
+	GetSuperclass,
+	IsAssignableFrom,
+	ToReflectedField,
+	Throw,
+	ThrowNew,
+	ExceptionOccurred,
+	ExceptionDescribe,
+	ExceptionClear,
+	FatalError,
+	PushLocalFrame,
+	PopLocalFrame,
+	NewGlobalRef,
+	DeleteGlobalRef,
+	DeleteLocalRef,
+	IsSameObject,
+	NewLocalRef,
+	EnsureLocalCapacity,
+	AllocObject,
+	NewObject,
+	NewObjectV,
+	NewObjectA,
+	GetObjectClass,
+	IsInstanceOf,
+	GetMethodID,
+	CallObjectMethod,
+	CallObjectMethodV,
+	CallObjectMethodA,
+	CallBooleanMethod,
+	CallBooleanMethodV,
+	CallBooleanMethodA,
+	CallByteMethod,
+	CallByteMethodV,
+	CallByteMethodA,
+	CallCharMethod,
+	CallCharMethodV,
+	CallCharMethodA,
+	CallShortMethod,
+	CallShortMethodV,
+	CallShortMethodA,
+	CallIntMethod,
+	CallIntMethodV,
+	CallIntMethodA,
+	CallLongMethod,
+	CallLongMethodV,
+	CallLongMethodA,
+	CallFloatMethod,
+	CallFloatMethodV,
+	CallFloatMethodA,
+	CallDoubleMethod,
+	CallDoubleMethodV,
+	CallDoubleMethodA,
+	CallVoidMethod,
+	CallVoidMethodV,
+	CallVoidMethodA,
+	CallNonvirtualObjectMethod,
+	CallNonvirtualObjectMethodV,
+	CallNonvirtualObjectMethodA,
+	CallNonvirtualBooleanMethod,
+	CallNonvirtualBooleanMethodV,
+	CallNonvirtualBooleanMethodA,
+	CallNonvirtualByteMethod,
+	CallNonvirtualByteMethodV,
+	CallNonvirtualByteMethodA,
+	CallNonvirtualCharMethod,
+	CallNonvirtualCharMethodV,
+	CallNonvirtualCharMethodA,
+	CallNonvirtualShortMethod,
+	CallNonvirtualShortMethodV,
+	CallNonvirtualShortMethodA,
+	CallNonvirtualIntMethod,
+	CallNonvirtualIntMethodV,
+	CallNonvirtualIntMethodA,
+	CallNonvirtualLongMethod,
+	CallNonvirtualLongMethodV,
+	CallNonvirtualLongMethodA,
+	CallNonvirtualFloatMethod,
+	CallNonvirtualFloatMethodV,
+	CallNonvirtualFloatMethodA,
+	CallNonvirtualDoubleMethod,
+	CallNonvirtualDoubleMethodV,
+	CallNonvirtualDoubleMethodA,
+	CallNonvirtualVoidMethod,
+	CallNonvirtualVoidMethodV,
+	CallNonvirtualVoidMethodA,
+	GetFieldID,
+	GetObjectField,
+	GetBooleanField,
+	GetByteField,
+	GetCharField,
+	GetShortField,
+	GetIntField,
+	GetLongField,
+	GetFloatField,
+	GetDoubleField,
+	SetObjectField,
+	SetBooleanField,
+	SetByteField,
+	SetCharField,
+	SetShortField,
+	SetIntField,
+	SetLongField,
+	SetFloatField,
+	SetDoubleField,
+	GetStaticMethodID,
+	CallStaticObjectMethod,
+	CallStaticObjectMethodV,
+	CallStaticObjectMethodA,
+	CallStaticBooleanMethod,
+	CallStaticBooleanMethodV,
+	CallStaticBooleanMethodA,
+	CallStaticByteMethod,
+	CallStaticByteMethodV,
+	CallStaticByteMethodA,
+	CallStaticCharMethod,
+	CallStaticCharMethodV,
+	CallStaticCharMethodA,
+	CallStaticShortMethod,
+	CallStaticShortMethodV,
+	CallStaticShortMethodA,
+	CallStaticIntMethod,
+	CallStaticIntMethodV,
+	CallStaticIntMethodA,
+	CallStaticLongMethod,
+	CallStaticLongMethodV,
+	CallStaticLongMethodA,
+	CallStaticFloatMethod,
+	CallStaticFloatMethodV,
+	CallStaticFloatMethodA,
+	CallStaticDoubleMethod,
+	CallStaticDoubleMethodV,
+	CallStaticDoubleMethodA,
+	CallStaticVoidMethod,
+	CallStaticVoidMethodV,
+	CallStaticVoidMethodA,
+	GetStaticFieldID,
+	GetStaticObjectField,
+	GetStaticBooleanField,
+	GetStaticByteField,
+	GetStaticCharField,
+	GetStaticShortField,
+	GetStaticIntField,
+	GetStaticLongField,
+	GetStaticFloatField,
+	GetStaticDoubleField,
+	SetStaticObjectField,
+	SetStaticBooleanField,
+	SetStaticByteField,
+	SetStaticCharField,
+	SetStaticShortField,
+	SetStaticIntField,
+	SetStaticLongField,
+	SetStaticFloatField,
+	SetStaticDoubleField,
+	NewString,
+	GetStringLength,
+	GetStringChars,
+	ReleaseStringChars,
+	NewStringUTF,
+	GetStringUTFLength,
+	GetStringUTFChars,
+	ReleaseStringUTFChars,
+	GetArrayLength,
+	NewObjectArray,
+	GetObjectArrayElement,
+	SetObjectArrayElement,
+	NewBooleanArray,
+	NewByteArray,
+	NewCharArray,
+	NewShortArray,
+	NewIntArray,
+	NewLongArray,
+	NewFloatArray,
+	NewDoubleArray,
+	GetBooleanArrayElements,
+	GetByteArrayElements,
+	GetCharArrayElements,
+	GetShortArrayElements,
+	GetIntArrayElements,
+	GetLongArrayElements,
+	GetFloatArrayElements,
+	GetDoubleArrayElements,
+	ReleaseBooleanArrayElements,
+	ReleaseByteArrayElements,
+	ReleaseCharArrayElements,
+	ReleaseShortArrayElements,
+	ReleaseIntArrayElements,
+	ReleaseLongArrayElements,
+	ReleaseFloatArrayElements,
+	ReleaseDoubleArrayElements,
+	GetBooleanArrayRegion,
+	GetByteArrayRegion,
+	GetCharArrayRegion,
+	GetShortArrayRegion,
+	GetIntArrayRegion,
+	GetLongArrayRegion,
+	GetFloatArrayRegion,
+	GetDoubleArrayRegion,
+	SetBooleanArrayRegion,
+	SetByteArrayRegion,
+	SetCharArrayRegion,
+	SetShortArrayRegion,
+	SetIntArrayRegion,
+	SetLongArrayRegion,
+	SetFloatArrayRegion,
+	SetDoubleArrayRegion,
+	RegisterNatives,
+	UnregisterNatives,
+	MonitorEnter,
+	MonitorExit,
+	GetJavaVM,
+	GetStringRegion,
+	GetStringUTFRegion,
+	GetPrimitiveArrayCritical,
+	ReleasePrimitiveArrayCritical,
+	GetStringCritical,
+	ReleaseStringCritical,
+	NewWeakGlobalRef,
+	DeleteWeakGlobalRef,
+	ExceptionCheck,
+	NewDirectByteBuffer,
+	GetDirectBufferAddress,
+	GetDirectBufferCapacity,
+	GetObjectRefType
+#endif
+};
+
+/* Clear this. */
+#undef SJME_FUNC_PTR__
 
 /*--------------------------------------------------------------------------*/
 
