d783706110d259cfd561b3d1c98330548fe84e9d
Stephanie Gawroriski <xer@multiphasicapps.net>
2022-12-16 00:08:29 +0000

    Ignore Python venvs; Empty script for generating the function table.

---
+++ ratufacoat/src/sjmejni/funcTableGen.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3+, or later.
+# See license.mkd for licensing and copyright information.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Script that turns function table CSV to source code
+
