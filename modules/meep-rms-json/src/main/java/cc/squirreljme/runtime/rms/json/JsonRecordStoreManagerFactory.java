3b1a15e55d95c8a00fa653a04eb83e0ae97433f1
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-02-17 04:38:27 +0000

    Base blank classes for managers and factories.

---
+++ modules/meep-rms-json/src/main/java/cc/squirreljme/runtime/rms/json/JsonRecordStoreManagerFactory.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.rms.json;
+
+import cc.squirreljme.runtime.rms.RecordStoreManagerFactory;
+
+/**
+ * Factory for creating {@link JsonRecordStoreManager}.
+ *
+ * @since 2023/02/16
+ */
+public class JsonRecordStoreManagerFactory
+	extends RecordStoreManagerFactory
+{
+}
---
+++ modules/meep-rms/src/main/java/cc/squirreljme/runtime/rms/LocalRecordStoreManagerFactory.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.runtime.rms;
+
+/**
+ * Factory for creating {@link LocalRecordStoreManager}.
+ *
+ * @since 2023/02/16
+ */
+public class LocalRecordStoreManagerFactory
+	extends RecordStoreManagerFactory
+{
+}
