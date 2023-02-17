84ad9ef2e8e6f34e560fabd310276c68569924f7
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-02-17 04:24:09 +0000

    Add new sub-module for JSON implementation of RMS.

---
+++ modules/meep-rms-json/src/main/java/cc/squirreljme/runtime/rms/json/package-info.java
@@ -0,0 +1,16 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * JSON implementation of RMS.
+ *
+ * @since 2023/02/16
+ */
+
+package cc.squirreljme.runtime.rms.json;
