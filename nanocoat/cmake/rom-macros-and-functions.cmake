a1836a0e5e12b5473c7cd5e524fbbdeb2d41c4c3
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-07-19 23:53:16 +0000

    Add base for functions and macros for ROM related sources.

---
+++ nanocoat/cmake/rom-macros-and-functions.cmake
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------------------------
+# SquirrelJME
+#     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3+, or later.
+# See license.mkd for licensing and copyright information.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: Functions and macros for ROM source code, allows all the
+# ROM source information to be duplicated accordingly and to change all of
+# at once instead of auto-generating new code.
+
