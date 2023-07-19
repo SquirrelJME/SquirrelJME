205ee5fff097009a936b290971db7fe68b149c40
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-07-19 23:42:54 +0000

    Bring in some files from RatufaCoat.

---
+++ nanocoat/cmake/i-miss-you.cmake
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------------
+# Multi-Phasic Applications: SquirrelJME
+#     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+# ---------------------------------------------------------------------------
+# SquirrelJME is under the GNU General Public License v3+, or later.
+# See license.mkd for licensing and copyright information.
+# ---------------------------------------------------------------------------
+# DESCRIPTION: I really miss you...
+
+# To Emily and Near...
+message("*****************************************************************")
+message("To my close friends who are not here today, your friendships were")
+message("very important to me. I hope that you are both resting in piece:")
+message("  * Emily, developer of Mu. (1998-2020)")
+message("  * Near, developer of Higan and Ares. (1983-2021)")
+message(" -- Your friend, Stephanie")
+message("*****************************************************************")
