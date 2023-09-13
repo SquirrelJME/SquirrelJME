b94f6d52ee8350aa37f014278ed6cad9d3e76d48
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-09-13 00:47:28 +0000

    Reader/writer methods.

--- modules/csv/src/main/java/cc/squirreljme/csv/CsvReader.java
+++ modules/csv/src/main/java/cc/squirreljme/csv/CsvReader.java
@@ -10,6 +10,9 @@
 package cc.squirreljme.csv;
 
 import cc.squirreljme.runtime.cldc.debug.Debugging;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.NoSuchElementException;
 
 /**
  * Reads CSV files and writes to an output.
@@ -17,7 +20,7 @@ import cc.squirreljme.runtime.cldc.debug.Debugging;
  * @param <T> The type of value to read.
  * @since 2023/09/12
  */
-public class CsvReader<T>
+public final class CsvReader<T>
 {
 	/**
 	 * Initializes the reader.
@@ -30,6 +33,34 @@ public class CsvReader<T>
 	public CsvReader(CsvDeserializer<T> __deserializer,
 		CsvReaderInputStream __input)
 		throws NullPointerException
+	{
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * Reads a single value.
+	 *
+	 * @return The single value.
+	 * @throws NoSuchElementException If there is nothing left.
+	 * @throws IOException On read errors.
+	 * @since 2023/09/12
+	 */
+	public T read()
+		throws NoSuchElementException, IOException
+	{
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * Reads all the values into the given collection.
+	 *
+	 * @param __into The collection to write into.
+	 * @throws IOException On read errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2023/09/12
+	 */
+	public void readAll(Collection<T> __into)
+		throws IOException, NullPointerException
 	{
 		throw Debugging.todo();
 	}
--- modules/csv/src/main/java/cc/squirreljme/csv/CsvWriter.java
+++ modules/csv/src/main/java/cc/squirreljme/csv/CsvWriter.java
@@ -10,6 +10,8 @@
 package cc.squirreljme.csv;
 
 import cc.squirreljme.runtime.cldc.debug.Debugging;
+import java.io.Closeable;
+import java.io.IOException;
 
 /**
  * Writes CSV data to an output file.
@@ -17,7 +19,8 @@ import cc.squirreljme.runtime.cldc.debug.Debugging;
  * @param <T> The type to write.
  * @since 2023/09/12
  */
-public class CsvWriter<T>
+public final class CsvWriter<T>
+	implements Closeable
 {
 	/**
 	 * Initializes the CSV writer.
@@ -29,6 +32,60 @@ public class CsvWriter<T>
 	 */
 	public CsvWriter(CsvSerializer<T> __serializer, Appendable __out)
 		throws NullPointerException
+	{
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2023/09/12
+	 */
+	@Override
+	public void close()
+		throws IOException
+	{
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * Writes the given value.
+	 *
+	 * @param __value The value to write.
+	 * @throws IOException On write errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2023/09/12
+	 */
+	public void write(T __value)
+		throws IOException, NullPointerException
+	{
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * Writes all the given values.
+	 *
+	 * @param __values The values to write.
+	 * @throws IOException On write errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2023/09/12
+	 */
+	@SuppressWarnings("unchecked")
+	public void writeAll(T... __values)
+		throws IOException, NullPointerException
+	{
+		throw Debugging.todo();
+	}
+	
+	/**
+	 * Writes all the given values.
+	 *
+	 * @param __values The values to write.
+	 * @throws IOException On write errors.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2023/09/12
+	 */
+	public void writeAll(Iterable<T> __values)
+		throws IOException, NullPointerException
 	{
 		throw Debugging.todo();
 	}
