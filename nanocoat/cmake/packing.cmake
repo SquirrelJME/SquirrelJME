28ab1cd564582892fe84dacd71f0bee145e1afa7
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-11-21 21:47:27 +0000

    More CPack base?

--- nanocoat/cmake/packing.cmake
+++ nanocoat/cmake/packing.cmake
@@ -76,6 +76,31 @@ foreach(assetHexFile ${SQUIRRELJME_ASSET_FILES})
 	endif()
 endforeach()
 
-# Icons and Branding
+# Include support for packing
+include(CPack)
+
+# Generic
+set(CPACK_PACKAGE_NAME
+	"SquirrelJME")
+set(CPACK_PACKAGE_VENDOR
+	"Stephanie Gawroriski")
+set(CPACK_PACKAGE_DESCRIPTION
+	"Portable Java ME Virtual Machine.")
+set(CPACK_PACKAGE_HOMEPAGE_URL
+	"https://squirreljme.cc/")
+
+# Debian
+set(CPACK_DEBIAN_PACKAGE_NAME
+	"squirreljme")
+set(CPACK_DEBIAN_PACKAGE_EPOCH
+	"")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER
+	"Stephanie Gawroriski <xerthesquirrel@gmail.com>")
+set(CPACK_DEBIAN_ENABLE_COMPONENT_DEPENDS
+	TRUE)
+set(CPACK_DEBIAN_PACKAGE_SOURCE
+	"squirreljme")
+
+# NSIS
 set(CPACK_NSIS_MUI_ICON
 	"${SQUIRRELJME_ASSET_BIN}/win32.ico")
