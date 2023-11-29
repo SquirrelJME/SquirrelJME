54675a78d5c4354007cb838845f5e7b507836d16
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-11-29 04:54:28 +0000

    Implement test for exception handling.

--- nanocoat/tests/testExceptFail.c
+++ nanocoat/tests/testExceptFail.c
@@ -7,8 +7,12 @@
 // See license.mkd for licensing and copyright information.
 // -------------------------------------------------------------------------*/
 
+#include <string.h>
+
 #include "proto.h"
+#include "sjme/except.h"
 #include "test.h"
+#include "unit.h"
 
 /**
  * Tests failing of the exception handler.
@@ -17,6 +21,25 @@
  */
 SJME_TEST_DECLARE(testExceptFail)
 {
-	sjme_todo("Implement %s", __func__);
-	return SJME_TEST_RESULT_FAIL;
+	SJME_EXCEPT_VDEF;
+	sjme_nvm_frame xframe;
+	sjme_nvm_frame* frame;
+	
+	/* Exception handler takes frame info. */
+	memset(&xframe, 0, sizeof(xframe));
+	frame = &xframe;
+	
+	/* Fail. */
+SJME_EXCEPT_WITH:
+	SJME_EXCEPT_TOSS(SJME_ERROR_CODE_TOP_NOT_LONG);
+
+	/* Should hopefully not be reached. */
+	sjme_unitFail(test, "Should not be reached here?");
+	
+SJME_EXCEPT_FAIL:
+	sjme_unitEqualI(test, errorCode, SJME_ERROR_CODE_TOP_NOT_LONG,
+		"Error code was not set?");
+	
+	/* Success otherwise. */
+	return SJME_TEST_RESULT_PASS;
 }
