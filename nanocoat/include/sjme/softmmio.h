42363d1bc6271463c12dc8d949f90f023851336c
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-11-30 18:56:33 +0000

    Add blank software MMIO.

---
+++ nanocoat/include/sjme/softmmio.h
@@ -0,0 +1,41 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Software Memory Mapped I/O.
+ * 
+ * @since 2023/11/30
+ */
+
+#ifndef SQUIRRELJME_SOFTMMIO_H
+#define SQUIRRELJME_SOFTMMIO_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifndef SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_SQUIRRELJME_SOFTMMIO_H
+extern "C" {
+	#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif     /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifdef SJME_CXX_SQUIRRELJME_SOFTMMIO_H
+}
+		#undef SJME_CXX_SQUIRRELJME_SOFTMMIO_H
+		#undef SJME_CXX_IS_EXTERNED
+	#endif /* #ifdef SJME_CXX_SQUIRRELJME_SOFTMMIO_H */
+#endif     /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_SOFTMMIO_H */
