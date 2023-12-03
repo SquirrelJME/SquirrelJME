51e5ad12e4377ee3a497fea72e6cad3fc64fd0e2
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-12-03 19:21:39 +0000

    Add base blank file for virtual machine configuration.

---
+++ nanocoat/include/sjme/vmConfig.h
@@ -0,0 +1,41 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Virtual machine configuration.
+ * 
+ * @since 2023/12/03
+ */
+
+#ifndef SQUIRRELJME_VMCONFIG_H
+#define SQUIRRELJME_VMCONFIG_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifndef SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_SQUIRRELJME_VMCONFIG_H
+extern "C" {
+	#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif     /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifdef SJME_CXX_SQUIRRELJME_VMCONFIG_H
+}
+		#undef SJME_CXX_SQUIRRELJME_VMCONFIG_H
+		#undef SJME_CXX_IS_EXTERNED
+	#endif /* #ifdef SJME_CXX_SQUIRRELJME_VMCONFIG_H */
+#endif     /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_VMCONFIG_H */
