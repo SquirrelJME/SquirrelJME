b044b7449238889fe60ae1842e1f8b5f14dd65df
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-12-12 22:45:58 +0000

    Add a nothing test.

---
+++ modules/cldc-compact/src/test/java/lang/TestNothing.java
@@ -0,0 +1,31 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package lang;
+
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * Does not do anything.
+ *
+ * @since 2023/12/12
+ */
+public class TestNothing
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @since 2023/12/12
+	 */
+	@Override
+	public void test()
+	{
+	}
+}
