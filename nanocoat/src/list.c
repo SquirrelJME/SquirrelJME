0f051788b814c8a21e01b56629dbbc62d42f0c02
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-12-17 21:58:49 +0000

    Initialization of both variadic and array lists.

--- nanocoat/src/list.c
+++ nanocoat/src/list.c
@@ -8,6 +8,7 @@
 // -------------------------------------------------------------------------*/
 
 #include <string.h>
+#include <stdarg.h>
 
 #include "sjme/list.h"
 #include "sjme/alloc.h"
@@ -40,6 +41,8 @@ static sjme_errorCode sjme_list_newInit(
 	sjme_attrInPositive sjme_jint length,
 	sjme_attrInPositive sjme_jint extraFill)
 {
+	sjme_errorCode error;
+
 	if (inPool == NULL || newData == NULL)
 		return SJME_ERROR_NULL_ARGUMENTS;
 
@@ -51,18 +54,28 @@ static sjme_errorCode sjme_list_newInit(
 
 	/* Contains root elements only? */
 	if (numPointerStars == 0)
-		newData->allocSize = elementOffset + (rootElementSize * length);
+		newData->allocSize = elementOffset + (rootElementSize * length) +
+			extraFill;
 
 	/* Is a list of pointer types. */
 	else
-		newData->allocSize = elementOffset + (elementSize * length);
+		newData->allocSize = elementOffset + (elementSize * length) +
+			extraFill;
 
 	/* Underflow? */
 	if (newData->allocSize <= 0)
 		return SJME_ERROR_INVALID_ARGUMENT;
 
-	sjme_todo("Implement this?");
-	return SJME_ERROR_NOT_IMPLEMENTED;
+	/* Allocate list. */
+	if (SJME_IS_ERROR(error = sjme_alloc(inPool, newData->allocSize,
+		&newData->outList)) || newData->outList == NULL)
+		return error;
+
+	/* Store list length. */
+	(*((sjme_jint*)newData->outList)) = length;
+
+	/* Success! */
+	return SJME_ERROR_NONE;
 }
 
 sjme_errorCode sjme_list_allocR(
@@ -165,7 +178,9 @@ sjme_errorCode sjme_list_newAR(
 {
 	sjme_errorCode error;
 	sjme_list_newData newData;
-	sjme_jint at;
+	sjme_jint at, toOff, fromOff;
+	void* toPtr;
+	void* fromPtr;
 
 	/* Common initialization of new lists. */
 	error = SJME_ERROR_UNKNOWN;
@@ -175,15 +190,10 @@ sjme_errorCode sjme_list_newAR(
 		basicTypeId, numPointerStars, length, 0)))
 		return error;
 
-	/* Store list length. */
-	(*((sjme_jint*)newData.outList)) = length;
-
-	/* Store elements from array argument. */
-	for (at = 0; at < length; at++)
-	{
-		sjme_todo("Implement this?");
-		return SJME_ERROR_NOT_IMPLEMENTED;
-	}
+	/* Because the input is a sequential "array", we can just copy it all. */
+	/* This can turn out to be a very fast operation. */
+	memmove((void*)(((intptr_t)newData.outList) + elementOffset),
+		inElements, elementSize * length);
 
 	/* Return resultant list. */
 	*outList = newData.outList;
@@ -228,9 +238,15 @@ sjme_errorCode sjme_list_newVAR(
 	sjme_attrOutNotNull void** outList,
 	va_list elements)
 {
+#define SJME_BLA(basicType, cType, pType) \
+	case SJME_TOKEN_PASTE_PP(SJME_BASIC_TYPE_ID_, basicType): \
+		*((cType*)atPtr) = va_arg(elements, pType); \
+		break
+
 	sjme_errorCode error;
 	sjme_list_newData newData;
-	sjme_jint at;
+	sjme_jint at, off;
+	void* atPtr;
 
 	/* Common initialization of new lists. */
 	error = SJME_ERROR_UNKNOWN;
@@ -240,17 +256,34 @@ sjme_errorCode sjme_list_newVAR(
 		basicTypeId, numPointerStars, length, 0)))
 		return error;
 
-	/* Store list length. */
-	(*((sjme_jint*)newData.outList)) = length;
-
 	/* Store elements from variadic arguments. */
-	for (at = 0; at < length; at++)
+	for (at = 0, off = elementOffset; at < length; at++, off += elementSize)
 	{
-		sjme_todo("Implement this?");
-		return SJME_ERROR_NOT_IMPLEMENTED;
+		/* Calculate the base pointer address. */
+		atPtr = (void*)(((intptr_t)newData.outList) + off);
+
+		/* Read in depending on the basic type. */
+		switch (basicTypeId)
+		{
+			SJME_BLA(BOOLEAN, sjme_jboolean, int);
+			SJME_BLA(BYTE, sjme_jbyte, int);
+			SJME_BLA(SHORT, sjme_jshort, int);
+			SJME_BLA(CHARACTER, sjme_jchar, int);
+			SJME_BLA(INTEGER, sjme_jint, sjme_jint);
+			SJME_BLA(LONG, sjme_jlong, sjme_jlong);
+			SJME_BLA(FLOAT, sjme_jfloat, sjme_jfloat);
+			SJME_BLA(DOUBLE, sjme_jdouble, sjme_jdouble);
+			SJME_BLA(OBJECT, void*, void*);
+
+				/* Type not implemented. */
+			default:
+				sjme_todo("Implement: %d", basicTypeId);
+				return SJME_ERROR_NOT_IMPLEMENTED;
+		}
 	}
 
 	/* Return resultant list. */
 	*outList = newData.outList;
 	return SJME_ERROR_NONE;
+#undef SJME_BLA
 }
