44bef02e361740e511c0fce2a5b88e29a7235a32
Stephanie Gawroriski <xer@multiphasicapps.net>
2023-12-31 06:41:25 +0000

    Add blank test for reading an empty stream.

---
+++ nanocoat/tests/testStreamEmpty.c
@@ -0,0 +1,24 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include "mock.h"
+#include "proto.h"
+#include "test.h"
+#include "unit.h"
+
+/**
+ * Tests reading of an empty stream.
+ *  
+ * @since 2023/12/31 
+ */
+SJME_TEST_DECLARE(testStreamEmpty)
+{
+	sjme_todo("Implement %s", __func__);
+	return SJME_TEST_RESULT_FAIL;
+}
