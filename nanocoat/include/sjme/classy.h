f49d35f65305cca3a66e6a88d242ad7e838d0e85
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-01-02 03:35:33 +0000

    Base handling for class files.

---
+++ nanocoat/include/sjme/classy.h
@@ -0,0 +1,41 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Handling of classes.
+ * 
+ * @since 2024/01/01
+ */
+
+#ifndef SQUIRRELJME_CLASSY_H
+#define SQUIRRELJME_CLASSY_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifndef SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_SQUIRRELJME_CLASSY_H
+extern "C" {
+	#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif     /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifdef SJME_CXX_SQUIRRELJME_CLASSY_H
+}
+		#undef SJME_CXX_SQUIRRELJME_CLASSY_H
+		#undef SJME_CXX_IS_EXTERNED
+	#endif /* #ifdef SJME_CXX_SQUIRRELJME_CLASSY_H */
+#endif     /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_CLASSY_H */
