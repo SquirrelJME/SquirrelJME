ee262d44feee818f8228c2d0e78a4cca62acce40
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-01-20 10:38:47 +0000

    For inspection dialogs, do a little more organizing of the layout.

--- tools/squirreljme-debugger/src/main/java/cc/squirreljme/debugger/Inspect.java
+++ tools/squirreljme-debugger/src/main/java/cc/squirreljme/debugger/Inspect.java
@@ -9,14 +9,17 @@
 
 package cc.squirreljme.debugger;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * Base inspector dialog.
@@ -33,6 +36,9 @@ public abstract class Inspect<I>
 	/** Known items being inspected. */
 	private final List<InspectKnownValue> _known =
 		new ArrayList<>();
+	
+	/** The root panel. */
+	protected final JPanel panel;
 	
 	/**
 	 * Initializes the base inspector.
@@ -55,20 +61,43 @@ public abstract class Inspect<I>
 		// This is hard to use when tiny
 		this.setMinimumSize(new Dimension(320, 240));
 		
+		// Setup stretching panel
+		JPanel panel = new JPanel();
+		this.panel = panel;
+		this.setLayout(new BorderLayout());
+		this.add(panel, BorderLayout.CENTER);
+		
 		// Setup grid view for items
-		GridLayout layout = new GridLayout(0, 2);
-		this.setLayout(layout);
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints constraints = new GridBagConstraints();
+		panel.setLayout(layout);
+		
+		// Default settings
+		constraints.gridwidth = 1;
+		constraints.gridheight = 1;
+		constraints.anchor = GridBagConstraints.FIRST_LINE_START; 
 		
 		// Refresh button
 		JButton refresh = new JButton("Refresh");
 		refresh.addActionListener(this::__refreshButton);
 		
-		this.add(new JLabel(""));
-		this.add(refresh);
+		// Add to form
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		panel.add(refresh, constraints);
+		
+		// Revert
+		constraints.gridwidth = 1;
+		constraints.fill = GridBagConstraints.NONE;
 		
 		// Add initial top items
-		this.add(new JLabel("Key"));
-		this.add(new JLabel("Value"));
+		constraints.gridy = 1;
+		constraints.gridx = 0;
+		panel.add(new JLabel("Key"), constraints);
+		constraints.gridx = 2;
+		panel.add(new JLabel("Value"), constraints);
 	}
 	
 	/**
@@ -96,9 +125,22 @@ public abstract class Inspect<I>
 		JLabel key = new JLabel(__desc);
 		InspectKnownValue value = new InspectKnownValue(__value);
 		
+		// Setup constraints
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridwidth = 1;
+		constraints.gridheight = 1;
+		
+		// The Y position is depending on which entry this is
+		synchronized (this)
+		{
+			constraints.gridy = 2 + this._known.size();
+		}
+		
 		// Add both
-		this.add(key);
-		this.add(value);
+		constraints.gridx = 0;
+		this.panel.add(key, constraints);
+		constraints.gridx = 1;
+		this.panel.add(value, constraints);
 		
 		// Perform packing
 		this.pack();
