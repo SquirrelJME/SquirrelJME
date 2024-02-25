f268dd91db999877889be9dc306564d820f242d9
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-02-25 23:35:33 +0000

    Base for EmulatedPacketCommShelf.

---
+++ emulators/emulator-base/src/main/java/cc/squirreljme/emulator/EmulatedPacketCommShelf.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.emulator;
+
+import cc.squirreljme.jvm.mle.PacketCommShelf;
+
+/**
+ * Java SE implementation of {@link PacketCommShelf}.
+ *
+ * @since 2024/02/25
+ */
+public class EmulatedPacketCommShelf
+{
+}
