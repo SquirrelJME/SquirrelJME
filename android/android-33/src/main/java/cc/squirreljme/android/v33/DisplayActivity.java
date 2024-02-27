e81c14edce010d31e9c336089b52b681f6550bd7
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-02-27 19:13:17 +0000

    More base Android work.

---
+++ android/android-33/src/main/java/cc/squirreljme/android/v33/DisplayActivity.java
@@ -0,0 +1,32 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.android.v33;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * An activity for displaying visible graphics.
+ *
+ * @since 2024/02/27
+ */
+public class DisplayActivity
+	extends Activity
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2024/02/27
+	 */
+	@Override
+	protected void onCreate(Bundle __savedInstanceState)
+	{
+		super.onCreate(__savedInstanceState);
+	}
+}
