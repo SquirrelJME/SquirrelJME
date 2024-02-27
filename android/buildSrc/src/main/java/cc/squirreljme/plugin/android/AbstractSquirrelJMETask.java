163a64482ac7cca0cb28d9332b9d41c7eeffe45f
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-02-27 07:50:00 +0000

    More base Android.

---
+++ android/buildSrc/src/main/java/cc/squirreljme/plugin/android/AbstractSquirrelJMETask.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.plugin.android;
+
+/**
+ * Base task for anything which requires a library, file, or otherwise from
+ * the base SquirrelJME project.
+ *
+ * @since 2024/02/27
+ */
+public abstract class AbstractSquirrelJMETask
+{
+}
---
+++ android/buildSrc/src/main/java/cc/squirreljme/plugin/android/package-info.java
@@ -0,0 +1,17 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * SquirrelJME Android Plugin Support, since SquirrelJME is a bit more
+ * advanced and complicated than the standard Gradle project.
+ *
+ * @since 2024/02/27
+ */
+
+package cc.squirreljme.plugin.android;
