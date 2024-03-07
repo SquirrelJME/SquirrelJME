95a119e8860333d8121b22c18f284bcc55e4085a
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-03-07 21:55:41 +0000

    Add emulated ScritchUI layer; Add base classes around ScritchUI framebuffer.

---
+++ modules/scritch-ui-fb/src/main/java/cc/squirreljme/scritchui/fb/DefaultScritchInterface.java
@@ -0,0 +1,75 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.scritchui.fb;
+
+import cc.squirreljme.jvm.mle.scritchui.NativeScritchInterface;
+import cc.squirreljme.jvm.mle.scritchui.ScritchInterface;
+import cc.squirreljme.runtime.cldc.annotation.SquirrelJMEVendorApi;
+
+/**
+ * For returning the default ScritchUI interface.
+ *
+ * @since 2024/03/07
+ */
+@SquirrelJMEVendorApi
+public final class DefaultScritchInterface
+{
+	/**
+	 * {@squirreljme.property cc.squirreljme.scritchui.force=(boolean)
+	 * Should the framebuffer be forced to be used for ScritchUI?}
+	 */
+	@SquirrelJMEVendorApi
+	public static final String FORCE_PROPERTY =
+		"cc.squirreljme.scritchui.force";
+	
+	/** The native instance. */
+	private static volatile ScritchInterface _instance;
+	
+	/**
+	 * Not used.
+	 *
+	 * @since 2024/03/07
+	 */
+	private DefaultScritchInterface()
+	{
+	}
+	
+	/**
+	 * Returns the interface for ScritchUI.
+	 *
+	 * @return The interface for ScritchUI.
+	 * @since 2024/03/07
+	 */
+	@SquirrelJMEVendorApi
+	public static ScritchInterface instance()
+	{
+		// Was this already obtained?
+		ScritchInterface instance = DefaultScritchInterface._instance;
+		if (instance != null)
+			return instance;
+		
+		// Use native interface, if enabled, otherwise framebuffer
+		instance = NativeScritchInterface.nativeInterface();
+		if (instance == null ||
+			Boolean.getBoolean(DefaultScritchInterface.FORCE_PROPERTY))
+		{
+			// Use a default screen provider
+			FramebufferScreensProvider provider =
+				new DefaultFramebufferScreensProvider();
+			
+			// Setup framebuffer instance
+			instance = new FramebufferScritchInterface(provider);
+		}
+		
+		// Cache and return
+		DefaultScritchInterface._instance = instance;
+		return instance;
+	}
+}
--- modules/scritch-ui-fb/src/main/java/cc/squirreljme/scritchui/fb/FramebufferScritchInterface.java
+++ modules/scritch-ui-fb/src/main/java/cc/squirreljme/scritchui/fb/FramebufferScritchInterface.java
@@ -11,6 +11,7 @@ package cc.squirreljme.scritchui.fb;
 
 import cc.squirreljme.jvm.mle.scritchui.ScritchEnvironmentInterface;
 import cc.squirreljme.jvm.mle.scritchui.ScritchInterface;
+import cc.squirreljme.runtime.cldc.annotation.SquirrelJMEVendorApi;
 import cc.squirreljme.runtime.cldc.debug.Debugging;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,14 +20,36 @@ import org.jetbrains.annotations.NotNull;
  *
  * @since 2024/03/07
  */
+@SquirrelJMEVendorApi
 public class FramebufferScritchInterface
 	implements ScritchInterface
 {
+	/** The provider for screens. */
+	protected final FramebufferScreensProvider provider;
+	
+	/**
+	 * Initializes the framebuffer interface.
+	 *
+	 * @param __provider The provider for framebuffer screens.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2024/03/07
+	 */
+	@SquirrelJMEVendorApi
+	public FramebufferScritchInterface(FramebufferScreensProvider __provider)
+		throws NullPointerException
+	{
+		if (__provider == null)
+			throw new NullPointerException("NARG");
+		
+		this.provider = __provider;
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 * @since 2024/03/07
 	 */
 	@Override
+	@SquirrelJMEVendorApi
 	public ScritchEnvironmentInterface environment()
 	{
 		throw Debugging.todo();
