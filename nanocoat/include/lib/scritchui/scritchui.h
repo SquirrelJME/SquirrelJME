d53b73bc92e608622ad83db4310a8081fba6c5bd
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-03-28 00:43:07 +0000

    Add base CMake projects and headers for ScritchUI on NanoCoat.

---
+++ nanocoat/include/lib/scritchui/scritchui.h
@@ -0,0 +1,41 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * ScritchUI Library Header.
+ * 
+ * @since 2024/03/27
+ */
+
+#ifndef SQUIRRELJME_SCRITCHUI_H
+#define SQUIRRELJME_SCRITCHUI_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifndef SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_SQUIRRELJME_SCRITCHUI_H
+extern "C" {
+	#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif     /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifdef SJME_CXX_SQUIRRELJME_SCRITCHUI_H
+}
+		#undef SJME_CXX_SQUIRRELJME_SCRITCHUI_H
+		#undef SJME_CXX_IS_EXTERNED
+	#endif /* #ifdef SJME_CXX_SQUIRRELJME_SCRITCHUI_H */
+#endif     /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_SCRITCHUI_H */
