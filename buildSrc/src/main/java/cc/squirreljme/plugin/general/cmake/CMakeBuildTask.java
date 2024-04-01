a77235678c9e2030d31047ba1cf8ad194405b707
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-04-01 19:06:02 +0000

    Make CMake's output executable optional.

--- buildSrc/src/main/java/cc/squirreljme/plugin/general/cmake/CMakeBuildTask.java
+++ buildSrc/src/main/java/cc/squirreljme/plugin/general/cmake/CMakeBuildTask.java
@@ -28,7 +28,7 @@ public class CMakeBuildTask
 	/** CMake build path, where output goes. */
 	public final Path cmakeBuild;
 	
-	/** The output library we want. */
+	/** The output file we want. */
 	public final Path cmakeOutFile;
 	
 	/** The rule to use. */
@@ -38,7 +38,7 @@ public class CMakeBuildTask
 	 * Initializes the build task.
 	 *
 	 * @param __source The project source directory.
-	 * @param __outputFile The output library we want.
+	 * @param __outputFile The output file we want, this is optional.
 	 * @param __rule The rule to execute.
 	 * @since 2024/03/15
 	 */
@@ -47,7 +47,7 @@ public class CMakeBuildTask
 		String __rule)
 		throws NullPointerException
 	{
-		if (__source == null || __outputFile == null || __rule == null)
+		if (__source == null || __rule == null)
 			throw new NullPointerException("NARG");
 		
 		// Set source for later
@@ -59,8 +59,11 @@ public class CMakeBuildTask
 			.resolve("cmake-" + this.getName());
 		
 		// The desired output we want
-		this.cmakeOutFile = this.cmakeBuild.resolve(
-			Paths.get(__outputFile));
+		if (__outputFile == null)
+			this.cmakeOutFile = null;
+		else
+			this.cmakeOutFile = this.cmakeBuild.resolve(
+				Paths.get(__outputFile));
 		
 		// Description
 		this.setGroup("squirreljme");
@@ -74,7 +77,8 @@ public class CMakeBuildTask
 		this.getInputs().files(
 			this.cmakeSource.resolve("CMakeLists.txt"));
 		this.getOutputs().dirs(this.cmakeBuild);
-		this.getOutputs().files(this.cmakeOutFile);
+		if (__outputFile != null)
+			this.getOutputs().files(this.cmakeOutFile);
 		
 		// What to do for this
 		this.doFirst(new CMakeBuildTaskAction());
