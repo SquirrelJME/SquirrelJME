7ff95f4027a19d737c03a70120b07545f27038ae
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-04-23 00:27:33 +0000

    Add actual check for pthreads to ensure it is valid.

--- nanocoat/cmake/threads.cmake
+++ nanocoat/cmake/threads.cmake
@@ -14,18 +14,29 @@ if(WIN32)
 	add_compile_definitions(SJME_CONFIG_HAS_THREADS_WIN32=1)
 elseif(Threads_FOUND)
 	if(CMAKE_USE_PTHREADS_INIT)
-		add_compile_definitions(
-			SJME_CONFIG_HAS_THREADS=1)
-		add_compile_definitions(
-			SJME_CONFIG_HAS_THREADS_PTHREAD=1)
+		# Does pthread actually exist?
+		try_compile(SQUIRRELJME_PTHREADS_VALID
+			SOURCES "${CMAKE_CURRENT_LIST_DIR}/tryPThread.c")
 
-		# Specific OSes?
-		if (LINUX)
+		# Valid?
+		if(SQUIRRELJME_PTHREADS_VALID)
+			message("PThread: Valid!")
+
 			add_compile_definitions(
-				SJME_CONFIG_HAS_THREADS_PTHREAD_LINUX=1)
-		elseif (APPLE OR BSD)
+				SJME_CONFIG_HAS_THREADS=1)
 			add_compile_definitions(
-				SJME_CONFIG_HAS_THREADS_PTHREAD_BSD=1)
+				SJME_CONFIG_HAS_THREADS_PTHREAD=1)
+
+			# Specific OSes?
+			if (LINUX)
+				add_compile_definitions(
+					SJME_CONFIG_HAS_THREADS_PTHREAD_LINUX=1)
+			elseif (APPLE OR BSD)
+				add_compile_definitions(
+					SJME_CONFIG_HAS_THREADS_PTHREAD_BSD=1)
+			endif()
+		else()
+			message("PThread: Not available or misconfigured.")
 		endif()
 	endif()
 endif()
