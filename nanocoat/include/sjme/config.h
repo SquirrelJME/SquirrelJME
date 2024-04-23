00a376c022b7d767763459fe87954b6ae21b2e7e
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-04-23 07:51:50 +0000

    Add additional define for 3DS.

--- nanocoat/include/sjme/config.h
+++ nanocoat/include/sjme/config.h
@@ -43,7 +43,7 @@ extern "C" {
 #endif
 
 /* The current operating system. */
-#if defined(__3DS__)
+#if defined(__3DS__) || defined(_3DS)
 	/** Nintendo 3DS is available. */
 	#define SJME_CONFIG_HAS_NINTENDO_3DS
 #elif defined(__linux__) || defined(linux) || defined(__linux)
