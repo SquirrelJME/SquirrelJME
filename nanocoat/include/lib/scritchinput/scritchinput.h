33cd2452fcee0aeee9b72d19f40b43ae44debb36
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-05-01 06:04:28 +0000

    Add base for ScritchInput which will help commonize input mechanisms.

---
+++ nanocoat/include/lib/scritchinput/scritchinput.h
@@ -0,0 +1,41 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Scritch Input Library.
+ * 
+ * @since 2024/05/01
+ */
+
+#ifndef SQUIRRELJME_SCRITCHINPUT_H
+#define SQUIRRELJME_SCRITCHINPUT_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifndef SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_SQUIRRELJME_SCRITCHINPUT_H
+extern "C" {
+	#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif     /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifdef SJME_CXX_SQUIRRELJME_SCRITCHINPUT_H
+}
+		#undef SJME_CXX_SQUIRRELJME_SCRITCHINPUT_H
+		#undef SJME_CXX_IS_EXTERNED
+	#endif /* #ifdef SJME_CXX_SQUIRRELJME_SCRITCHINPUT_H */
+#endif     /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_SCRITCHINPUT_H */
