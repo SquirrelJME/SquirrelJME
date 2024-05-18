2928616dab7c09260007c9108551752e9018432e
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-05-18 01:56:47 +0000

    Add blank base for font compiler.

---
+++ tools/squirreljme-font/src/main/java/cc/squirreljme/fontcompile/Main.java
@@ -0,0 +1,29 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.fontcompile;
+
+/**
+ * Main entry point for the font compiler.
+ *
+ * @since 2024/05/17
+ */
+public class Main
+{
+	/**
+	 * Main entry point.
+	 *
+	 * @param __args Input arguments.
+	 * @since 2024/05/17
+	 */
+	public static void main(String... __args)
+	{
+		throw Debugging.todo();
+	}
+}
