2c7c07a023a6dd025953bc6328703cb07a5b8423
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-06-16 17:43:14 +0000

    Add checks to see if JNI/JAWT are valid to be linked against.

---
+++ nanocoat/cmake/tryJawt.c
@@ -0,0 +1,16 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include <jni.h>
+#include <jawt_md.h>
+
+int main(int argc, char** argv)
+{
+	return JAWT_GetAWT(NULL, NULL);
+}
