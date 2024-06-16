9ed9230fee4e7908b7d9305bd02afeec6fcc0341
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-06-16 16:58:26 +0000

    Add full stubs for JNI/JAWT.

--- nanocoat/cmake/utils/stub_jawt.c
+++ nanocoat/cmake/utils/stub_jawt.c
@@ -6,3 +6,14 @@
 // SquirrelJME is under the Mozilla Public License Version 2.0.
 // See license.mkd for licensing and copyright information.
 // -------------------------------------------------------------------------*/
+
+#include <stdlib.h>
+
+#include "../../include/3rdparty/jni/jni.h"
+#include "../../include/3rdparty/jni/jawt_md.h"
+
+JNIEXPORT jboolean JNICALL JAWT_GetAWT(JNIEnv* env, JAWT* awt)
+{
+	abort();
+	return JNI_FALSE;
+}
--- nanocoat/cmake/utils/stub_jvm.c
+++ nanocoat/cmake/utils/stub_jvm.c
@@ -6,3 +6,27 @@
 // SquirrelJME is under the Mozilla Public License Version 2.0.
 // See license.mkd for licensing and copyright information.
 // -------------------------------------------------------------------------*/
+
+#include <stdlib.h>
+
+#include "../../include/3rdparty/jni/jni.h"
+
+JNIEXPORT jint JNICALL JNI_GetDefaultJavaVMInitArgs(void* args)
+{
+	abort();
+	return -1;
+}
+
+JNIEXPORT jint JNICALL JNI_CreateJavaVM(JavaVM** pvm, void** penv,
+	void* args)
+{
+	abort();
+	return -1;
+}
+
+JNIEXPORT jint JNICALL JNI_GetCreatedJavaVMs(JavaVM** pvm,
+	jsize a, jsize* b)
+{
+	abort();
+	return -1;
+}
