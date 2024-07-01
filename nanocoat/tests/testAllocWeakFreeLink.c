b3d718fa0589e78a9da55bd86ecc350a1313cfe3
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-07-01 13:14:58 +0000

    Add base blank tests for weak references.

---
+++ nanocoat/tests/testAllocWeakFreeLink.c
@@ -0,0 +1,24 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include "test.h"
+#include "proto.h"
+#include "mock.h"
+#include "unit.h"
+
+/**
+ * Tests freeing a link and any changes to the resulting weak link.
+ *  
+ * @since 2024/07/01 
+ */
+SJME_TEST_DECLARE(testAllocWeakFreeLink)
+{
+	sjme_todo("Implement %s", __func__);
+	return SJME_TEST_RESULT_FAIL;
+}
---
+++ nanocoat/tests/testAllocWeakRef.c
@@ -0,0 +1,24 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include "test.h"
+#include "proto.h"
+#include "mock.h"
+#include "unit.h"
+
+/**
+ * Tests general creation of a weak reference.
+ *  
+ * @since 2024/07/01 
+ */
+SJME_TEST_DECLARE(testAllocWeakRef)
+{
+	sjme_todo("Implement %s", __func__);
+	return SJME_TEST_RESULT_FAIL;
+}
