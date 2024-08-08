3783087d598785ed0486240a70bd954fd3d7656c
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-08-08 16:16:40 +0000

    Add base main entry point for stdc.

---
+++ nanocoat/frontend/stdc/stdcMain.c
@@ -0,0 +1,25 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include <stdlib.h>
+
+#include "sjme/debug.h"
+
+/**
+ * Main program entry point.
+ * 
+ * @param argc Argument count. 
+ * @param argv Arguments passed.
+ * @return Returns @c 0 on success, otherwise another exit code.
+ */
+int main(int argc, char** argv)
+{
+	sjme_todo("Impl?");
+	return EXIT_FAILURE;
+}
