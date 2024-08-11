74f1c1eb253da7270d8bbfdb7cfb176a8627e369
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-08-11 23:19:04 +0000

    Add initial AffineTrans class.

---
+++ modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/opt/ui/j3d/AffineTrans.java
@@ -0,0 +1,18 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package com.nttdocomo.opt.ui.j3d;
+
+import cc.squirreljme.runtime.cldc.annotation.Api;
+
+@Api
+public class AffineTrans
+	extends com.jblend.graphics.j3d.AffineTrans
+{
+}
---
+++ modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/opt/ui/j3d/package-info.java
@@ -0,0 +1,16 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * DoJa Java 3D Classes.
+ *
+ * @since 2024/08/11
+ */
+
+package com.nttdocomo.ui.j3d;
