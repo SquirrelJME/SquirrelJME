c678d75d9fb3806c2a68cc411fac1585f63b22ae
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-08-12 21:12:52 +0000

    Implement get and store of key states, needed for LoN.

--- modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/ui/Display.java
+++ modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/ui/Display.java
@@ -24,73 +24,181 @@ import javax.microedition.lcdui.Canvas;
 public class Display
 {
 	@Api
-	public static final int KEY_0 = 0x00;
+	public static final int KEY_0 =
+		0;
 	
 	@Api
-	public static final int KEY_1 = 0x01;
+	public static final int KEY_1 =
+		1;
 	
 	@Api
-	public static final int KEY_2 = 0x02;
+	public static final int KEY_2 =
+		2;
 	
 	@Api
-	public static final int KEY_3 = 0x03;
+	public static final int KEY_3 =
+		3;
 	
 	@Api
-	public static final int KEY_4 = 0x04;
+	public static final int KEY_4 =
+		4;
 	
 	@Api
-	public static final int KEY_5 = 0x05;
+	public static final int KEY_5 =
+		5;
 	
 	@Api
-	public static final int KEY_6 = 0x06;
+	public static final int KEY_6 =
+		6;
 	
 	@Api
-	public static final int KEY_7 = 0x07;
+	public static final int KEY_7 =
+		7;
 	
 	@Api
-	public static final int KEY_8 = 0x08;
+	public static final int KEY_8 =
+		8;
 	
 	@Api
-	public static final int KEY_9 = 0x09;
+	public static final int KEY_9 =
+		9;
 	
 	@Api
-	public static final int KEY_ASTERISK = 0x0A;
+	public static final int KEY_ASTERISK =
+		10;
 	
 	@Api
-	public static final int KEY_DOWN = 0x13;
+	public static final int KEY_CAMERA_SELECT =
+		59;
 	
 	@Api
-	public static final int KEY_LEFT = 0x10;
+	public static final int KEY_CAMERA_ZOOM_IN =
+		57;
 	
 	@Api
-	public static final int KEY_POUND = 0x0B;
+	public static final int KEY_CAMERA_ZOOM_OUT =
+		58;
 	
 	@Api
-	public static final int KEY_PRESSED_EVENT = 0;
+	public static final int KEY_CLEAR =
+		32;
 	
 	@Api
-	public static final int KEY_RELEASED_EVENT = 1;
+	public static final int KEY_DOWN =
+		19;
 	
 	@Api
-	public static final int KEY_RIGHT = 0x12;
+	public static final int KEY_GPS =
+		42;
 	
 	@Api
-	public static final int KEY_SELECT = 0x14;
+	public static final int KEY_IAPP =
+		24;
 	
 	@Api
-	public static final int KEY_SOFT1 = 0x15;
+	public static final int KEY_LEFT =
+		16;
 	
 	@Api
-	public static final int KEY_SOFT2 = 0x16;
+	public static final int KEY_LOWER_LEFT =
+		29;
 	
 	@Api
-	public static final int KEY_UP = 0x11;
+	public static final int KEY_LOWER_RIGHT =
+		28;
 	
 	@Api
-	public static final int MAX_VENDOR_EVENT = 127;
+	public static final int KEY_MAIL =
+		33;
 	
 	@Api
-	public static final int MAX_VENDOR_KEY = 0x1F;
+	public static final int KEY_MEMO =
+		34;
+	
+	@Api
+	public static final int KEY_MY_SELECT =
+		53;
+	
+	@Api
+	public static final int KEY_PAGE_DOWN =
+		31;
+	
+	@Api
+	public static final int KEY_PAGE_UP =
+		30;
+	
+	@Api
+	public static final int KEY_POUND =
+		11;
+	
+	@Api
+	public static final int KEY_PRESSED_EVENT =
+		0;
+	
+	@Api
+	public static final int KEY_RELEASED_EVENT =
+		1;
+	
+	@Api
+	public static final int KEY_RIGHT =
+		18;
+	
+	@Api
+	public static final int KEY_ROLL_LEFT =
+		48;
+	
+	@Api
+	public static final int KEY_ROLL_RIGHT =
+		49;
+	
+	@Api
+	public static final int KEY_SELECT =
+		20;
+	
+	@Api
+	public static final int KEY_SOFT1 =
+		21;
+	
+	@Api
+	public static final int KEY_SOFT2 =
+		22;
+	
+	@Api
+	public static final int KEY_SUB1 =
+		50;
+	
+	@Api
+	public static final int KEY_SUB2 =
+		51;
+	
+	@Api
+	public static final int KEY_SUB3 =
+		52;
+	
+	@Api
+	public static final int KEY_UP =
+		17;
+	
+	@Api
+	public static final int KEY_UPPER_LEFT =
+		26;
+	
+	@Api
+	public static final int KEY_UPPER_RIGHT =
+		27;
+	
+	@Api
+	public static final int KEY_VOICE =
+		39;
+	
+	@Api
+	protected static final int MAX_OPTION_KEY = 63;
+	
+	@Api
+	protected static final int MAX_VENDOR_EVENT = 127;
+	
+	@Api
+	protected static final int MAX_VENDOR_KEY = 0x1F;
 	
 	@Api
 	public static final int MEDIA_EVENT = 8;
