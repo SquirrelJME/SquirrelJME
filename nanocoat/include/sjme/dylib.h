0ffe3391e8a963e6c6b7c178dd4f9d76a72dd5b6
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-08-17 18:57:42 +0000

    Add sjme_dylib_open() to get the current executable as a dylib.

--- nanocoat/include/sjme/dylib.h
+++ nanocoat/include/sjme/dylib.h
@@ -105,6 +105,16 @@ sjme_errorCode sjme_dylib_open(
 	sjme_attrInNotNull sjme_lpcstr libPath,
 	sjme_attrInOutNotNull sjme_dylib* outLib);
 
+/**
+ * Returns the current executable as a dynamic library.
+ * 
+ * @param outLib The resultant library which points to ourself.
+ * @return Any resultant error, if any.
+ * @since 2024/08/17
+ */
+sjme_errorCode sjme_dylib_self(
+	sjme_attrInOutNotNull sjme_dylib* outLib);
+
 /*--------------------------------------------------------------------------*/
 
 /* Anti-C++. */
