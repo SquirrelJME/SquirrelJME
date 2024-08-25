56daf32c845bbcdc6bec7439dd2f766d9d5999e0
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-08-25 16:53:08 +0000

    Add blank base for circle buffer, the inflate buffer will be moved to it.

---
+++ nanocoat/include/sjme/circleBuffer.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Circle buffer, also known as a round robin.
+ * 
+ * @since 2024/08/25
+ */
+
+#ifndef SQUIRRELJME_CIRCLEBUFFER_H
+#define SQUIRRELJME_CIRCLEBUFFER_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifndef SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_SQUIRRELJME_CIRCLEBUFFER_H
+extern "C"
+{
+	#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifdef SJME_CXX_SQUIRRELJME_CIRCLEBUFFER_H
+}
+		#undef SJME_CXX_SQUIRRELJME_CIRCLEBUFFER_H
+		#undef SJME_CXX_IS_EXTERNED
+	#endif /* #ifdef SJME_CXX_SQUIRRELJME_CIRCLEBUFFER_H */
+#endif /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_CIRCLEBUFFER_H */
