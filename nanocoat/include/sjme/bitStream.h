2f4bb2086cda00a083e67b2872761d1c272689e3
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-08-25 16:55:04 +0000

    Add blank base for bit streams, inflate bit related streams will go there as well and be generic.

---
+++ nanocoat/include/sjme/bitStream.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Bit streams.
+ * 
+ * @since 2024/08/25
+ */
+
+#ifndef SQUIRRELJME_BITSTREAM_H
+#define SQUIRRELJME_BITSTREAM_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifndef SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_IS_EXTERNED
+		#define SJME_CXX_SQUIRRELJME_BITSTREAM_H
+extern "C"
+{
+	#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+	#ifdef SJME_CXX_SQUIRRELJME_BITSTREAM_H
+}
+		#undef SJME_CXX_SQUIRRELJME_BITSTREAM_H
+		#undef SJME_CXX_IS_EXTERNED
+	#endif /* #ifdef SJME_CXX_SQUIRRELJME_BITSTREAM_H */
+#endif /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_BITSTREAM_H */
