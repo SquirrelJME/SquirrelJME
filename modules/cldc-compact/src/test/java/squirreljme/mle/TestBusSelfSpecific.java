fc32445a16318e2a6b0b4c82e7d7867b0fa4294d
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-11-02 21:13:59 +0000

    Add tests which should be included as part of the todobus.

---
+++ modules/cldc-compact/src/test/java/squirreljme/mle/TestBusSelfSpecific.java
@@ -0,0 +1,32 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package squirreljme.mle;
+
+import net.multiphasicapps.tac.TestRunnable;
+
+/**
+ * Not Described.
+ *
+ * @since 2024/10/15
+ */
+public class TestBusSelfSpecific
+	extends TestRunnable
+{
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @since 2024/10/15
+	 */
+	@Override
+	public void test()
+	{
+		this.secondary("key", "value");
+	}
+}
