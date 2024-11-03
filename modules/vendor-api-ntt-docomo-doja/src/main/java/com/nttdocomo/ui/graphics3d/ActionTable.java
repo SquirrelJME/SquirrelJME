a13dcf8a40afa6e45d004f158f4f9cebead65704
Stephanie Gawroriski <xer@multiphasicapps.net>
2024-11-03 20:35:52 +0000

    Add blank classes for DoJa graphics3d.

---
+++ modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/ui/graphics3d/ActionTable.java
@@ -0,0 +1,17 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package com.nttdocomo.ui.graphics3d;
+
+import cc.squirreljme.runtime.cldc.annotation.SquirrelJMEVendorApi;
+
+@SquirrelJMEVendorApi
+public class ActionTable
+{
+}
---
+++ modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/ui/graphics3d/package-info.java
@@ -0,0 +1,17 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+/**
+ * Additional 3D Graphics APIs.
+ *
+ * @since 2024/11/03
+ */
+
+@cc.squirreljme.runtime.cldc.annotation.SquirrelJMEVendorApi
+package com.nttdocomo.ui.graphics3d;
