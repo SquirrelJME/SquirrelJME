f559db5166268fd40a5c9d73ffa82d5842b40d3b
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-01-24 00:55:26 +0000

    Use code from trunk.

--- modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/ui/Graphics.java
+++ modules/vendor-api-ntt-docomo-doja/src/main/java/com/nttdocomo/ui/Graphics.java
@@ -266,91 +266,17 @@ public class Graphics
 	{
 		if (__i == null)
 			throw new NullPointerException("NARG");
-		if (__w < 0 || __h < 0)
-			throw new IllegalArgumentException("ILLA");
 		
-		// Which image is being drawn?
-		javax.microedition.lcdui.Image target;
-		if (__i instanceof __MIDPImage__)
-			target = ((__MIDPImage__)__i).__midpImage();
-		
-		// Mutable image
-		else if (__i instanceof __MutableImage__)
-			target = __i._midpImage;
-		
-		// 8-bit image
-		else if (__i instanceof __8BitImage__)
-		{
-			// Get the actual image to be drawn
-			__8BitImage__ bitImage = (__8BitImage__)__i;
-			EightBitImageStore store = bitImage._store;
-			
-			// Disposed?
-			if (store == null)
-				throw new UIException(UIException.ILLEGAL_STATE);
-			
-			// Get realized image
-			target = store.midpImage();
-		}
-		
-		// Not supported at all
-		else
-		{
-			// Debug
-			Debugging.todoNote("Unsupported image %s", __i.getClass());
-			
-			throw new UIException(UIException.UNSUPPORTED_FORMAT);
-		}
-		
-		// Which flip mode
-		int trans;
-		switch (this._flipMode)
-		{
-			case Graphics.FLIP_HORIZONTAL:
-				trans = Sprite.TRANS_MIRROR;
-				break;
-			
-			case Graphics.FLIP_ROTATE:
-				trans = Sprite.TRANS_ROT180;
-				break;
-			
-			case Graphics.FLIP_ROTATE_LEFT:
-				trans = Sprite.TRANS_ROT270;
-				break;
-			
-			case Graphics.FLIP_ROTATE_RIGHT:
-				trans = Sprite.TRANS_ROT90;
-				break;
-			
-			case Graphics.FLIP_ROTATE_RIGHT_HORIZONTAL:
-				trans = Sprite.TRANS_MIRROR_ROT270;
-				break;
-			
-			case Graphics.FLIP_ROTATE_RIGHT_VERTICAL:
-				trans = Sprite.TRANS_MIRROR_ROT90;
-				break;
-				
-			case Graphics.FLIP_VERTICAL:
-				trans = Sprite.TRANS_MIRROR_ROT180;
-				break;
-				
-			case Graphics.FLIP_NONE:
-			default:
-				trans = Sprite.TRANS_NONE;
-				break;
-		}
-		
-		// Draw it
-		this._graphics.drawRegion(target, __sx, __sy,
-			__w, __h, trans, __dx, __dy,
-			javax.microedition.lcdui.Graphics.TOP |
-			javax.microedition.lcdui.Graphics.LEFT);
+		// Forward to other call, as it simplifies the shared logic
+		this.drawScaledImage(__i,
+			__dx, __dy, __w, __h,
+			__sx, __sy, __w, __h);
 	}
 	
 	@Api
 	public void drawLine(int __x1, int __y1, int __x2, int __y2)
 	{
-		throw Debugging.todo();
+		this._graphics.drawLine(__x1, __y1, __x2, __y2);
 	}
 	
 	@Api
@@ -373,6 +299,62 @@ public class Graphics
 			return;
 		
 		this._graphics.drawRect(__x, __y, __w, __h);
+	}
+	
+	@Api
+	public void drawScaledImage(Image __i, int __dx, int __dy,
+		int __dw, int __dh, int __sx, int __sy, int __sw, int __sh)
+		throws IllegalArgumentException, UIException, NullPointerException
+	{
+		if (__i == null)
+			throw new NullPointerException("NARG");
+		if (__dw < 0 || __dh < 0 || __sw < 0 || __sh < 0)
+			throw new IllegalArgumentException("ILLA");
+		
+		// Which image is being drawn?
+		javax.microedition.lcdui.Image target;
+		target = Graphics.__recoverImage(__i);
+		
+		// Which flip mode
+		int trans = this.__mapFlip();
+		
+		// DoJa is more lenient when drawing out of range graphics, it just
+		// gets clipped into range
+		if (__sx < 0)
+		{
+			// Note sx is negative, so we subtract width
+			__sw += __sx;
+			__sx = 0;
+		}
+		
+		if (__sy < 0)
+		{
+			// Note sy is negative, so we subtract height
+			__sh += __sy;
+			__sy = 0;
+		}
+		
+		int ex = __sx + __sw;
+		int ey = __sy + __sh;
+		if (ex > target.getWidth())
+			ex = target.getWidth();
+		if (ey > target.getHeight())
+			ey = target.getHeight();
+		
+		// Get corrected size
+		__sw = ex - __sx;
+		__sh = ey - __sy;
+		
+		// Not drawing anything after correcting?
+		if (__sw <= 0 || __sh <= 0)
+			return;
+		
+		// Draw it
+		this._graphics.drawRegion(target, __sx, __sy,
+			__sw, __sh, trans, __dx, __dy,
+			javax.microedition.lcdui.Graphics.TOP |
+			javax.microedition.lcdui.Graphics.LEFT,
+			__dw, __dh);
 	}
 	
 	@Api
@@ -554,6 +536,43 @@ public class Graphics
 			lockFlush.__unlock(__forced);
 	}
 	
+	/**
+	 * Maps the flip mode.
+	 *
+	 * @return The flip mode.
+	 * @since 2024/08/13
+	 */
+	private int __mapFlip()
+	{
+		switch (this._flipMode)
+		{
+			case Graphics.FLIP_HORIZONTAL:
+				return Sprite.TRANS_MIRROR;
+			
+			case Graphics.FLIP_ROTATE:
+				return Sprite.TRANS_ROT180;
+			
+			case Graphics.FLIP_ROTATE_LEFT:
+				return Sprite.TRANS_ROT270;
+			
+			case Graphics.FLIP_ROTATE_RIGHT:
+				return Sprite.TRANS_ROT90;
+			
+			case Graphics.FLIP_ROTATE_RIGHT_HORIZONTAL:
+				return Sprite.TRANS_MIRROR_ROT270;
+			
+			case Graphics.FLIP_ROTATE_RIGHT_VERTICAL:
+				return Sprite.TRANS_MIRROR_ROT90;
+				
+			case Graphics.FLIP_VERTICAL:
+				return Sprite.TRANS_MIRROR_ROT180;
+				
+			case Graphics.FLIP_NONE:
+			default:
+				return Sprite.TRANS_NONE;
+		}
+	}
+	
 	@SuppressWarnings("MagicNumber")
 	@Api
 	public static int getColorOfName(int __name)
@@ -631,5 +650,50 @@ public class Graphics
 			(__r << 16) |
 			(__g << 8) |
 			__b;
+	}
+	
+	
+	/**
+	 * Recovers the image to draw.
+	 *
+	 * @param __i The source image.
+	 * @return The resultant image.
+	 * @throws UIException If the image has been disposed of or is otherwise
+	 * invalid.
+	 * @since 2024/08/13
+	 */
+	private static javax.microedition.lcdui.Image __recoverImage(Image __i)
+		throws UIException
+	{
+		if (__i instanceof __MIDPImage__)
+			return ((__MIDPImage__)__i).__midpImage();
+		
+		// Mutable image
+		else if (__i instanceof __MutableImage__)
+			return __i._midpImage;
+		
+		// 8-bit image
+		else if (__i instanceof __8BitImage__)
+		{
+			// Get the actual image to be drawn
+			__8BitImage__ bitImage = (__8BitImage__)__i;
+			EightBitImageStore store = bitImage._store;
+			
+			// Disposed?
+			if (store == null)
+				throw new UIException(UIException.ILLEGAL_STATE);
+			
+			// Get realized image
+			return store.midpImage();
+		}
+		
+		// Not supported at all
+		else
+		{
+			// Debug
+			Debugging.todoNote("Unsupported image %s", __i.getClass());
+			
+			throw new UIException(UIException.UNSUPPORTED_FORMAT);
+		}
 	}
 }
