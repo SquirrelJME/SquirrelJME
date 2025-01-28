a310570e8bad0c149133aa01983e62bf1aa1ad25
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-01-28 22:08:18 +0000

    Base blank file for stackmap.

---
+++ nanocoat/include/sjme/nvm/stackMap.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Functions and support for parsing @c StackMap and @c StackMapTable within.
+ * 
+ * @since 2025/01/28
+ */
+
+#ifndef SQUIRRELJME_STACKMAP_H
+#define SQUIRRELJME_STACKMAP_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_CXX_STACKMAP_H
+extern "C"
+{
+#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_CXX_STACKMAP_H
+}
+#undef SJME_CXX_STACKMAP_H
+#undef SJME_CXX_IS_EXTERNED
+#endif /* #ifdef SJME_CXX_STACKMAP_H */
+#endif /* #ifdef __cplusplus */
+
+#endif /* SQUIRRELJME_STACKMAP_H */
