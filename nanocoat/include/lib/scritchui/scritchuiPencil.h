f4f408331c5cbca3cb23c7ec5bf5ffb676d670dd
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-02-08 20:22:48 +0000

    Add specific note for SJME_ERROR_RESOURCE_NOT_FOUND.

--- nanocoat/include/lib/scritchui/scritchuiPencil.h
+++ nanocoat/include/lib/scritchui/scritchuiPencil.h
@@ -579,7 +579,9 @@ typedef sjme_errorCode (*sjme_scritchui_pencilFillTriangleFunc)(
  * Locks the pencil for drawing.
  * 
  * @param g The pencil to lock.
- * @return Any resultant error, if any.
+ * @return Any resultant error, if any. Return the specific
+ * error code @c SJME_ERROR_RESOURCE_NOT_FOUND if the resource backing the
+ * pencil is no longer available.
  * @since 2024/07/08
  */
 typedef sjme_errorCode (*sjme_scritchui_pencilLockFunc)(
