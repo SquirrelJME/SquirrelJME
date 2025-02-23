1d02b41e5f34892cde18d477e776bc70929052a8
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-02-23 07:54:02 +0000

    Add blank sources for all of the shelves.

---
+++ nanocoat/src/mlePencilFont.c
@@ -0,0 +1,12 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include "sjme/config.h"
+#include "sjme/nvm/mle.h"
+#include "sjme/nvm/mleShelves.h"
