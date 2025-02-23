ba51e1d3f6ea3c791874ee4955d732bf529d5f2c
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-02-23 04:21:59 +0000

    Implement the integer comparison branches.

--- nanocoat/src/bytecodeSlowFlow.c
+++ nanocoat/src/bytecodeSlowFlow.c
@@ -12,7 +12,63 @@
 #include "sjme/nvm/bytecodeSlow.h"
 #include "sjme/nvm/task.h"
 
-SJME_NVM_BYTECODE_SLOW(IfEq)
+typedef sjme_jboolean (*sjme_nvm_byteCode_compareFunc)(
+	sjme_attrInValue sjme_jint a,
+	sjme_attrInValue sjme_jint b);
+
+sjme_jboolean sjme_nvm_byteCode_compareEq(
+	sjme_attrInValue sjme_jint a,
+	sjme_attrInValue sjme_jint b)
+{
+	return a == b;
+}
+
+sjme_jboolean sjme_nvm_byteCode_compareGe(
+	sjme_attrInValue sjme_jint a,
+	sjme_attrInValue sjme_jint b)
+{
+	return a >= b;
+}
+
+sjme_jboolean sjme_nvm_byteCode_compareGt(
+	sjme_attrInValue sjme_jint a,
+	sjme_attrInValue sjme_jint b)
+{
+	return a > b;
+}
+
+sjme_jboolean sjme_nvm_byteCode_compareLe(
+	sjme_attrInValue sjme_jint a,
+	sjme_attrInValue sjme_jint b)
+{
+	return a <= b;
+}
+
+sjme_jboolean sjme_nvm_byteCode_compareLt(
+	sjme_attrInValue sjme_jint a,
+	sjme_attrInValue sjme_jint b)
+{
+	return a < b;
+}
+
+sjme_jboolean sjme_nvm_byteCode_compareNe(
+	sjme_attrInValue sjme_jint a,
+	sjme_attrInValue sjme_jint b)
+{
+	return a != b;
+}
+
+static const sjme_nvm_byteCode_compareFunc sjme_nvm_byteCode_compares[6] =
+{
+	sjme_nvm_byteCode_compareEq,
+	sjme_nvm_byteCode_compareNe,
+	sjme_nvm_byteCode_compareLt,
+	sjme_nvm_byteCode_compareGe,
+	sjme_nvm_byteCode_compareGt,
+	sjme_nvm_byteCode_compareLe,
+};
+
+SJME_NVM_BYTECODE_SLOW(IfX)
 {
 	sjme_jint offset;
 	sjme_jvalueTyped value;
@@ -28,7 +84,37 @@ SJME_NVM_BYTECODE_SLOW(IfEq)
 		return sjme_error_vmError(inFrame, error);
 
 	/* Successful branch? */
-	if (value.value.i == 0)
+	if (sjme_nvm_byteCode_compares[id - 153](value.value.i, 0))
+		pcNew->adjust = offset;
+
+	/* Failed branch. */
+	else
+		pcNew->adjust = 3;
+
+	SJME_NVM_BYTECODE_SLOW_EXIT;
+}
+
+SJME_NVM_BYTECODE_SLOW(IfICmpX)
+{
+	sjme_jint offset;
+	sjme_jvalueTyped a, b;
+	SJME_NVM_BYTECODE_SLOW_ENTRY;
+	
+	/* Read the branch value. */
+	offset = sjme_big_short(*sjme_util_memUnaligned16(&relRawCode[1]));
+	
+	/* Pop both integer values. */
+	memset(&b, 0, sizeof(b));
+	memset(&a, 0, sizeof(a));
+	if (sjme_error_is(error = sjme_nvm_task_frameStackPop(inFrame,
+		SJME_JAVA_TYPE_ID_INTEGER, &b)))
+		return sjme_error_vmError(inFrame, error);
+	if (sjme_error_is(error = sjme_nvm_task_frameStackPop(inFrame,
+		SJME_JAVA_TYPE_ID_INTEGER, &a)))
+		return sjme_error_vmError(inFrame, error);
+
+	/* Successful branch? */
+	if (sjme_nvm_byteCode_compares[id - 159](a.value.i, b.value.i))
 		pcNew->adjust = offset;
 
 	/* Failed branch. */
