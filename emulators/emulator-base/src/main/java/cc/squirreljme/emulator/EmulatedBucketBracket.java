77ce2227871bbd941abc2d8f31a3313a1d2d251e
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-04-19 21:54:57 +0000

    Add base for emulated bucket brackets.

---
+++ emulators/emulator-base/src/main/java/cc/squirreljme/emulator/EmulatedBucketBracket.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.emulator;
+
+import cc.squirreljme.jvm.mle.brackets.BucketBracket;
+
+/**
+ * Emulated buckets.
+ *
+ * @since 2025/04/19
+ */
+public final class EmulatedBucketBracket
+	implements BucketBracket
+{
+}
