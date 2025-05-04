8940de651066c8c8d5d1de41524c297476c07217
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-05-04 17:22:34 +0000

    Add blank base for AudioStreamShelf.

---
+++ modules/cldc-compact/src/main/java/cc/squirreljme/jvm/mle/AudioStreamShelf.java
@@ -0,0 +1,30 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.jvm.mle;
+
+import cc.squirreljme.runtime.cldc.annotation.SquirrelJMEVendorApi;
+
+/**
+ * This shelf provides access and callbacks for audio streaming.
+ *
+ * @since 2025/05/04
+ */
+@SquirrelJMEVendorApi
+public final class AudioStreamShelf
+{
+	/**
+	 * Not used. 
+	 *
+	 * @since 2025/05/04
+	 */
+	private AudioStreamShelf()
+	{
+	}
+}
