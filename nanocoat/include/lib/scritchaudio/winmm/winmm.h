c233983707e0556d36ab9e232e01910741fc4e0d
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-06-03 23:41:17 +0000

    Add blank base for WinMM.

---
+++ nanocoat/include/lib/scritchaudio/winmm/winmm.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+/**
+ * Describe this.
+ * 
+ * @since 2025/06/03
+ */
+
+#ifndef SJME_C_WINMM_H
+#define SJME_C_WINMM_H
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifndef SJME_CXX_IS_EXTERNED
+#define SJME_CXX_IS_EXTERNED
+#define SJME_CXX_WINMM_H
+extern "C"
+{
+#endif /* #ifdef SJME_CXX_IS_EXTERNED */
+#endif /* #ifdef __cplusplus */
+
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+
+/* Anti-C++. */
+#ifdef __cplusplus
+#ifdef SJME_CXX_WINMM_H
+}
+#undef SJME_CXX_WINMM_H
+#undef SJME_CXX_IS_EXTERNED
+#endif /* #ifdef SJME_CXX_WINMM_H */
+#endif /* #ifdef __cplusplus */
+
+#endif /* SJME_C_WINMM_H */
