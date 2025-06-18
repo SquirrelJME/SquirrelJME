8f3f5a6fd09ff8ecea3f59962df913aa6834bec2
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-06-18 21:27:26 +0000

    Base entry for math.

--- nanocoat/src/bytecodeSlowMath.c
+++ nanocoat/src/bytecodeSlowMath.c
@@ -8,3 +8,64 @@
 // -------------------------------------------------------------------------*/
 
 #include "sjme/nvm/bytecode.h"
+#include "sjme/nvm/bytecodeSlow.h"
+
+SJME_NVM_BYTECODE_SLOW(MathBinaryInt)
+{
+	SJME_NVM_BYTECODE_SLOW_ENTRY;
+
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+	
+	SJME_NVM_BYTECODE_SLOW_EXIT;
+}
+
+SJME_NVM_BYTECODE_SLOW(MathBinaryLong)
+{
+	SJME_NVM_BYTECODE_SLOW_ENTRY;
+
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+	
+	SJME_NVM_BYTECODE_SLOW_EXIT;
+}
+
+SJME_NVM_BYTECODE_SLOW(MathDouble)
+{
+	SJME_NVM_BYTECODE_SLOW_ENTRY;
+
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+	
+	SJME_NVM_BYTECODE_SLOW_EXIT;
+}
+
+SJME_NVM_BYTECODE_SLOW(MathFloat)
+{
+	SJME_NVM_BYTECODE_SLOW_ENTRY;
+
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+	
+	SJME_NVM_BYTECODE_SLOW_EXIT;
+}
+
+SJME_NVM_BYTECODE_SLOW(MathInt)
+{
+	SJME_NVM_BYTECODE_SLOW_ENTRY;
+
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+	
+	SJME_NVM_BYTECODE_SLOW_EXIT;
+}
+
+SJME_NVM_BYTECODE_SLOW(MathLong)
+{
+	SJME_NVM_BYTECODE_SLOW_ENTRY;
+
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+	
+	SJME_NVM_BYTECODE_SLOW_EXIT;
+}
