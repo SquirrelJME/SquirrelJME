aed1f219d95a17e9e0796e624f1e5a8d6b934376
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-07-21 21:42:56 +0000

    Add blanks for stack cached methods.

--- nanocoat/src/taskCache.c
+++ nanocoat/src/taskCache.c
@@ -8,3 +8,89 @@
 // -------------------------------------------------------------------------*/
 
 #include "sjme/nvm/taskCache.h"
+
+sjme_errorCode sjme_nvm_cache_opCopy(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrInOutNotNull sjme_nvm_cache_slot** opIn,
+	sjme_attrInOutNotNull sjme_nvm_cache_slot** opOut)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opDelete(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrInOutNotNull sjme_nvm_cache_slot** opInOut)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opDeleteFlood(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrInNotNull sjme_nvm_cache_flood* opInOut)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opEvict(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrInOutNotNull sjme_nvm_cache_slot** opInOut)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opEvictFlood(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrInOutNotNull sjme_nvm_cache_flood** opInOut)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opIsoLocal(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrOutNotNull sjme_nvm_cache_slot** opOut,
+	sjme_attrInRange(0, INT32_MAX) sjme_jint localIndex,
+	sjme_attrInNotNull sjme_jvalueTyped* inValue)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opIsoStackPush(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrOutNotNull sjme_nvm_cache_slot** opOut,
+	sjme_attrInNotNull sjme_jvalueTyped* inValue)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opLocalPush(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrOutNullable sjme_nvm_cache_slot** opIn,
+	sjme_attrOutNullable sjme_nvm_cache_slot** opOut,
+	sjme_attrInRange(0, INT32_MAX) sjme_jint localIndex)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opStackPeek(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrInOutNotNull sjme_nvm_cache_flood* opInOut)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
+
+sjme_errorCode sjme_nvm_cache_opStackPop(
+	sjme_attrInNotNull sjme_nvm_frame inFrame,
+	sjme_attrInOutNotNull sjme_nvm_cache_flood* opInOut)
+{
+	sjme_todo("Impl?");
+	return sjme_error_notImplemented(0);
+}
