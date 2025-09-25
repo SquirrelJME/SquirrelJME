aa25bb27bbd3a3393feb1553c34bea7e03a164a9
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-09-25 00:30:28 +0000

    Add missing enum; Add detection for NanoTests in CMake, currently call into a stub.

---
+++ nanocoat/tests/src/nano.c
@@ -0,0 +1,23 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include <string.h>
+
+#include "sjme/nvm/boot.h"
+#include "sjme/nvm/nvm.h"
+#include "sjme/nvm/nvmFunc.h"
+#include "sjme/nvm/payload.h"
+#include "sjme/nvm/loop.h"
+#include "test.h"
+
+int main(int argc, sjme_lpstr* argv)
+{
+	sjme_todo("Impl?");
+	return EXIT_FAILURE;
+}
