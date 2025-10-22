6d0090970e5697b7255f1c9d80132c68e6ca8b7d
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-10-22 02:39:04 +0000

    Just process headers for Doxygen, which simplifies everything.

--- nanocoat/cmake/doxygen.cmake
+++ nanocoat/cmake/doxygen.cmake
@@ -12,43 +12,22 @@ find_package(Doxygen)
 
 # Did we find it?
 if(DOXYGEN_FOUND)
-	# Target to generate all documentation per each target
-	add_custom_target(Doxygen)
+	# Configure Doxygen inputs and outputs
+	configure_file("${CMAKE_SOURCE_DIR}/cmake/Doxyfile.in"
+		"${CMAKE_BINARY_DIR}/Doxyfile")
 
-	# Do not run this all target by default
+	# Add target to build all the documentation
+	add_custom_target(Doxygen
+		COMMAND "${DOXYGEN_EXECUTABLE}"
+			"${CMAKE_BINARY_DIR}/Doxyfile"
+		DEPENDS ${target}
+		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+		SOURCES "${CMAKE_SOURCE_DIR}/cmake/Doxyfile.in"
+			"${CMAKE_BINARY_DIR}/Doxyfile"
+		BYPRODUCTS "${CMAKE_BINARY_DIR}/docs/")
+
+	# Do not run this by default
 	set_target_properties(Doxygen PROPERTIES
 		EXCLUDE_FROM_ALL TRUE
 		EXCLUDE_FROM_DEFAULT_BUILD TRUE)
-
-	# Document for Doxygen
-	function(squirreljme_doxygen target)
-		# Get list of sources
-		get_target_property(targetSourcesList ${target} SOURCES)
-		list(JOIN targetSourcesList " " targetSources)
-
-		# Configure Doxygen inputs and outputs
-		configure_file("${CMAKE_SOURCE_DIR}/cmake/Doxyfile.in"
-			"${CMAKE_CURRENT_BINARY_DIR}/Doxyfile")
-
-		# Add target to build all the documentation
-		add_custom_target(Doxygen${target}
-			COMMAND "${DOXYGEN_EXECUTABLE}"
-				"${CMAKE_CURRENT_BINARY_DIR}/Doxyfile"
-			DEPENDS ${target}
-			WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-			SOURCES "${CMAKE_SOURCE_DIR}/cmake/Doxyfile.in"
-				"${CMAKE_CURRENT_BINARY_DIR}/Doxyfile"
-			BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/docs/")
-
-		# Make all documentation depend on this
-		add_dependencies(Doxygen Doxygen${target})
-
-		# Do not run this by default
-		set_target_properties(Doxygen${target} PROPERTIES
-			EXCLUDE_FROM_ALL TRUE
-			EXCLUDE_FROM_DEFAULT_BUILD TRUE)
-	endfunction()
-else()
-	function(squirreljme_doxygen target)
-	endfunction()
 endif()
