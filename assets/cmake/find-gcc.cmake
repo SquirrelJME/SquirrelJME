47ab76cabceb5fcdc6fb062690fdd270cb3ba635
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-11-24 01:44:00 +0000

    Correct condition where GCC compilers are not searched for if they happened to already be cached.

--- assets/cmake/find-gcc.cmake
+++ assets/cmake/find-gcc.cmake
@@ -19,28 +19,25 @@ foreach(systemMap IN LISTS SQUIRRELJME_SYSTEM_MAP)
 		squirreljme_unmap(systemNormal 0 "${systemMap}")
 		squirreljme_unmap(archNormal 0 "${archMap}")
 
-		# Ignore if already found as there can be multiple aliases for the
-		# same system an architecture
-		if(GCC_${systemNormal}_${archNormal}_EXECUTABLE)
-			continue()
+		# Was an executable never found/cached for this specific GCC?
+		if(NOT DEFINED GCC_${systemNormal}_${archNormal}_EXECUTABLE)
+			# Build GCC compiler name (such as i686-linux-gnu-gcc)
+			set(gccName "${arch}-${system}-gcc")
+
+			# Is this GCC compiler available? Note that for Windows there could
+			# be the win32 variant (prefer Windows threads over Posix threads)
+			if("${systemNormal}" STREQUAL "windows")
+				find_program(GCC_${systemNormal}_${archNormal}_EXECUTABLE
+					NAMES "${gccName}"
+						"${gccName}-win32"
+						"${gccName}-posix")
+			else()
+				find_program(GCC_${systemNormal}_${archNormal}_EXECUTABLE
+					NAMES "${gccName}")
+			endif()
 		endif()
 
-		# Build GCC compiler name (such as i686-linux-gnu-gcc)
-		set(gccName "${arch}-${system}-gcc")
-
-		# Is this GCC compiler available? Note that for Windows there could
-		# be the win32 variant (prefer Windows threads over Posix threads)
-		if("${systemNormal}" STREQUAL "windows")
-			find_program(GCC_${systemNormal}_${archNormal}_EXECUTABLE
-				NAMES "${gccName}"
-					"${gccName}-win32"
-					"${gccName}-posix")
-		else()
-			find_program(GCC_${systemNormal}_${archNormal}_EXECUTABLE
-				NAMES "${gccName}")
-		endif()
-
-		# If found, we want to store it somewhere
+		# If found, we need to store it somewhere
 		if(GCC_${systemNormal}_${archNormal}_EXECUTABLE)
 			squirreljme_compiler_by_exe(
 				"${systemNormal}" "${archNormal}"
