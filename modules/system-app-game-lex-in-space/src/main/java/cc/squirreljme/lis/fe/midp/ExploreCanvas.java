fb17902f1aedabe070363459c7f14ea0848fd77f
Stephanie Gawroriski <xer@multiphasicapps.net>
2025-12-22 16:10:15 +0000

    Add more base classes.

---
+++ modules/system-app-game-lex-in-space/src/main/java/cc/squirreljme/lis/fe/midp/ExploreCanvas.java
@@ -0,0 +1,33 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.lis.fe.midp;
+
+import cc.squirreljme.runtime.cldc.debug.Debugging;
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Graphics;
+
+/**
+ * Canvas that is used in the exploration interface.
+ *
+ * @since 2025/12/21
+ */
+public class ExploreCanvas
+	extends Canvas
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2025/12/21
+	 */
+	@Override
+	protected void paint(Graphics __g)
+	{
+		throw Debugging.todo();
+	}
+}
---
+++ modules/system-app-game-lex-in-space/src/main/java/cc/squirreljme/lis/worldgen/WorldGenerator.java
@@ -0,0 +1,20 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package cc.squirreljme.lis.worldgen;
+
+/**
+ * Base interface for world generation, all generators implement this
+ * interface to produce what would be in a realm.
+ *
+ * @since 2025/12/21
+ */
+public interface WorldGenerator
+{
+}
