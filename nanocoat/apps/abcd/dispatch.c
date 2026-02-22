ba188465faa6a8579cc654088569618ef05ab8f5
Stephanie Gawroriski <xer@multiphasicapps.net>
2026-02-22 22:40:40 +0000

    Add blank base for ABCD: Ancient Build Contraption Device, for one day hopefully being modern build tools for ancient things.

---
+++ nanocoat/apps/abcd/dispatch.c
@@ -0,0 +1,18 @@
+/* -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// SquirrelJME
+//     Copyright (C) Stephanie Gawroriski <xer@multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// SquirrelJME is under the Mozilla Public License Version 2.0.
+// See license.mkd for licensing and copyright information.
+// -------------------------------------------------------------------------*/
+
+#include "sjme/config.h"
+#include "sjme/debug.h"
+#include "dispatch.h"
+
+int main(int argc, char** argv)
+{
+	sjme_todo("Impl?");
+	return EXIT_FAILURE;
+}
