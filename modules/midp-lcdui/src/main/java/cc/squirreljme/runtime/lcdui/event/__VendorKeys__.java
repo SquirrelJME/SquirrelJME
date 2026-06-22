1f015ac7c221f1f2c9f5669769683f3f477f7dad
Stephanie Gawroriski <xer@multiphasicapps.net>
2026-06-22 02:02:22 +0000

    Correct VendorKeys enumeration.

--- modules/midp-lcdui/src/main/java/cc/squirreljme/runtime/lcdui/event/__VendorKeys__.java
+++ modules/midp-lcdui/src/main/java/cc/squirreljme/runtime/lcdui/event/__VendorKeys__.java
@@ -20,64 +20,64 @@ enum __VendorKeys__
 {
 	/** Enum indicating the Back/CLR key. */
 	KEYBACK,
-
+	
 	/** Enum indicating the Center/Fire key. */
 	KEYCENTER,
-
+	
 	/** Enum indicating the Down Arrow key. */
 	KEYDOWN,
-
+	
 	/** Enum indicating the Left Arrow key. */
 	KEYLEFT,
-
+	
 	/** Enum indicating a vendor-specific Numpad 0 key. */
 	KEYNUM0,
-
+	
 	/** Enum indicating a vendor-specific Numpad 1 key. */
 	KEYNUM1,
-
+	
 	/** Enum indicating a vendor-specific Numpad 2 key. */
 	KEYNUM2,
-
+	
 	/** Enum indicating a vendor-specific Numpad 3 key. */
 	KEYNUM3,
-
+	
 	/** Enum indicating a vendor-specific Numpad 4 key. */
 	KEYNUM4,
-
+	
 	/** Enum indicating a vendor-specific Numpad 5 key. */
 	KEYNUM5,
-
+	
 	/** Enum indicating a vendor-specific Numpad 6 key. */
 	KEYNUM6,
-
+	
 	/** Enum indicating a vendor-specific Numpad 7 key. */
 	KEYNUM7,
-
+	
 	/** Enum indicating a vendor-specific Numpad 8 key. */
 	KEYNUM8,
-
+	
 	/** Enum indicating a vendor-specific Numpad 9 key. */
 	KEYNUM9,
-
+	
 	/** Enum indicating a vendor-specific Numpad '#'' key. */
 	KEYPOUND,
-
+	
 	/** Enum indicating the Right Arrow key. */
 	KEYRIGHT,
-
+	
 	/** Enum indicating the Left Soft key. */
 	KEYSOFT1,
-
+	
 	/** Enum indicating the Right Soft key. */
 	KEYSOFT2,
-
+	
 	/** Enum indicating a vendor-specific Numpad '*'' key. */
 	KEYSTAR,
-
+	
 	/** Enum indicating the Up Arrow key. */
 	KEYUP,
-
+	
 	/** Enum indicating the amount of vendor keys that can be mapped. */
 	VENDORKEYNUM,
-
+}
