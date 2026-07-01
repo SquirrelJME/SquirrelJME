b9c6d4bc20751d1d216e706c27ba940f392feec9
Stephanie Gawroriski <xer@multiphasicapps.net>
2026-07-01 15:53:11 +0000

    Cherrypick the configure based file from wip-running.

--- nanocoat/cmake/environment-options.cmake
+++ nanocoat/cmake/environment-options.cmake
@@ -12,32 +12,66 @@ define_property(GLOBAL PROPERTY globalEnvOptions
 	BRIEF_DOCS "globalEnvOptions"
 	FULL_DOCS "globalEnvOptions")
 
+# Path to the local configure file
+if(NOT DEFINED SJME_CONFIG_USE_PATH)
+	set(SJME_CONFIG_USE_PATH "${CMAKE_BINARY_DIR}/configure.h")
+	set(SJME_CONFIG_MAKE_CONFIG YES)
+else()
+	set(SJME_CONFIG_MAKE_CONFIG NO)
+endif()
+
+# Erase the configure file if it is being generated
+if(SJME_CONFIG_MAKE_CONFIG)
+	file(REMOVE "${SJME_CONFIG_USE_PATH}.work")
+endif()
+
+# Always use this path when compiling
+add_compile_definitions(SJME_CONFIG_USE_PATH="${SJME_CONFIG_USE_PATH}")
+
 # Used to simplify options
-function(squirreljme_env_option key value default)
+macro(squirreljme_env_option key helpText default)
 	# Declare the option
-	option(${key} "${value}" ${default})
+	option(${key} "${helpText}" ${default})
 
 	# Add this key to the global property list
 	get_property(globalEnvOptions GLOBAL PROPERTY globalEnvOptions)
-	if("${globalEnvOptions}" STREQUAL "globalEnvOptions-NOTFOUND" OR
-		"${globalEnvOptions}" STREQUAL "")
+	if("${globalEnvOptions}" STREQUAL "globalEnvOptions-NOTFOUND")
 		set_property(GLOBAL PROPERTY globalEnvOptions "${key}")
 	else()
 		list(APPEND globalEnvOptions "${key}")
 		set_property(GLOBAL PROPERTY globalEnvOptions "${globalEnvOptions}")
 	endif()
-endfunction()
+
+	# Generate configure file?
+	if(SJME_CONFIG_MAKE_CONFIG)
+		file(APPEND "${SJME_CONFIG_USE_PATH}.work"
+			"set(${key} ${${key}} CACHE STRING \"\")\n")
+
+		if(${key})
+			file(APPEND "${SJME_CONFIG_USE_PATH}.work"
+				"#define ${key} ${${key}}\n")
+		endif()
+	endif()
+endmacro()
 
 # Generate a list of options to forward to another CMake call
 function(squirreljme_env_forward result)
-	# Initialize list to nothing
-	set(${result})
+	if(TRUE)
+		# Use the configure file
+		set(${result} "-C")
+		list(APPEND ${result} "${SJME_CONFIG_USE_PATH}")
+		list(APPEND ${result} "-DSJME_CONFIG_USE_PATH=${SJME_CONFIG_USE_PATH}")
+	else()
+		# Initialize list to nothing
+		set(${result})
 
-	# Go through each possible property
-	get_property(keys GLOBAL PROPERTY globalEnvOptions)
-	foreach(key IN ITEMS ${keys})
-		list(APPEND ${result} "-D${key}=${${key}}")
-	endforeach()
+		# Go through each possible property
+		get_property(globalEnvOptions GLOBAL PROPERTY globalEnvOptions)
+		foreach(globalEnvOption IN LISTS globalEnvOptions)
+			list(APPEND ${result}
+				"-D${globalEnvOption}=${${globalEnvOption}}")
+		endforeach()
+	endif()
 
 	# Return the result
 	squirreljme_bp_return_propagate(${result})
@@ -47,3 +81,50 @@ endfunction()
 squirreljme_env_option(SJME_CONFIG_EO_LIBWINE_NATIVE_THREADS
 	"Use libwine threading?"
 	NO)
+
+# Debugging options
+squirreljme_env_option(SJME_CONFIG_DEBUG_ALLOC
+	"Debug output: Allocations."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_BYTECODES
+	"Debug output: Bytecode operations."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_CIRCLEBUF
+	"Debug output: Circle buffer."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_CLOSEABLE
+	"Debug output: Closeable."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_ENTRY
+	"Debug output: Method entry and exit."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_FIELD
+	"Debug output: Field access."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_GC
+	"Debug output: Garbage collection."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_INFLATE
+	"Debug output: Deflate decompression (inflate)."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_MLE
+	"Debug output: Middle Layer Emulation calls."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_TREAD
+	"Debug output: Stack treads."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_VERBOSE
+	"Debug output: Extra verbosity for no longer needed debug messages."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_VLSVLG
+	"Debug output: Variable length Set and Get."
+	NO)
+squirreljme_env_option(SJME_CONFIG_DEBUG_ZIP
+	"Debug output: Zip files."
+	NO)
+
+# If we made the configure file, then use whatever was generated
+if(SJME_CONFIG_MAKE_CONFIG)
+	file(RENAME "${SJME_CONFIG_USE_PATH}.work"
+		"${SJME_CONFIG_USE_PATH}")
+endif()
